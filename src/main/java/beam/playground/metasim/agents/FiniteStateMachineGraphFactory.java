3a44484344cdcad0206a775f59a19488ac565aca
Colin Sheppard <colinthegaunt@gmail.com>
2016-12-29 12:23:25 -0800

    new convention for interfaces with one implementation cont.

--- src/main/java/beam/playground/metasim/agents/FiniteStateMachineGraphFactory.java
+++ src/main/java/beam/playground/metasim/agents/FiniteStateMachineGraphFactory.java
@@ -2,10 +2,12 @@ package beam.playground.metasim.agents;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 import com.google.inject.Inject;
@@ -15,34 +17,44 @@ import beam.playground.metasim.agents.actions.ActionFactory;
 import beam.playground.metasim.agents.transition.TransitionFactory;
 import beam.playground.metasim.services.BeamServices;
 
-public class FiniteStateMachineGraphFactoryImpl implements FiniteStateMachineGraphFactory{
-	ActionFactory actionFactory;
-	Provider<BeamServices> beamServicesProvider;
-	TransitionFactory transitionFactory;
+public interface FiniteStateMachineGraphFactory {
+	FiniteStateMachineGraph create(String filePath);
 
-	@Inject
-	public FiniteStateMachineGraphFactoryImpl(Provider<BeamServices> beamServicesProvider, ActionFactory actionFactory, TransitionFactory transitionFactory){
-		this.beamServicesProvider = beamServicesProvider;
-		this.actionFactory = actionFactory;
-		this.transitionFactory = transitionFactory;
-	}
+	public class Default implements FiniteStateMachineGraphFactory{
+		ActionFactory actionFactory;
+		Provider<BeamServices> beamServicesProvider;
+		TransitionFactory transitionFactory;
 
-	@Override
-	public FiniteStateMachineGraph create(String filePath) {
-		SAXBuilder saxBuilder = new SAXBuilder();
-		InputStream stream = new FileInputStream(new File(filePath));
-		Document document = saxBuilder.build(stream);
-		
-		for(int i=0; i < document.getRootElement().getChildren().size(); i++){
-			Element elem = (Element)document.getRootElement().getChildren().get(i);
-			if(elem.getName().toLowerCase().equals("state")){
-				isMeteredByHour = false;
-				for(int j=0; j < elem.getChildren().size(); j++){
-					Element levelElem = (Element)elem.getChildren().get(j);
+		@Inject
+		public Default(Provider<BeamServices> beamServicesProvider, ActionFactory actionFactory, TransitionFactory transitionFactory){
+			this.beamServicesProvider = beamServicesProvider;
+			this.actionFactory = actionFactory;
+			this.transitionFactory = transitionFactory;
+		}
+
+		@Override
+		public FiniteStateMachineGraph create(String filePath) {
+			FiniteStateMachineGraph graph = new FiniteStateMachineGraph();
+			SAXBuilder saxBuilder = new SAXBuilder();
+			InputStream stream = null;
+			Document document = null;
+			try {
+				stream = new FileInputStream(new File(filePath));
+				document = saxBuilder.build(stream);
+			} catch (JDOMException | IOException e) {
+				e.printStackTrace();
+			}
+
+			for(int i=0; i < document.getRootElement().getChildren().size(); i++){
+				Element elem = (Element)document.getRootElement().getChildren().get(i);
+				if(elem.getName().toLowerCase().equals("fsm")){
+					for(int j=0; j < elem.getChildren().size(); j++){
+						Element levelElem = (Element)elem.getChildren().get(j);
+					}
 				}
 			}
+			return graph;
 		}
-		return null;
-	}
 
+	}
 }
