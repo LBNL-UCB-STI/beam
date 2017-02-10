707711ba0a2f2ee46c3cec347d7a8c2115ed3444
Colin Sheppard <colinthegaunt@gmail.com>
2017-02-10 14:52:58 -0800

    fixing compiler errors, more coming

--- src/main/scala/beam/metasim/agents/Trigger.scala
+++ src/main/scala/beam/metasim/agents/Trigger.scala
@@ -1,11 +1,12 @@
-package beam.metasim.playground.colin
+package beam.metasim.agents
 
 import akka.actor.ActorRef
+
 import scala.math.Ordered.orderingToOrdered
 
 class TriggerData(val agent: ActorRef, val tick: Double, val priority: Int = 0, var id: Int = 0)
 
-abstract case class Trigger() extends Ordered[Trigger] {
+abstract class Trigger() extends Ordered[Trigger] {
   val data: TriggerData
   def compare(that: Trigger): Int = (that.data.tick, that.data.priority) compare
     (this.data.tick, this.data.priority)
