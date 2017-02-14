9c3761f86479a9a4268f91faddd37ff664594051
Colin Sheppard <colinthegaunt@gmail.com>
2017-02-13 18:35:42 -0800

    framing up next tests to write

--- src/test/scala/beam/BeamAgentSchedulerSpec.scala
+++ src/test/scala/beam/BeamAgentSchedulerSpec.scala
@@ -14,18 +14,22 @@ import org.matsim.api.core.v01.population.Person
 
 class BeamAgentSchedulerSpec extends TestKit(ActorSystem("beam-actor-system")) with MustMatchers with FunSpecLike with ImplicitSender  {
 
-  class TestFSMAgent(override val id: Id[Person]) extends BeamAgent(id)
+  class TestBeamAgent(override val id: Id[Person]) extends BeamAgent(id)
 
-  describe("BEAM Scheduler"){
-      it("should send trigger to a BeamAgent"){
-        val beamAgentSchedulerRef = TestActorRef[BeamAgentScheduler]
-        val beamAgentRef = TestFSMRef(new TestFSMAgent(Id.createPersonId(0)))
-        beamAgentRef.stateName should be(Uninitialized)
-        beamAgentSchedulerRef ! Initialize(new TriggerData(beamAgentRef, 0.0))
-        beamAgentRef.stateName should be(Uninitialized)
-        beamAgentSchedulerRef ! StartSchedule(stopTick = 10.0)
-        beamAgentRef.stateName should be(Initialized)
-      }
+  describe("BEAM Agent Scheduler") {
+    it("should send trigger to a BeamAgent") {
+      val beamAgentSchedulerRef = TestActorRef[BeamAgentScheduler]
+      val beamAgentRef = TestFSMRef(new TestBeamAgent(Id.createPersonId(0)))
+      beamAgentRef.stateName should be(Uninitialized)
+      beamAgentSchedulerRef ! Initialize(new TriggerData(beamAgentRef, 0.0))
+      beamAgentRef.stateName should be(Uninitialized)
+      beamAgentSchedulerRef ! StartSchedule(stopTick = 10.0)
+      beamAgentRef.stateName should be(Initialized)
+    }
+    it("should fail to schedule events with negative tick value") {}
+    it("should allow for addition of non-chronological triggers") {}
+    it("should dispatch triggers in chronological order") {}
+    it("should not dispatch triggers beyond a window when old triggers have not completed") {}
+    //    it(""){}
   }
-
 }
