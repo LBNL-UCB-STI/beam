49651c85a7e973ef41825e2ef9ef9ca09beb2c55
wrashid <wrashid@users.noreply.github.com>
2017-02-15 12:03:26 -0800

    jdeqsim barebone version in scala running

--- src/main/scala/beam/metasim/playground/rashid/physicalSim/jdeqsim/oneZone/Main.scala
+++ src/main/scala/beam/metasim/playground/rashid/physicalSim/jdeqsim/oneZone/Main.scala
@@ -1,5 +1,34 @@
 package beam.metasim.playground.rashid.physicalSim.jdeqsim.oneZone
 
-object Main extends App {
+import org.matsim.core.config.ConfigUtils
+import org.matsim.core.scenario.ScenarioUtils
+import org.matsim.core.events.EventsUtils
+import beam.playground.jdeqsim.CountEnterLinkEvents
+import org.matsim.core.mobsim.jdeqsim.JDEQSimConfigGroup
+import org.matsim.core.mobsim.jdeqsim.JDEQSimulation
+
+object Main {
+
+  def main(args: Array[String]) {
+    val config = ConfigUtils.loadConfig(
+      "C:/Users/rwaraich/git/matsim_1/examples/scenarios/equil/config.xml");
+
+    val scenario = ScenarioUtils.loadScenario(config);
+
+    val eventsManager = EventsUtils.createEventsManager(scenario.getConfig());
+    val countEnterLinkEvents = new CountEnterLinkEvents();
+    eventsManager.addHandler(countEnterLinkEvents);
+    eventsManager.initProcessing();
+
+    val jdeqSimConfigGroup = new JDEQSimConfigGroup();
+    val jdeqSimulation = new JDEQSimulation(jdeqSimConfigGroup, scenario, eventsManager);
+
+    jdeqSimulation.run();
+    
+    eventsManager.finishProcessing();
+
+    println(countEnterLinkEvents.getLinkEnterCount());
+  }
+  
   
 } 
---
+++ src/main/scala/beam/metasim/playground/rashid/physicalSim/jdeqsim/oneZone/Main_2.scala
@@ -0,0 +1,5 @@
+package beam.metasim.playground.rashid.physicalSim.jdeqsim.oneZone
+
+object Main_2 extends App {
+  println("sdfdfs")
+}
