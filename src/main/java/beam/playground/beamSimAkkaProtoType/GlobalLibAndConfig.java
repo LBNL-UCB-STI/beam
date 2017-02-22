bea7a748877b5a8658687f75d78a142d48360b40
wrashid <wrashid@users.noreply.github.com>
2017-02-22 14:14:14 -0800

    changing config param for scheduler window

--- src/main/java/beam/playground/beamSimAkkaProtoType/GlobalLibAndConfig.java
+++ src/main/java/beam/playground/beamSimAkkaProtoType/GlobalLibAndConfig.java
@@ -5,22 +5,22 @@ public class GlobalLibAndConfig {
 	// if window size is small (e.g. 1 second), then it might make sense to
 	// decrease tick size, as it might allows for slightly more parallelism (window can
 	// move forward faster)
-	public static final double sizeOfWindowTickInSeconds = 0.1;
+	public static final double sizeOfTickInSeconds = 0.1;
 
-	public static final int windowSizeInSeconds = 100;
+	public static final int windowSizeInSeconds = 10;
 
 	public static final int latencyRttDelayInMs = 10;
 
 	public static int getWindowSizeInTicks() {
-		return getTick(sizeOfWindowTickInSeconds * windowSizeInSeconds);
+		return getTick(windowSizeInSeconds);
 	}
 
 	public static int getTick(double time) {
-		return (int) Math.round(Math.floor(time / sizeOfWindowTickInSeconds));
+		return (int) Math.round(Math.floor(time / sizeOfTickInSeconds));
 	}
 
 	public static double getTime(int tick) {
-		return tick * sizeOfWindowTickInSeconds;
+		return tick * sizeOfTickInSeconds;
 	}
 
 }
