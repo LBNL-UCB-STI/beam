9429bce26c99e6812bd355565e7b8ddceb4ee602
sfwatergit <sid.feygin@gmail.com>
2017-02-22 10:35:21 -0800

    test for registry

--- src/test/scala/beam/metasim/agents/PersonAgentSpec.scala
+++ src/test/scala/beam/metasim/agents/PersonAgentSpec.scala
@@ -1,39 +1,57 @@
 package beam.metasim.agents
 
 
-import akka.actor.ActorSystem
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestActorRef, TestFSMRef, TestKit}
+import akka.util.Timeout
 import beam.metasim.agents.PersonAgent._
+import glokka.Registry
+import glokka.Registry.Created
 import org.matsim.api.core.v01.Id
 import org.matsim.core.population.PopulationUtils
 import org.scalatest.Matchers._
 import org.scalatest.{FunSpecLike, MustMatchers}
 
+import scala.concurrent.Await
+
 /**
   * Created by sfeygin on 2/7/17.
   */
 class PersonAgentSpec extends TestKit(ActorSystem("beam-actor-system"))
-  with MustMatchers with FunSpecLike with ImplicitSender  {
+  with MustMatchers with FunSpecLike with ImplicitSender {
 
-  describe("PersonAgent FSM"){
+  private implicit val timeout = Timeout(60, TimeUnit.SECONDS)
 
-      it("should allow scheduler to set the first activity"){
-        val homeActivity = PopulationUtils.createActivityFromLinkId("home", Id.createLinkId(1))
-        val personAgentRef = TestFSMRef(new PersonAgent(Id.create("dummyAgent",classOf[PersonAgent]), PersonAgentData(homeActivity)))
-        val beamAgentSchedulerRef = TestActorRef[BeamAgentScheduler]
+  describe("PersonAgent FSM") {
 
-        val workActivity = PopulationUtils.createActivityFromLinkId("work", Id.createLinkId(2))
-        beamAgentSchedulerRef ! Initialize(new TriggerData(personAgentRef, 0.0))
+    it("should allow scheduler to set the first activity") {
+      val homeActivity = PopulationUtils.createActivityFromLinkId("home", Id.createLinkId(1))
+      val personAgentRef = TestFSMRef(new PersonAgent(Id.create("dummyAgent", classOf[PersonAgent]), PersonAgentData(homeActivity)))
+      val beamAgentSchedulerRef = TestActorRef[BeamAgentScheduler]
 
-        beamAgentSchedulerRef ! DepartActivity(new TriggerData(personAgentRef, 1.0), homeActivity)
-        beamAgentSchedulerRef ! DepartActivity(new TriggerData(personAgentRef, 10.0), workActivity)
-        beamAgentSchedulerRef ! StartSchedule(stopTick = 10.0, maxWindow = 10.0)
+      val workActivity = PopulationUtils.createActivityFromLinkId("work", Id.createLinkId(2))
+      beamAgentSchedulerRef ! Initialize(new TriggerData(personAgentRef, 0.0))
 
-        personAgentRef.stateName should be(ChoosingMode)
-        personAgentRef.stateData.data.currentPlanElement should be(workActivity)
-      }
-    it("should be able to get from registry"){
+      beamAgentSchedulerRef ! DepartActivity(new TriggerData(personAgentRef, 1.0), homeActivity)
+      beamAgentSchedulerRef ! DepartActivity(new TriggerData(personAgentRef, 10.0), workActivity)
+      beamAgentSchedulerRef ! StartSchedule(stopTick = 10.0, maxWindow = 10.0)
 
+      personAgentRef.stateName should be(ChoosingMode)
+      personAgentRef.stateData.data.currentPlanElement should be(workActivity)
+    }
+
+    it("should be able to be registered in registry") {
+      val registry = Registry.start(this.system, "actor-registry")
+      val homeActivity = PopulationUtils.createActivityFromLinkId("home", Id.createLinkId(1))
+      val name = "0"
+      val props = Props(classOf[PersonAgent], Id.createPersonId(name), PersonAgentData(homeActivity))
+      val future = registry ? Registry.Register(name, props)
+      val result = Await.result(future, timeout.duration).asInstanceOf[AnyRef]
+      val ok = result.asInstanceOf[Created]
+      ok.name mustEqual name
     }
   }
 
