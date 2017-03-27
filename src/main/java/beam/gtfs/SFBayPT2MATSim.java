19a0d472c3fda06ef9e36eff0644d7e7418a097f
Colin Sheppard <colinthegaunt@gmail.com>
2017-03-27 16:32:05 -0700

    attempt to debug dep issue but persists so commented out code for now

--- src/main/java/beam/gtfs/SFBayPT2MATSim.java
+++ src/main/java/beam/gtfs/SFBayPT2MATSim.java
@@ -8,11 +8,11 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.ConfigWriter;
 import org.matsim.core.utils.io.UncheckedIOException;
 import org.matsim.pt.utils.TransitScheduleValidator;
-import org.matsim.pt2matsim.config.CreateDefaultConfig;
-import org.matsim.pt2matsim.config.PublicTransitMappingConfigGroup;
-import org.matsim.pt2matsim.gtfs.Gtfs2TransitSchedule;
-import org.matsim.pt2matsim.mapping.RunPublicTransitMapper;
-import org.matsim.pt2matsim.run.CheckMappedSchedulePlausibility;
+//import org.matsim.pt2matsim.config.CreateDefaultConfig;
+//import org.matsim.pt2matsim.config.PublicTransitMappingConfigGroup;
+//import org.matsim.pt2matsim.gtfs.Gtfs2TransitSchedule;
+//import org.matsim.pt2matsim.mapping.RunPublicTransitMapper;
+//import org.matsim.pt2matsim.run.CheckMappedSchedulePlausibility;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -100,65 +100,65 @@ public class SFBayPT2MATSim {
                 //      all trips contained in the schedule.
                 String.format("%s%s/ScheduleShapes/Shapes.shp", outputDir, agency)
         };
-        Gtfs2TransitSchedule.main(gtfsConverterArgs);
+//        Gtfs2TransitSchedule.main(gtfsConverterArgs);
     }
 
     private void mapSchedule2Network(String agency, String agencyId) {
         // Create a mapping config
         final String mapperConfigPath = String.format("%s%s/MapperConfig.xml", outputDir, agency);
-        CreateDefaultConfig.main(new String[]{mapperConfigPath});
+//        CreateDefaultConfig.main(new String[]{mapperConfigPath});
 
         // Open the mapping-Config and set the paramters to the required values
         // (usually done manually by opening the config with a simple editor)
-        Config mapperConfig = ConfigUtils.loadConfig(
-                mapperConfigPath,
-                PublicTransitMappingConfigGroup.createDefaultConfig());
+//        Config mapperConfig = ConfigUtils.loadConfig(
+//                mapperConfigPath,
+//                PublicTransitMappingConfigGroup.createDefaultConfig());
 
-        // Inputs
-        mapperConfig.getModule("PublicTransitMapping").addParam("networkFile", String.format("%sNetworkBase.xml.gz", outputDir));  // Always starting from same network
-        mapperConfig.getModule("PublicTransitMapping").addParam("scheduleFile", String.format("%s%s/%s_UnmappedTransitSchedule.xml.gz", outputDir, agency, agencyId));
-
-        // Outputs
-        mapperConfig.getModule("PublicTransitMapping").addParam("outputNetworkFile", String.format("%s%s/%s_MultiModalNetwork.xml.gz", outputDir, agency, agencyId));
-        mapperConfig.getModule("PublicTransitMapping").addParam("outputScheduleFile", String.format("%s%s/%s_MappedTransitSchedule.xml.gz", outputDir, agency, agencyId));
-        mapperConfig.getModule("PublicTransitMapping").addParam("outputStreetNetworkFile", String.format("%s%s/%s_MultiModalNetwork_StreetOnly.xml.gz", outputDir, agency, agencyId));
+//        // Inputs
+//        mapperConfig.getModule("PublicTransitMapping").addParam("networkFile", String.format("%sNetworkBase.xml.gz", outputDir));  // Always starting from same network
+//        mapperConfig.getModule("PublicTransitMapping").addParam("scheduleFile", String.format("%s%s/%s_UnmappedTransitSchedule.xml.gz", outputDir, agency, agencyId));
+//
+//        // Outputs
+//        mapperConfig.getModule("PublicTransitMapping").addParam("outputNetworkFile", String.format("%s%s/%s_MultiModalNetwork.xml.gz", outputDir, agency, agencyId));
+//        mapperConfig.getModule("PublicTransitMapping").addParam("outputScheduleFile", String.format("%s%s/%s_MappedTransitSchedule.xml.gz", outputDir, agency, agencyId));
+//        mapperConfig.getModule("PublicTransitMapping").addParam("outputStreetNetworkFile", String.format("%s%s/%s_MultiModalNetwork_StreetOnly.xml.gz", outputDir, agency, agencyId));
 
         // Link Candidate changes
-
-        final ConfigGroup subwayParamSet = mapperConfig.getModule("PublicTransitMapping").createParameterSet("linkCandidateCreator");
-        subwayParamSet.addParam("scheduleMode", "subway");
-        subwayParamSet.addParam("networkModes", "rail,subway");
-        mapperConfig.getModule("PublicTransitMapping").addParameterSet(subwayParamSet);
-
-        final ConfigGroup cableCarParamSet = mapperConfig.getModule("PublicTransitMapping").createParameterSet("linkCandidateCreator");
-        cableCarParamSet.addParam("scheduleMode", "cable car");
-        cableCarParamSet.addParam("networkModes", "light_rail,rail");
-        mapperConfig.getModule("PublicTransitMapping").addParameterSet(cableCarParamSet);
-
-        // Other changes
-        mapperConfig.getModule("PublicTransitMapping").addParam("scheduleFreespeedModes", "subway");
-        mapperConfig.getModule("PublicTransitMapping").addParam("uTurnCost", "30.0");
-        mapperConfig.getModule("PublicTransitMapping").addParam("prefixArtificial", String.format("%s_", agencyId));
-
-//         Save the mapping-Config
-//         (usually done manually)
-        final String adjustedMapperConfigPath = String.format("%s%s/MapperConfigAdjusted.xml", outputDir, agency);
-        new ConfigWriter(mapperConfig).write(adjustedMapperConfigPath);
-        // Map the schedule to the network using the config
-        RunPublicTransitMapper.main(new String[]{adjustedMapperConfigPath});
+//
+//        final ConfigGroup subwayParamSet = mapperConfig.getModule("PublicTransitMapping").createParameterSet("linkCandidateCreator");
+//        subwayParamSet.addParam("scheduleMode", "subway");
+//        subwayParamSet.addParam("networkModes", "rail,subway");
+//        mapperConfig.getModule("PublicTransitMapping").addParameterSet(subwayParamSet);
+//
+//        final ConfigGroup cableCarParamSet = mapperConfig.getModule("PublicTransitMapping").createParameterSet("linkCandidateCreator");
+//        cableCarParamSet.addParam("scheduleMode", "cable car");
+//        cableCarParamSet.addParam("networkModes", "light_rail,rail");
+//        mapperConfig.getModule("PublicTransitMapping").addParameterSet(cableCarParamSet);
+//
+//        // Other changes
+//        mapperConfig.getModule("PublicTransitMapping").addParam("scheduleFreespeedModes", "subway");
+//        mapperConfig.getModule("PublicTransitMapping").addParam("uTurnCost", "30.0");
+//        mapperConfig.getModule("PublicTransitMapping").addParam("prefixArtificial", String.format("%s_", agencyId));
+//
+////         Save the mapping-Config
+////         (usually done manually)
+//        final String adjustedMapperConfigPath = String.format("%s%s/MapperConfigAdjusted.xml", outputDir, agency);
+//        new ConfigWriter(mapperConfig).write(adjustedMapperConfigPath);
+//        // Map the schedule to the network using the config
+//        RunPublicTransitMapper.main(new String[]{adjustedMapperConfigPath});
     }
 
     private void checkPlausability(String agency, String agencyId) {
-        try {
-            CheckMappedSchedulePlausibility.run(
-                    String.format("%s%s/%s_MappedTransitSchedule.xml.gz", outputDir, agency, agencyId),
-                    String.format("%s%s/%s_MultiModalNetwork.xml.gz", outputDir, agency, agencyId),
-                    BEAM_CONFIG.beam().routing().gtfs().crs(),
-                    String.format("%s%s/PlausabilityResults/", outputDir, agency)
-            );
-        } catch (UncheckedIOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            CheckMappedSchedulePlausibility.run(
+//                    String.format("%s%s/%s_MappedTransitSchedule.xml.gz", outputDir, agency, agencyId),
+//                    String.format("%s%s/%s_MultiModalNetwork.xml.gz", outputDir, agency, agencyId),
+//                    BEAM_CONFIG.beam().routing().gtfs().crs(),
+//                    String.format("%s%s/PlausabilityResults/", outputDir, agency)
+//            );
+//        } catch (UncheckedIOException e) {
+//            e.printStackTrace();
+//        }
     }
 
 
