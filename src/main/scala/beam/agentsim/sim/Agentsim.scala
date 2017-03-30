c7286082af7875a11db2ddaed8b022e009a36584
sfwatergit <sid.feygin@gmail.com>
2017-03-29 19:02:13 -0700

    Format output to shorter lat/lon

--- src/main/scala/beam/agentsim/sim/Agentsim.scala
+++ src/main/scala/beam/agentsim/sim/Agentsim.scala
@@ -46,7 +46,7 @@ class Agentsim @Inject()(private val actorSystem: ActorSystem,
   var currentIter = 0
 
 
-  private implicit val timeout = Timeout(60, TimeUnit.SECONDS)
+  private implicit val timeout = Timeout(1200, TimeUnit.SECONDS)
 
   override def notifyStartup(event: StartupEvent): Unit = {
 
