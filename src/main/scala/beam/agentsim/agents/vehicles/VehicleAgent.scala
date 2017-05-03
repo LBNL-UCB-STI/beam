0c824891d1a0790b5f7874ea1b6b6c7c4888529a
Colin Sheppard <colinthegaunt@gmail.com>
2017-05-02 20:40:21 -0700

    initial thinking on vehicle implementation

--- src/main/scala/beam/agentsim/agents/vehicles/VehicleAgent.scala
+++ src/main/scala/beam/agentsim/agents/vehicles/VehicleAgent.scala
@@ -1,7 +1,8 @@
-package beam.agentsim.agents
+package beam.agentsim.agents.vehicles
 
+import beam.agentsim.agents.BeamAgent
 import beam.agentsim.agents.BeamAgent.BeamAgentData
-import beam.agentsim.agents.VehicleAgent.VehicleData
+import beam.agentsim.agents.vehicles.VehicleAgent.VehicleData
 import org.matsim.api.core.v01.Id
 
 
@@ -20,3 +21,25 @@ class VehicleAgent(override val id: Id[VehicleAgent], override val data: Vehicle
 
 }
 
+/*
+driver is a BeamAgent, need not be physically inside vehicle
+passengers are BeamVehicles and are physically in vehicle and move with the vehicle (e.g. a PersonAgent who is driving alone would be the driver of the vehicle and his/her HumanBodyVehicle would be a passenger)
+HumanBody is a special case, it has no passengers, just a driver
+
+BeamVeh fields
+enclosingVehicle: Option[BeamVehicle] (None if top of nest)
+driver: Option[BeamAgent]
+passengers: List[BeamVehicles]
+defaultLocationanager: LocationManger
+getLocationManager: enclosingVehicle match {
+Some[enclosing] -> delegate to enclosing
+None -> defaultLocationManager
+powertrain: PowerTrain
+
+
+LocationMan
+agent: BeamAgent
+location: Coord
+
+
+ */
