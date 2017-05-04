6e9bfb1c560c02e9c6e6ed76b414233075f2c9b3
Colin Sheppard <colinthegaunt@gmail.com>
2017-05-04 11:23:24 -0700

    stubbing classes needed for VehicleAgents

--- src/main/scala/beam/agentsim/agents/vehicles/VehicleAgent.scala
+++ src/main/scala/beam/agentsim/agents/vehicles/VehicleAgent.scala
@@ -21,25 +21,3 @@ class VehicleAgent(override val id: Id[VehicleAgent], override val data: Vehicle
 
 }
 
-/*
-driver is a BeamAgent, need not be physically inside vehicle
-passengers are BeamVehicles and are physically in vehicle and move with the vehicle (e.g. a PersonAgent who is driving alone would be the driver of the vehicle and his/her HumanBodyVehicle would be a passenger)
-HumanBody is a special case, it has no passengers, just a driver
-
-BeamVeh fields
-enclosingVehicle: Option[BeamVehicle] (None if top of nest)
-driver: Option[BeamAgent]
-passengers: List[BeamVehicles]
-defaultLocationanager: LocationManger
-getLocationManager: enclosingVehicle match {
-Some[enclosing] -> delegate to enclosing
-None -> defaultLocationManager
-powertrain: PowerTrain
-
-
-LocationMan
-agent: BeamAgent
-location: Coord
-
-
- */
