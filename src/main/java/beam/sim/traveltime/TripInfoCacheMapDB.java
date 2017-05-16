3487330b08aff37e7a091e7049c260be18e6d266
Colin Sheppard <colinthegaunt@gmail.com>
2017-05-15 19:47:36 -0700

    use mapdb inmem vs ondisk overflow feature

--- src/main/java/beam/sim/traveltime/TripInfoCacheMapDB.java
+++ src/main/java/beam/sim/traveltime/TripInfoCacheMapDB.java
@@ -5,8 +5,12 @@ import org.apache.log4j.Logger;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * BEAM
@@ -15,56 +19,86 @@ public class TripInfoCacheMapDB {
     private static final Logger log = Logger.getLogger(TripInfoCacheMapDB.class);
 
     private File dbFile, dbTempFile;
-    public DB db;
-    public HTreeMap<String, TripInformation> cache;
+    public DB dbDisk, dbMemory;
+    public HTreeMap<String, TripInformation> onDisk, inMemory;
 
     public TripInfoCacheMapDB(String dbPath) {
         String permanentDbPath = dbPath;
         String temporaryDbPath = dbPath + "-LIVE";
         dbFile = new File(permanentDbPath);
         dbTempFile = new File(temporaryDbPath);
-        if(dbFile.exists()){
+        if (dbFile.exists()) {
             try {
-                FileUtils.copyFile(dbFile,dbTempFile);
+                FileUtils.copyFile(dbFile, dbTempFile);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }else if(dbTempFile.exists()){
+        } else if (dbTempFile.exists()) {
             FileUtils.deleteQuietly(dbTempFile);
         }
-        db = DBMaker.fileDB(dbTempFile).make();
-        cache = (HTreeMap<String, TripInformation>) db.hashMap("cache").createOrOpen();
+        openDBs();
+        log.info("In memory cache opened of size: "+inMemory.size()+" with on disk overflow of size "+onDisk.size());
     }
+
+    public void openDBs(){
+        dbDisk = DBMaker
+                .fileDB(dbTempFile)
+                .make();
+
+        dbMemory = DBMaker
+                .memoryDB()
+                .make();
+
+        // Big map populated with data expired from cache
+        onDisk = (HTreeMap<String, TripInformation>)dbDisk
+                .hashMap("onDisk")
+                .keySerializer(Serializer.STRING)
+                .createOrOpen();
+
+        // fast in-memory collection with limited size
+        inMemory = (HTreeMap<String, TripInformation>) dbMemory
+                .hashMap("inMemory")
+                .keySerializer(Serializer.STRING)
+                .expireStoreSize(64*1024*1024*1024)
+                .expireAfterCreate()
+                //this registers overflow to `onDisk`
+                .expireOverflow((HTreeMap) onDisk)
+                //good idea is to enable background expiration
+                .expireExecutor(Executors.newScheduledThreadPool(2))
+                .create();
+
+    }
+
     public synchronized TripInformation getTripInformation(String key){
-        return cache.isClosed() ? null : cache.get(key);
+        return inMemory.isClosed() ? null : inMemory.get(key);
     }
 
     public Integer getCacheSize() {
-        return cache.size();
-    }
-
-    private void flushHotCache() {
+        return inMemory.size();
     }
 
     public synchronized void putTripInformation(String key, TripInformation tripInfo){
-        if(!cache.isClosed())cache.put(key,tripInfo);
+        if(!inMemory.isClosed())inMemory.put(key,tripInfo);
     }
     public String toString(){
-        return "MapDB Cache contains "+cache.size()+" trips.";
+        return "MapDB Cache contains "+inMemory.size()+" trips.";
     }
 
     public synchronized void persistStore(){
-        cache.close();
-        db.close();
+        log.info("In memory cache about to be persisted of size: "+inMemory.size()+" with on disk overflow of size "+onDisk.size());
+        close();
         try {
             FileUtils.copyFile(dbTempFile,dbFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        db = DBMaker.fileDB(dbTempFile).make();
-        cache = (HTreeMap<String, TripInformation>) db.hashMap("cache").createOrOpen();
+        openDBs();
+        log.info("In memory cache now persisted of size: "+inMemory.size()+" with on disk overflow of size "+onDisk.size());
     }
-    public void close(){
-        db.close();
+    public void close() {
+        inMemory.close();
+        dbMemory.close();
+        onDisk.close();
+        dbDisk.close();
     }
 }
