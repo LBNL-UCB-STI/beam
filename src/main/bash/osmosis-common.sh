8dff99d71c8f451c95297f4c70b72c74a3a3d56c
Colin Sheppard <colinthegaunt@gmail.com>
2017-05-31 15:49:16 -0700

    common use of osmosis

--- src/main/bash/osmosis-common.sh
+++ src/main/bash/osmosis-common.sh
@@ -1,1 +1,2 @@
 osmosis --read-pbf file=~/Downloads/ohio-latest.osm.pbf --bounding-box top=40.532006 left=-83.715868 bottom=39.323214 right=-82.160572 --tf reject-ways highway=service --write-pbf file=columbus.osm.pbf
+osmosis --read-pbf file=~/Downloads/california-latest.osm.pbf --bounding-box top=38.857 left=-123.736 bottom=36.885 right=-121.416 --write-pbf file=sf-bay-area.osm.pbf
