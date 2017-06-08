a5d4309d2e0014945be9cd0e25f6256aa11d8815
Zeeshan Bilal <zeeshan.bilal@hotmail.com>
2017-06-08 16:12:48 +0500

    logging response for edge info and calculate distance

--- src/main/java/beam/playground/r5/TripPlanner.java
+++ src/main/java/beam/playground/r5/TripPlanner.java
@@ -170,55 +170,67 @@ public class TripPlanner {
             LOG.info("NUM: {}", stats.num);
 
             ifPresentThenForEach(option.itinerary, iten -> {
-                LOG.info("*****ITINERARY START*****");
-                LOG.info("Total Distance is: {}", iten.distance);
-                LOG.info("Total Duration is: {}", convertIntToTimeFormat(iten.duration));
-                LOG.info("Start Time is: {}", iten.startTime);
-                LOG.info("End Time is: {}", iten.endTime);
-                LOG.info("Total Waiting Time is: {}", convertIntToTimeFormat(iten.waitingTime));
-                LOG.info("Total Transit Time is: {}", convertIntToTimeFormat(iten.transitTime));
-                LOG.info("Total Walk Time is: {}", convertIntToTimeFormat(iten.walkTime));
+                LOG.info("\t*****ITINERARY START*****");
+                LOG.info("\tTotal Distance is: {}", iten.distance);
+                LOG.info("\tTotal Duration is: {}", convertIntToTimeFormat(iten.duration));
+                LOG.info("\tStart Time is: {}", iten.startTime);
+                LOG.info("\tEnd Time is: {}", iten.endTime);
+                LOG.info("\tTotal Waiting Time is: {}", convertIntToTimeFormat(iten.waitingTime));
+                LOG.info("\tTotal Transit Time is: {}", convertIntToTimeFormat(iten.transitTime));
+                LOG.info("\tTotal Walk Time is: {}", convertIntToTimeFormat(iten.walkTime));
 
                 PointToPointConnection conn = iten.connection;
 
                 ifPresentThenForEach(conn.transit, transit -> {
-                    LOG.info("*****TRANSIT START*****");
-                    LOG.info("Transit Time: {}", convertIntToTimeFormat(transit.time));
-                    LOG.info("Transit Pattern: {}", transit.pattern);
-                    LOG.info("*****TRANSIT END*****");
+                    LOG.info("\t\t*****TRANSIT START*****");
+                    LOG.info("\t\tTransit Time: {}", convertIntToTimeFormat(transit.time));
+                    LOG.info("\t\tTransit Pattern: {}", transit.pattern);
+                    LOG.info("\t\t*****TRANSIT END*****");
                 });
 
                 LOG.info("P2P Connection Access: {}", conn.access);
                 LOG.info("P2P Connection Egress: {}", conn.egress);
 
-                LOG.info("*****ITINERARY END*****");
+                LOG.info("\t*****ITINERARY END*****");
             });
 
             ifPresentThenForEach(option.access, segment -> {
-                LOG.info("*****SEGMENT START*****");
+                LOG.info("\t*****SEGMENT START*****");
 
-                LOG.info("Access MODE: {}", segment.mode);
-                LOG.info("Access Distance: {}", segment.distance);
-                LOG.info("Access Elevation: {}", segment.elevation);
-                LOG.info("Access Duration: {}", convertIntToTimeFormat(segment.duration));
+                LOG.info("\tAccess MODE: {}", segment.mode);
+                LOG.info("\tAccess Distance: {}", segment.distance);
+                LOG.info("\tTotal Edge Distance: {}", segment.streetEdges.parallelStream().mapToInt(edge -> edge.distance).sum());
+                LOG.info("\tAccess Elevation: {}", segment.elevation);
+                LOG.info("\tAccess Duration: {}", convertIntToTimeFormat(segment.duration));
 
                 LineString geom = segment.geometry;
-                LOG.info("Segment Area: {}", geom.getArea());
-                LOG.info("Coordinates: {}", geom.getCoordinate());
-                LOG.info("Boundary Dimensions are: {}", geom.getBoundaryDimension());
-                LOG.info("Segment Starting Point: {}", geom.getStartPoint());
-                LOG.info("End Point is: {}", geom.getEndPoint());
-                LOG.info("Geometry Dimensions: {}", geom.getDimension());
-                LOG.info("Geometry Type: {}", geom.getGeometryType());
-                LOG.info("Segment Length: {}", geom.getLength());
-                LOG.info("Segment Num Points: {}", geom.getNumPoints());
+                LOG.info("\tSegment Area: {}", geom.getArea());
+                LOG.info("\tCoordinates: {}", geom.getCoordinate());
+                LOG.info("\tBoundary Dimensions are: {}", geom.getBoundaryDimension());
+                LOG.info("\tSegment Starting Point: {}", geom.getStartPoint());
+                LOG.info("\tEnd Point is: {}", geom.getEndPoint());
+                LOG.info("\tGeometry Dimensions: {}", geom.getDimension());
+                LOG.info("\tGeometry Type: {}", geom.getGeometryType());
+                LOG.info("\tSegment Length: {}", geom.getLength());
+                LOG.info("\tSegment Num Points: {}", geom.getNumPoints());
 
                 Coordinate coordinate = geom.getCoordinate();
-                LOG.info("Coordinate-X: {}", coordinate.x);
-                LOG.info("Coordinate-Y: {}", coordinate.y);
-                LOG.info("Coordinate-Z: {}", coordinate.z);
+                LOG.info("\tCoordinate-X: {}", coordinate.x);
+                LOG.info("\tCoordinate-Y: {}", coordinate.y);
+                LOG.info("\tCoordinate-Z: {}", coordinate.z);
 
-                LOG.info("*****SEGMENT END*****");
+                LOG.info("\tTotal Edges are: {}", segment.streetEdges.size());
+                ifPresentThenForEach(segment.streetEdges, edge -> {
+                    LOG.info("\t\t*****EDGE START*****");
+                    LOG.info("\t\tStreet Name: {}", edge.streetName);
+                    LOG.info("\t\tMode: {}", edge.mode);
+                    LOG.info("\t\tDistance: {}", edge.distance);
+                    LOG.info("\t\tEdge Id: {}", edge.edgeId);
+
+                    LOG.info("\t\t*****Edge END*****");
+                });
+
+                LOG.info("\t*****SEGMENT END*****");
             });
             LOG.info("*****OPTION END*****");
         });
