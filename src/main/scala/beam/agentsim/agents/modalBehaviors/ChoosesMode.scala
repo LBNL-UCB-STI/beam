d8f0c2ec04e096a772979f1c8ebcdb76a41f3c89
Colin Sheppard <colinthegaunt@gmail.com>
2017-06-30 11:11:27 -0700

    reimplementing mode chocie

--- src/main/scala/beam/agentsim/agents/modalBehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalBehaviors/ChoosesMode.scala
@@ -16,23 +16,28 @@ import beam.sim.HasServices
   */
 trait ChoosesMode extends BeamAgent[PersonData] with TriggerShortcuts with HasServices {
   this: PersonAgent => // Self type restricts this trait to only mix into a PersonAgent
-  import akka.pattern.{ask, pipe}
 
   var routerResult: Option[RoutingResponse] = None
   var taxiResult: Option[TaxiInquiryResponse] = None
   var hasReceivedCompleteChoiceTrigger = false
 
-  def completeChoiceIfReady(theTriggerId: Option[Long]): State = {
+  def completeChoiceIfReady(): State = {
     if (hasReceivedCompleteChoiceTrigger && routerResult != None && taxiResult != None) {
       // Choice happens here
       hasReceivedCompleteChoiceTrigger = false
-      goto(Walking) using stateData.copy(triggerId = None, tick = None) replying completed(theTriggerId.get, schedule[TeleportationArrivalTrigger](stateData.tick.get, self))
+      goto(Walking) using stateData.copy(triggerId = None, tick = None) replying completed(stateData.triggerId.get, schedule[TeleportationArrivalTrigger](stateData.tick.get, self))
     } else {
-      stay() using stateData.copy(triggerId = theTriggerId)
+      stay()
     }
   }
 
   chainedWhen(ChoosingMode) {
+    /*
+     * Begin Choice Process
+     *
+     * When we begin the mode choice process, we send out requests for data that we need from other system components.
+     * Then we reply with a completion notice and schedule the finalize choice trigger.
+     */
     case Event(TriggerWithId(BeginModeChoiceTrigger(tick), triggerId), info: BeamAgentInfo[PersonData]) =>
       logInfo(s"inside ChoosesMode @ ${stateData.tick}")
       val nextAct = stateData.data.nextActivity.right.get // No danger of failure here
@@ -40,17 +45,26 @@ trait ChoosesMode extends BeamAgent[PersonData] with TriggerShortcuts with HasSe
       //TODO parameterize search distance
       services.taxiManager ! TaxiInquiry(stateData.data.currentActivity.getCoord, 2000)
       stay() using stateData.copy(triggerId = Some(triggerId)) replying completed(triggerId, schedule[FinalizeModeChoiceTrigger](tick, self))
+    /*
+     * Receive and store data needed for choice.
+     */
     case Event(theRouterResult: RoutingResponse, info: BeamAgentInfo[PersonData]) =>
       routerResult = Some(theRouterResult)
-      completeChoiceIfReady(stateData.triggerId)
+      completeChoiceIfReady()
     case Event(theTaxiResult: TaxiInquiryResponse, info: BeamAgentInfo[PersonData]) =>
       taxiResult = Some(theTaxiResult)
-      completeChoiceIfReady(stateData.triggerId)
-    case Event(TriggerWithId(FinalizeModeChoiceTrigger(tick), triggerId), info: BeamAgentInfo[PersonData]) =>
+      completeChoiceIfReady()
+    /*
+     * Finishing choice.
+     */
+    case Event(TriggerWithId(FinalizeModeChoiceTrigger(tick), theTriggerId), info: BeamAgentInfo[PersonData]) =>
       hasReceivedCompleteChoiceTrigger = true
-      completeChoiceIfReady(Some(triggerId))
-    case ev @ Event(_,_) =>
-      stay()
+      val result : State = completeChoiceIfReady()
+      if(result.stateName == ChoosesMode){
+        stay() using info.copy(triggerId = Some(theTriggerId))
+      }else{
+        result
+      }
   }
 
 }
