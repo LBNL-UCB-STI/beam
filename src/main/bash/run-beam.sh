ef9d2f725c756218fe00d7ef7b2b62b03f741858
Colin Sheppard <colinthegaunt@gmail.com>
2017-07-31 19:18:21 -0700

    running on laptop

--- src/main/bash/run-beam.sh
+++ src/main/bash/run-beam.sh
@@ -5,6 +5,6 @@
 
 #java -cp . -Xmx30g -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/beam.jar "$BEAM_SHARED_INPUTS/../beam-core/" "model-inputs/calibration-v2/config-bigger-batteries-1x.xml" "$BEAM_OUTPUTS" 
 
-java -cp . -Xmx30g -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/beam.jar "$BEAM_SHARED_INPUTS/../beam-core/" "model-inputs/calibration-v2/config-bigger-batteries-1.5x.xml" "$BEAM_OUTPUTS" 
+java -cp . -Xmx15g -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/beam.jar "$BEAM_SHARED_INPUTS/../beam-core/" "model-inputs/calibration-v2/config-bigger-batteries-1.5x.xml" "$BEAM_OUTPUTS" 
 
 #java -cp . -Xmx30g -XX:+HeapDumpOnOutOfMemoryError -jar build/libs/beam.jar "$BEAM_SHARED_INPUTS/../beam-core/" "model-inputs/calibration-v2/config-bigger-batteries-2x.xml" "$BEAM_OUTPUTS" 
