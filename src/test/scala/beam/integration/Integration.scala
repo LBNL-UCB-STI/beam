81833f0d7e9a2655d83a76b6f3f3bd82f0d2d9fd
David Arias <fdariasm@gmail.com>
2017-08-30 16:51:41 -0500

    #76 - Integration tests for running beam: running without errors, testing event file creation

---
+++ src/test/scala/beam/integration/Integration.scala
@@ -0,0 +1,59 @@
+package beam.integration
+
+import beam.sim.RunBeam
+import beam.sim.config.ConfigModule
+import org.matsim.core.events.{EventsManagerImpl, EventsReaderXMLv1}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import java.io.File
+
+import scala.io.Source
+import scala.util.Try
+
+/**
+  * Created by fdariasm on 29/08/2017
+  * 
+  */
+class Integration extends WordSpecLike with Matchers with RunBeam{
+
+  val eventManager = new EventsManagerImpl()
+  val eventsReader = new EventsReaderXMLv1(eventManager)
+
+
+
+  // assumes that dir is a directory known to exist
+  def getListOfSubDirectories(dir: File): List[String] =
+    dir.listFiles
+      .filter(_.isDirectory)
+      .map(_.getName)
+      .toList
+
+
+  "Run Beam" must {
+
+    var route_output = ""
+    var file: File = null
+
+
+
+    "Start without errors" in {
+
+
+      val exc = Try(rumBeamWithConfigFile(Some(s"${System.getenv("PWD")}/test/input/beam-developers/model-inputs/beamville/beam.conf")))
+      route_output = ConfigModule.beamConfig.beam.outputs.outputDirectory
+      exc.isSuccess shouldBe true
+    }
+
+    "Create file events.xml in output directory" in {
+      val route = s"$route_output/${getListOfSubDirectories(new File(route_output)).sorted.reverse.head}/ITERS/it.0/0.events.xml"
+      file = new File(route)
+      file.exists() shouldBe true
+    }
+
+    "Events  file  is correct" in {
+      val fileContents = Source.fromFile(file.getPath).getLines.mkString
+      (fileContents.contains("<events version") && fileContents.contains("</events>")) shouldBe true
+    }
+
+  }
+
+}
