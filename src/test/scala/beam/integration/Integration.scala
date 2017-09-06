19b9bfcdb771f7e7e1958ce2daefe110bae1b137
David Arias <fdariasm@gmail.com>
2017-09-06 11:36:41 -0500

    #76 - improving format detection

--- src/test/scala/beam/integration/Integration.scala
+++ src/test/scala/beam/integration/Integration.scala
@@ -14,19 +14,45 @@ import scala.util.Try
   * Created by fdariasm on 29/08/2017
   * 
   */
-class Integration extends WordSpecLike with Matchers with RunBeam{
-
-  val eventManager = new EventsManagerImpl()
-  val eventsReader = new EventsReaderXMLv1(eventManager)
 
 
+trait ReadEvents{
+  def getListTagsFrom(file: File, stringContain: String, tagContain: String): scala.List[String]
+}
 
-  // assumes that dir is a directory known to exist
-  def getListOfSubDirectories(dir: File): List[String] =
+class ReadEventsXml extends ReadEvents {
+  def getListTagsFrom(file: File, stringContain: String, tagContain: String): scala.List[String] = {
+    var listResult = List[String]()
+    for (line <- Source.fromFile(file.getPath).getLines) {
+      if (line.contains(stringContain)) {
+        val temp = scala.xml.XML.loadString(line)
+        val value = temp.attributes(tagContain).toString
+        listResult = value:: listResult
+
+      }
+    }
+    return  listResult
+  }
+
+
+}
+
+class Integration extends WordSpecLike with Matchers with RunBeam with BeforeAndAfterAll{
+
+
+  //Obtains name of latest created folder
+  //Assumes that dir is a directory known to exist
+  def getListOfSubDirectories(dir: File): String = {
+    val simName = ConfigModule.beamConfig.beam.agentsim.simulationName
+    val prefix = s"${simName}_"
     dir.listFiles
-      .filter(_.isDirectory)
+      .filter(s => s.isDirectory && s.getName.startsWith(prefix))
       .map(_.getName)
       .toList
+      .sorted
+      .reverse
+      .head
+  }
 
   def getListIDsWithTag(file: File, tagIgnore: String, positionID: Int): List[String] = {
     var listResult = List[String]()
@@ -53,33 +79,41 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
     }
 
     return  listResult
-
   }
 
+  def getRouteFile(route_output: String): File = {
+    val route = s"$route_output/${getListOfSubDirectories(new File(route_output))}/ITERS/it.0/0.events.xml"
+    new File(route)
+  }
 
+  lazy val exc = Try(rumBeamWithConfigFile(Some(s"${System.getenv("PWD")}/test/input/beamville/beam.conf")))
+  lazy val file: File = getRouteFile(ConfigModule.beamConfig.beam.outputs.outputDirectory)
 
+  lazy val route_input = ConfigModule.beamConfig.beam.sharedInputs
 
+  lazy val eventsReader: ReadEvents = {
+    ConfigModule.beamConfig.beam.outputs.eventsFileOutputFormats match{
+      case "xml" => new ReadEventsXml
+      case "csv" => ???
+      case "xml.gz" => ???
+      case "csv.gz" => ???
+      case _ => throw new RuntimeException("Unsupported format")
+    }
+  }
+
+  override def beforeAll(): Unit = {
+    exc
+    file
+    route_input
+    eventsReader
+  }
 
   "Run Beam" must {
-
-    var route_output = ""
-    var file: File = null
-    var route_input  =""
-
-
-
     "Start without errors" in {
-
-
-      val exc = Try(rumBeamWithConfigFile(Some(s"${System.getenv("PWD")}/test/input/beamville/beam.conf")))
-      route_output = ConfigModule.beamConfig.beam.outputs.outputDirectory
-      route_input = ConfigModule.beamConfig.beam.sharedInputs
       exc.isSuccess shouldBe true
     }
 
     "Create file events.xml in output directory" in {
-      val route = s"$route_output/${getListOfSubDirectories(new File(route_output)).sorted.reverse.head}/ITERS/it.0/0.events.xml"
-      file = new File(route)
       file.exists() shouldBe true
     }
 
@@ -92,24 +126,22 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
       val route = s"$route_input/r5/bus/trips.txt"
       val listTrips = getListIDsWithTag(new File(route), "route_id", 2).sorted
 
-      val listValueTagEventFile = getListTagsFromXml(new File(file.getPath),"person='TransitDriverAgent-bus.gtfs","vehicle")
+      val listValueTagEventFile = eventsReader.getListTagsFrom(new File(file.getPath),"person='TransitDriverAgent-bus.gtfs","vehicle")
 
       listTrips.size shouldBe(listValueTagEventFile.size)
 
 
     }
-    "Events file contains all trail routes" in {
+    "Events file contains all train routes" in {
       val route = s"$route_input/r5/train/trips.txt"
       val listTrips = getListIDsWithTag(new File(route), "route_id", 2).sorted
 
       val listValueTagEventFile = getListTagsFromXml(new File(file.getPath),"person='TransitDriverAgent-train.gtfs","vehicle")
 
       listTrips.size shouldBe(listValueTagEventFile.size)
-
     }
 
-
-    "Events file contains exactly the same bus trips of entry" in {
+    "Events file contains exactly the same bus trips entries" in {
 
       val route = s"$route_input/r5/bus/trips.txt"
       val listTrips = getListIDsWithTag(new File(route), "route_id", 2).sorted
@@ -121,7 +153,7 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
 
     }
 
-    "Events file contains exactly the same train trips of entry" in {
+    "Events file contains exactly the same train trips entries" in {
 
       val route = s"$route_input/r5/train/trips.txt"
       val listTrips = getListIDsWithTag(new File(route), "route_id", 2).sorted
@@ -132,7 +164,7 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
       listTrips shouldBe(listTripsEventFile)
 
     }
-    "Events file should contain same pathTraversal defined at stop times file for train input file" in {
+    "Events file contain same pathTraversal defined at stop times file for train input file" in {
       val route = s"$route_input/r5/train/stop_times.txt"
       val listTrips = getListIDsWithTag(new File(route), "trip_id", 0).sorted
 
@@ -148,8 +180,7 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
 
     }
 
-    "Events file should contain same pathTraversal defined at stop times file for bus input file" in {
-
+    "Events file contain same pathTraversal defined at stop times file for bus input file" in {
       val route = s"$route_input/r5/bus/stop_times.txt"
       val listTrips = getListIDsWithTag(new File(route), "trip_id", 0).sorted
       val grouped = listTrips.groupBy(identity)
@@ -161,15 +192,6 @@ class Integration extends WordSpecLike with Matchers with RunBeam{
       val groupedXmlWithCount = groupedXml.map{case (k,v) => (k, v.size)}
 
       groupedWithCount should contain theSameElementsAs(groupedXmlWithCount)
-
-
     }
-
-
-
-
-
-
   }
-
 }
