44b65d1416c68c944775ba6819476e57e6c0764f
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2017-09-09 06:04:31 +0500

    script termination troubleshooting.

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -2,4 +2,8 @@
 
 cd ~/beam
 
+echo 'Starting BEAM...'
 java -Xmx2g -jar build/libs/beam.jar --config test/input/beamville/beam.conf > ~/beam/logs/out.log
+echo 'BEAM Simulation Completed...'
+
+sleep 60
