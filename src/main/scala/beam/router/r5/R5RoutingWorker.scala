c141d84adf3219f31d89cfdadc77040fcd0851d1
dserdiuk <denys.serdiuk@northstar-its.com>
2017-09-10 23:56:30 +0300

    Merge branch 'agentsim' into dserdiuk/beam_path_transit minor changes # Conflicts: #	src/main/scala/beam/router/r5/R5RoutingWorker.scala

--- src/main/scala/beam/router/r5/R5RoutingWorker.scala
+++ src/main/scala/beam/router/r5/R5RoutingWorker.scala
@@ -18,10 +18,11 @@ import beam.router.Modes.{BeamMode, _}
 import beam.router.RoutingModel.BeamLeg._
 import beam.router.RoutingModel._
 import beam.router.RoutingWorker.HasProps
-import beam.router.r5.R5RoutingWorker.{GRAPH_FILE, ProfileRequestToVehicles, stopForIndex, transportNetwork, beamPathBuilder}
+import beam.router.r5.R5RoutingWorker.{GRAPH_FILE, ProfileRequestToVehicles, beamPathBuilder, stopForIndex, transportNetwork}
 import beam.router.{Modes, RoutingWorker, TrajectoryByEdgeIdsResolver}
+import com.conveyal.gtfs.model.Stop
 import beam.sim.BeamServices
-import beam.utils.RefectionUtils
+import beam.utils.reflection.RefectionUtils
 import com.conveyal.r5.api.ProfileResponse
 import com.conveyal.r5.api.util._
 import com.conveyal.r5.point_to_point.builder.PointToPointQuery
