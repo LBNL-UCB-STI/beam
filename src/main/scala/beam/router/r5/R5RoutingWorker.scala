41e3819c417c40f7b4b60b027cdf3e8e58aa38cb
Colin Sheppard <colinthegaunt@gmail.com>
2017-09-11 23:32:28 -0700

    debugging

--- src/main/scala/beam/router/r5/R5RoutingWorker.scala
+++ src/main/scala/beam/router/r5/R5RoutingWorker.scala
@@ -108,7 +108,7 @@ class R5RoutingWorker(val beamServices: BeamServices) extends RoutingWorker {
             previousBeamLeg = Some(theLeg)
             stopStopDepartTuple = (previousBeamLeg.get.travelPath.transitStops.get.fromStopId, previousBeamLeg.get.travelPath.transitStops.get.toStopId, previousBeamLeg.get.startTime)
 //            if(stopStopDepartTuple._1.eq("0") && stopStopDepartTuple._2.eq("23")){
-            if(stopStopDepartTuple._1.eq("0")){
+            if(stopStopDepartTuple._1.equals("0") || stopStopDepartTuple._2.equals("0")){
               val i = 0
             }
           }
