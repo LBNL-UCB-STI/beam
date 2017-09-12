bdc0e1f27fe62d579153234b1f94275efc02d26e
sfwatergit <sid.feygin@gmail.com>
2017-09-12 00:59:30 -0700

    Temp fix to ReflectionUtils so that we can have working events

--- src/main/scala/beam/utils/reflection/RefectionUtils.scala
+++ src/main/scala/beam/utils/reflection/RefectionUtils.scala
@@ -17,17 +17,17 @@ import scala.reflect.ClassTag
 object RefectionUtils {
 
   val reflections = {
-    val classLoader = RefectionUtils.getClass.getClassLoader
-    val builder = new ConfigurationBuilder
+//    val classLoader = RefectionUtils.getClass.getClassLoader
+//    val builder = new ConfigurationBuilder
+//
+//    val urlTypes = Lists.newArrayList[Vfs.UrlType]
+//    // include a list of file extensions / filenames to be recognized
+//    urlTypes.add(new EmptyIfFileEndingsUrlType(".jnilib"))
+//    Vfs.DefaultUrlTypes.values.foreach(typ => urlTypes.add(typ))
+//
+//    Vfs.setDefaultURLTypes(urlTypes)
 
-    val urlTypes = Lists.newArrayList[Vfs.UrlType]
-    // include a list of file extensions / filenames to be recognized
-    urlTypes.add(new EmptyIfFileEndingsUrlType(".jnilib"))
-    Vfs.DefaultUrlTypes.values.foreach(typ => urlTypes.add(typ))
-
-    Vfs.setDefaultURLTypes(urlTypes)
-
-    new Reflections(builder.addClassLoader(classLoader))
+    new Reflections("org.matsim")
   }
 
   def classesOfType[T](implicit ct: ClassTag[T]): List[Class[T]] = {
