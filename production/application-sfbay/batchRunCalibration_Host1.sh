e861a5f422d547001edc64b23f29f943c3928c46
wrashid <wrashid@users.noreply.github.com>
2017-09-25 16:08:48 -0700

    adding/updating calibration configs/mode choice/batches

--- production/application-sfbay/batchRunCalibration_Host1.sh
+++ production/application-sfbay/batchRunCalibration_Host1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers1pct.conf']"
+./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pmlA.conf']"
 ./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pct.conf']"
--- production/application-sfbay/batchRunCalibration_Host2.sh
+++ production/application-sfbay/batchRunCalibration_Host2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers1pml.conf']"
+./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pmlB.conf']"
 ./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers2pct.conf']"
