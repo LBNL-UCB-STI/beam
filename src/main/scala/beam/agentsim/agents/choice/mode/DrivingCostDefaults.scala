69171a0555a439023ae4fa2b72a468def0410e24
Colin Sheppard <colinthegaunt@gmail.com>
2017-09-24 20:48:33 -0700

    fix NPE

--- src/main/scala/beam/agentsim/agents/choice/mode/DrivingCostDefaults.scala
+++ src/main/scala/beam/agentsim/agents/choice/mode/DrivingCostDefaults.scala
@@ -14,7 +14,11 @@ object DrivingCostDefaults {
       alt.tripClassifier match {
         case CAR if alt.costEstimate == 0.0 =>
           val vehicle = beamServices.vehicles(alt.legs.filter(_.beamLeg.mode == CAR).head.beamVehicleId)
-          val litersPerMeter = vehicle.getType.getEngineInformation.getGasConsumption
+          val litersPerMeter = if(vehicle == null || vehicle.getType == null || vehicle.getType.getEngineInformation == null || vehicle.getType.getEngineInformation.getGasConsumption == null){
+            0.0001069
+          }else{
+            vehicle.getType.getEngineInformation.getGasConsumption
+          }
           val cost = alt.legs.map(_.beamLeg.travelPath.distanceInM).sum * litersPerMeter / 3.78541 * 3.115 // 3.78 liters per gallon and 3.115 $/gal in CA: http://www.californiagasprices.com/Prices_Nationally.aspx
           BigDecimal(cost)
         case _ =>
