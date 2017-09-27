26e8260873c2b3a1b793323cb541baf17352d725
sfwatergit <sid.feygin@gmail.com>
2017-09-26 17:26:33 -0700

    Remove scoring events

--- src/main/java/beam/sim/controler/corelisteners/BeamControllerCoreListenersModule.java
+++ src/main/java/beam/sim/controler/corelisteners/BeamControllerCoreListenersModule.java
@@ -1,18 +1,18 @@
 package beam.sim.controler.corelisteners;
 
+import beam.agentsim.events.handling.BeamEventsHandling;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.corelisteners.DumpDataAtEnd;
 import org.matsim.core.controler.corelisteners.EventsHandling;
 import org.matsim.core.controler.corelisteners.PlansDumping;
 import org.matsim.core.controler.corelisteners.PlansReplanning;
 import org.matsim.core.scoring.PlansScoringModule;
-import beam.agentsim.events.handling.BeamEventsHandling;
 
 public class BeamControllerCoreListenersModule extends AbstractModule {
 
 	@Override
 	public void install() {
-		install(new PlansScoringModule());
+		install(new beam.agentsim.events.handling.PlansScoringModule());
 		bind( PlansReplanning.class ).to( PlansReplanningImpl.class );
 		bind( PlansDumping.class ).to( PlansDumpingImpl.class );
 		bind( EventsHandling.class ).to( BeamEventsHandling.class );
