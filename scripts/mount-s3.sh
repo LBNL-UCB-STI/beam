fbed9d5ce3b16077404f29496b381060597650bb
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2017-09-28 18:58:59 +0500

    s3 mound at system boot and zip fix in cloud-init

--- scripts/mount-s3.sh
+++ scripts/mount-s3.sh
@@ -2,12 +2,12 @@
 sudo apt install s3fs
 echo $1 > ~/.passwd-s3fs
 chmod 600 ~/.passwd-s3fs
-mkdir /tmp/cache
-chmod 777 /tmp/cache
-sudo mkdir ~/s3
-sudo s3fs -o use_cache=/tmp/cache -oallow_other beam-outputs ~/s3
+mkdir ~/.cache-s3
+chmod 777 ~/.cache-s3
+mkdir ~/s3
+s3fs -o use_cache=/.cache-s3 -oallow_other beam-outputs ~/s3
 
 # To setup so it can be mounted after reboot
 sudo cp .passwd-s3fs /etc/passwd-s3fs
 sudo chmod 640 /etc/passwd-s3fs
-sudo echo 's3fs#beam-outputs /home/ubuntu/s3 fuse allow_other,use_cache=/tmp/cache 0 0' >> /etc/fstab
+sudo echo 'beam-outputs /home/ubuntu/s3 fuse.s3fs _netdev,allow_other,use_cache=/home/ubuntu/.cache-s3 0 0' >> /etc/fstab
