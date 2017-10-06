b1fcfdc2380ca474a15cf4324a502bb0680d7e3b
Colin Sheppard <colinthegaunt@gmail.com>
2017-10-06 07:03:52 -0700

    remove production data

--- production/application-sfbay/batchExperiment-4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_toll_high.conf']"
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_toll_low.conf']"
