26696423fd8518f54eb9491abbafbdde417b7476
wrashid <wrashid@users.noreply.github.com>
2017-10-26 03:11:09 -0700

    updating run batch files.

--- production/application-sfbay/batchExperiment-3.sh
+++ production/application-sfbay/batchExperiment-3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MAXRAM=1920g
+export MAXRAM=240g
 ./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_transit_price_high.conf']"
 ./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_transit_price_low.conf']"
