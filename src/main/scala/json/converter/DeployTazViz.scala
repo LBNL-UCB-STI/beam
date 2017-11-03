5fa2c8986a3fae33d724c27b15d9221d7fcd4971
David Arias <fdariasm@gmail.com>
2017-11-03 17:21:28 -0500

    Code cleanup

--- src/main/scala/json/converter/DeployTazViz.scala
+++ src/main/scala/json/converter/DeployTazViz.scala
@@ -3,13 +3,9 @@ package json.converter
 import java.io.{File, FileNotFoundException, IOException}
 import java.util
 
-import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
 import net.freeutils.httpserver.HTTPServer
-import org.geotools.data.Base64.InputStream
-import org.apache.commons.io.IOUtils
-import java.io.BufferedReader
-import java.util.Arrays
+
 
 import play.api.libs.json.Json
 
@@ -22,7 +18,7 @@ object DeployTazViz extends App {
     val trip_totals = "tnc_taz_totals.json"
     val taz_boundarys = "taz_boundaries.json"
 
-    val rootzip = new java.util.zip.ZipFile(s"${args(0)}/db-files_2.zip")
+    val rootzip = new java.util.zip.ZipFile(s"${args(0)}/db-files.zip")
 
     val dd = rootzip.getEntry(trip_stats)
     val totalsTaz = rootzip.getEntry(trip_totals)
@@ -33,36 +29,31 @@ object DeployTazViz extends App {
     val input_total = rootzip.getInputStream(totalsTaz)
     val input_boundary = rootzip.getInputStream(boundarys)
 
-    System.out.println("Proccessing JSON....")
+    println("Proccessing JSON....")
 
     val tazBoundarys = scala.io.Source.fromInputStream(input_boundary)
       .mkString
 
-
-
-
-    val tazStatsByTazDay = Json
+    val tazStripTotals = Json
       .parse(scala.io.Source.fromInputStream(input_f)
-      .mkString)
+        .mkString)
       .as[Seq[TazStats]]
-      .groupBy( e => (e.taz, e.day_of_week))
 
-    val tazStripTotals = tazStatsByTazDay.values.flatten.toSeq
+    val tazStatsByTazDay = tazStripTotals.groupBy( e => (e.taz, e.day_of_week))
 
     val tazTotals = Json
       .parse(scala.io.Source.fromInputStream(input_total)
         .mkString)
       .as[Seq[TazStatsTotals]]
 
-    System.out.println("Finish Proccess...")
+    println("Finish Proccess...")
 
     val dir = new File(args(0))
     val port = args(1).toInt
-    System.out.println("HTTPServer dir " + dir)
-    System.out.println("HTTPServer port " + port)
+    println("HTTPServer dir " + dir)
+    println("HTTPServer port " + port)
 
     if (!dir.canRead) throw new FileNotFoundException(dir.getAbsolutePath)
-
 
     for (f <- util.Arrays.asList(new File("/etc/mime.types"), new File(dir, ".mime.types"))) {
       if (f.exists) HTTPServer.addContentTypes(f)
@@ -93,10 +84,8 @@ object DeployTazViz extends App {
             resp.send(200, response)
             0
           }
-
       }
     })
-
     host.addContext("/api/tnc_taz_totals", new HTTPServer.ContextHandler() {
       //@throws[IOException]
       override def serve(req: HTTPServer#Request, resp: HTTPServer#Response): Int = {
@@ -108,9 +97,6 @@ object DeployTazViz extends App {
       }
     })
 
-
-
-
     host.addContext("/api/taz_boundaries", new HTTPServer.ContextHandler() {
       //@throws[IOException]
       override def serve(req: HTTPServer#Request, resp: HTTPServer#Response): Int = {
@@ -121,18 +107,12 @@ object DeployTazViz extends App {
         0
       }
     })
-
-
-
-
-
-
     server.start()
-    System.out.println("HTTPServer is listening on port " + port)
+    println("HTTPServer is listening on port " + port)
   }
   catch {
     case e: Exception =>
-      System.err.println("error: " + e)
+      println("error: " + e)
   }
 
   def readJsonFile(f: File): String = {
@@ -140,11 +120,5 @@ object DeployTazViz extends App {
     val lines = try source.mkString finally source.close()
     lines
   }
-
-  import java.io.File
-  import java.io.FileOutputStream
-  import java.io.IOException
-
-
 
 }
