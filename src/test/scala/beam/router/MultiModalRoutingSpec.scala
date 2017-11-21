5999030a82b4ef52fe5e4ec850d0227174b20e0e
Colin Sheppard <colinthegaunt@gmail.com>
2017-11-21 14:40:01 -0800

    test to guard against missing GTFS data which results in bad start time of trips

---
+++ src/test/scala/beam/router/MultiModalRoutingSpec.scala
@@ -0,0 +1,75 @@
+package beam.router
+
+import java.io.File
+import java.time.ZonedDateTime
+
+import akka.actor.{ActorIdentity, ActorRef, ActorSystem, Identify}
+import akka.testkit.{ImplicitSender, TestKit}
+import beam.agentsim.agents.vehicles.BeamVehicle.StreetVehicle
+import beam.agentsim.events.SpaceTime
+import beam.router.BeamRouter._
+import beam.router.Modes.BeamMode.WALK
+import beam.router.RoutingModel.BeamLegWithNext
+import beam.router.gtfs.FareCalculator
+import beam.sim.BeamServices
+import beam.sim.common.GeoUtilsImpl
+import beam.sim.config.BeamConfig
+import beam.utils.DateUtils
+import com.typesafe.config.ConfigFactory
+import org.matsim.api.core.v01.network.Link
+import org.matsim.api.core.v01.population.Person
+import org.matsim.api.core.v01.{Coord, Id}
+import org.matsim.core.config.ConfigUtils
+import org.matsim.core.controler.MatsimServices
+import org.matsim.core.scenario.ScenarioUtils
+import org.matsim.vehicles.Vehicle
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.collection.concurrent.TrieMap
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class MultiModalRoutingSpec extends TestKit(ActorSystem("router-test")) with WordSpecLike with Matchers
+  with ImplicitSender with MockitoSugar with BeforeAndAfterAll {
+
+  var router: ActorRef = _
+
+  override def beforeAll: Unit = {
+//    val beamConfig = BeamConfig(ConfigFactory.parseFile(new File("test/input/beamville/beam.conf")).resolve())
+    val beamConfig = BeamConfig(ConfigFactory.parseFile(new File("application/sf-light/base-sf-light.conf")).resolve())
+
+    // Have to mock a lot of things to get the router going
+    val services: BeamServices = mock[BeamServices]
+    val scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig())
+    when(services.beamConfig).thenReturn(beamConfig)
+    when(services.geo).thenReturn(new GeoUtilsImpl(services))
+    val matsimServices = mock[MatsimServices]
+    when(matsimServices.getScenario).thenReturn(scenario)
+    when(services.matsimServices).thenReturn(matsimServices)
+    when(services.dates).thenReturn(DateUtils(beamConfig.beam.routing.baseDate,ZonedDateTime.parse(beamConfig.beam.routing.baseDate).toLocalDateTime,ZonedDateTime.parse(beamConfig.beam.routing.baseDate)))
+    val tupleToNext = new TrieMap[Tuple3[Int, Int, Long],BeamLegWithNext]
+
+    val fareCalculator = new FareCalculator(beamConfig.beam.routing.r5.directory)
+    router = system.actorOf(BeamRouter.props(services, scenario.getTransitVehicles, fareCalculator))
+
+    within(60 seconds) { // Router can take a while to initialize
+      router ! Identify(0)
+      expectMsgType[ActorIdentity]
+    }
+  }
+
+  "A multi-modal router" must {
+    "return a route with a starting time consistent with profile request" in {
+      val origin = new BeamRouter.Location(552788,4179300) // -122.4007,37.7595
+      val destination = new BeamRouter.Location(548918,4182749) // -122.4444,37.7908
+      val time = RoutingModel.WindowTime(100,200)
+      router ! RoutingRequest(RoutingRequestTripInfo(origin, destination, time, Vector(), Vector(StreetVehicle(Id.createVehicleId("body-667520-0"), new SpaceTime(new Coord(origin.getX, origin.getY), time.atTime), Modes.BeamMode.WALK, asDriver = true)), Id.createPersonId("667520-0")))
+      val response = expectMsgType[RoutingResponse]
+      val routedStartTime = response.itineraries.head.beamLegs().head.startTime
+      assert(routedStartTime >= 100 && routedStartTime <= 200)
+    }
+  }
+
+}
