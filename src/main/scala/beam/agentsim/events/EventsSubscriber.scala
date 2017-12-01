91c89042db34fe221b35e2f5a0314a385cd8502e
Michael Zilske <michael.zilske@tu-berlin.de>
2017-11-30 17:02:06 -0800

    Remove some unused stuff

--- src/main/scala/beam/agentsim/events/EventsSubscriber.scala
+++ src/main/scala/beam/agentsim/events/EventsSubscriber.scala
@@ -2,20 +2,10 @@ package beam.agentsim.events
 
 import akka.actor.{Actor, ActorLogging}
 import beam.agentsim.events.AgentsimEventsBus.MatsimEvent
-import beam.agentsim.events.EventsSubscriber.{EndIteration, ProcessingFinished}
 import org.matsim.core.api.experimental.events.EventsManager
 
 object EventsSubscriber{
   val SUBSCRIBER_NAME:String = "MATSIMEventsSubscriber"
-
-  trait SubscriberEvent
-  case object StartProcessing extends SubscriberEvent
-  case class StartIteration(iteration: Int) extends SubscriberEvent
-  case class EndIteration(iteration: Int) extends SubscriberEvent
-  case class AfterSimStep(when: Int) extends SubscriberEvent
-  case class FinishProcessing(iteration: Int) extends SubscriberEvent
-  case class ProcessingFinished(iteration: Int) extends SubscriberEvent
-  case object FileWritten extends SubscriberEvent
 }
 
 class EventsSubscriber (private val eventsManager: EventsManager) extends Actor with ActorLogging {
@@ -25,8 +15,6 @@ class EventsSubscriber (private val eventsManager: EventsManager) extends Actor 
   def receive: Receive = {
     case event: Event =>
       eventsManager.processEvent(event.wrappedEvent)
-    case EndIteration(it) =>
-      sender() ! ProcessingFinished(it)
   }
 
 }
