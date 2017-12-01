18decbb3105ba23f6a0cfb98c8d47e6c1dd94821
Michael Zilske <michael.zilske@tu-berlin.de>
2017-11-30 16:58:18 -0800

    Remove some unused stuff

--- src/main/scala/beam/agentsim/events/EventsSubscriber.scala
+++ src/main/scala/beam/agentsim/events/EventsSubscriber.scala
@@ -1,11 +1,9 @@
 package beam.agentsim.events
 
-import akka.actor.Actor
-import akka.event.Logging
+import akka.actor.{Actor, ActorLogging}
 import beam.agentsim.events.AgentsimEventsBus.MatsimEvent
-import beam.agentsim.events.EventsSubscriber.{AfterSimStep, EndIteration, FinishProcessing, ProcessingFinished}
+import beam.agentsim.events.EventsSubscriber.{EndIteration, ProcessingFinished}
 import org.matsim.core.api.experimental.events.EventsManager
-import org.matsim.core.controler.events.IterationEndsEvent
 
 object EventsSubscriber{
   val SUBSCRIBER_NAME:String = "MATSIMEventsSubscriber"
@@ -18,41 +16,19 @@ object EventsSubscriber{
   case class FinishProcessing(iteration: Int) extends SubscriberEvent
   case class ProcessingFinished(iteration: Int) extends SubscriberEvent
   case object FileWritten extends SubscriberEvent
-
 }
 
-class EventsSubscriber (private val eventsManager: EventsManager) extends Actor {
-  val log = Logging(context.system, this)
-
+class EventsSubscriber (private val eventsManager: EventsManager) extends Actor with ActorLogging {
 
   type Event = MatsimEvent
 
-
   def receive: Receive = {
-
     case event: Event =>
-      try {
-        eventsManager.processEvent(event.wrappedEvent)
-
-      }catch {
-        case e:IllegalArgumentException =>
-          log.error(s"$e")
-      }
-
+      eventsManager.processEvent(event.wrappedEvent)
     case EndIteration(it) =>
       sender() ! ProcessingFinished(it)
-
-    case _ => log.info("received unknown message")
   }
 
-  /**
-    * Used to process the events file into output useful for visualization.
-    */
-  // XXXX: comment out until a) we figure out fate of viz format, b) realtime considerations, or c) necessary [saf 10/10/2017]
-  private def cleanupWriter(): Unit = {
-    //    JsonUtils.processEventsFileVizData(beamServices.matsimServices.getControlerIO.getIterationFilename(currentIter, s"events.xml${gzExtension}"),
-    //      beamServices.matsimServices.getControlerIO.getOutputFilename("trips.json"))
-  }
 }
 
 
