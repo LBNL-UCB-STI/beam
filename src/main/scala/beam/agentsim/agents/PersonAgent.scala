b545fa2c5e6973e335530151d8339dff0ef5b59e
Michael Zilske <michael.zilske@tu-berlin.de>
2017-12-05 13:21:47 -0800

    reschedule
    (cherry picked from commit cd637ea)

--- src/main/scala/beam/agentsim/agents/PersonAgent.scala
+++ src/main/scala/beam/agentsim/agents/PersonAgent.scala
@@ -236,8 +236,8 @@ class PersonAgent(val beamServices: BeamServices,
           }
       }
 
-    case Event(TriggerWithId(NotifyLegEndTrigger(_,beamLeg),_), _) =>
-      stop(Failure(s"Going to Error: NotifyLegEndTrigger while in state Waiting with beamLeg: $beamLeg"))
+    case Event(TriggerWithId(NotifyLegEndTrigger(tick,beamLeg), triggerId), _) =>
+      warnAndRescheduleNotifyLeg(tick, triggerId, beamLeg, false)
   }
 
   chainedWhen(Moving) {
