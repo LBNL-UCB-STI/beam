b1b5a970a1a3add5a2cc14de2641a25b5a042fd2
Colin Sheppard <colinthegaunt@gmail.com>
2017-12-06 08:01:18 -0800

    remove prod

--- production/application-sfbay/calibration/batchRunCalibration_Host1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pmlC.conf']"
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pct.conf']"
