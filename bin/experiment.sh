6dc69efec7e5a398b4a6cc17876a20ae531d9ecc
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2017-12-09 02:44:03 +0500

    automated cloud deployment with experement

---
+++ bin/experiment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Generating experiments for config: $1"
+java -cp build/libs/beam.jar beam.experiment.ExperimentGenerator --experiments $1
+
+echo "Experiments generated in: $(dirname $1)"
+IFS=$'\n' read -d '' -r -a experiments < $(dirname $1)/experiments.csv
+
+#for exp in "${experiments[@]}"
+for (( i=1; i<${#experiments[@]}; i++ ));
+do
+    IFS=', ' read -r -a csv_rows <<< "${experiments[$i]}"
+    echo "Running experiment using config ${csv_rows[4]}/beam.conf"
+    ${csv_rows[4]}/runExperiment.sh  > ${csv_rows[4]}/console.log 2>&1
+done
+unset experiments
+
