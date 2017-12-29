606079534e99778d336e3185ef357c77132cca5b
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2017-12-30 02:12:12 +0500

    update beanch name

--- bin/experiment.sh
+++ bin/experiment.sh
@@ -4,5 +4,6 @@ java -cp build/libs/beam.jar beam.experiment.ExperimentGenerator --experiments $
 
 echo "Experiments generated in: $(dirname $1)"
 base_dir=$(dirname $1)
+    
 echo "Running experiment batch on base $base_dir"
 $base_dir/runs/batchRunExperiment.sh $2
