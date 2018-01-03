cc991c2485dedd703096f25f012149048f9b36c5
Colin Sheppard <colinthegaunt@gmail.com>
2018-01-02 20:34:57 -0800

    cleaning out old config cruft and prepping for new run

--- production/application-sfbay/calibration/batchRunCalibration_Host2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers5pmlD.conf']"
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/sfBay_mode_calibration_reduceAvailableRideHailingDrivers2pct.conf']"
--- production/application-sfbay/walk/batchWalk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export MAXRAM=1920g
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/walk6.conf']"
-./gradlew run -PappArgs="['--config', 'production/application-sfbay/walk10.conf']"
