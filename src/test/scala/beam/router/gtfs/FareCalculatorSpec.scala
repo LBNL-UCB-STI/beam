6825e0a1aa0a4f71786bbba49f08dd04f189d469
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-01-10 00:26:10 +0500

    sf-light fare tests added

--- src/test/scala/beam/router/gtfs/FareCalculatorSpec.scala
+++ src/test/scala/beam/router/gtfs/FareCalculatorSpec.scala
@@ -3,66 +3,99 @@ package beam.router.gtfs
 import java.nio.file.{Path, Paths}
 
 import beam.router.gtfs.FareCalculator._
-import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import org.scalatest.WordSpecLike
 
 import scala.language.postfixOps
 
-class FareCalculatorSpec extends WordSpecLike with BeforeAndAfterAll  {
+class FareCalculatorSpec extends WordSpecLike {
 
-  var fareCalculator: FareCalculator = _
+  "Using sf-light calculator" when {
+    val sfLightGTFS: Path = Paths.get("test", "input", "sf-light", "r5")
+    val sfLightFareCalc = new FareCalculator(sfLightGTFS.toString)
 
-  override def beforeAll: Unit = {
-    val fareGTFS: Path = Paths.get("test", "input", "fares")
-    fareCalculator = new FareCalculator(fareGTFS.toString)
-  }
+    "calculate fare for 3555 to 6332" should {
+      "return $2.5 fare" in {
+        assert(sfLightFareCalc.calcFare("SFMTA", "1033", "3555", "6332") == 2.75)
+      }
+    }
 
-  "calculate fare from 55448 to 55450" should {
-    "return 5.5 fare" in {
-      assert(fareCalculator.calcFare("CE", "ACE", "55448", "55450") == 5.5)
+    "calculate fare for cable car route 1060 from stops 5156 to 5067" should {
+      "return $7.0 fare" in {
+        assert(sfLightFareCalc.calcFare("SFMTA", "1060", "5156", "5067") == 7.0)
+      }
+    }
+
+    "calculate fare with null route id, it" should {
+      "not return any fare" in {
+        assert(sfLightFareCalc.calcFare("SFMTA", null, "3832", "7302") == 0.0)
+      }
+    }
+
+    "calculate fare with null agency id, it" should {
+      "not return any fare" in {
+        assert(sfLightFareCalc.calcFare(null, null, "3832", "7302") == 0.0)
+      }
+    }
+
+    "calculate fare  with null origin and destination and provided contains" should {
+      "return a segment fare of $2.75" in {
+        assert(sfLightFareCalc.calcFare("SFMTA", "1034", null, null, Set("3548", "3887", "7302")) == 2.75)
+      }
     }
   }
 
-  "calculate fare with null route id, it" should {
-    "return proper fare" in {
-      assert(fareCalculator.calcFare("CE", null, "55448", "55643") == 9.5)
-    }
-  }
+  "Using test Calculator" when {
+    val testGTFS: Path = Paths.get("test", "input", "fares")
+    val testFareCalc = new FareCalculator(testGTFS.toString)
 
-  "calculate fare from 55448 to 55449 against contains_id" should {
-    "return 4.5 fare" in {
-      assert(fareCalculator.calcFare("CE", "ACE", "55448", "55449") == 4.5)
+    "calculate fare from 55448 to 55450" should {
+      "return 5.5 fare" in {
+        assert(testFareCalc.calcFare("CE", "ACE", "55448", "55450") == 5.5)
+      }
     }
-  }
 
-  "calculate fare with null agency id, it" should {
-    "return zero fare" in {
-      assert(fareCalculator.calcFare(null, null, "55448", "55643") == 0.0)
+    "calculate fare with null route id, it" should {
+      "return proper fare" in {
+        assert(testFareCalc.calcFare("CE", null, "55448", "55643") == 9.5)
+      }
     }
-  }
 
-  "calculate fare with wrong route id, it" should {
-    "return zero fare" in {
-      assert(fareCalculator.calcFare("CE", "1", "55448", "55449") == 0.0)
+    "calculate fare from 55448 to 55449 against contains_id" should {
+      "return 4.5 fare" in {
+        assert(testFareCalc.calcFare("CE", "ACE", "55448", "55449") == 4.5)
+      }
     }
-  }
 
-  "getFareSegments with null origin and destination and provided contains" should {
-    "return a segment fare" in {
-      assert(fareCalculator.calcFare("CE", "ACE", null, null, Set("55448", "55449", "55643")) == 13.75)
+    "calculate fare with null agency id, it" should {
+      "not return any fare" in {
+        assert(testFareCalc.calcFare(null, null, "55448", "55643") == 0.0)
+      }
     }
-  }
 
-  "filterTransferFares with four segments" should {
-    "return 3 segments within transfer duration" in {
-      val fr = fareCalculator.getFareSegments("CE", "ACE", null, null, Set("55448", "55449", "55643")).map(BeamFareSegment(_, 0, 3200)) ++
-        fareCalculator.getFareSegments("CE", "ACE", "55643", "55644").map(BeamFareSegment(_, 0, 3800)) ++
-        fareCalculator.getFareSegments("CE", "ACE", "55644", "55645").map(BeamFareSegment(_, 0, 4300)) ++
-        fareCalculator.getFareSegments("CE", "ACE", "55645", "55645").map(BeamFareSegment(_, 0, 4700))
-      val fsf = filterTransferFares(fr)
-      assert(fsf.nonEmpty)
-      assert(fsf.size == 3)
-      assert(fsf.head.fare.transferDuration == 3800)
-      assert(fsf.map(_.fare.price).sum == 23.0)
+    "calculate fare with wrong route id, it" should {
+      "return zero fare" in {
+        assert(testFareCalc.calcFare("CE", "1", "55448", "55449") == 0.0)
+      }
+    }
+
+    "calculate fare  with null origin and destination and provided contains" should {
+      "return a segment fare" in {
+        assert(testFareCalc.calcFare("CE", "ACE", null, null, Set("55448", "55449", "55643")) == 13.75)
+      }
+    }
+
+    "filterTransferFares with four segments" should {
+      "return 3 segments within transfer duration" in {
+        val fr = testFareCalc.getFareSegments("CE", "ACE", null, null, Set("55448", "55449", "55643")).map(BeamFareSegment(_, 0, 3200)) ++
+          testFareCalc.getFareSegments("CE", "ACE", "55643", "55644").map(BeamFareSegment(_, 0, 3800)) ++
+          testFareCalc.getFareSegments("CE", "ACE", "55644", "55645").map(BeamFareSegment(_, 0, 4300)) ++
+          testFareCalc.getFareSegments("CE", "ACE", "55645", "55645").map(BeamFareSegment(_, 0, 4700))
+        val fsf = filterTransferFares(fr)
+        assert(fsf.nonEmpty)
+        assert(fsf.size == 3)
+        assert(fsf.head.fare.transferDuration == 3800)
+        assert(fsf.map(_.fare.price).sum == 23.0)
+      }
     }
   }
 }
