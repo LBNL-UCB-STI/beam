5f5d6a15b78389ec33ab7fe0ec96bc93c656847f
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-01-16 23:22:43 +0500

    added ec2 instance types in validation list.

--- src/main/python/BeamSimLambda.py
+++ src/main/python/BeamSimLambda.py
@@ -57,7 +57,17 @@ runcmd:
 
 instance_types = ['t2.nano', 't2.micro', 't2.small', 't2.medium', 't2.large', 't2.xlarge', 't2.2xlarge',
                   'm4.large', 'm4.xlarge', 'm4.2xlarge', 'm4.4xlarge', 'm4.10xlarge', 'm4.16xlarge',
-                  'm3.medium', 'm3.large', 'm3.xlarge', 'm3.2xlarge']
+                  'c4.large', 'c4.xlarge', 'c4.2xlarge', 'c4.4xlarge', 'c4.8xlarge',
+                  'g3.4xlarge', 'g3.8xlarge', 'g3.16xlarge',
+                  'p2.xlarge', 'p2.8xlarge', 'p2.16xlarge',
+                  'p3.2xlarge', 'p3.8xlarge', 'p3.16xlarge',
+                  'r4.large', 'r4.xlarge', 'r4.2xlarge', 'r4.4xlarge', 'r4.8xlarge', 'r4.16xlarge',
+                  'r3.large', 'r3.xlarge', 'r3.2xlarge', 'r3.4xlarge', 'r3.8xlarge',
+                  'x1.16xlarge', 'x1.32xlarge',
+                  'd2.xlarge', 'd2.2xlarge', 'd2.4xlarge', 'd2.8xlarge',
+                  'i2.xlarge', 'i2.2xlarge', 'i2.4xlarge', 'i2.8xlarge',
+                  'h1.2xlarge', 'h1.4xlarge', 'h1.8xlarge', 'h1.16xlarge',
+                  'i3.large', 'i3.xlarge', 'i3.2xlarge', 'i3.4xlarge', 'i3.8xlarge', 'i3.16xlarge']
 
 s3 = boto3.client('s3')
 ec2 = boto3.client('ec2',region_name=os.environ['REGION'])
