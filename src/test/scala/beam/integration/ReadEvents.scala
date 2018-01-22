a3b0293275844d43899830d466e82a5744091efa
David Arias <fdariasm@gmail.com>
2018-01-22 10:33:32 -0500

    Updating Tests

--- src/test/scala/beam/integration/ReadEvents.scala
+++ src/test/scala/beam/integration/ReadEvents.scala
@@ -5,10 +5,18 @@ import java.io.File
 trait ReadEvents{
   def getListTagsFromFile(file: File, mkeyValue: Option[(String, String)] = None,
                           tagToReturn: String,
-                          eventType: Option[String] = None): Seq[String]
+                          eventType: Option[String] = None,
+                          tagTwoToReturn: Option[String] = None): Seq[String]
 
   def getListTagsFrom(filePath: String, mkeyValue: Option[(String, String)] = None,
                       tagToReturn: String,
                       eventType: Option[String] = None): Seq[String]
 
+
+  def getListTwoTagsFromFile(file: File, mkeyValue: Option[(String, String)] = None,
+                             tagToReturn: String,
+                             eventType: Option[String] = None,
+                             tagTwoToReturn: Option[String] = None): Seq[(String, String)]
+
+  def getLinesFrom(file: File): String
 }
--- src/test/scala/beam/integration/ReadEventsBeam.scala
+++ src/test/scala/beam/integration/ReadEventsBeam.scala
@@ -6,22 +6,32 @@ import org.matsim.api.core.v01.events.Event
 import org.matsim.core.events.handler.BasicEventHandler
 import org.matsim.core.events.{EventsUtils, MatsimEventsReader}
 
+import scala.io.Source
 import scala.collection.JavaConverters._
-import scala.collection.immutable.Queue
 
 class ReadEventsBeam extends ReadEvents{
   val basicEventHandler = new BasicEventHandler{
-    var events: Queue[Event] = Queue()
+    var events: Seq[Event] = Seq()
     def handleEvent(event: Event): Unit = {
       events = events :+ event
     }
+
   }
 
   def getListTagsFromFile(file: File, mkeyValue: Option[(String, String)] = None,
                           tagToReturn: String,
-                          eventType: Option[String] = None): Seq[String] = {
+                          eventType: Option[String] = None,
+                          tagTwoToReturn: Option[String] = None): Seq[String] = {
     getListTagsFrom(file.getAbsolutePath, mkeyValue, tagToReturn, eventType)
   }
+
+  def getListTwoTagsFromFile(file: File, mkeyValue: Option[(String, String)] = None,
+                             tagToReturn: String,
+                             eventType: Option[String] = None,
+                             tagTwoToReturn: Option[String] = None): Seq[(String, String)] = {
+    getListTwoTagsFrom(file.getAbsolutePath, mkeyValue, tagToReturn, eventType, tagTwoToReturn.get)
+  }
+
 
   def getListTagsFrom(filePath: String, mkeyValue: Option[(String, String)] = None,
                       tagToReturn: String,
@@ -35,8 +45,8 @@ class ReadEventsBeam extends ReadEvents{
     val events = basicEventHandler.events
     val filteredEvents = events.filter{ event =>
       val attributes = event.getAttributes.asScala
-      eventType.forall(_.equals(event.getEventType)) &&
-        mkeyValue.forall { case (key, value) => attributes.get(key).exists(_.contains(value)) }
+      eventType.map(_.equals(event.getEventType)).getOrElse(true) &&
+        mkeyValue.map{case (key, value) => attributes.get(key).filter(_.contains(value)).isDefined}.getOrElse(true)
 
     }
     filteredEvents
@@ -46,4 +56,28 @@ class ReadEventsBeam extends ReadEvents{
 
   }
 
+
+  def getListTwoTagsFrom(filePath: String, mkeyValue: Option[(String, String)] = None,
+                         tagToReturn: String,
+                         eventType: Option[String] = None,
+                         tagTwoToReturn: String): Seq[(String, String)] = {
+    val eventsMan = EventsUtils.createEventsManager()
+    eventsMan.addHandler(basicEventHandler)
+
+    val reader = new MatsimEventsReader(eventsMan)
+    reader.readFile(filePath)
+
+    val events = basicEventHandler.events
+    val filteredEvents = events.filter{ event =>
+      val attributes = event.getAttributes.asScala
+      eventType.map(_.equals(event.getEventType)).getOrElse(true) &&
+        mkeyValue.map{case (key, value) => attributes.get(key).filter(_.contains(value)).isDefined}.getOrElse(true)
+
+    }
+    filteredEvents.map( f =>(f.getAttributes.asScala(tagToReturn) ,f.getAttributes.asScala(tagTwoToReturn) ))
+  }
+
+  def getLinesFrom(file: File): String = {
+    Source.fromFile(file.getPath).getLines.mkString
+  }
 }
