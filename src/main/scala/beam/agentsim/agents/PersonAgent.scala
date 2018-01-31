3041b0a9888972a71c8b36e1ea75cfb4f5e44585
sfwatergit <sid.feygin@gmail.com>
2018-01-31 12:53:57 -0800

    Merge

--- src/main/scala/beam/agentsim/agents/PersonAgent.scala
+++ src/main/scala/beam/agentsim/agents/PersonAgent.scala
@@ -7,8 +7,6 @@ import beam.agentsim.agents.BeamAgent._
 import beam.agentsim.agents.PersonAgent._
 import beam.agentsim.agents.TriggerUtils._
 import beam.agentsim.agents.household.HouseholdActor.{AttributesOfIndividual, ReleaseVehicleReservation}
-import beam.agentsim.agents.modalBehaviors.ChoosesMode.BeginModeChoiceTrigger
-import beam.agentsim.agents.household.HouseholdActor.ReleaseVehicleReservation
 import beam.agentsim.agents.modalBehaviors.DrivesVehicle.{NotifyLegEndTrigger, NotifyLegStartTrigger, StartLegTrigger}
 import beam.agentsim.agents.modalBehaviors.{ChoosesMode, DrivesVehicle, ModeChoiceCalculator}
 import beam.agentsim.agents.planning.{BeamPlan, Tour}
