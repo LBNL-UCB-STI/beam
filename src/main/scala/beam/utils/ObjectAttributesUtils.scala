989ec09060648c0b1d3c4cd3f7024bc3cb97e864
wrashid <waraich.rashid@gmail.com>
2018-02-03 04:38:07 -0800

    adding methods to object attribute util I

--- src/main/scala/beam/utils/ObjectAttributesUtils.scala
+++ src/main/scala/beam/utils/ObjectAttributesUtils.scala
@@ -1,5 +1,8 @@
 package beam.utils
 
+import java.util
+import java.util.Collection
+
 import beam.agentsim.infrastructure.TAZCreatorSctript.tazinfrastructureAttributesFilePath
 import org.matsim.utils.objectattributes.{ObjectAttributes, ObjectAttributesXmlReader, ObjectAttributesXmlWriter}
 
@@ -25,5 +28,20 @@ object ObjectAttributesUtils {
     new ObjectAttributesXmlWriter(ObjectAttributes).writeFile(xmlFilePath)
   }
 
+  def merge(objectIds:util.Collection[String],objAttrA:ObjectAttributes,objAttrB:ObjectAttributes):ObjectAttributes={
+    val result=new ObjectAttributes()
+
+    for (objectId <- objectIds){
+      org.matsim.utils.objectattributes.ObjectAttributesUtils.copyAllAttributes(objAttrA,result,objectId)
+      org.matsim.utils.objectattributes.ObjectAttributesUtils.copyAllAttributes(objAttrB,result,objectId)
+    }
+
+    result
+  }
+
+  def getAllAttributeNames(attributes: ObjectAttributes, objectId: String): util.Collection[String] ={
+    org.matsim.utils.objectattributes.ObjectAttributesUtils.getAllAttributeNames(attributes, objectId)
+  }
 
 }
+
