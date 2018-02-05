e980efcd06a3e2a677ba8ea4aa022a48261422bd
Colin Sheppard <colinthegaunt@gmail.com>
2018-02-05 15:43:50 -0800

    the tests are not specific enough to work reliably, so removing for now but keeping an example in case we want to get more precise

--- src/test/scala/beam/integration/ModeChoiceSpec.scala
+++ src/test/scala/beam/integration/ModeChoiceSpec.scala
@@ -11,45 +11,19 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 class ModeChoiceSpec extends WordSpecLike with Matchers with BeamHelper with BeforeAndAfterAll with IntegrationSpecCommon {
 
-  "Running beam with modeChoiceClass ModeChoiceDriveIfAvailable" must {
-    "prefer mode choice car type than other modes" in {
-      val theRun = new StartWithCustomConfig(
-        baseConfig.withValue("beam.agentsim.agents.modalBehaviors.modeChoiceClass", ConfigValueFactory.fromAnyRef
-        ("ModeChoiceDriveIfAvailable"))
-      )
-      val testModeCount = theRun.groupedCount.getOrElse("car", 0)
-      val otherModesCount = theRun.groupedCount.getOrElse("ride_hailing", 0) +
-        theRun.groupedCount.getOrElse("walk_transit", 0) + theRun.groupedCount.getOrElse("drive_transit", 0) +
-        theRun.groupedCount.getOrElse("bike", 0)
-      testModeCount should be >= otherModesCount
-    }
-  }
+//  "Running beam with modeChoiceClass ModeChoiceDriveIfAvailable" must {
+//    "prefer mode choice car type than other modes" in {
+//      val theRun = new StartWithCustomConfig(
+//        baseConfig.withValue("beam.agentsim.agents.modalBehaviors.modeChoiceClass", ConfigValueFactory.fromAnyRef
+//        ("ModeChoiceDriveIfAvailable"))
+//      )
+//      val testModeCount = theRun.groupedCount.getOrElse("car", 0)
+//      val otherModesCount = theRun.groupedCount.getOrElse("ride_hailing", 0) +
+//        theRun.groupedCount.getOrElse("walk_transit", 0) + theRun.groupedCount.getOrElse("drive_transit", 0) +
+//        theRun.groupedCount.getOrElse("bike", 0)
+//      testModeCount should be >= otherModesCount
+//    }
+//  }
 
-  "Running beam with modeChoiceClass ModeChoiceTransitIfAvailable" must {
-    "prefer mode choice transit type than other modes" in {
-      val theRun = new StartWithCustomConfig(
-        baseConfig.withValue("beam.agentsim.agents.modalBehaviors.modeChoiceClass", ConfigValueFactory.fromAnyRef
-        ("ModeChoiceTransitIfAvailable"))
-      )
-      val testModeCount = theRun.groupedCount.getOrElse("walk_transit", 0) + theRun.groupedCount.getOrElse("drive_transit", 0)
-//      val otherModesCount = theRun.groupedCount.getOrElse("ride_hailing", 0) + theRun.groupedCount.getOrElse("bike", 0) + theRun.groupedCount.getOrElse("car", 0) + theRun.groupedCount.getOrElse("walk", 0)
-      val otherModesCount = theRun.groupedCount.getOrElse("ride_hailing", 0) + theRun.groupedCount.getOrElse("bike", 0)
-      testModeCount should be >= otherModesCount
-    }
-  }
-
-  "Running beam with modeChoiceClass ModeChoiceRideHailIfAvailable" must {
-    "prefer more mode choice ride hail type than other modes" in {
-      val theRun = new StartWithCustomConfig(
-        baseConfig.withValue("beam.agentsim.agents.modalBehaviors.modeChoiceClass", ConfigValueFactory.fromAnyRef
-        ("ModeChoiceRideHailIfAvailable"))
-      )
-      val testModeCount = theRun.groupedCount.getOrElse("ride_hailing", 0)
-      val otherModesCount = theRun.groupedCount.getOrElse("car", 0) +
-        theRun.groupedCount.getOrElse("walk_transit", 0) + theRun.groupedCount.getOrElse("drive_transit", 0) +
-        theRun.groupedCount.getOrElse("bike", 0) + theRun.groupedCount.getOrElse("walk", 0)
-      testModeCount should be >= otherModesCount
-    }
-  }
 
 }
