73fd5721d278c4e85475934554a1a2c2cc90df57
David Arias <fdariasm@gmail.com>
2018-02-12 16:11:38 -0500

    Taz-value initialization, guarding block

--- src/main/scala/beam/sim/BeamSim.scala
+++ src/main/scala/beam/sim/BeamSim.scala
@@ -59,7 +59,8 @@ class BeamSim @Inject()(private val actorSystem: ActorSystem,
     beamServices.beamRouter = actorSystem.actorOf(BeamRouter.props(beamServices, transportNetwork, scenario.getNetwork, eventsManager, scenario.getTransitVehicles, fareCalculator, tollCalculator), "router")
     Await.result(beamServices.beamRouter ? Identify(0), timeout.duration)
 
-    beamServices.taz = TAZTreeMap.fromCsv(beamServices.beamConfig.beam.agentsim.taz.file)
+    if(null != beamServices.beamConfig.beam.agentsim.taz.file && beamServices.beamConfig.beam.agentsim.taz.file.isEmpty)
+      beamServices.taz = TAZTreeMap.fromCsv(beamServices.beamConfig.beam.agentsim.taz.file)
 
     agentSimToPhysSimPlanConverter = new AgentSimToPhysSimPlanConverter(
       eventsManager,
