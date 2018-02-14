a93053f34b82cd27aa2af51e9460f49cff43f0f0
Colin Sheppard <colinthegaunt@gmail.com>
2018-02-14 12:14:24 -0800

    another tweak to deal with bad experienced plans

--- src/main/scala/beam/replanning/GrabExperiencedPlan.scala
+++ src/main/scala/beam/replanning/GrabExperiencedPlan.scala
@@ -19,8 +19,7 @@ class GrabExperiencedPlan @Inject()(config: Config) extends PlanStrategy {
 
   override def run(person: HasPlansAndId[Plan, Person]): Unit = {
     val experiencedPlan = person.getSelectedPlan.getCustomAttributes.get(PlanCalcScoreConfigGroup.EXPERIENCED_PLAN_KEY).asInstanceOf[Plan]
-    assert(experiencedPlan != null)
-    if(experiencedPlan.getPlanElements.size() > 0){
+    if(experiencedPlan != null && experiencedPlan.getPlanElements.size() > 0){
       // BeamMobsim needs activities with coords
       val plannedActivities = person.getSelectedPlan.getPlanElements.asScala.filter(e => e.isInstanceOf[Activity])
       val experiencedActivities = experiencedPlan.getPlanElements.asScala.filter(e => e.isInstanceOf[Activity])
