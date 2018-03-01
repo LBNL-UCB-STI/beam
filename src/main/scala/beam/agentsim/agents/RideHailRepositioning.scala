6a856a110d543ad8c1e2ec896ad0cf9fc3babc73
wrashid <waraich.rashid@gmail.com>
2018-03-01 13:47:43 -0800

    prepareing repositioning: force vetor to waiting passenger, from idling TNC away

---
+++ src/main/scala/beam/agentsim/agents/RideHailRepositioning.scala
@@ -0,0 +1,48 @@
+package beam.agentsim.agents
+
+
+
+
+// TODO
+// what functions does RHM need?
+
+/*
+
+class Force(startCoord,endCoord){
+  getVector();
+}
+
+def getPowerForceAt(coordinate, time):Force{
+  waitingLocations=getPreviousIteration.AtTime(time).getWaitingLocationsInRadius(coordinate, radius);
+
+  idlingLocations=getPreviousIteration.AtTime(time).getIdlingLocationsInRadius(coordinate, radius);
+
+  val forces=ArrayBuffer[Force]();
+
+  for (waitingLoc <- waitingLocations){
+    forces.add(forces);
+  }
+
+}
+
+
+def getPullingForceTowardsWaitingPassenger(coordPassenger,waitingTime, coordTNC): Force{
+  val f:force = Force(coordTNC,coordPassenger)
+  f.scale(waitingTime^2);
+  // do we need a maximum force length here?
+}
+
+
+def getRepulsiveForceAwayFromIdlingVehicle(idlingTNC, coordTNC): Force{
+  val f:force = Force(idlingTNC,coordTNC)
+  f.scale(1/f.length^2);
+  // do we need a maximum force length here?
+}
+
+
+
+*
+ */
+
+
+  // previousIteration.getWaiting()
