7774928f73f384edbbe7b51d4e41fab64e16329b
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-04-05 05:40:21 +0500

    refactor to scala logging

--- src/main/scala/beam/utils/FileUtils.scala
+++ src/main/scala/beam/utils/FileUtils.scala
@@ -6,57 +6,54 @@ import java.text.SimpleDateFormat
 import java.util.zip.GZIPInputStream
 
 import beam.sim.config.BeamConfig
+import com.typesafe.scalalogging.LazyLogging
 import org.matsim.core.config.Config
-import org.slf4j.LoggerFactory
 
 import scala.util.Try
 
 /**
   * Created by sfeygin on 1/30/17.
   */
-object FileUtils {
-
-  private val logger = LoggerFactory.getLogger(getClass.getName)
+object FileUtils extends LazyLogging {
 
   def setConfigOutputFile(beamConfig: BeamConfig, matsimConfig: Config): Unit = {
     val baseOutputDir = Paths.get(beamConfig.beam.outputs.baseOutputDirectory)
     if (!Files.exists(baseOutputDir)) baseOutputDir.toFile.mkdir()
 
-    val outputDir = if (beamConfig.beam.outputs.addTimestampToOutputDirectory) {
-      val timestamp: String = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new java.util.Date())
-      Paths.get(beamConfig.beam.outputs.baseOutputDirectory + File.separator + beamConfig.beam.agentsim.simulationName + "_" + timestamp)
-    } else {
-      Paths.get(beamConfig.beam.outputs.baseOutputDirectory + File.separator + beamConfig.beam.agentsim.simulationName)
-    }
-    outputDir.toFile.mkdir()
-    matsimConfig.controler.setOutputDirectory(outputDir.toAbsolutePath.toString)
+    val optionalSuffix: String = getOptionalOutputPathSuffix(beamConfig.beam.outputs.addTimestampToOutputDirectory)
+
+    val outputDir = Paths.get(beamConfig.beam.outputs.baseOutputDirectory + File.separator + beamConfig.beam.agentsim.simulationName + optionalSuffix).toFile
+    outputDir.mkdir()
+    logger.debug("Beam output directory is: {}", outputDir.getAbsolutePath)
+    matsimConfig.controler.setOutputDirectory(outputDir.getAbsolutePath)
   }
 
   def getConfigOutputFile(outputDirectoryBasePath: String, simulationName: String, addTimestampToOutputDirectory: Boolean): String = {
     val baseOutputDir = Paths.get(outputDirectoryBasePath)
     if (!Files.exists(baseOutputDir)) baseOutputDir.toFile.mkdir()
 
-    val outputDir = (if (addTimestampToOutputDirectory) {
-      val timestamp: String = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new java.util.Date())
-      Paths.get(outputDirectoryBasePath + File.separator + simulationName + "_" + timestamp)
-    } else {
-      Paths.get(outputDirectoryBasePath + File.separator + simulationName)
-    }).toFile
+    val optionalSuffix: String = getOptionalOutputPathSuffix(addTimestampToOutputDirectory)
+    val outputDir = Paths.get(outputDirectoryBasePath + File.separator + simulationName + "_" + optionalSuffix).toFile
+    logger.debug("Beam output directory is: {}", outputDir.getAbsolutePath)
     outputDir.mkdir()
     outputDir.getAbsolutePath
   }
 
-  def decompress(compressed: Array[Byte]): Option[String] =
-    Try {
-      val inputStream = new GZIPInputStream(new ByteArrayInputStream(compressed))
-      scala.io.Source.fromInputStream(inputStream).mkString
-    }.toOption
-
-
-  def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B =
-    try {
-      f(resource)
-    } finally {
-      resource.close()
+  def getOptionalOutputPathSuffix(addTimestampToOutputDirectory: Boolean): String = {
+    if (addTimestampToOutputDirectory) {
+      return "_" + new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new java.util.Date())
     }
+    ""
+  }
+
+  def decompress(compressed: Array[Byte]): Option[String] = Try {
+    val inputStream = new GZIPInputStream(new ByteArrayInputStream(compressed))
+    scala.io.Source.fromInputStream(inputStream).mkString
+  }.toOption
+
+  def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B = try {
+    f(resource)
+  } finally {
+    resource.close()
+  }
 }
