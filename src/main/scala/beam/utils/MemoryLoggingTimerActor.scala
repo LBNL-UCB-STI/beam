0ff9dc9cbd4da7bc6fba16f11ae2e58249f2e687
wrashid <waraich.rashid@gmail.com>
2018-04-09 13:32:20 -0700

    refactoring and adding memory log after each hour in jdeqsim

--- src/main/scala/beam/utils/MemoryLoggingTimerActor.scala
+++ src/main/scala/beam/utils/MemoryLoggingTimerActor.scala
@@ -8,8 +8,7 @@ import scala.concurrent.duration._
 class MemoryLoggingTimerActor extends Actor with ActorLogging{
   def receive = {
     case Tick ⇒
-      Runtime.getRuntime.gc()
-      log.info("Memory Used: " + math.round(10 * (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (Math.pow(1000, 3))) / 10.0 + "(GB)")
+      log.info(DebugLib.gcAndGetMemoryLogMessage("Memory use after GC: "))
   }
 
 
