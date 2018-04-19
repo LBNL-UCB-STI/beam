f575e9ab29c040a3dbd8efb6a7c25b7bdb373fe5
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-04-19 08:46:21 +0500

    pass args to test

--- src/test/scala/beam/periodic/ApplicationSfbayRunSpec.scala
+++ src/test/scala/beam/periodic/ApplicationSfbayRunSpec.scala
@@ -5,20 +5,28 @@ import java.nio.file.Paths
 import beam.sim.BeamHelper
 import beam.tags.Periodic
 import beam.utils.BeamConfigUtils
-import com.typesafe.config.ConfigValueFactory
-import org.scalatest.{Matchers, WordSpecLike}
+import com.typesafe.config.{Config, ConfigValueFactory}
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, Matchers, WordSpecLike}
 
-class ApplicationSfbayRunSpec extends WordSpecLike with Matchers with BeamHelper {
+class ApplicationSfbayRunSpec extends WordSpecLike with Matchers with BeforeAndAfterAllConfigMap with BeamHelper {
+
+  var baseConf: Config = _
+  var totalIterations: Int = _
+
+  override def beforeAll(configMap: ConfigMap) = {
+    val conf = configMap.getWithDefault("config", "production/application-sfbay/base.conf")
+    totalIterations = configMap.getWithDefault("iterations", 11)
+    baseConf = BeamConfigUtils.parseFileSubstitutingInputDirectory(conf).resolve()
+  }
 
   "SF Bay Run" must {
-    val baseConf = BeamConfigUtils.parseFileSubstitutingInputDirectory("production/application-sfbay/base.conf").resolve()
 
     "run beam 11 iterations and generate output for each " taggedAs Periodic in {
-      val NUM_ITERATIONS = 11
 
-      val config = baseConf.withValue("matsim.modules.controler.lastIteration", ConfigValueFactory.fromAnyRef(NUM_ITERATIONS))
 
-      config.getInt("matsim.modules.controler.lastIteration") should be (NUM_ITERATIONS)
+      val config = baseConf.withValue("matsim.modules.controler.lastIteration", ConfigValueFactory.fromAnyRef(totalIterations-1))
+
+      config.getInt("matsim.modules.controler.lastIteration") should be (totalIterations-1)
 
       val (_, output) = runBeamWithConfig(config)
 
@@ -29,7 +37,7 @@ class ApplicationSfbayRunSpec extends WordSpecLike with Matchers with BeamHelper
       outDir should be a 'directory
       outDir.list should not be empty
       outDir.list should contain ("ITERS")
-      itrDir.list should have length NUM_ITERATIONS+1
+      itrDir.list should have length totalIterations
       itrDir.listFiles().foreach(itr => exactly(1, itr.list) should endWith (".events.csv"))
     }
   }
