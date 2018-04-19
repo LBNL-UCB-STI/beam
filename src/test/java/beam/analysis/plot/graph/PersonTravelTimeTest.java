4b6852c0a8c880e4f0e2ec9cfcab9d753f6552e2
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-04-19 14:12:37 +0500

    test cleanup

--- src/test/java/beam/analysis/plot/graph/PersonTravelTimeTest.java
+++ src/test/java/beam/analysis/plot/graph/PersonTravelTimeTest.java
@@ -1,18 +1,9 @@
 package beam.analysis.plot.graph;
 
-import beam.analysis.PathTraversalSpatialTemporalTableGenerator;
-import beam.analysis.plots.GraphsStatsAgentSimEventsListener;
 import beam.analysis.plots.PersonTravelTimeStats;
 import org.junit.Test;
-import org.matsim.core.api.experimental.events.EventsManager;
-import org.matsim.core.events.EventsUtils;
-import org.matsim.core.events.MatsimEventsReader;
-
-import java.io.File;
-import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class PersonTravelTimeTest {
     private PersonTravelTimeStats personTravelTimeStats = new PersonTravelTimeStats();
