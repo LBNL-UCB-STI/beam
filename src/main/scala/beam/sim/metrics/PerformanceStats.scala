3c955be6acbbe8644f31d4ed33ce1776a825723d
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-01 11:51:40 +0500

    added guard for /zero exception

--- src/main/scala/beam/sim/metrics/PerformanceStats.scala
+++ src/main/scala/beam/sim/metrics/PerformanceStats.scala
@@ -1,27 +1,28 @@
 package beam.sim.metrics
 
 class PerformanceStats {
-    var totalTime: Long = 0
-    var numberOfStats:Int = 0
+  var totalTime: Long = 0
+  var numberOfStats: Int = 0
 
-    def avg = totalTime / numberOfStats
+  def avg = if (numberOfStats > 0) totalTime / numberOfStats else -1L
 
-    def addTime(time: Long): Unit = {
-      totalTime = totalTime + time
-      numberOfStats = numberOfStats + 1
-    }
-
-    def combine(stats: PerformanceStats): PerformanceStats = {
-      val combined = new PerformanceStats
-      combined.totalTime = this.totalTime + stats.totalTime
-      combined.numberOfStats = this.numberOfStats + stats.numberOfStats
-      combined
-    }
-
-    def reset: Unit = {
-      totalTime = 0
-      numberOfStats = 0
-    }
-
-    override def toString: String = s"$numberOfStats (average time: $avg [ms]; total time: $totalTime [ms])"
+  def addTime(time: Long): Unit = {
+    totalTime = totalTime + time
+    numberOfStats = numberOfStats + 1
   }
+
+  def combine(stats: PerformanceStats): PerformanceStats = {
+    val combined = new PerformanceStats
+    combined.totalTime = this.totalTime + stats.totalTime
+    combined.numberOfStats = this.numberOfStats + stats.numberOfStats
+    combined
+  }
+
+  def reset: Unit = {
+    totalTime = 0
+    numberOfStats = 0
+  }
+
+  override def toString: String = s"$numberOfStats (average time: $avg [ms]; total time: $totalTime [ms])"
+
+}
