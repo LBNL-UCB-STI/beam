19939b7ca357628afbbdbbf896521936b68a16b8
Artavazd Balaian <reasyu@gmail.com>
2018-05-01 20:42:45 +0700

    Broadcasting TransitInited message

--- src/main/scala/beam/router/RouteFrontend.scala
+++ src/main/scala/beam/router/RouteFrontend.scala
@@ -1,7 +1,8 @@
 package beam.router
 
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.routing.FromConfig
+import akka.routing.{Broadcast, FromConfig}
+import beam.router.BeamRouter.TransitInited
 import beam.router.gtfs.FareCalculator
 import beam.router.osm.TollCalculator
 import beam.router.r5.{R5RoutingWorker, R5RoutingWorker_v2}
@@ -19,11 +20,19 @@ class RouteFrontend(config: Config) extends Actor with ActorLogging{
   val workerRouter = context.actorOf(FromConfig.props(Props(classOf[R5RoutingWorker_v2], config)),
     name = "workerRouter")
 
-  log.info("RouteFrontend inited. workerRouter => {}", workerRouter)
+
+  def getNameAndHashCode: String = s"RouteFrontend[${hashCode()}], Path: `${self.path}`"
+
+
+  log.info("{} inited. workerRouter => {}", getNameAndHashCode, workerRouter)
 
   def receive = {
+    // We have to send TransitInited as Broadcast because our R5RoutingWorker is stateful!
+    case transitInited: TransitInited =>
+      log.info("{} Sending Broadcast", getNameAndHashCode)
+      workerRouter.tell(Broadcast(transitInited), sender())
     case other =>
-      log.info("RouteFrontend received {}", other)
-      workerRouter ! other
+      log.info("{} received {}", getNameAndHashCode, other)
+      workerRouter.forward(other)
   }
 }
