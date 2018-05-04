e78374d52ed746a7a8f916dc43c19e3d1eea60ad
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-04 19:54:41 +0500

    bootup host by specifying instance id

--- src/main/python/beam_lambda/lambda_function.py
+++ src/main/python/beam_lambda/lambda_function.py
@@ -78,9 +78,14 @@ instance_types = ['t2.nano', 't2.micro', 't2.small', 't2.medium', 't2.large', 't
 
 regions = ['us-east-2', 'us-west-2']
 shutdown_behaviours = ['stop', 'terminate']
+instance_operations = ['start']
 
 s3 = boto3.client('s3')
 ec2 = None
+
+def init_ec2(region):
+    global ec2
+    ec2 = boto3.client('ec2',region_name=region)
 
 def check_resource(bucket, key):
     try:
@@ -130,7 +135,10 @@ def get_dns(instance_id):
                     host = dns
     return host
 
-def lambda_handler(event, context):
+def start_instance(instance_ids):
+    return ec2.start_instances(InstanceIds=instance_ids)
+
+def deploy_handler(event):
     titled = event.get('title', 'hostname-test')
     if titled is None:
         return "Unable to start the run, runName is required. Please restart with appropriate runName."
@@ -166,8 +174,7 @@ def lambda_handler(event, context):
     if region not in regions:
         return "Unable to start run, {region} region not supported.".format(region=region)
 
-    global ec2
-    ec2 = boto3.client('ec2',region_name=region)
+    init_ec2(region)
 
     if validate(branch) and validate(commit_id):
         for arg in configs:
@@ -175,9 +182,35 @@ def lambda_handler(event, context):
             script = initscript.replace('$RUN_SCRIPT',selected_script).replace('$REGION',region).replace('$S3_REGION',os.environ['REGION']).replace('$BRANCH',branch).replace('$COMMIT', commit_id).replace('$CONFIG', arg).replace('$IS_EXPERIMENT', is_experiment).replace('$UID', uid).replace('$SHUTDOWN_WAIT', shutdown_wait).replace('$TITLED', titled).replace('$MAX_RAM', max_ram)
             instance_id = deploy(script, instance_type, region.replace("-", "_")+'_', shutdown_behaviour)
             host = get_dns(instance_id)
-            txt = txt + 'Started batch: {batch} for branch/commit {branch}/{commit} at host {dns}. '.format(branch=branch, commit=commit_id, dns=host, batch=uid)
+            txt = txt + 'Started batch: {batch} for branch/commit {branch}/{commit} at host {dns} (InstanceID: {instance_id}). '.format(branch=branch, commit=commit_id, dns=host, batch=uid, instance_id=instance_id)
             # txt = txt + 'Script is {script}.'.format(script=script)
     else:
         txt = 'Unable to start bach for branch/commit {branch}/{commit}. '.format(branch=branch, commit=commit_id)
 
     return txt
+
+def instance_handler(event):
+    region = event.get('region', os.environ['REGION'])
+    instance_ids = event.get('instance_ids')
+    command_id = event.get('command')
+
+    if region not in regions:
+        return "Unable to {command} instance(s), {region} region not supported.".format(com=command_id, region=region)
+
+    init_ec2(region)
+
+    instance_ids = instance_ids.split(',')
+
+    if command_id == 'start':
+        return start_instance(instance_ids)
+
+def lambda_handler(event, context):
+    command_id = event.get('command', 'deploy') # deploy | start
+
+    if command_id == 'deploy':
+        return deploy_handler(event)
+
+    if command_id in instance_operations:
+        return instance_handler(event)
+
+    return "Operation {command} not supported, please specify one of the supported operations (deploy | start). "
