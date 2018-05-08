e797934b295349035da55f3e5d2be33050c7ab3b
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-08 05:41:40 +0500

    extracted test output dir

--- src/test/scala/beam/utils/TestConfigUtils.scala
+++ src/test/scala/beam/utils/TestConfigUtils.scala
@@ -3,5 +3,7 @@ package beam.utils
 import com.typesafe.config.ConfigValueFactory
 
 object TestConfigUtils {
-  def testConfig(conf: String) = BeamConfigUtils.parseFileSubstitutingInputDirectory(conf).withValue("beam.outputs.baseOutputDirectory", ConfigValueFactory.fromAnyRef("output/test")).resolve()
+  val testOutputDir = "output/test/"
+
+  def testConfig(conf: String) = BeamConfigUtils.parseFileSubstitutingInputDirectory(conf).withValue("beam.outputs.baseOutputDirectory", ConfigValueFactory.fromAnyRef(testOutputDir)).resolve()
 }
