2b242ce0fad844b3bb6723d71f18739932b0ddb8
Colin Sheppard <colinthegaunt@gmail.com>
2018-05-09 17:36:10 -0700

    analysis

--- src/main/R/clipper.R
+++ src/main/R/clipper.R
@@ -1,24 +1,29 @@
 
-load("/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/2014-Clipper.Rdata")
-
 plots.dir <- '/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/'
 
-df[,on:=to.posix(as.character(TagOnTime_Time),'%H:%M:%S.0000000')]
-df[,off:=to.posix(as.character(TagOffTime_Time),'%H:%M:%S.0000000')]
+if(file.exists("/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/2014-Clipper-prepped.Rdata")){
+  load("/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/2014-Clipper-prepped.Rdata")
+}else{
+  load("/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/2014-Clipper.Rdata")
+  df[,on:=to.posix(as.character(TagOnTime_Time),'%H:%M:%S.0000000')]
+  df[,off:=to.posix(as.character(TagOffTime_Time),'%H:%M:%S.0000000')]
 
-df[,on.hr:=hour(on)]
-df[,off.hr:=hour(off)]
+  df[,on.hr:=hour(on)]
+  df[,off.hr:=hour(off)]
 
-df[,AgencyName:=str_trim(AgencyName)]
+  df[,AgencyName:=str_trim(AgencyName)]
 
-stations <- data.table(read.csv('/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/BayAreaTransitStations.csv'))
-setkey(stations,AgencyName,Station)
-stations <- unique(stations)
+  stations <- data.table(read.csv('/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/BayAreaTransitStations.csv'))
+  setkey(stations,AgencyName,Station)
+  stations <- unique(stations)
 
-df <- join.on(df, stations,c('AgencyName','TagOnLocationName'),c('AgencyName','Station'),c('Lat','Lon'))
-df[,':='(x.on=Lon,y.on=Lat,Lon=NULL,Lat=NULL)]
-df <- join.on(df, stations,c('AgencyName','TagOffLocationName'),c('AgencyName','Station'),c('Lat','Lon'))
-df[,':='(x.off=Lon,y.off=Lat,Lon=NULL,Lat=NULL)]
+  df <- join.on(df, stations,c('AgencyName','TagOnLocationName'),c('AgencyName','Station'),c('Lat','Lon'))
+  df[,':='(x.on=Lon,y.on=Lat,Lon=NULL,Lat=NULL)]
+  df <- join.on(df, stations,c('AgencyName','TagOffLocationName'),c('AgencyName','Station'),c('Lat','Lon'))
+  df[,':='(x.off=Lon,y.off=Lat,Lon=NULL,Lat=NULL)]
+  save(stations,df,file="/Users/critter/Dropbox/ucb/vto/beam-colin/sf-bay-area/clipper/2014-Clipper-prepped.Rdata")
+}
+
 
 p <- ggplot(df[,.(n=length(Year)),by=c('AgencyName','on.hr')],aes(x=on.hr,y=n,fill= AgencyName))+geom_bar(stat='identity')+facet_wrap(~AgencyName)
 pdf.scale <- .8
@@ -45,3 +50,10 @@ toplot <- join.on(df[CircadianDayOfWeek_Name=='Wednesday' & AgencyName%in%c('AC 
 p <- ggplot(toplot,aes(x=on.hr,y=n*scale,fill=AgencyName))+geom_bar(stat='identity')+facet_wrap(~AgencyName)
 pdf.scale <- .8
 ggsave(pp(plots.dir,'clipper-use-wednesday-big4-scaled.pdf'),p,width=10*pdf.scale,height=8*pdf.scale,units='in')
+
+toplot <- df[CircadianDayOfWeek_Name=='Wednesday' & AgencyName%in%c('AC Transit','SF Muni','VTA','BART'),.(n=length(Year)),by=c('AgencyName','on.hr')]
+toplot <- join.on(toplot,toplot[,.(max=max(n)),by=c('AgencyName')],c('AgencyName'),c('AgencyName'))
+
+p <- ggplot(toplot[on.hr>4],aes(x=on.hr,y=n/max,fill=AgencyName))+geom_bar(stat='identity')+facet_wrap(~AgencyName)+labs(x="Hour",y="Normalized Boarding Passengers",title='',fill="Transit Agency")
+pdf.scale <- .8
+ggsave(pp(plots.dir,'clipper-use-wednesday-big4-normalized.pdf'),p,width=10*pdf.scale,height=8*pdf.scale,units='in')
