1dabbdc18f5d4bdd7dcc674aed2c8f1d2a93aa44
Artavazd Balaian <reasyu@gmail.com>
2018-05-12 02:51:20 +0700

    kryo for serialization

--- src/main/scala/beam/sim/RunBeam.scala
+++ src/main/scala/beam/sim/RunBeam.scala
@@ -1,5 +1,14 @@
 package beam.sim
 
+import java.io.{ByteArrayOutputStream, FileOutputStream, ObjectOutputStream}
+import java.time.ZonedDateTime
+
+import akka.actor.{ActorSystem, ExtendedActorSystem}
+import beam.router.BeamRouter.RoutingResponse
+import beam.router.RoutingModel.{BeamLeg, EmbodiedBeamLeg, EmbodiedBeamTrip}
+import com.romix.akka.serialization.kryo.KryoSerializer
+import org.matsim.api.core.v01.Id
+
 object RunBeam extends BeamHelper with App {
   print(
     """
@@ -13,7 +22,29 @@ object RunBeam extends BeamHelper with App {
 
  """)
 
+//
+//  val itineraries = Vector[EmbodiedBeamTrip](
+//    EmbodiedBeamTrip(Vector[EmbodiedBeamLeg](
+//      EmbodiedBeamLeg(BeamLeg.dummyWalk(1), beamVehicleId = Id.createVehicleId("ASDASD"),
+//        asDriver = true, passengerSchedule = None, cost = BigDecimal(10), unbecomeDriverOnCompletion = true)
+//    ))
+//  )
 
+//  val res = RoutingResponse(itineraries = itineraries, requestReceivedAt = ZonedDateTime.now(),
+//    requestCreatedAt = ZonedDateTime.now(),
+//    createdAt = ZonedDateTime.now())
+//            val bos = new FileOutputStream("1.bin")
+//            val out = new ObjectOutputStream(bos)
+//            out.writeObject(res)
+//            out.flush()
+//
+//  val a = new KryoSerializer(ActorSystem().asInstanceOf[ExtendedActorSystem])
+//  val bos1 = new FileOutputStream("2.bin")
+//  bos1.write(a.toBinary(res))
+//  bos1.flush()
+//  bos1.close()
+// println(a.toBinary(res).length)
+//
   val argsMap = parseArgs(args)
 
   runBeamWithConfigFile(argsMap)
