95f1151d8ab5f69f9dafff577f71d225746263de
Carlos Caldas <ccaldas@gmail.com>
2018-05-14 09:38:13 -0800

    refactored utility function

--- src/main/java/beam/agentsim/agents/choice/logit/NestedLogit.java
+++ src/main/java/beam/agentsim/agents/choice/logit/NestedLogit.java
@@ -23,7 +23,7 @@ public class NestedLogit implements AbstractLogit{
 	public NestedLogit(NestedLogit tree) {
 		this.data = new NestedLogitData();
 		this.data.setElasticity(tree.data.getElasticity().doubleValue());
-		this.data.setNestName(tree.data.nestName);
+		this.data.setNestName(tree.data.getNestName());
 		this.data.setUtility(tree.data.getUtility());
 		this.parent = tree.parent;
 		this.children = tree.children;
@@ -106,7 +106,7 @@ public class NestedLogit implements AbstractLogit{
 		}else {
 			for (NestedLogit child : this.children) {
 				if (inputData.containsKey(child.getName())) {
-					return child.data.utility.evaluateFunction(inputData.get(child.getName()));
+					return child.data.getUtility().evaluateFunction(inputData.get(child.getName()));
 				}
 			}
 		}
@@ -195,7 +195,7 @@ public class NestedLogit implements AbstractLogit{
 		return this.data.getExpectedMaxUtility();
 	}
 	public Double getExpectedMaximumUtility(String nestName) {
-		if(this.data.nestName.equals(nestName)){
+		if(this.data.getNestName().equals(nestName)){
 			return this.data.getExpectedMaxUtility();
 		}else if(!this.isAlternative()){
 			for(NestedLogit child : this.children){
@@ -209,7 +209,7 @@ public class NestedLogit implements AbstractLogit{
 		return sumMarginalProbsOfNest(this,nestName,pdf,false);
 	}
 	private Double sumMarginalProbsOfNest(NestedLogit node, String nestName, LinkedHashMap<String,Double> pdf, Boolean startSumming) {
-		if(!startSumming && node.data.nestName.equals(nestName)){
+		if(!startSumming && node.data.getNestName().equals(nestName)){
 			return sumMarginalProbsOfNest(node,nestName,pdf,true);
 		}
 		if(node.isAlternative()){
