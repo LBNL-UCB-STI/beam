034d13b3c20bb19af9c5c868521ae5d0ef8ee54f
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-16 01:40:55 +0500

    run name added in ride hailing revenue analysis

--- src/main/scala/beam/analysis/AnalysisCollector.scala
+++ src/main/scala/beam/analysis/AnalysisCollector.scala
@@ -7,10 +7,13 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.collection.mutable.ArrayBuffer
 
 object AnalysisCollector extends MetricsSupport with LazyLogging {
-  def rideHailRevenueAnalytics(data: ArrayBuffer[_]): Unit = {
+  def rideHailRevenueAnalytics(data: ArrayBuffer[_], runName: String): Unit = {
     data.headOption match {
       case Some(value) =>
-        record("ride-hailing-revenue", ShortLevel, value.asInstanceOf[Double].toLong, Map("iteration"->(""+(data.size-1))))
+        record("ride-hailing-revenue",
+          ShortLevel,
+          value.asInstanceOf[Double].toLong,
+          Map("iteration"->(""+(data.size-1)), "run-name"-> runName))
     }
   }
 }
