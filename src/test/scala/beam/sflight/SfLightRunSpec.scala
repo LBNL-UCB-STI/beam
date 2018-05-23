563ee9e5ab6bcfe1e803079ce7caf24eb90ba5f3
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-23 09:34:19 +0500

    sf-light periodic run test

--- src/test/scala/beam/sflight/SfLightRunSpec.scala
+++ src/test/scala/beam/sflight/SfLightRunSpec.scala
@@ -1,27 +1,41 @@
 package beam.sflight
 
+import java.nio.file.Paths
+
 import beam.agentsim.events.ModeChoiceEvent
 import beam.router.r5.NetworkCoordinator
 import beam.sim.config.{BeamConfig, MatSimBeamConfigBuilder}
 import beam.sim.{BeamHelper, BeamServices}
-import beam.tags.Periodic
+import beam.tags.{ExcludeRegular, Periodic}
 import beam.utils.FileUtils
 import beam.utils.TestConfigUtils.testConfig
-import com.typesafe.config.ConfigValueFactory
+import com.typesafe.config.{Config, ConfigValueFactory}
 import org.matsim.api.core.v01.events.Event
 import org.matsim.core.controler.AbstractModule
 import org.matsim.core.events.handler.BasicEventHandler
 import org.matsim.core.scenario.{MutableScenario, ScenarioUtils}
-import org.scalatest.{Matchers, WordSpecLike}
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, Matchers, WordSpecLike}
 
 /**
   * Created by colinsheppard
   */
 
-class SfLightRunSpec extends WordSpecLike with Matchers with BeamHelper {
+class SfLightRunSpec extends WordSpecLike with Matchers with BeamHelper with BeforeAndAfterAllConfigMap {
+
+  private val ITERS_DIR = "ITERS"
+  private val LAST_ITER_CONF_PATH = "matsim.modules.controler.lastIteration"
+
+  private var baseConf: Config = _
+  private var totalIterations: Int = _
+
+  override def beforeAll(configMap: ConfigMap): Unit = {
+    val confPath = configMap.getWithDefault("config", "test/input/sf-light/sf-light-5k.conf")
+    totalIterations = configMap.getWithDefault("iterations", 1)
+    baseConf = testConfig(confPath)
+  }
 
   "SF Light" must {
-    "run without error and at least one person chooses car mode" taggedAs Periodic in {
+    "run without error and at least one person chooses car mode" in {
       val config = testConfig("test/input/sf-light/sf-light.conf")
         .withValue("beam.outputs.events.fileOutputFormats", ConfigValueFactory.fromAnyRef("xml"))
       val configBuilder = new MatSimBeamConfigBuilder(config)
@@ -54,6 +68,25 @@ class SfLightRunSpec extends WordSpecLike with Matchers with BeamHelper {
       val controler = injector.getInstance(classOf[BeamServices]).controler
       controler.run()
       assert(nCarTrips > 1)
+    }
+
+    "run 5k(default) scenario for one iteration" taggedAs (Periodic, ExcludeRegular) in {
+      val config = baseConf.withValue(LAST_ITER_CONF_PATH, ConfigValueFactory.fromAnyRef(totalIterations-1))
+
+      config.getInt(LAST_ITER_CONF_PATH) should be (totalIterations-1)
+
+      val (_, output) = runBeamWithConfig(config)
+
+
+      val outDir = Paths.get(output).toFile
+
+      val itrDir = Paths.get(output, ITERS_DIR).toFile
+
+      outDir should be a 'directory
+      outDir.list should not be empty
+      outDir.list should contain (ITERS_DIR)
+      itrDir.list should have length totalIterations
+      itrDir.listFiles().foreach(itr => exactly(1, itr.list) should endWith (".events.csv").or(endWith (".events.csv.gz")))
     }
   }
 
