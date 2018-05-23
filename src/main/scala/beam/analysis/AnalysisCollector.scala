9e7d27201eafdc80723c18d21a05409328cd2cb4
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-05-23 06:58:06 +0500

    ride hailing revenue stats fix

--- src/main/scala/beam/analysis/AnalysisCollector.scala
+++ src/main/scala/beam/analysis/AnalysisCollector.scala
@@ -8,7 +8,7 @@ import scala.collection.mutable.ArrayBuffer
 
 object AnalysisCollector extends MetricsSupport with LazyLogging {
   def rideHailRevenueAnalytics(data: ArrayBuffer[_], runName: String): Unit = {
-    data.headOption match {
+    data.lastOption match {
       case Some(value) =>
         record("ride-hailing-revenue",
           ShortLevel,
