18bfb4489bfdbcc3896ce37a75b3f998375cc121
sfwatergit <sid.feygin@gmail.com>
2018-05-30 21:12:28 -0700

    - Additional params - Allow num iters to be set on cli

--- src/main/scala/beam/calibration/RunCalibration.scala
+++ src/main/scala/beam/calibration/RunCalibration.scala
@@ -8,20 +8,22 @@ object RunCalibration extends App with BeamHelper {
 
   private val EXPERIMENTS_TAG = "experiments"
   private val BENCHMARK_EXPERIMENTS_TAG = "benchmark"
+  private val NUM_ITERATIONS_TAG = "num_iters"
 
   val argsMap = parseArgs(args)
 
   if (System.getenv("SIGOPT_DEV_ID") != null) Sigopt.clientToken = System.getenv("SIGOPT_CLIENT_ID")
   else throw new APIConnectionError("Correct developer client token must be present in environment as SIGOPT_CLIENT_ID")
 
-  private val experimentLoc = argsMap(EXPERIMENTS_TAG)
-  private val benchmarkLoc = argsMap(BENCHMARK_EXPERIMENTS_TAG)
+  private val experimentLoc: String = argsMap(EXPERIMENTS_TAG)
+  private val benchmarkLoc: String = argsMap(BENCHMARK_EXPERIMENTS_TAG)
+  private val numIters: Int = argsMap(NUM_ITERATIONS_TAG).toInt
 
   private implicit val experimentData: SigoptExperimentData = SigoptExperimentData(experimentLoc, benchmarkLoc, development = false)
-  
+
   private val experimentRunner: ExperimentRunner = ExperimentRunner()
 
-  experimentRunner.runExperiment(20)
+  experimentRunner.runExperiment(numIters)
 
   // METHODS //
 
@@ -29,6 +31,8 @@ object RunCalibration extends App with BeamHelper {
     args.sliding(2, 2).toList.collect {
       case Array("--experiments", filePath: String) if filePath.trim.nonEmpty => (EXPERIMENTS_TAG, filePath)
       case Array("--benchmark", filePath: String) if filePath.trim.nonEmpty => (BENCHMARK_EXPERIMENTS_TAG, filePath)
+      case Array("--num_iters", numIters: String) if numIters.trim.nonEmpty =>
+        (NUM_ITERATIONS_TAG, numIters)
       case arg@_ =>
         throw new IllegalArgumentException(arg.mkString(" "))
     }.toMap
