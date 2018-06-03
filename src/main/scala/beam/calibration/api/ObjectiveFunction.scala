e3f3711241b6e9a79d7772526b336f78f7e88c5d
sfwatergit <sid.feygin@gmail.com>
2018-06-03 00:51:03 -0700

    Potentially use type class to make input to objective function more flexible

--- src/main/scala/beam/calibration/api/ObjectiveFunction.scala
+++ src/main/scala/beam/calibration/api/ObjectiveFunction.scala
@@ -1,5 +1,15 @@
 package beam.calibration.api
 
+trait RunEvaluator[A] {
+  def evaluateFromRun(runData: A): Double
+}
+
+object EvaluationUtil {
+  def evaluate[A](data: A)(implicit ev: RunEvaluator[A]): Double = {
+    ev.evaluateFromRun(data)
+  }
+}
+
 trait ObjectiveFunction {
   def evaluateFromRun(outputFileLoc: String): Double
 }
