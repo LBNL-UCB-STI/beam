115b03ec084a3e24dcf6c5c9b01f5d0230d3e8e3
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-06-19 00:50:59 +0500

    updated log statement

--- src/main/scala/beam/agentsim/agents/rideHail/TNCIterationsStatsCollector.scala
+++ src/main/scala/beam/agentsim/agents/rideHail/TNCIterationsStatsCollector.scala
@@ -272,7 +272,7 @@ class TNCIterationsStatsCollector(eventsManager: EventsManager, beamServices: Be
     // Ride Hail Vehicles that never encounter any path traversal with some passenger
     // val numIdleVehicles = vehicles.count(_._2 < 1)
 
-    log.info(s"$numAlwaysIdleVehicles rideHail vehicles (out of ${vehicles.size}) were never moved and $numIdleVehiclesWithoutPassenger vehicles had no passenger during whole day.")
+    log.info(s"$numAlwaysIdleVehicles rideHail vehicles (out of ${vehicles.size}) were never moved and $numIdleVehiclesWithoutPassenger vehicles were moved without a passenger, during whole day.")
     log.info(s"Ride hail vehicles with no passengers: ${vehicles.filter(_._2 == 0).map(_._1).mkString(", ")}")
     log.info(s"Ride hail vehicles that never moved: ${vehicles.filter(_._2 == -1).map(_._1).mkString(", ")}")
 
