6fc2df2d86a37b55b09d223b92f59fe38561788f
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-07-04 10:39:15 +0500

    refactored warmStart for pathType and archives

--- src/main/scala/beam/sim/BeamWarmStart.scala
+++ src/main/scala/beam/sim/BeamWarmStart.scala
@@ -5,57 +5,91 @@ import java.nio.file.{Files, Paths}
 
 import beam.router.BeamRouter.UpdateTravelTime
 import beam.router.LinkTravelTimeContainer
+import beam.utils.FileUtils.downloadFile
+import beam.utils.UnzipUtility
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.commons.io.FileUtils.getTempDirectoryPath
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.io.FilenameUtils.getExtension
 import org.matsim.core.router.util.TravelTime
 
 class BeamWarmStart(val beamServices: BeamServices) extends LazyLogging {
   private val beamConfig = beamServices.beamConfig
-  private val parentRun = beamConfig.beam.warmStart.parentRun
-  private val warmIteration = beamConfig.beam.warmStart.iterationToUse
+  // beamConfig.beam.warmStart.pathType=PARENT_RUN, ABSOLUTE_PATH
+  private val pathType = beamConfig.beam.warmStart.pathType
+  private val srcPath = beamConfig.beam.warmStart.path
 
   def init(): Unit = {
     if (isWarmMode) {
-      if (Files.isDirectory(Paths.get(parentRun))) {
-        val warmIteration = getWarmIteration
-        if (warmIteration >= 0) {
-          val warmPath = Paths.get(beamConfig.beam.warmStart.parentRun, "ITERS", s"it.$warmIteration").toString
-          beamServices.beamRouter ! UpdateTravelTime(getWarmTravelTime(warmPath, warmIteration))
+
+      val warmPath = pathType match {
+        case "PARENT_RUN" =>
+          var runPath = srcPath
+          if (isArchive(srcPath)) {
+            var archivePath = srcPath
+            if (isS3Url(srcPath)) {
+              archivePath = Paths.get(getTempDirectoryPath, FilenameUtils.getName(srcPath)).toString
+              downloadFile(srcPath, archivePath)
+            }
+            runPath = Paths.get(getTempDirectoryPath, FilenameUtils.getBaseName(srcPath)).toString
+            UnzipUtility.unzip(archivePath, getTempDirectoryPath, true)
+          }
+
+          val warmIteration = getWarmIteration(runPath)
+          if (warmIteration >= 0) {
+            Paths.get(runPath, "ITERS", s"it.$warmIteration", s"$warmIteration.linkstats.csv.gz").toString
+          } else {
+            logger.warn(s"Warm mode initialization failed, not a valid parent run ( $srcPath )")
+            null
+          }
+        case "ABSOLUTE_PATH" =>
+          srcPath
+        case _ =>
+          logger.warn(s"Warm mode initialization failed, not a valid path type ( $pathType )")
+          null
+      }
+
+      if (warmPath != null) {
+        if (Files.exists(Paths.get(warmPath))) {
+          beamServices.beamRouter ! UpdateTravelTime(getWarmTravelTime(warmPath))
+        } else {
+          logger.warn(s"Warm mode initialization failed, stats not found at path ( $warmPath )")
         }
       }
-    } else {
-      logger.warn(s"Warm mode initialization failed, not a valid parent run ( $parentRun )")
     }
   }
 
   def isWarmMode: Boolean = beamConfig.beam.warmStart.enabled
 
-  def getWarmIteration: Int = {
+  private def isS3Url(source: String): Boolean = {
+    assert(source != null)
+    source.startsWith("https://s3.us-east-2.amazonaws.com/beam-outputs/")
+  }
 
-    def getWarmIter(itr: Int): Int = if (itr < 0 || isWarmIteration(itr)) itr else getWarmIter(itr - 1)
+  private def isArchive(source: String): Boolean = {
+    assert(source != null)
+    "zip".equalsIgnoreCase(getExtension(source))
+  }
 
-    val itrIndex = if (warmIteration < 0) {
-      val itrBaseDir = Paths.get(parentRun, "ITERS")
-      getWarmIter(new File(itrBaseDir.toUri).list().length - 1)
-    } else {
-      if (Files.isDirectory(Paths.get(parentRun, "ITERS", s"it.$warmIteration")) &&
-        isWarmIteration(warmIteration))
-        warmIteration
-      else
-        -1
-    }
+  private def getWarmTravelTime(statsFile: String): TravelTime = {
+    val binSize = beamConfig.beam.agentsim.agents.rideHail.surgePricing.timeBinSize
+
+    new LinkTravelTimeContainer(statsFile, binSize)
+  }
+
+  private def getWarmIteration(parentRun: String): Int = {
+
+    val itrBaseDir = Paths.get(parentRun, "ITERS")
+
+    def getWarmIter(itr: Int): Int = if (itr < 0 || isWarmIteration(itrBaseDir.toString, itr)) itr else getWarmIter(itr - 1)
+
+    val itrIndex = getWarmIter(new File(itrBaseDir.toUri).list().length - 1)
 
     itrIndex
   }
 
-  def isWarmIteration(itr: Int): Boolean = {
-    val linkStats = Paths.get(parentRun, "ITERS", s"it.$itr", s"$itr.linkstats.csv.gz")
+  private def isWarmIteration(itrBaseDir: String, itr: Int): Boolean = {
+    val linkStats = Paths.get(itrBaseDir, s"it.$itr", s"$itr.linkstats.csv.gz")
     Files.exists(linkStats)
-  }
-
-  def getWarmTravelTime(warmPath: String, warmIteration: Int): TravelTime = {
-    val statsFile = Paths.get(warmPath, s"$warmIteration.linkstats.csv.gz").toString
-    val binSize = beamConfig.beam.agentsim.agents.rideHail.surgePricing.timeBinSize
-
-    new LinkTravelTimeContainer(statsFile, binSize)
   }
 }
