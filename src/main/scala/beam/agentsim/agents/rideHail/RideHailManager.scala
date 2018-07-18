f16c1c07ed2760729c271bc592f96dbb203b7664
Justin Pihony <justin.pihony@gmail.com>
2018-07-18 14:43:08 -0400

    One last memory optimization

--- src/main/scala/beam/agentsim/agents/rideHail/RideHailManager.scala
+++ src/main/scala/beam/agentsim/agents/rideHail/RideHailManager.scala
@@ -590,7 +590,12 @@ class RideHailManager(
       (rideHailAgentLocation, distance)
     })
     //TODO: Possibly get multiple taxis in this block
-    val result = distances2RideHailAgents.filter(x => availableRideHailVehicles.contains(x._1.vehicleId)).sortBy(_._2).map(_._1)
+    val result = distances2RideHailAgents.filter(x => availableRideHailVehicles.contains(x._1.vehicleId))
+      .sorted((vehicleRadius1: (RideHailAgentLocation, Double),
+               vehicleRadius2: (RideHailAgentLocation, Double)) =>
+        java.lang.Double.compare(vehicleRadius1._2, vehicleRadius2._2)
+      )
+      .map(_._1)
     if (result.isEmpty) {
       val i = 0
     }
