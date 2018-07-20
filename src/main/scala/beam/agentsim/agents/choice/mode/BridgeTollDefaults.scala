ff333072972434c59dbc2af9b8f5d8ea0afe3774
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-07-21 02:15:51 +0500

    remove dead code and ide warnings

--- src/main/scala/beam/agentsim/agents/choice/mode/BridgeTollDefaults.scala
+++ src/main/scala/beam/agentsim/agents/choice/mode/BridgeTollDefaults.scala
@@ -10,45 +10,32 @@ import scala.io.Source
   * BEAM
   */
 object BridgeTollDefaults {
-//  val tollPricesBeamVille: Map[Int, Double] = Map(
-//    1 -> 1,
-//    200 -> 1
-//  )
-//
-//  // source: https://www.transit.wiki/
-//  val tollPricesSFBay: Map[Int, Double] = Map(
-//    1191692 -> 5,
-//    502 -> 5,
-//    998142 -> 5,
-//    722556 -> 5,
-//    1523426 -> 5,
-//    1053032 -> 5,
-//    1457468 -> 7,
-//    668214 -> 5
-//  )
-
   private var tollPrices: Map[Int, Double] = _
 
-  def estimateBridgeFares(alternatives: Seq[EmbodiedBeamTrip], beamServices: BeamServices): Seq[BigDecimal] = {
-//    var tollPrices: Map[Int, Double] = Map()
+  def estimateBridgeFares(
+    alternatives: Seq[EmbodiedBeamTrip],
+    beamServices: BeamServices
+  ): Seq[BigDecimal] = {
+
     val tollPriceFile = beamServices.beamConfig.beam.agentsim.toll.file
-    if(tollPrices == null) tollPrices = readTollPrices(tollPriceFile)
-//    if (beamServices.beamConfig.beam.agentsim.simulationName.equalsIgnoreCase("beamville")) {
-//      tollPrices = tollPricesBeamVille
-//    } else {
-//      tollPrices = tollPricesSFBay
-//    }
+    if (tollPrices == null) tollPrices = readTollPrices(tollPriceFile)
 
     alternatives.map { alt =>
       alt.tripClassifier match {
         case CAR =>
-          BigDecimal(alt.toBeamTrip().legs.map { beamLeg =>
-            if (beamLeg.mode.toString.equalsIgnoreCase("CAR")) {
-              beamLeg.travelPath.linkIds.filter(tollPrices.contains).map(tollPrices).sum
-            } else {
-              0
-            }
-          }.sum)
+          BigDecimal(
+            alt
+              .toBeamTrip()
+              .legs
+              .map { beamLeg =>
+                if (beamLeg.mode.toString.equalsIgnoreCase("CAR")) {
+                  beamLeg.travelPath.linkIds.filter(tollPrices.contains).map(tollPrices).sum
+                } else {
+                  0
+                }
+              }
+              .sum
+          )
         case _ =>
           BigDecimal(0)
       }
@@ -56,8 +43,12 @@ object BridgeTollDefaults {
   }
 
   private def readTollPrices(tollPricesFile: String): Map[Int, Double] = {
-    Source.fromFile(tollPricesFile).getLines().map(_.split(","))
+    Source
+      .fromFile(tollPricesFile)
+      .getLines()
+      .map(_.split(","))
       .filterNot(_(0).equalsIgnoreCase("linkId"))
-      .map(t => t(0).toInt -> t(1).toDouble).toMap
+      .map(t => t(0).toInt -> t(1).toDouble)
+      .toMap
   }
 }
--- src/main/scala/beam/agentsim/agents/choice/mode/RideHailDefaults.scala
+++ src/main/scala/beam/agentsim/agents/choice/mode/RideHailDefaults.scala
@@ -8,17 +8,23 @@ import beam.router.RoutingModel.EmbodiedBeamTrip
   *
   * If no fare is found, these defaults can be use.
   */
-object RideHailDefaults{
+object RideHailDefaults {
   val DEFAULT_COST_PER_MILE = 2.00
 
-  def estimateRideHailCost(alternatives: Seq[EmbodiedBeamTrip]) = {
-    alternatives.map{ alt =>
+  def estimateRideHailCost(alternatives: Seq[EmbodiedBeamTrip]): Seq[BigDecimal] = {
+    alternatives.map { alt =>
       alt.tripClassifier match {
         case RIDE_HAIL if alt.costEstimate == 0.0 =>
-          val cost = alt.legs.filter(_.beamLeg.mode == CAR).map(_.beamLeg.travelPath.distanceInM).sum * DEFAULT_COST_PER_MILE / 1607
+          val cost = alt.legs
+            .filter(_.beamLeg.mode == CAR)
+            .map(_.beamLeg.travelPath.distanceInM)
+            .sum * DEFAULT_COST_PER_MILE / 1607
           BigDecimal(cost)
         case RIDE_HAIL_TRANSIT if alt.costEstimate == 0.0 =>
-          val cost = alt.legs.filter(_.beamLeg.mode == CAR).map(_.beamLeg.travelPath.distanceInM).sum * DEFAULT_COST_PER_MILE / 1607
+          val cost = alt.legs
+            .filter(_.beamLeg.mode == CAR)
+            .map(_.beamLeg.travelPath.distanceInM)
+            .sum * DEFAULT_COST_PER_MILE / 1607
           BigDecimal(cost)
         case _ =>
           BigDecimal(0)
