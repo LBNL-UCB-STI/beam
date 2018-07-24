6ad52adf26a6367e44a579d310bee68c613cf9dc
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-07-25 00:46:44 +0500

    restore LITERS_PER_GALLON as const

--- src/main/scala/beam/agentsim/agents/choice/mode/DrivingCostDefaults.scala
+++ src/main/scala/beam/agentsim/agents/choice/mode/DrivingCostDefaults.scala
@@ -8,20 +8,29 @@ import beam.sim.BeamServices
   * BEAM
   */
 object DrivingCostDefaults {
+  val LITERS_PER_GALLON = 3.78541
 
-  def estimateDrivingCost(alternatives: Seq[EmbodiedBeamTrip], beamServices: BeamServices): Seq[BigDecimal] = {
+  def estimateDrivingCost(
+    alternatives: Seq[EmbodiedBeamTrip],
+    beamServices: BeamServices
+  ): Seq[BigDecimal] = {
+
     val drivingCostConfig = beamServices.beamConfig.beam.agentsim.agents.drivingCost
 
-    alternatives.map{ alt =>
+    alternatives.map { alt =>
       alt.tripClassifier match {
         case CAR if alt.costEstimate == 0.0 =>
-          val vehicle = beamServices.vehicles(alt.legs.filter(_.beamLeg.mode == CAR).head.beamVehicleId)
-          val litersPerMeter = if(vehicle == null || vehicle.getType == null || vehicle.getType.getEngineInformation == null){
-            drivingCostConfig.defaultLitersPerMeter
-          }else{
-            vehicle.getType.getEngineInformation.getGasConsumption
-          }
-          val cost = alt.legs.map(_.beamLeg.travelPath.distanceInM).sum * litersPerMeter / drivingCostConfig.defaultLitersPerGallon * drivingCostConfig.defaultPricePerGallon // 3.78 liters per gallon and 3.115 $/gal in CA: http://www.californiagasprices.com/Prices_Nationally.aspx
+          val vehicle =
+            beamServices.vehicles(alt.legs.filter(_.beamLeg.mode == CAR).head.beamVehicleId)
+          val litersPerMeter =
+            if (vehicle == null || vehicle.getType == null || vehicle.getType.getEngineInformation == null) {
+              drivingCostConfig.defaultLitersPerMeter
+            } else {
+              vehicle.getType.getEngineInformation.getGasConsumption
+            }
+          val cost = alt.legs
+            .map(_.beamLeg.travelPath.distanceInM)
+            .sum * litersPerMeter / LITERS_PER_GALLON * drivingCostConfig.defaultPricePerGallon // 3.78 liters per gallon and 3.115 $/gal in CA: http://www.californiagasprices.com/Prices_Nationally.aspx
           BigDecimal(cost)
         case _ =>
           BigDecimal(0)
