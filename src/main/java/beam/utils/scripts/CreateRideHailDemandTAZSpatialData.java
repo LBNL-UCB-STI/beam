036d848b75faf443e50e12b12eeba740f56fedba
wrashid <waraich.rashid@gmail.com>
2018-07-31 22:47:36 -0700

    script ride hail spatial data

---
+++ src/main/java/beam/utils/scripts/CreateRideHailDemandTAZSpatialData.java
@@ -0,0 +1,19 @@
+package beam.utils.scripts;
+
+public class CreateRideHailDemandTAZSpatialData {
+
+    public static void main(String[] args) {
+
+        // read TAZ
+
+        // convert coordinates
+
+        // get events
+
+        //
+        
+    }
+
+
+
+}
