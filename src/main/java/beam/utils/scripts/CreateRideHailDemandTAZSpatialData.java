ddc8d6c310a41b019268ee638f9ed77e5f076131
wrashid <waraich.rashid@gmail.com>
2018-07-31 23:15:21 -0700

    script ride hail spatial data

--- src/main/java/beam/utils/scripts/CreateRideHailDemandTAZSpatialData.java
+++ src/main/java/beam/utils/scripts/CreateRideHailDemandTAZSpatialData.java
@@ -4,11 +4,24 @@ public class CreateRideHailDemandTAZSpatialData {
 
     public static void main(String[] args) {
 
+        // input: taz-centers file
+
+        // input: events.xml file
+
+        //    time bin Size, e.g. 1800 sec.
+
         // read TAZ
 
         // convert coordinates
 
         // get events
+
+
+
+        //  output (csv or console println): TAZ center coord (x,y), rideHailDemandInTAZ, timeBin you could help to visualize that
+
+        // timbin
+        // 1           taz-x, taz-y         ridehaildemand
 
         //
         
