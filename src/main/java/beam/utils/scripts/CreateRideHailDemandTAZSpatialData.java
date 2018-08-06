a210d6992b1760bc6d2449fab8d56fc03e412174
rajnikant <rk_sharmait@yahoo.com>
2018-08-07 04:28:11 +0530

    calculate minimum distance between taz coordinates and vehicle pickup coorinates

--- src/main/java/beam/utils/scripts/CreateRideHailDemandTAZSpatialData.java
+++ src/main/java/beam/utils/scripts/CreateRideHailDemandTAZSpatialData.java
@@ -1,32 +1,104 @@
 package beam.utils.scripts;
 
-public class CreateRideHailDemandTAZSpatialData {
+import beam.agentsim.events.ReserveRideHailEvent;
+import beam.agentsim.infrastructure.TAZTreeMap;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+import beam.analysis.plots.GraphsStatsAgentSimEventsListener;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.events.Event;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.handler.BasicEventHandler;
+import org.matsim.core.utils.geometry.transformations.GeotoolsTransformation;
+
+public class CreateRideHailDemandTAZSpatialData implements BasicEventHandler {
+
+    private static final int BIN_SIZE = 1800;
+    private static Map<Integer, Map<Coord, Integer>> timeBinsCoord = new HashMap();
+    private static TAZTreeMap tree;
+    GeotoolsTransformation utm2wgs = new GeotoolsTransformation("EPSG:4326", "EPSG:26910");
+
 
     public static void main(String[] args) {
+        tree = TAZTreeMap.fromCsv("/home/rajnikant/IdeaProjects/beam/test/input/sf-light/taz-centers.csv.gz");
+        EventsManager events = EventsUtils.createEventsManager();
+        CreateRideHailDemandTAZSpatialData createRideHailDemandTAZSpatialData = new CreateRideHailDemandTAZSpatialData();
+        events.addHandler(createRideHailDemandTAZSpatialData);
 
-        // input: taz-centers file
+        MatsimEventsReader reader = new MatsimEventsReader(events);
+        reader.readFile("/home/rajnikant/IdeaProjects/beam/output/sf-light/sf-light-1k__2018-08-07_00-50-11/ITERS/it.0/0.events.xml");
 
-        // input: events.xml file
-
-        //    time bin Size, e.g. 1800 sec.
-
-        // read TAZ
-
-        // convert coordinates
-
-        // get events
-
-
-
-        //  output (csv or console println): TAZ center coord (x,y), rideHailDemandInTAZ, timeBin you could help to visualize that
-
-        // timbin
-        // 1           taz-x, taz-y         ridehaildemand
-
-        //
-        
+        printMinDistance();
     }
 
 
+    public static void printMinDistance() {
 
+        String csvFileName = "/home/rajnikant/IdeaProjects/beam/test/input/minDistance.csv";
+        try
+                (BufferedWriter out = new BufferedWriter(new FileWriter(new File(csvFileName)))) {
+            StringBuilder heading = new StringBuilder("timeBin,taz-x-coord,taz-y-coord,ridehaildemand");
+
+            out.write(heading.toString());
+            out.newLine();
+
+            timeBinsCoord.keySet().stream().forEach(timeBin -> {
+                Map<Coord, Integer> coordCount = timeBinsCoord.get(timeBin);
+                coordCount.keySet().stream().forEach(coord -> {
+                    Integer count = coordCount.get(coord);
+                    try {
+                        out.write(timeBin + "," + coord.getX() + "," + coord.getY() + "," + count);
+                        out.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+            });
+
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void updateCoordMap(int timeBin, Coord coord) {
+        Map<Coord, Integer> coordCount = timeBinsCoord.get(timeBin);
+        if (coordCount == null) {
+            coordCount = new HashMap<>();
+            coordCount.put(coord, 1);
+        } else {
+            Integer count = coordCount.get(coord);
+            if (count == null) {
+                coordCount.put(coord, 1);
+            } else {
+                coordCount.put(coord, ++count);
+            }
+        }
+        timeBinsCoord.put(timeBin, coordCount);
+
+    }
+
+    @Override
+    public void handleEvent(Event event) {
+        if (event.getEventType().equals("ReserveRideHail")) {
+            double time = Double.parseDouble(event.getAttributes().get(ReserveRideHailEvent.ATTRIBUTE_TIME));
+            double pickLocationXCoord = Double.parseDouble(event.getAttributes().get(ReserveRideHailEvent.ATTRIBUTE_PICKUP_LOCATION_X));
+            double pickLocationYCoord = Double.parseDouble(event.getAttributes().get(ReserveRideHailEvent.ATTRIBUTE_PICKUP_LOCATION_Y));
+
+            Coord transformed = utm2wgs.transform(new Coord(pickLocationXCoord, pickLocationYCoord));
+
+            TAZTreeMap.TAZ taz = tree.getTAZ(transformed.getX(), transformed.getY());
+            int timeBin = (int) (time / BIN_SIZE);
+            updateCoordMap(timeBin, taz.coord());
+        }
+    }
 }
