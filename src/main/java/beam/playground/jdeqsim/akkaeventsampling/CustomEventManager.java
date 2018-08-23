e5cda41983d5835d52b47787d283801cadc2707e
Carlos <ccaldas@gmail.com>
2018-08-23 20:01:39 +0100

    removed commented log

--- src/main/java/beam/playground/jdeqsim/akkaeventsampling/CustomEventManager.java
+++ src/main/java/beam/playground/jdeqsim/akkaeventsampling/CustomEventManager.java
@@ -4,12 +4,8 @@ import akka.actor.ActorRef;
 import beam.playground.jdeqsim.akkaeventsampling.messages.RouterMessageRequest;
 import org.matsim.api.core.v01.events.Event;
 import org.matsim.core.events.EventsManagerImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CustomEventManager extends EventsManagerImpl {
-
-    private static final Logger logger = LoggerFactory.getLogger(CustomEventManager.class);
 
     private final ActorRef eventRouter;
 
@@ -21,7 +17,6 @@ public class CustomEventManager extends EventsManagerImpl {
     public void processEvent(final Event event) {
         this.eventRouter.tell(new RouterMessageRequest(event), ActorRef.noSender());
         super.processEvent(event);
-        logger.debug(event.toString() );
     }
 
 }
