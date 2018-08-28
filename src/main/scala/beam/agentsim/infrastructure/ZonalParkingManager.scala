2db66709a6347aeed3ed3df5ca85a27b2f25fd73
fdariasm <fdariasm@gmail.com>
2018-08-28 09:36:16 -0500

    Fdariasm/#416 naming conventions 4ci (#444)
    * Copy ouput directory in parkingSpec to avoid ovewriting issue #396
    
    * Merge branch 'master' into fdariasm/#396-parkingSpec
    
    * Merge branch 'master' into fdariasm/#396-parkingSpec
    
    * Change in naming convention for #416: https://reviewable.io/reviews/lbnl-ucb-sti/beam/384#-LJjHASR0N5PxgHqGUYj:-LJjHASR0N5PxgHqGUYk:b-231ynl
    
    * Naming conventions and improvements for #416
    
    Comes from comments in PR #384, items:
    
    - https://reviewable.io/reviews/lbnl-ucb-sti/beam/384#-LJjHASR0N5PxgHqGUYj:-LJjHASR0N5PxgHqGUYk:b-231ynl
    - https://reviewable.io/reviews/lbnl-ucb-sti/beam/384#-LJjLw7a6x-Vrk1VErpC:-LJjLw7a6x-Vrk1VErpD:b-1a7r6l
    - https://reviewable.io/reviews/lbnl-ucb-sti/beam/384#-LJjC1Kc4JXpjrmPsd2l:-LJjC1Kc4JXpjrmPsd2m:b4iljft
    - https://reviewable.io/reviews/lbnl-ucb-sti/beam/384#-LJjCtg632iOuKENUB_Z:-LJjCtg75lclyuewP6fE:b-12mr6i
    
    * Leave parking scenarios run lazily in ParkingSpec #416
    
    * Merge branch 'master' into fdariasm/#416-naming_conventions
    
    * Leave parking scenarios run lazily in ParkingSpec #416

--- src/main/scala/beam/agentsim/infrastructure/ZonalParkingManager.scala
+++ src/main/scala/beam/agentsim/infrastructure/ZonalParkingManager.scala
@@ -102,8 +102,8 @@ class ZonalParkingManager(
       )
 
     case inquiry @ DepotParkingInquiry(location: Location, reservedFor: ReservedParkingType) =>
-      val mNearestTaz = findTAZsWithDistances(location, 1000.0).headOption
-      val mStalls = mNearestTaz.flatMap {
+      val nearestTaz = findTAZsWithDistances(location, 1000.0).headOption
+      val stalls = nearestTaz.flatMap {
         case (taz, _) =>
           pooledResources.find {
             case (attr, values) =>
@@ -113,12 +113,12 @@ class ZonalParkingManager(
           }
       }
 
-      val mParkingStall = mStalls.flatMap {
+      val parkingStall = stalls.flatMap {
         case (attr, values) =>
           maybeCreateNewStall(attr, location, 0.0, Some(values))
       }
 
-      mParkingStall.foreach { stall =>
+      parkingStall.foreach { stall =>
         resources.put(stall.id, stall)
         val stallValues = pooledResources(stall.attributes)
         pooledResources.update(
@@ -127,7 +127,7 @@ class ZonalParkingManager(
         )
       }
 
-      val response = DepotParkingInquiryResponse(mParkingStall)
+      val response = DepotParkingInquiryResponse(parkingStall)
       sender() ! response
 
     case inquiry @ ParkingInquiry(
