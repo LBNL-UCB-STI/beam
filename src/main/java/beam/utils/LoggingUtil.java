2894ff03be4097d87a9f6083f0156e3bdd3ded36
carloscaldas <ccaldas@gmail.com>
2018-09-01 00:34:16 +0700

    Moved the test classes to the test directory (#477)

--- src/main/java/beam/utils/LoggingUtil.java
+++ src/main/java/beam/utils/LoggingUtil.java
@@ -1,13 +1,5 @@
 package beam.utils;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.AsyncAppender;
-import org.apache.logging.log4j.core.appender.FileAppender;
-import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,35 +12,35 @@ public class LoggingUtil {
      * @param outputDirectory path of ths output directory
      */
     public static void createFileLogger(String outputDirectory) {
-        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        Configuration config = ctx.getConfiguration();
-
-        PatternLayout layout = PatternLayout.newBuilder()
-                .withConfiguration(config)
-                .withPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n")
-                .build();
-
-        Appender appender = FileAppender.newBuilder()
-                .setConfiguration(config)
-                .withName("BeamFile")
-                .withLayout(layout)
-                .withFileName(String.format("%s/beam-log.out", outputDirectory))
-                .build();
-
-        appender.start();
-        config.addAppender(appender);
-
-        AppenderRef[] refs = new AppenderRef[]{AppenderRef.createAppenderRef(appender.getName(), null, null)};
-        Appender asyncAppender = AsyncAppender.newBuilder()
-                .setConfiguration(config)
-                .setName("BeamAsync")
-                .setAppenderRefs(refs)
-                .build();
-
-        asyncAppender.start();
-
-        config.addLoggerAppender((org.apache.logging.log4j.core.Logger) LogManager.getRootLogger(), asyncAppender);
-        ctx.updateLoggers();
+//        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+//        Configuration config = ctx.getConfiguration();
+//
+//        PatternLayout layout = PatternLayout.newBuilder()
+//                .withConfiguration(config)
+//                .withPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n")
+//                .build();
+//
+//        Appender appender = FileAppender.newBuilder()
+//                .setConfiguration(config)
+//                .withName("BeamFile")
+//                .withLayout(layout)
+//                .withFileName(String.format("%s/beam-log.out", outputDirectory))
+//                .build();
+//
+//        appender.start();
+//        config.addAppender(appender);
+//
+//        AppenderRef[] refs = new AppenderRef[]{AppenderRef.createAppenderRef(appender.getName(), null, null)};
+//        Appender asyncAppender = AsyncAppender.newBuilder()
+//                .setConfiguration(config)
+//                .setName("BeamAsync")
+//                .setAppenderRefs(refs)
+//                .build();
+//
+//        asyncAppender.start();
+//
+//        config.addLoggerAppender((org.apache.logging.log4j.core.Logger) LogManager.getRootLogger(), asyncAppender);
+//        ctx.updateLoggers();
     }
 
     /**
