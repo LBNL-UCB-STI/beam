4002a54c501c01193db6c201cc8a967590f913b7
Artavazd Balaian <reasyu@gmail.com>
2018-09-03 21:12:29 +0700

    Use `getClosestIdleRideHailAgent` instead of `getClosestIdleVehiclesWithinRadius` because only head element is consumed

--- src/main/scala/beam/agentsim/agents/ridehail/allocation/ImmediateDispatchWithOverwrite.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/allocation/ImmediateDispatchWithOverwrite.scala
@@ -26,11 +26,10 @@ class ImmediateDispatchWithOverwrite(val rideHailManager: RideHailManager)
 
     // just go with closest request
     rideHailManager
-      .getClosestIdleVehiclesWithinRadius(
+      .getClosestIdleRideHailAgent(
         vehicleAllocationRequest.request.pickUpLocation,
         rideHailManager.radiusInMeters
-      )
-      .headOption match {
+      ) match {
       case Some(agentLocation) =>
         VehicleAllocation(agentLocation, None)
       case None =>
