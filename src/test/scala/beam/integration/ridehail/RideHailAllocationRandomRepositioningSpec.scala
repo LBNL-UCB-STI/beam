ea39ad82f041f2f71210d3916362f1aa50309ab0
Carlos <ccaldas@gmail.com>
2018-09-04 17:32:20 +0100

    Extracted method and linearization of variables declaration

--- src/test/scala/beam/integration/ridehail/RideHailAllocationRandomRepositioningSpec.scala
+++ src/test/scala/beam/integration/ridehail/RideHailAllocationRandomRepositioningSpec.scala
@@ -1,11 +1,9 @@
 package beam.integration.ridehail
 
 import beam.router.r5.NetworkCoordinator
-import beam.sim.config.{BeamConfig, MatSimBeamConfigBuilder}
 import beam.sim.{BeamHelper, BeamServices}
+import beam.sim.config.BeamConfig
 import beam.utils.FileUtils
-import beam.utils.TestConfigUtils.testConfig
-import com.typesafe.config.ConfigValueFactory
 import org.matsim.core.controler.AbstractModule
 import org.matsim.core.controler.listener.IterationEndsListener
 import org.matsim.core.scenario.{MutableScenario, ScenarioUtils}
@@ -17,32 +15,20 @@ import org.scalatest.mockito.MockitoSugar
 class RideHailAllocationRandomRepositioningSpec extends FlatSpec with BeamHelper with MockitoSugar {
 
   it should "be able to run for 1 iteration without exceptions" in {
-    val config = testConfig("test/input/beamville/beam.conf")
-      .withValue("beam.outputs.events.fileOutputFormats", ConfigValueFactory.fromAnyRef("xml,csv"))
-      .withValue(
-        "beam.agentsim.agents.rideHail.allocationManager.name",
-        ConfigValueFactory.fromAnyRef("RANDOM_REPOSITIONING")
-      )
-      .withValue(
-        "beam.agentsim.agents.modalBehaviors.modeChoiceClass",
-        ConfigValueFactory.fromAnyRef("ModeChoiceRideHailIfAvailable")
-      )
-      .withValue(
-        "beam.agentsim.agents.rideHail.numDriversAsFractionOfPopulation",
-        ConfigValueFactory.fromAnyRef(0.1)
-      )
-      .resolve()
-    val configBuilder = new MatSimBeamConfigBuilder(config)
-    val matsimConfig = configBuilder.buildMatSamConf()
-    matsimConfig.controler().setLastIteration(0)
-    matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+    val config = RideHailTestHelper.buildConfig
+
+    val matsimConfig = RideHailTestHelper.buildMatsimConfig(config)
+
     val beamConfig = BeamConfig(config)
+
     FileUtils.setConfigOutputFile(beamConfig, matsimConfig)
-    val scenario =
-      ScenarioUtils.loadScenario(matsimConfig).asInstanceOf[MutableScenario]
+
     val networkCoordinator = new NetworkCoordinator(beamConfig)
     networkCoordinator.loadNetwork()
+
+    val scenario = ScenarioUtils.loadScenario(matsimConfig).asInstanceOf[MutableScenario]
     scenario.setNetwork(networkCoordinator.network)
+
     val iterationCounter = mock[IterationEndsListener]
     val injector = org.matsim.core.controler.Injector.createInjector(
       scenario.getConfig,
@@ -55,6 +41,7 @@ class RideHailAllocationRandomRepositioningSpec extends FlatSpec with BeamHelper
     )
     val controler = injector.getInstance(classOf[BeamServices]).controler
     controler.run()
+
     verify(iterationCounter, times(1)).notifyIterationEnds(any())
   }
 
---
+++ src/test/scala/beam/integration/ridehail/RideHailTestHelper.scala
@@ -0,0 +1,42 @@
+package beam.integration.ridehail
+
+import beam.agentsim.agents.ridehail.allocation.RideHailResourceAllocationManager
+import beam.sim.config.MatSimBeamConfigBuilder
+import beam.utils.TestConfigUtils.testConfig
+import com.typesafe.config.{Config, ConfigValueFactory}
+import org.matsim.core.config.{Config => MatSimConfig}
+
+object RideHailTestHelper {
+
+  def buildConfig: Config = {
+    val config = testConfig("test/input/beamville/beam.conf")
+      .withValue("beam.outputs.events.fileOutputFormats", ConfigValueFactory.fromAnyRef("xml,csv"))
+      .withValue(
+        "beam.agentsim.agents.rideHail.allocationManager.name",
+        ConfigValueFactory.fromAnyRef(
+          RideHailResourceAllocationManager.IMMEDIATE_DISPATCH_WITH_OVERWRITE
+        )
+      )
+      .withValue(
+        "beam.agentsim.agents.modalBehaviors.modeChoiceClass",
+        ConfigValueFactory.fromAnyRef("ModeChoiceRideHailIfAvailable")
+      )
+      .withValue(
+        "beam.agentsim.agents.rideHail.numDriversAsFractionOfPopulation",
+        ConfigValueFactory.fromAnyRef(0.1)
+      )
+      .withValue("beam.debug.skipOverBadActors", ConfigValueFactory.fromAnyRef(true))
+      .resolve()
+
+    config
+  }
+
+  def buildMatsimConfig(config: Config): MatSimConfig = {
+    val configBuilder = new MatSimBeamConfigBuilder(config)
+
+    val matsimConfig = configBuilder.buildMatSamConf()
+    matsimConfig.controler().setLastIteration(0)
+    matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+    matsimConfig
+  }
+}
