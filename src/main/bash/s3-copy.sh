d5f31b11ce3b35f20d9d28a0e557c604f2099542
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-09-11 04:14:02 +0500

    script to publish output ot s3

---
+++ src/main/bash/s3-copy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export opth=$1
+export s3p=""
+for file in $opth/*; do sudo cp /var/log/cloud-init-output.log "$file" && sudo zip -r "${file%.*}.zip" "$file"; done;
+for file in $opth/*.zip; do s3p="$s3p https://s3.us-east-2.amazonaws.com/beam-outputs/$(basename $file)"; done;
+sudo aws --region "us-east-2" s3 cp $opth/*.zip s3://beam-outputs/
+echo "S3 URLs: $s3p"
