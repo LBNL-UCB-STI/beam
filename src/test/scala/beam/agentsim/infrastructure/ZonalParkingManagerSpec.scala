6d4ea880abffdeb5709176f0a412504846db7cfc
Justin Pihony <justin.pihony@lightbend.com>
2018-09-10 01:45:22 -0400

    lazify all non-trivial constructor vals (#529)

--- src/test/scala/beam/agentsim/infrastructure/ZonalParkingManagerSpec.scala
+++ src/test/scala/beam/agentsim/infrastructure/ZonalParkingManagerSpec.scala
@@ -40,7 +40,7 @@ class ZonalParkingManagerSpec
 
   private implicit val timeout: Timeout = Timeout(60, TimeUnit.SECONDS)
 
-  val tAZTreeMap: TAZTreeMap =
+  lazy val tAZTreeMap: TAZTreeMap =
     BeamServices.getTazTreeMap("test/test-resources/beam/agentsim/infrastructure/taz-centers.csv")
 
   def beamServices(config: BeamConfig): BeamServices = {
