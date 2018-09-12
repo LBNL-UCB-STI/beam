8be404af2309f87c9b3dccbfb87917cebd48fd21
David Arias <fdariasm@gmail.com>
2018-09-12 16:22:52 -0500

    #502 - generating CSV files for vehicles data

--- src/main/scala/beam/utils/matsim_conversion/MatsimPlanConversion.scala
+++ src/main/scala/beam/utils/matsim_conversion/MatsimPlanConversion.scala
@@ -1,8 +1,11 @@
 package beam.utils.matsim_conversion
 
-import java.io.File
+import java.io.FileWriter
+import java.util
 
-import org.apache.commons.io.FileUtils
+import beam.utils.FileUtils
+import org.supercsv.io.CsvMapWriter
+import org.supercsv.prefs.CsvPreference
 
 import scala.xml._
 import scala.xml.dtd.{DocType, SystemID}
@@ -10,7 +13,33 @@ import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 object MatsimPlanConversion {
 
-  def generateSiouxFallsXml(conversionConfig: ConversionConfig): Unit = {
+  val beamFuelTypesTitles = Seq("fuelTypeId","priceInDollarsPerMJoule")
+  val beamFuelTypes = Seq(
+    Seq("gasoline","0.03"), Seq("diesel","0.02")
+  )
+
+  val beamVehicleTypeTitles = Seq(
+    "vehicleTypeId","seatingCapacity","standingRoomCapacity","lengthInMeter","primaryFuelType",
+    "primaryFuelConsumptionInJoulePerMeter","primaryFuelCapacityInJoule","secondaryFuelType",
+    "secondaryFuelConsumptionInJoulePerMeter","secondaryFuelCapacityInJoule","automationLevel",
+    "maxVelocity","passengerCarUnit","rechargeLevel2RateLimitInWatts","rechargeLevel3RateLimitInWatts",
+    "vehicleCategory"
+  )
+  val beamVehicleTypes = Seq(
+    Seq("CAR-1","4","0","4.5","gasoline","2","4","gasoline","80","3","level","60","unit","50","40","CAR"),
+    Seq("SUV-2","6","0","5.5","gasoline","2","4","gasoline","80","3","level","50","unit","50","40","SUV"),
+    Seq("BUS-DEFAULT","50","50","12","diesel","2","4","gasoline","80","3","level","50","unit","50","40","TRANSIT-BUS"),
+    Seq("SUBWAY-DEFAULT","50","50","12","diesel","2","4","gasoline","80","3","level","50","unit","50","40","TRANSIT-SUBWAY"),
+    Seq("TRAM-DEFAULT","50","50","7.5","diesel","2","4","gasoline","80","3","level","50","unit","50","40","TRANSIT-TRAM"),
+    Seq("RAIL-DEFAULT","50","50","7.5","diesel","2","4","gasoline","80","3","level","50","unit","50","40","TRANSIT-RAIL"),
+    Seq("CABLE_CAR-DEFAULT","50","50","7.5","diesel","2","4","gasoline","80","3","level","50","unit","50","40","TRANSIT-CABLE_CAR"),
+  )
+
+  val beamVehicleTitles = Seq(
+    "vehicleId","vehicleTypeId"
+  )
+
+  def generateScenarioXml(conversionConfig: ConversionConfig): Unit = {
     val populationFile = conversionConfig.populationInput
     val populationDoc = XML.loadFile(populationFile)
 
@@ -18,15 +47,21 @@ object MatsimPlanConversion {
 
     val persons = transformedPopulationDoc \\ "person"
 
-    val xmlVehiclesOutput = if (conversionConfig.generateVehicles) {
-      generateVehiclesFromPerson(persons)
-    } else {
+    //Generate vehicles data
+    generateFuelTypesDefaults(conversionConfig)
+    val vehiclesWithTypeId = if (conversionConfig.generateVehicles){
+      generateVehicleTypesDefaults(conversionConfig, Seq())
+      generateVehiclesDataFromPersons(persons,conversionConfig)
+    }else {
       val vehiclesFile = conversionConfig.vehiclesInput.get
       val vehiclesDoc = XML.loadFile(vehiclesFile)
-      matsimVehiclesToBeam(vehiclesDoc)
+      val vehicleTypes = generateVehicleTypesFromSource(vehiclesDoc \\ "vehicleType")
+      generateVehicleTypesDefaults(conversionConfig, vehicleTypes)
+      generateVehiclesDataFromSource(vehiclesDoc, conversionConfig)
     }
+
     val houseHolds =
-      generateHouseholds(persons, xmlVehiclesOutput \\ "vehicle", conversionConfig.income)
+      generateHouseholds(persons, vehiclesWithTypeId.map(_.head), conversionConfig.income)
 
     val populationAttrs = generatePopulationAttributes(persons)
 
@@ -42,22 +77,12 @@ object MatsimPlanConversion {
 
     val populationDoctype = DocType("population", SystemID("../dtd/population_v6.dtd"), Nil)
 
-    val transitVehiclesOutput = conversionConfig.scenarioDirectory + "/transitVehicles.xml"
     val populationOutput = conversionConfig.scenarioDirectory + "/population.xml"
-    val vehiclesOutput = conversionConfig.scenarioDirectory + "/vehicles.xml"
     val householdsOutput = conversionConfig.scenarioDirectory + "/households.xml"
     val householdAttrsOutput = conversionConfig.scenarioDirectory + "/householdAttributes.xml"
     val populationAttrsOutput = conversionConfig.scenarioDirectory + "/populationAttributes.xml"
 
-//    if(conversionConfig.transitVehiclesInput.isDefined){
-//      FileUtils.copyFile(
-//        new File(conversionConfig.transitVehiclesInput.get),
-//        new File(transitVehiclesOutput))
-//    }
-
-    XML.save(transitVehiclesOutput, transitVehiclesDoc, "UTF-8", true)
     XML.save(populationOutput, transformedPopulationDoc, "UTF-8", true, populationDoctype)
-    XML.save(vehiclesOutput, xmlVehiclesOutput, "UTF-8", true)
     XML.save(householdsOutput, houseHolds, "UTF-8", true)
     XML.save(householdAttrsOutput, householdAtrrs, "UTF-8", true, householdsAttrDoctype)
     XML.save(populationAttrsOutput, populationAttrs, "UTF-8", true, populationAttrDoctype)
@@ -74,6 +99,72 @@ object MatsimPlanConversion {
     <objectattributes>
       {popAttrs}
     </objectattributes>
+  }
+
+  def generateFuelTypesDefaults(conversionConfig: ConversionConfig) = {
+    val beamFuelTypesPath = conversionConfig.scenarioDirectory + "/beamFuelTypes.csv"
+
+    writeCsvFile(beamFuelTypesPath, beamFuelTypes, beamFuelTypesTitles)
+  }
+
+  def generateVehicleTypesDefaults(conversionConfig: ConversionConfig, vehicleTypes: Seq[Seq[String]]) = {
+    val beamVehTypesPath = conversionConfig.scenarioDirectory + "/vehicleTypes.csv"
+
+    writeCsvFile(beamVehTypesPath, beamVehicleTypes ++ vehicleTypes, beamVehicleTypeTitles)
+  }
+
+  def generateVehicleTypesFromSource(vehicleTypeSeq: NodeSeq): Seq[Seq[String]] = {
+    val requiredFieldsForType = List("description", "capacity", "length", "engineInformation")
+
+    for {
+      vehicleType  <- vehicleTypeSeq
+      requiredElem <- requiredFieldsForType if (vehicleType \\ requiredElem).isEmpty
+    } yield {
+      println(s"Input vehicle data is missing $requiredElem xml element in ${vehicleType.label}")
+    }
+
+    vehicleTypeSeq.map { vt =>
+      val description = (vt \\ "description").text
+      val seatingCap = vt \ "capacity" \\ "seats" \@ "persons"
+      val standingCap = vt \ "capacity" \\ "standingRoom" \@ "persons"
+      val length = vt \\ "length" \@ "meter"
+      val fuelType = (vt \ "engineInformation" \\ "fuelType").text
+      Seq(description,seatingCap,standingCap,length,fuelType,"2","4","gasoline","80","3","level","60","unit","50","40","CAR")
+    }
+  }
+
+  def generateVehiclesDataFromPersons(persons: NodeSeq, conversionConfig: ConversionConfig): Seq[Seq[String]] = {
+    val vehicles = persons.zipWithIndex.map {
+      case (_, index) =>
+        Seq(s"${index + 1}", "CAR-1")
+    }
+    val beamVehiclesPath = conversionConfig.scenarioDirectory + "/vehicles.csv"
+    writeCsvFile(beamVehiclesPath, vehicles, beamVehicleTitles)
+    vehicles
+  }
+
+  def writeCsvFile(beamVehiclesPath: String, data: Seq[Seq[String]], titles: Seq[String]) = {
+    FileUtils.using(
+      new CsvMapWriter(new FileWriter(beamVehiclesPath), CsvPreference.STANDARD_PREFERENCE)){ writer =>
+      writer.writeHeader(titles :_*)
+      val rows = data.map{ row =>
+        row.zipWithIndex.foldRight(new util.HashMap[String, Object]()){ case ((s, i), acc) =>
+          acc.put(titles(i), s)
+          acc
+        }
+      }
+      val titlesArray = titles.toArray
+      rows.foreach(row => writer.write(row, titlesArray :_*))
+    }
+  }
+
+  def generateVehiclesDataFromSource(vehiclesDoc: Elem, conversionConfig: ConversionConfig): Seq[Seq[String]] = {
+    val vehicles = (vehiclesDoc \ "vehicle").map{ vehicle =>
+      Seq(vehicle \@ "id", vehicle \@ "type")
+    }
+    val beamVehiclesPath = conversionConfig.scenarioDirectory + "/vehicles.csv"
+    writeCsvFile(beamVehiclesPath, vehicles, beamVehicleTitles)
+    vehicles
   }
 
   def generateHouseholdAttributes(persons: NodeSeq): Elem = {
@@ -101,7 +192,7 @@ object MatsimPlanConversion {
     </objectattributes>
   }
 
-  def generateHouseholds(persons: NodeSeq, vehicles: NodeSeq, income: HouseholdIncome) = {
+  def generateHouseholds(persons: NodeSeq, vehicles: Seq[String], income: HouseholdIncome) = {
 
     val mPersons = persons.map(Option(_))
     val mVehicles = vehicles.map(Option(_))
@@ -124,8 +215,7 @@ object MatsimPlanConversion {
         <vehicles>
           {
           (for{
-            n <- mVeh
-            vehId <- n.attribute("id").map(_.toString)
+            vehId <- mVeh
           } yield {
               <vehicleDefinitionId refId={s"$vehId"} />
           }).getOrElse(NodeSeq.Empty)
@@ -175,60 +265,6 @@ object MatsimPlanConversion {
     transform(populationDoc)
   }
 
-  def generateVehiclesFromPerson(persons: NodeSeq): Elem = {
-
-    val vehicles = persons.zipWithIndex.map {
-      case (_, index) =>
-        <vehicle id={s"${index + 1}"} type="1"/>
-    }
-
-    <vehicleDefinitions xmlns="dtd"
-                        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                        xsi:schemaLocation="dtd ../dtd/vehicleDefinitions_v1.0.xsd">
-      <vehicleType id="1">
-        <description>Car</description>
-        <capacity>
-          <seats persons="4"/>
-          <standingRoom persons="0"/>
-        </capacity>
-        <length meter="4.5"/>
-        <engineInformation>
-          <fuelType>gasoline</fuelType>
-          <gasConsumption literPerMeter="0.0001069"/> <!-- == 22 MPG U.S. Avg. Light Duty https://www.rita.dot.gov/bts/sites/rita.dot.gov.bts/files/publications/national_transportation_statistics/html/table_04_23.html -->
-        </engineInformation>
-      </vehicleType>
-      {vehicles}
-    </vehicleDefinitions>
-  }
-
-  def matsimVehiclesToBeam(vehiclesDoc: Elem): Node = {
-    val requiredFieldsForType = List("description", "capacity", "length", "engineInformation")
-    val vehicleTypeSeq = vehiclesDoc \\ "vehicleType"
-
-    for {
-      vehicleType  <- vehicleTypeSeq
-      requiredElem <- requiredFieldsForType if (vehicleType \\ requiredElem).isEmpty
-    } yield {
-      println(s"Input vehicle data is missing $requiredElem xml element in ${vehicleType.label}")
-    }
-
-    val vehicleTransformRule = new RewriteRule {
-      override def transform(n: Node): Seq[Node] = n match {
-        case elem: Elem if elem.label == "vehicleType" =>
-          val child = elem.child
-          val filteredChild = child.filter {
-            case e: Elem =>
-              requiredFieldsForType.contains(e.label)
-            case _ => true
-          }
-          elem.copy(child = filteredChild)
-        case m => m
-      }
-    }
-    val transform = new RuleTransformer(vehicleTransformRule)
-    transform(vehiclesDoc)
-  }
-
   case class GenAttr(pre: Option[String], key: String, value: Seq[Node], next: MetaData) {
     def toMetaData = Attribute(pre, key, value, next)
   }
@@ -251,97 +287,5 @@ object MatsimPlanConversion {
 
   def mapMetaData(m: MetaData)(f: GenAttr => GenAttr): MetaData =
     chainMetaData(unchainMetaData(m).map(f))
-
-  /*
-   * #################################################################################################################
-   * Transit vehicles XML data
-   * */
-
-  val transitVehiclesDoc: Elem =
-    <vehicleDefinitions
-    xmlns="http://www.matsim.org/files/dtd"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.matsim.org/files/dtd http://www.matsim.org/files/dtd/vehicleDefinitions_v1.0.xsd">
-      <vehicleType id="BUS-DEFAULT">
-        <capacity>
-          <seats persons="50"/>
-          <standingRoom persons="50"/>
-        </capacity>
-        <length meter="12.0"/>
-        <engineInformation>
-          <fuelType>diesel</fuelType>
-          <gasConsumption literPerMeter="0.0007215"/> <!-- == 3.26 MPG U.S. Avg. Transit Bus Fuel Economy https://www.afdc.energy.gov/data/ -->
-        </engineInformation>
-        <accessTime secondsPerPerson="4"/>
-        <egressTime secondsPerPerson="4"/>
-      </vehicleType>
-      <vehicleType id="SUBWAY-DEFAULT">
-        <capacity>
-          <seats persons="50"/>
-          <standingRoom persons="50"/>
-        </capacity>
-        <length meter="12.0"/>
-        <engineInformation>
-          <fuelType>diesel</fuelType>
-          <gasConsumption literPerMeter="0.0034163"/> <!--
-				== 0.69 MPG which is equivalent consumption of a BART car, derived from:
-				https://www.bart.gov/sites/default/files/docs/2015%20Fact%20Sheet.pdf
-				https://www.bart.gov/sites/default/files/docs/2015%20Green%20Fact%20Sheet%20-%20electronic_0.pdf
-				The estimate is based on BART assumption of 21 MPG car that each passenger would otherwise take and claim of saving 280K gal per day)
-				(14 miles/passenger trip) * (421K trips per day) / (21 MPG * 421K trips per day - 280K gallons saved per day)
-				-->
-        </engineInformation>
-        <accessTime secondsPerPerson="1"/>
-        <egressTime secondsPerPerson="1"/>
-      </vehicleType>
-      <vehicleType id="TRAM-DEFAULT">
-        <capacity>
-          <seats persons="50"/>
-          <standingRoom persons="50"/>
-        </capacity>
-        <length meter="7.5"/>
-        <width meter="1.0"/>
-        <engineInformation>
-          <fuelType>diesel</fuelType>
-          <gasConsumption literPerMeter="0.0007215"/> <!-- == 3.26 MPG U.S. Avg. Transit Bus Fuel Economy https://www.afdc.energy.gov/data/ -->
-        </engineInformation>
-        <accessTime secondsPerPerson="1.0"/>
-        <egressTime secondsPerPerson="1.0"/>
-        <doorOperation mode="serial"/>
-        <passengerCarEquivalents pce="1.0"/>
-      </vehicleType>
-      <vehicleType id="RAIL-DEFAULT">
-        <capacity>
-          <seats persons="50"/>
-          <standingRoom persons="50"/>
-        </capacity>
-        <length meter="7.5"/>
-        <width meter="1.0"/>
-        <engineInformation>
-          <fuelType>diesel</fuelType>
-          <gasConsumption literPerMeter="0.0007215"/> <!-- == 3.26 MPG U.S. Avg. Transit Bus Fuel Economy https://www.afdc.energy.gov/data/ -->
-        </engineInformation>
-        <accessTime secondsPerPerson="1.0"/>
-        <egressTime secondsPerPerson="1.0"/>
-        <doorOperation mode="serial"/>
-        <passengerCarEquivalents pce="1.0"/>
-      </vehicleType>
-      <vehicleType id="CABLE_CAR-DEFAULT">
-        <capacity>
-          <seats persons="50"/>
-          <standingRoom persons="50"/>
-        </capacity>
-        <length meter="7.5"/>
-        <width meter="1.0"/>
-        <engineInformation>
-          <fuelType>diesel</fuelType>
-          <gasConsumption literPerMeter="0.0007215"/> <!-- == 3.26 MPG U.S. Avg. Transit Bus Fuel Economy https://www.afdc.energy.gov/data/ -->
-        </engineInformation>
-        <accessTime secondsPerPerson="1.0"/>
-        <egressTime secondsPerPerson="1.0"/>
-        <doorOperation mode="serial"/>
-        <passengerCarEquivalents pce="1.0"/>
-      </vehicleType>
-    </vehicleDefinitions>
 
 }
