7dd52a0dd680798661073a48051c411ba933e8f3
wrashid <waraich.rashid@gmail.com>
2018-09-12 14:38:11 -0700

    prepare run

--- src/main/scala/beam/calibration/utils/CreateExperiment.scala
+++ src/main/scala/beam/calibration/utils/CreateExperiment.scala
@@ -11,7 +11,8 @@ object CreateExperiment extends LazyLogging {
   Sigopt.clientToken = SigOptApiToken.getClientAPIToken
 
   def main(args: Array[String]): Unit = {
-    val experimentLoc: String = "production/application-sfbay/calibration/experiment_counts_calibration_network.yml"
+    val experimentLoc: String =
+      "production/application-sfbay/calibration/experiment_modes_calibration_intercept_network.yml"
     val benchmarkLoc: String = "production/application-sfbay/calibration/benchmark.csv"
     SigoptExperimentData(experimentLoc, benchmarkLoc, NEW_EXPERIMENT_FLAG, development = false)
   }
