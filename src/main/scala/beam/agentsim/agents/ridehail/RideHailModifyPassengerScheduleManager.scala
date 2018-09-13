97c4e30fa361e5046fb28574668a398174be4380
Carlos <ccaldas@gmail.com>
2018-09-13 20:18:30 +0100

    Extracted reused class to top-level file

--- src/main/scala/beam/agentsim/agents/ridehail/RideHailModifyPassengerScheduleManager.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/RideHailModifyPassengerScheduleManager.scala
@@ -576,13 +576,6 @@ case class RideHailModifyPassengerScheduleStatus(
   var status: InterruptMessageStatus.Value = InterruptMessageStatus.UNDEFINED
 )
 
-case class RepositionVehicleRequest(
-  passengerSchedule: PassengerSchedule,
-  tick: Double,
-  vehicleId: Id[Vehicle],
-  rideHailAgent: ActorRef
-)
-
 case object ReduceAwaitingRepositioningAckMessagesByOne
 
 object RideHailModifyPassengerScheduleManager {
