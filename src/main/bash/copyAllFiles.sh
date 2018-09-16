86024c41fd5f97bb43102ab9e951bd3003ef80ac
Muhammad Asif <muhammad.asif1.northstar@gmail.com>
2018-09-17 04:59:59 +0500

    cs/#496-deploy-ev-to-sfbay-4ci Implemented some shell scripts for collecting results.

---
+++ src/main/bash/copyAllFiles.sh
@@ -0,0 +1,5 @@
+sudo chmod 600 ~/.ssh/result_host_cert.pem
+echo "$1"
+echo "$2"
+sudo scp -i ~/.ssh/result_host_cert.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $1 ubuntu@$2:~/sigoptResults/
+sudo mv $1/* $1/../
