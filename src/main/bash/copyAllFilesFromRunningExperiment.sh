23714dcd9a8a35179ebbb1414022ab84dd23b156
Muhammad Asif <muhammad.asif1.northstar@gmail.com>
2018-09-18 02:18:19 +0500

    cs/#496-deploy-ev-to-sfbay-4ci Added a couple of files for the running experiments results copying

--- src/main/bash/copyAllFilesFromRunningExperiment.sh
+++ src/main/bash/copyAllFilesFromRunningExperiment.sh
@@ -42,6 +42,7 @@ else
         if [ $(($current-$last_modified)) -gt 180 ]; then
             echo "moving dir ${d::-1}"
             #echo "mv ${d::-1} $source_dir/"
+            echo mv "${d::-1}" "$1/to_copy/$exp_id/suggestions/${d::-1}"
             sudo mv "${d::-1}" "$1/to_copy/$exp_id/suggestions/${d::-1}"
         else
             echo "${d::-1} in progress cur_date: $current - last_modified: $last_modified";
@@ -50,13 +51,16 @@ else
 
 
     echo $(pwd)
+    echo "Exiting for test purposes.."
+    exit 0
+
     if [ -z "$(ls -A $source_exp_dir)" ]; then
        echo "Empty source directory $source_dir"
        sudo mv "$source_exp_dir" "$target_exp_dir"
        exit 0
     else
         echo "Copying the files... from $source_dir"
-        sudo scp -i ~/.ssh/result_host_cert.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $source_dir ubuntu@$2:~/sigoptResults/
+        #sudo scp -i ~/.ssh/result_host_cert.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $source_dir ubuntu@$2:~/sigoptResults/
         echo "Copying completed..."
         sudo mv "$source_exp_dir" "$target_exp_dir"
         echo "Moved the suggestions from $source_exp_dir to $target_exp_dir"
