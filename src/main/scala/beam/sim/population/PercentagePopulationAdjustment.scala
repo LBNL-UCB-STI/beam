ae6a78849f111f03f27b357830cedbcdda5121f3
Zeeshan Bilal <zeeshan.bilal@gmail.com>
2018-09-18 03:28:45 +0000

    index fix

--- src/main/scala/beam/sim/population/PercentagePopulationAdjustment.scala
+++ src/main/scala/beam/sim/population/PercentagePopulationAdjustment.scala
@@ -3,7 +3,8 @@ package beam.sim.population
 import java.util.Random
 
 import beam.sim.config.BeamConfig
-import org.matsim.api.core.v01.population.Population
+import org.matsim.api.core.v01.Id
+import org.matsim.api.core.v01.population.{Person, Population}
 
 class PercentagePopulationAdjustment(beamConfig: BeamConfig) extends PopulationAdjustment {
   override def update(population: Population): Population = {
@@ -31,11 +32,12 @@ class PercentagePopulationAdjustment(beamConfig: BeamConfig) extends PopulationA
   def assignModeUniformDistribution(population: Population, mode: String, pct: Double): Unit = {
     val rand: Random = new Random(beamConfig.matsim.modules.global.randomSeed)
     val numPop = population.getPersons.size()
-    rand.ints((numPop * pct).toLong, 1, numPop +1).forEach { num =>
-      val modes = population.getPersonAttributes.getAttribute(num.toString, "available-modes").toString
+    rand.ints((numPop * pct).toLong, 0, numPop).forEach { num =>
+      val personId = population.getPersons.keySet().toArray(new Array[Id[Person]](0))(num).toString
+      val modes = population.getPersonAttributes.getAttribute(personId, "available-modes").toString
       population.getPersonAttributes
         .putAttribute(
-          num.toString,
+          personId,
           "available-modes",
           s"$modes,$mode"
         )
