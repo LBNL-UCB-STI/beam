f81b787b29e47e6d11d2072f13b04a2f4cec1ed0
Colin Sheppard <colinthegaunt@gmail.com>
2018-09-21 11:48:40 -0700

    new parking data and experiment to run

---
+++ production/application-sfbay/experiments/ev-fleet-qos/runs/batchRunExperiment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+experiment_dir="/Users/critter/Dropbox/ucb/vto/beam-all/beam/production/application-sfbay/experiments/ev-fleet-qos/runs"
+# search through runs directory and run individual runBeam.sh
+for dir in ${experiment_dir//\\//}/*/; do
+    echo $dir
+    $dir/runBeam.sh $1 > $dir/console.log 2>&1
+    #grep ModeChoice $dir/output/ITERS/it.0/0.events.csv | grep -Eo "ModeChoice,,\w*" | sort | uniq -c
+done
