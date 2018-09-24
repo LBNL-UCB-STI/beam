ddc882bd942328c42c9d6fea741ecd294eb4c9fb
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-09-25 04:28:21 +0500

    prepare warm start #636

---
+++ src/main/bash/prep-warmstart.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export s3_src=$1
+export src_name=$(basename $s3_src)
+
+mkdir prepare-warmstart
+cd prepare-warmstart
+wget $s3_src
+unzip $src_name
+rm -rf $src_name
+
+cd `find ./ -name ITERS`
+mv `find -maxdepth 1 -name 'it.*' | sort -V  | tail -n1` it_ws
+rm -rf it.*
+mkdir it.0
+mv it_ws/*.linkstats.csv.gz it.0/0.linkstats.csv.gz
+rm -rf it_ws ../*.csv ../*.png ../*.gz ../*.txt
+cd ..
+
+t_name=$(basename `pwd`)
+
+cd ..
+zip -r "${t_name}_warmstart.zip" "$t_name"
+aws --region "us-east-2" s3 cp *.zip s3://beam-outputs/
+echo "S3 URL: https://s3.us-east-2.amazonaws.com/beam-outputs/${t_name}_warmstart.zip"
