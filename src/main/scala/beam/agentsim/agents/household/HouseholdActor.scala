9f94cb478b0a27603fccd40f3308d2712248b22f
Colin Sheppard <colinthegaunt@gmail.com>
2018-09-25 11:37:20 -0700

    debugging

--- src/main/scala/beam/agentsim/agents/household/HouseholdActor.scala
+++ src/main/scala/beam/agentsim/agents/household/HouseholdActor.scala
@@ -381,6 +381,9 @@ object HouseholdActor {
           .filter { theveh =>
             // also make sure there isn't another driver using this vehicle
             val existingDriver = beamServices.vehicles(theveh.id).driver
+            if(existingDriver.isDefined){
+              val i = 0
+            }
             (existingDriver.isEmpty || existingDriver.get.path.toString.contains(personId.toString))
           }
           .foreach { x =>
