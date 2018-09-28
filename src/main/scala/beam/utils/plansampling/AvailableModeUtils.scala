850e80600742aba8b0ff4395da2b2631924cf31b
Colin Sheppard <colinthegaunt@gmail.com>
2018-09-28 15:50:28 -0700

    make walk and drive transit always available for now

--- src/main/scala/beam/utils/plansampling/AvailableModeUtils.scala
+++ src/main/scala/beam/utils/plansampling/AvailableModeUtils.scala
@@ -4,7 +4,7 @@ import java.util
 
 import beam.agentsim.agents.household.HouseholdActor.AttributesOfIndividual
 import beam.router.Modes.BeamMode
-import beam.router.Modes.BeamMode.{RIDE_HAIL, RIDE_HAIL_TRANSIT, WALK}
+import beam.router.Modes.BeamMode.{DRIVE_TRANSIT, RIDE_HAIL, RIDE_HAIL_TRANSIT, WALK, WALK_TRANSIT}
 import org.matsim.api.core.v01.Id
 import org.matsim.api.core.v01.population.{Person, Plan}
 import org.matsim.core.population.algorithms.PermissibleModesCalculator
@@ -32,7 +32,7 @@ object AvailableModeUtils {
     val availModes = person.getCustomAttributes
       .get("beam-attributes")
       .asInstanceOf[AttributesOfIndividual]
-      .availableModes :+ WALK
+      .availableModes :+ WALK :+ WALK_TRANSIT :+ DRIVE_TRANSIT
     if(availModes.contains(RIDE_HAIL)){
       availModes :+ RIDE_HAIL_TRANSIT
     }else{
