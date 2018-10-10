b7281b18787b09520222a5de62b62db33a9289ad
Carlos <ccaldas@gmail.com>
2018-10-10 20:55:39 +0100

    Fixed issues with AnyVal types

--- src/main/scala/beam/utils/logging/LoggerWrapper.scala
+++ src/main/scala/beam/utils/logging/LoggerWrapper.scala
@@ -2,12 +2,14 @@ package beam.utils.logging
 
 trait LoggerWrapper {
 
-  def error(messageTemplate: String, args: Object*): Unit
+  def error(messageTemplate: String, args: Any*): Unit
 
-  def warn(messageTemplate: String, args: Object*): Unit
+  def warn(messageTemplate: String, args: Any*): Unit
 
-  def info(messageTemplate: String, args: Object*): Unit
+  def info(messageTemplate: String, args: Any*): Unit
 
-  def debug(messageTemplate: String, args: Object*): Unit
+  def debug(messageTemplate: String, args: Any*): Unit
+
+  def reset(): Unit
 
 }
