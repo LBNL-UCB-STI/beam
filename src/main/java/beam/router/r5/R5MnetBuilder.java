ca4ea17a79087b16733775e2c5884e385c19ffab
Carlos <ccaldas@gmail.com>
2018-10-16 18:42:52 +0100

    Refactored R5MnetBuilder

--- src/main/java/beam/router/r5/R5MnetBuilder.java
+++ src/main/java/beam/router/r5/R5MnetBuilder.java
@@ -24,26 +24,18 @@ import java.util.Set;
 /**
  * Build the pruned R5 network and MATSim network. These two networks have 1-1 link parity.
  * The R5 edgeIndex values match the MATSim link IDs w/ 1-1 parity.
- * <p>
- * Created by Andrew A. Campbell on 6/7/17.
  */
 public class R5MnetBuilder {
+
     private static final Logger log = LoggerFactory.getLogger(R5MnetBuilder.class);
 
     private final TransportNetwork r5Network;
-    private Network mNetowrk = null;  // MATSim mNetowrk
+    private final Network mNetwork;
+    private final GeotoolsTransformation transform;
+    private final String osmFile;
+    private final Map<Coord, Id<Node>> coordinateNodes = new HashMap<>();
 
-    private String fromCRS;
-    private String toCRS;
-    private GeotoolsTransformation transform;
-
-    private String osmFile;
-
-    private HashMap<Coord, Id<Node>> nodeMap = new HashMap<>();  // Maps x,y Coord to node ID
-    private int nodeId = 0;  // Current new MATSim network Node ids
-
-    private Long lastProcessedOSMId = -1L;
-    private Set<Integer> lastProcessedNodes = new HashSet<>(2);
+    private int matsimNetworkNodeId = 0;
 
     /**
      * @param r5Net     R5 network.
@@ -51,47 +43,47 @@ public class R5MnetBuilder {
      */
     public R5MnetBuilder(TransportNetwork r5Net, BeamConfig beamConfig) {
 
-        this.osmFile = beamConfig.beam().routing().r5().osmMapdbFile();
-        this.fromCRS = beamConfig.beam().routing().r5().mNetBuilder().fromCRS();
-        this.toCRS = beamConfig.beam().routing().r5().mNetBuilder().toCRS();
-        this.transform = new GeotoolsTransformation(this.fromCRS, this.toCRS);
+        final BeamConfig.Beam.Routing.R5 r5 = beamConfig.beam().routing().r5();
+
+        osmFile = r5.osmMapdbFile();
+
+        this.transform = new GeotoolsTransformation(r5.mNetBuilder().fromCRS(), r5.mNetBuilder().toCRS());
+
         log.debug("Found R5 Transport Network file, loading....");
         this.r5Network = r5Net;
-        this.mNetowrk = NetworkUtils.createNetwork();
+
+        this.mNetwork = NetworkUtils.createNetwork();
     }
 
     public void buildMNet() {
         // Load the OSM file for retrieving the number of lanes, which is not stored in the R5 network
-        OSM osm = new OSM(this.osmFile);
+        OSM osm = new OSM(osmFile);
         Map<Long, Way> ways = osm.ways;
         EdgeStore.Edge cursor = r5Network.streetLayer.edgeStore.getCursor();  // Iterator of edges in R5 network
-        OsmToMATSim OTM = new OsmToMATSim(this.mNetowrk, true);
+        OsmToMATSim OTM = new OsmToMATSim(this.mNetwork, true);
         while (cursor.advance()) {
             log.debug(String.valueOf(cursor.getEdgeIndex()));
             log.debug(cursor.toString());
             // TODO - eventually, we should pass each R5 link to OsmToMATSim and do the two-way handling there.
             // Check if we have already seen this OSM way. Skip if we have.
             Integer edgeIndex = cursor.getEdgeIndex();
+
             Long osmID = cursor.getOSMID();  // id of edge in the OSM db
+
             Way way = ways.get(osmID);
+
             Set<Integer> deezNodes = new HashSet<>(2);
             deezNodes.add(cursor.getFromVertex());
             deezNodes.add(cursor.getToVertex());
 
-            // Convert flags to strings that the MATSim network will recognize.
-            HashSet<String> flagStrings = new HashSet<>();
+            Set<String> flagStrings = new HashSet<>();
             for (EdgeStore.EdgeFlag eF : cursor.getFlags()) {
                 flagStrings.add(flagToString(eF));
             }
-            ////
-            //Add the edge and its nodes
-            ////
-//			int lanes = Integer.valueOf(way.getTag("lanes"));
-//			Integer idx = cursor.getEdgeIndex();
+
             //TODO - length needs to be transformed to output CRS. It is important that we calculate the length here
             //TODO - because R5 often breaks single OSM links into pieces.
             double length = cursor.getLengthM();  // R5 link length in meters, as calculated by R5
-//			double speed = cursor.getSpeedMs();
             // Get start and end coordinates for the edge
             Coordinate tempFromCoord = cursor.getGeometry().getCoordinate();
             Coord fromCoord = transform.transform(new Coord(tempFromCoord.x, tempFromCoord.y));  // MATSim coord
@@ -104,28 +96,31 @@ public class R5MnetBuilder {
             Node toNode = this.getOrMakeNode(toCoord);
             // Make and add the link (only if way exists)
 
-            if (way != null) {
-                Link link = OTM.createLink(way, osmID, edgeIndex, fromNode, toNode, length, flagStrings);
-                mNetowrk.addLink(link);
-                log.debug("Created regular link: " + link);
-                this.lastProcessedOSMId = osmID;
-                this.lastProcessedNodes = deezNodes;
-            } else {
+            if (way == null) {
                 // Made up numbers, this is a PT to road network connector or something
-                Link link = mNetowrk.getFactory().createLink(Id.create(edgeIndex, Link.class), fromNode, toNode);
-                link.setLength(length);
-                link.setFreespeed(10.0 / 3.6);
-                link.setCapacity(300);
-                link.setNumberOfLanes(1);
-                link.setAllowedModes(flagStrings);
-                mNetowrk.addLink(link);
+                Link link = buildLink(edgeIndex, flagStrings, length, fromNode, toNode);
+                mNetwork.addLink(link);
                 log.debug("Created special link: " + link);
+            } else {
+                Link link = OTM.createLink(way, osmID, edgeIndex, fromNode, toNode, length, (HashSet<String>)flagStrings);
+                mNetwork.addLink(link);
+                log.debug("Created regular link: " + link);
             }
         }
     }
 
+    private Link buildLink(Integer edgeIndex, Set<String> flagStrings, double length, Node fromNode, Node toNode) {
+        Link link = mNetwork.getFactory().createLink(Id.create(edgeIndex, Link.class), fromNode, toNode);
+        link.setLength(length);
+        link.setFreespeed(10.0 / 3.6);
+        link.setCapacity(300);
+        link.setNumberOfLanes(1);
+        link.setAllowedModes(flagStrings);
+        return link;
+    }
+
     public Network getNetwork() {
-        return this.mNetowrk;
+        return this.mNetwork;
     }
 
     /**
@@ -138,15 +133,14 @@ public class R5MnetBuilder {
     private Node getOrMakeNode(Coord coord) {
         Node dummyNode;
         Id<Node> id;
-        if (this.nodeMap.containsKey(coord)) {  // node already exists.
-//			log.info("NODE ALREADY EXISTS");
-            id = this.nodeMap.get(coord);
-            dummyNode = this.mNetowrk.getNodes().get(id);
-        } else { // need to make new fromID and node and increment the nodeId
-            id = Id.createNodeId(this.nodeId);
-            this.nodeId++;
-            dummyNode = NetworkUtils.createAndAddNode(mNetowrk, id, coord);
-            this.nodeMap.put(coord, id);
+        if (coordinateNodes.containsKey(coord)) {
+            id = this.coordinateNodes.get(coord);
+            dummyNode = this.mNetwork.getNodes().get(id);
+        } else { // need to make new fromID and node and increment the matsimNetworkNodeId
+            id = Id.createNodeId(this.matsimNetworkNodeId);
+            this.matsimNetworkNodeId++;
+            dummyNode = NetworkUtils.createAndAddNode(mNetwork, id, coord);
+            this.coordinateNodes.put(coord, id);
         }
         return dummyNode;
     }
@@ -171,12 +165,6 @@ public class R5MnetBuilder {
             case ALLOWS_CAR:
                 out = "car";
                 break;
-        }
-        try {
-            return out;
-        } catch (NullPointerException exec) {
-            log.error("No matching flag");
-            exec.printStackTrace();
         }
         return out;
     }
