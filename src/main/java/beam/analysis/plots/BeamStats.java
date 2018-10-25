e1bca4fef6cc45a8048dc637b4817f788b56c497
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-10-26 00:14:23 +0500

    refactor agentsim stats

--- src/main/java/beam/analysis/plots/BeamStats.java
+++ src/main/java/beam/analysis/plots/BeamStats.java
@@ -5,16 +5,10 @@ import org.matsim.core.controler.events.IterationEndsEvent;
 
 import java.io.IOException;
 
-public interface IGraphStats {
+public interface BeamStats {
     void processStats(Event event);
 
     void createGraph(IterationEndsEvent event) throws IOException;
 
-    void createGraph(IterationEndsEvent event, String graphType) throws IOException;
-
     void resetStats();
-
-    default void collectEvents(Event event){
-        System.out.println("Does not need to be overridden by everyone");
-    }
 }
