181f058e0ac9cf74bad519bc465af8c2d262d401
Michael Zilske <michael.zilske@tu-berlin.de>
2018-10-31 18:34:30 +0100

    Remove more unused code from TollCalculator

--- src/main/scala/beam/router/osm/TollCalculator.scala
+++ src/main/scala/beam/router/osm/TollCalculator.scala
@@ -3,92 +3,22 @@ package beam.router.osm
 import java.io._
 import java.nio.file.{Files, Path, Paths}
 
-import beam.router.osm.TollCalculator.{Charge, Toll}
 import beam.sim.config.BeamConfig
-import com.conveyal.osmlib.OSMEntity.Tag
-import com.conveyal.osmlib.{OSM, Way}
+import com.conveyal.osmlib.OSM
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 import scala.io.Source
 
 class TollCalculator(val config: BeamConfig, val directory: String) extends LazyLogging {
-  private val dataDirectory: Path = Paths.get(directory)
-  private val cacheFile: File = dataDirectory.resolve("tolls.dat").toFile
-  private val tollPrices = readTollPrices(config.beam.agentsim.toll.file).withDefaultValue(0.0)
 
-  val ways: mutable.Map[java.lang.Long, Toll] = if (cacheFile.exists()) {
-    new ObjectInputStream(new FileInputStream(cacheFile))
-      .readObject()
-      .asInstanceOf[mutable.Map[java.lang.Long, Toll]]
-  } else {
-    val ways = fromDirectory(dataDirectory)
-    val stream = new ObjectOutputStream(new FileOutputStream(cacheFile))
-    stream.writeObject(ways)
-    stream.close()
-    ways
-  }
+  private val tollsByLinkId: Map[Int, Double] = readTollPrices(config.beam.agentsim.toll.file).withDefaultValue(0.0)
+  private val tollsByWayId: Map[Long, Double] = readFromCacheFileOrOSM().withDefaultValue(0.0)
 
-  logger.info("Ways keys size: {}", ways.keys.size)
+  logger.info("Ways keys size: {}", tollsByWayId.keys.size)
 
-  def fromDirectory(directory: Path): mutable.Map[java.lang.Long, Toll] = {
-    var ways: mutable.Map[java.lang.Long, Toll] = mutable.Map()
-
-    /**
-      * Checks whether its a osm.pbf feed and has fares data.
-      */
-    val hasOSM: FileFilter = _.getName.endsWith(".pbf")
-
-    def loadOSM(osmSourceFile: String): Unit = {
-      val dir = new File(osmSourceFile).getParentFile
-
-      // Load OSM data into MapDB
-      val osm = new OSM(new File(dir, "osm.mapdb").getPath)
-      osm.readFromFile(osmSourceFile)
-      ways = readTolls(osm)
-      osm.close()
-    }
-
-    def readTolls(osm: OSM) = {
-      val ways = osm.ways.asScala.filter(
-        ns =>
-          ns._2.tags != null && ns._2.tags.asScala.exists(
-            t => (t.key == "toll" && t.value != "no") || t.key.startsWith("toll:")
-          ) && ns._2.tags.asScala.exists(_.key == "charge")
-      )
-      //osm.nodes.values().asScala.filter(ns => ns.tags != null && ns.tags.size() > 1 && ns.tags.asScala.exists(t => (t.key == "fee" && t.value == "yes") || t.key == "charge") && ns.tags.asScala.exists(t => t.key == "toll" || (t.key == "barrier" && t.value == "toll_booth")))
-      ways.map(w => (w._1, wayToToll(w._2)))
-    }
-
-    def wayToToll(w: Way) = {
-      Toll(tagToCharge(w.tags.asScala.find(_.key == "charge")))
-    }
-
-    def tagToCharge(tag: Option[Tag]) = {
-      Charge(tag.getOrElse(new Tag("", "")).value)
-    }
-
-    if (Files.isDirectory(directory)) {
-      directory.toFile.listFiles(hasOSM).map(_.getAbsolutePath).headOption.foreach(loadOSM)
-    }
-
-    ways
-  }
-
-  var maxOsmIdsLen: Long = Long.MinValue
-
-  def calcTollByOsmIds(osmIds: Vector[Long]): Double = {
-    if (osmIds.length > maxOsmIdsLen) {
-      maxOsmIdsLen = osmIds.length
-      logger.debug("Max OsmIDS encountered: {}", maxOsmIdsLen)
-    }
-    // TODO: Do we need faster lookup like hashset
-    // TODO OSM data has no fee information, so using $1 as min toll, need to change with valid toll price
-    ways.view.filter(w => osmIds.contains(w._1)).map(_._2.charges.map(_.amount).sum).sum
-  }
-
-  def calcTollByLinkIds(linkIds: IndexedSeq[Int]): Double = linkIds.view.map(tollPrices).sum * config.beam.agentsim.tuning.tollPrice
+  def calcTollByOsmIds(osmIds: Seq[Long]): Double = osmIds.view.map(tollsByWayId).sum
+  def calcTollByLinkIds(linkIds: Seq[Int]): Double = linkIds.view.map(tollsByLinkId).sum * config.beam.agentsim.tuning.tollPrice
 
   private def readTollPrices(tollPricesFile: String): Map[Int, Double] = {
     if (Files.exists(Paths.get(tollPricesFile))) {
@@ -99,39 +29,62 @@ class TollCalculator(val config: BeamConfig, val directory: String) extends Lazy
         .filterNot(_(0).equalsIgnoreCase("linkId"))
         .map(t => t(0).toInt -> t(1).toDouble)
         .toMap
-
     } else {
       Map()
     }
   }
 
-}
+  def readFromCacheFileOrOSM(): Map[Long, Double] = {
+    val dataDirectory: Path = Paths.get(directory)
+    val cacheFile = dataDirectory.resolve("tolls.dat").toFile
+    if (cacheFile.exists()) {
+      new ObjectInputStream(new FileInputStream(cacheFile))
+        .readObject()
+        .asInstanceOf[Map[Long, Double]]
+    } else {
+      val ways = fromDirectory()
+      val stream = new ObjectOutputStream(new FileOutputStream(cacheFile))
+      stream.writeObject(ways)
+      stream.close()
+      ways
+    }
+  }
 
-object TollCalculator {
+  def fromDirectory(): Map[Long, Double] = {
 
-  case class Toll(charges: Vector[Charge])
-  case class Charge(amount: Double)
+    def loadOSM(osmSourceFile: File): Map[Long, Double] = {
+      // Load OSM data into MapDB
+      val dir = osmSourceFile.getParentFile
+      val osm = new OSM(new File(dir, "osm.mapdb").getPath)
+      osm.readFromFile(osmSourceFile.getAbsolutePath)
 
-  object Charge {
-
-    def apply(charge: String): Vector[Charge] = {
-      charge
-        .split(";")
-        .map(c => {
-          val tokens = c.split(" ")
-          val tts = tokens.length
-          if (tts >= 2) {
-            new Charge(
-              tokens(tts - 2).toDouble
-            )
-          } else empty
-        })
-        .toVector
+      val ways = readTolls(osm)
+      osm.close()
+      ways
     }
 
-    def empty: Charge = {
-      Charge(0.0)
+    def readTolls(osm: OSM): Map[Long, Double] = {
+      osm.ways.asScala.view.flatMap {
+        case (id, way) =>
+          val charge = way.tags.asScala.find(_.key == "charge")
+            .map(chargeTag => parseCharge(chargeTag.value))
+            .getOrElse(0.0)
+          if (charge != 0.0) Some(Long2long(id) -> charge) else None
+      }.toMap
     }
+
+    Paths.get(directory).toFile.listFiles(_.getName.endsWith(".pbf")).headOption.map(loadOSM).getOrElse(Map())
+  }
+
+  private def parseCharge(charge: String): Double = {
+    charge
+      .split(";")
+      .map(c => {
+        val tokens = c.split(" ")
+        val tts = tokens.length
+        if (tts >= 2) tokens(tts - 2).toDouble else 0.0
+      })
+      .head
   }
 
 }
