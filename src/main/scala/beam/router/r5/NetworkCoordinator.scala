b4aaa193529a80b883f417ef73d33eed61f960f4
Colin Sheppard <colinthegaunt@gmail.com>
2018-11-06 08:01:23 -1000

    convert frequencies into trips for transit routes that are specified … (#888)
    * convert frequencies into trips for transit routes that are specified that way
    
    * give access to NetCoordinator for third party to modify transit frequencies before doing conversion to trips

--- src/main/scala/beam/router/r5/NetworkCoordinator.scala
+++ src/main/scala/beam/router/r5/NetworkCoordinator.scala
@@ -4,13 +4,16 @@ import java.nio.file.Files.exists
 import java.nio.file.Paths
 
 import beam.sim.config.BeamConfig
-import com.conveyal.r5.transit.TransportNetwork
+import com.conveyal.r5.transit.{TransportNetwork, TripSchedule}
 import com.typesafe.scalalogging.LazyLogging
 import org.matsim.api.core.v01.network.{Network, NetworkWriter}
 import org.matsim.core.network.NetworkUtils
 import org.matsim.core.network.io.MatsimNetworkReader
 
+import scala.collection.JavaConverters._
+
 class NetworkCoordinator(beamConfig: BeamConfig) extends LazyLogging {
+
 
   var transportNetwork: TransportNetwork = _
   var network: Network = _
@@ -47,6 +50,38 @@ class NetworkCoordinator(beamConfig: BeamConfig) extends LazyLogging {
         .write(beamConfig.matsim.modules.network.inputNetworkFile)
       logger.info(s"MATSim network written")
     }
+  }
+
+  def convertFrequenciesToTrips() = {
+    transportNetwork.transitLayer.tripPatterns.asScala.foreach { tp =>
+      if (tp.hasFrequencies) {
+        val toAdd: Vector[TripSchedule] = tp.tripSchedules.asScala.toVector.flatMap { ts =>
+          val tripStartTimes = ts.startTimes(0).until(ts.endTimes(0)).by(ts.headwaySeconds(0)).toVector
+          tripStartTimes.zipWithIndex.map { case (startTime, ind) =>
+            val tsNew = ts.clone().asInstanceOf[TripSchedule]
+            val newTripId = s"${tsNew.tripId}-$ind"
+            val newArrivals = new Array[Int](ts.arrivals.size)
+            val newDepartures = new Array[Int](ts.arrivals.size)
+            for (i <- 0.until(tsNew.arrivals.length)) {
+              newArrivals(i) = tsNew.arrivals(i) + startTime
+              newDepartures(i) = tsNew.departures(i) + startTime
+            }
+            tsNew.arrivals = newArrivals
+            tsNew.departures = newDepartures
+            tsNew.tripId = newTripId
+            tsNew.frequencyEntryIds = null
+            tsNew.headwaySeconds = null
+            tsNew.startTimes = null
+            tsNew.endTimes = null
+            tsNew
+          }
+        }
+        tp.tripSchedules.clear()
+        toAdd.foreach(tp.tripSchedules.add(_))
+        tp.hasFrequencies = false
+      }
+    }
+    transportNetwork.transitLayer.hasFrequencies = false
   }
 
 }
