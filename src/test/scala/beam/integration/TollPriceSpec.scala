60825da8c895d9bf77c86ae0ebfcd04f37921397
Colin Sheppard <colinthegaunt@gmail.com>
2018-11-06 16:10:51 -1000

    this test no longer valid now that toll used in routing, though a fix is in the works but for now ignore

--- src/test/scala/beam/integration/TollPriceSpec.scala
+++ src/test/scala/beam/integration/TollPriceSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.{Matchers, WordSpecLike}
 class TollPriceSpec extends WordSpecLike with Matchers with BeamHelper with IntegrationSpecCommon {
 
   "Running beam with modeChoice ModeChoiceMultinomialLogit and increasing tollPrice value" must {
-    "create less entries for mode choice car as value increases" in {
+    "create less entries for mode choice car as value increases" ignore {
       val inputTollPrice = Seq(-100.0, 100.0)
       val modeChoice = inputTollPrice.map { tc =>
         new StartWithCustomConfig(
