c507d4a718267f0cbd7cf50f5f772c0dac4b5b2e
rajnikant <rajnikantsh@users.noreply.github.com>
2018-11-08 01:29:45 +0530

    beam utilities (#873)
    * add beam-utility project
    
    * insert beam-utilities dependency, split personVehicleTransitionStats, remove jai_codec,jai_imageio
    
    * just added a line to start build again
    
    * move classes from beam to beam-utility

--- src/main/java/beam/analysis/plots/PersonVehicleTransitionAnalysis.java
+++ src/main/java/beam/analysis/plots/PersonVehicleTransitionAnalysis.java
@@ -1,34 +1,20 @@
 package beam.analysis.plots;
 
+import beam.analysis.plot.PlotGraph;
 import beam.sim.config.BeamConfig;
 import beam.sim.metrics.MetricsSupport;
-import com.google.common.base.CaseFormat;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.matsim.api.core.v01.events.Event;
 import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.events.PersonLeavesVehicleEvent;
 import org.matsim.core.controler.events.IterationEndsEvent;
-import org.matsim.core.utils.io.UncheckedIOException;
 import org.matsim.core.utils.misc.Time;
 
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.*;
 import java.util.List;
 
 public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSupport {
 
-    private static final List<String> vehicleType = new ArrayList<>(Arrays.asList("body", "rideHail" , "others"));
+    private static final List<String> vehicleType = new ArrayList<>(Arrays.asList("body", "rideHail", "others"));
 
     private static Map<String, TreeMap<Integer, Integer>> personEnterCount = new HashMap<>();
     private static Map<String, TreeMap<Integer, Integer>> personExitCount = new HashMap<>();
@@ -36,6 +22,7 @@ public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSu
     private static Map<String, Integer> modePerson = new HashMap<>();
     private static final String fileName = "tripHistogram";
     private static final String xAxisLabel = "time (binSize=<?> sec)";
+    private PlotGraph plotGraph = new PlotGraph();
     private int binSize;
     private int numOfBins;
 
@@ -70,12 +57,13 @@ public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSu
             if (personEnterCount.size() == 0 && personExitCount.size() == 0) {
                 continue;
             }
-            writeGraphic(event.getIteration(), mode);
+            plotGraph.writeGraphic(GraphsStatsAgentSimEventsListener.CONTROLLER_IO, event.getIteration(), mode, fileName, personEnterCount, personExitCount, onRoutes, xAxisLabel, binSize);
+
         }
     }
 
     private void processPersonVehicleTransition(Event event) {
-        int index = getBinIndex(event.getTime());
+        int index = plotGraph.getBinIndex(event.getTime(), this.binSize, this.numOfBins);
         if (PersonEntersVehicleEvent.EVENT_TYPE.equals(event.getEventType())) {
 
             String personId = event.getAttributes().get(PersonEntersVehicleEvent.ATTRIBUTE_PERSON);
@@ -92,11 +80,10 @@ public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSu
             }
 
             String unitVehicle;
-            boolean isDigit = vehicleId.replace("-","").chars().allMatch( Character::isDigit );
-            if(isDigit){
+            boolean isDigit = vehicleId.replace("-", "").chars().allMatch(Character::isDigit);
+            if (isDigit) {
                 unitVehicle = "car";
-            }
-            else {
+            } else {
                 unitVehicle = vehicleType.stream().filter(vehicleId::contains).findAny().orElse("others");
             }
 
@@ -144,11 +131,10 @@ public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSu
             }
 
             String unitVehicle;
-            boolean isDigit = vehicleId.replace("-","").chars().allMatch( Character::isDigit );
-            if(isDigit){
+            boolean isDigit = vehicleId.replace("-", "").chars().allMatch(Character::isDigit);
+            if (isDigit) {
                 unitVehicle = "car";
-            }
-            else {
+            } else {
                 unitVehicle = vehicleType.stream().filter(vehicleId::contains).findAny().orElse("others");
             }
 
@@ -182,92 +168,5 @@ public class PersonVehicleTransitionAnalysis implements GraphAnalysis, MetricsSu
 
         }
 
-    }
-
-    private JFreeChart getGraphic(String mode, int iteration) {
-
-        final XYSeriesCollection xyData = new XYSeriesCollection();
-        final XYSeries enterSeries = new XYSeries("Enter", false, true);
-        final XYSeries exitSeries = new XYSeries("Leave", false, true);
-        final XYSeries onRouteSeries = new XYSeries("en route", false, true);
-
-        Map<Integer, Integer> personEnter = personEnterCount.get(mode);
-        if (personEnter != null && personEnter.size() > 0) {
-            Set<Integer> enterKeys = personEnter.keySet();
-            for (Integer key : enterKeys) {
-                enterSeries.add(key, personEnter.get(key));
-            }
-        }
-
-
-        Map<Integer, Integer> personExit = personExitCount.get(mode);
-        if (personExit != null && personExit.size() > 0) {
-            Set<Integer> exitKeys = personExit.keySet();
-            for (Integer key : exitKeys) {
-                exitSeries.add(key, personExit.get(key));
-            }
-        }
-
-
-        Map<Integer, Integer> indexCount = onRoutes.get(mode);
-        if (indexCount != null && indexCount.size() > 0) {
-            Set<Integer> indexKeys = indexCount.keySet();
-            for (Integer key : indexKeys) {
-                onRouteSeries.add(key, indexCount.get(key));
-            }
-        }
-
-        xyData.addSeries(enterSeries);
-        xyData.addSeries(exitSeries);
-        xyData.addSeries(onRouteSeries);
-
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-                "Trip Histogram, " + mode + ", it." + iteration,
-                xAxisLabel.replace("<?>", String.valueOf(binSize)), "# persons",
-                xyData,
-                PlotOrientation.VERTICAL,
-                true,   // legend
-                false,   // tooltips
-                false   // urls
-        );
-
-
-        XYPlot plot = chart.getXYPlot();
-        plot.getRangeAxis().setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-        final CategoryAxis axis1 = new CategoryAxis("sec");
-        axis1.setTickLabelFont(new Font("SansSerif", Font.PLAIN, 7));
-        plot.setDomainAxis(new NumberAxis(xAxisLabel.replace("<?>",String.valueOf(binSize))));
-
-        plot.getRenderer().setSeriesStroke(0, new BasicStroke(2.0f));
-        plot.getRenderer().setSeriesStroke(1, new BasicStroke(2.0f));
-        plot.getRenderer().setSeriesStroke(2, new BasicStroke(2.0f));
-        plot.setBackgroundPaint(Color.white);
-        plot.setRangeGridlinePaint(Color.gray);
-        plot.setDomainGridlinePaint(Color.gray);
-
-        return chart;
-    }
-
-    private void writeGraphic(Integer iteration, String mode) {
-        try {
-
-            String filename = fileName + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, mode) + ".png";
-            String path = GraphsStatsAgentSimEventsListener.CONTROLLER_IO.getIterationFilename(iteration, filename);
-            int index = path.lastIndexOf("/");
-            File outDir = new File(path.substring(0, index) + "/tripHistogram");
-            if (!outDir.isDirectory()) Files.createDirectories(outDir.toPath());
-            String newPath = outDir.getPath() + path.substring(index);
-            ChartUtilities.saveChartAsPNG(new File(newPath), getGraphic(mode, iteration), 1024, 768);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private int getBinIndex(final double time) {
-        int bin = (int) (time / this.binSize);
-        if (bin >= this.numOfBins) {
-            return this.numOfBins;
-        }
-        return bin;
     }
 }
