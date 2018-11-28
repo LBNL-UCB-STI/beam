2729775d03f789d1ba3b390236e27a39032019d0
Muhammad Asif <muhammad.asif1.northstar@gmail.com>
2018-11-29 02:11:05 +0500

    ma/#856-beamvill-csv-setup-4ci Have updated the method for sampling

--- src/main/scala/beam/sim/vehicles/DefaultVehiclesAdjustment.scala
+++ src/main/scala/beam/sim/vehicles/DefaultVehiclesAdjustment.scala
@@ -2,6 +2,7 @@ package beam.sim.vehicles
 import beam.agentsim.agents.Population
 import beam.agentsim.agents.vehicles.BeamVehicleType
 import beam.agentsim.agents.vehicles.BeamVehicleType.VehicleCategory
+import beam.replanning.BeamExpBeta
 import beam.sim.BeamServices
 import beam.sim.config.BeamConfig.Beam
 import org.matsim.api.core.v01.{Coord, Scenario}
@@ -11,60 +12,38 @@ import scala.util.Random
 
 case class DefaultVehiclesAdjustment(beamServices: BeamServices) extends VehiclesAdjustment {
 
-  override def sampleVehicleTypesForHousehold(
-    numVehicles: Int,
-    vehicleCategory: VehicleCategory,
-    householdIncome: Double,
-    householdSize: Int,
-    householdPopulation: Population,
-    householdLocation: Coord
+  override def sampleVehicleTypesForHousehold(numVehicles: Int, vehicleCategory: VehicleCategory,
+    householdIncome: Double, householdSize: Int, householdPopulation: Population, householdLocation: Coord
   ): List[BeamVehicleType] = {
 
-    /*
-    numVehicles of
-
-
-
-
-    Let say i have vehicleCategory passed to the function is 'Car'
-    In that case I go the beamServices.vehicleTypes
-    and I find that there is only one BeamVehicleType of category 'car'
-    So in that case i return list of BeamVehicleType of size = numVehicles of same category
-
-    One method will just loop for 5 time how many times we want to call
-     */
-
+    var listToReturn: ListBuffer[BeamVehicleType] = ListBuffer[BeamVehicleType]()
     var list: ListBuffer[BeamVehicleType] = ListBuffer[BeamVehicleType]()
 
     beamServices.vehicleTypes.values
-      .filter { v => {
-          /*v.vehicleCategory match {
-            case Some(vc) => vc == vehicleCategory
-            case None     => false
-          }*/
-          v.vehicleTypeId.equalsIgnoreCase(vehicleCategory.toString)
+      .filter { v =>
+        v.vehicleCategory match {
+          case Some(vc) => vc == vehicleCategory
+          case None     => false
         }
       }
       .foreach {
-        case (v: BeamVehicleType) => {
-          list += v
-        }
+        case (v: BeamVehicleType) => list += v
       }
 
-    var listToReturn: ListBuffer[BeamVehicleType] = ListBuffer[BeamVehicleType]()
+    val bvt: BeamVehicleType =
+      if(list.size == 0) throw new Exception("There are no vehicle types for the given category")
+      else if(list.size == 1) beamServices.vehicleTypes.values.take(1).toList.head
+      else{
 
-    if(list.size == 0) {
-      beamServices.vehicleTypes.values.take(1).foreach(v =>
-        for(i <- 1 to numVehicles){
-          listToReturn += v.copy()
-        }
-      )
-    }else{
-      val random = new Random()
-      for(i <- 1 to numVehicles){
-        listToReturn += list(random.nextInt(list.size)).copy()
+        val defaultVehicleTypes = list.filter(l => l.vehicleCategory.contains("default"))
+        val matchedVehicleTypes = list.filter(l => l.vehicleCategory.get == vehicleCategory)
+
+        if(defaultVehicleTypes.size > 0) defaultVehicleTypes(0)
+        else if(matchedVehicleTypes.size > 0) matchedVehicleTypes(0)
+        else list.take(1)(0)
       }
-    }
+
+    for(i <- 1 to numVehicles) listToReturn += bvt.copy()
 
     listToReturn.toList
   }
