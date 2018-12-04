a72dcd1998e23519f60ae973273beeb534ba4a4a
Colin Sheppard <colinthegaunt@gmail.com>
2018-12-03 16:38:05 -0800

    make fail fast actually fail and add pooling

--- src/main/scala/beam/utils/scripts/FailFast.scala
+++ src/main/scala/beam/utils/scripts/FailFast.scala
@@ -7,12 +7,29 @@ object FailFast extends LazyLogging {
 
   def run(beamServices: BeamServices): Unit = {
 
-    if ((beamServices.beamConfig.matsim.modules.strategy.planSelectorForRemoval.equals("tryToKeepOneOfEachClass"))
-        && (!beamServices.beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass.equals("ModeChoiceLCCM"))) {
-      logger.error(
-        "Simulation breaks due to beamServices.beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass!=ModeChoiceLCCM and beamServices.beamConfig.matsim.modules.strategy.planSelectorForRemoval = " + beamServices.beamConfig.matsim.modules.strategy.planSelectorForRemoval
+    /*
+     * ModeChoiceLCCM
+     * -- replanning delete strategy, tryToKeepOneOfEachClass, only relevant to LCCM
+     * -- vice versa of above, LCCM requires tryToKeepOneOfEachClass
+     */
+    if (beamServices.beamConfig.matsim.modules.strategy.planSelectorForRemoval.equals("tryToKeepOneOfEachClass") &&
+        !beamServices.beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass.equals("ModeChoiceLCCM")) {
+      throw new RuntimeException(
+        "The replanning deletion strategy 'tryToKeepOneOfEachClass' must only be used along with the 'ModeChoiceLCCM' mode choice class. In other words, if the parameter beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass!=ModeChoiceLCCM then beamConfig.matsim.modules.strategy.planSelectorForRemoval != 'tryToKeepOneOfEachClass'"
+      )
+    }
+    if (!beamServices.beamConfig.matsim.modules.strategy.planSelectorForRemoval.equals("tryToKeepOneOfEachClass") &&
+        beamServices.beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass.equals("ModeChoiceLCCM")) {
+      throw new RuntimeException(
+        "The replanning deletion strategy 'tryToKeepOneOfEachClass' must be used along with the 'ModeChoiceLCCM' mode choice class. In other words, if the parameter beamConfig.beam.agentsim.agents.modalBehaviors.modeChoiceClass==ModeChoiceLCCM then beamConfig.matsim.modules.strategy.planSelectorForRemoval == 'tryToKeepOneOfEachClass'"
       )
     }
 
+    /*
+     * Pooling not ready yet
+     */
+    if (beamServices.beamConfig.beam.agentsim.agents.rideHail.allocationManager.name.equals("POOLING")){
+      throw new RuntimeException("Pooling, while a class in the code base, is not ready for use yet. In other words, please do not set beamConfig.beam.agentsim.agents.rideHail.allocationManager.name == \"POOLING\"")
+    }
   }
 }
