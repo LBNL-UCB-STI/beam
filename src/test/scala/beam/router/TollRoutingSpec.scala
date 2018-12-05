540f9903dbb18563db59989b83c1ef3d0142318f
Michael Zilske <michael.zilske@tu-berlin.de>
2018-12-05 19:18:27 +0100

    Fix TollRoutingSpec

--- src/test/scala/beam/router/TollRoutingSpec.scala
+++ src/test/scala/beam/router/TollRoutingSpec.scala
@@ -9,6 +9,7 @@ import beam.agentsim.agents.choice.mode.PtFares.FareRule
 import beam.agentsim.agents.vehicles.VehicleProtocol.StreetVehicle
 import beam.agentsim.events.SpaceTime
 import beam.router.BeamRouter._
+import beam.router.Modes.BeamMode
 import beam.router.Modes.BeamMode.{CAR, WALK}
 import beam.router.gtfs.FareCalculator
 import beam.router.gtfs.FareCalculator.BeamFareSegment
@@ -18,6 +19,7 @@ import beam.router.r5.DefaultNetworkCoordinator
 import beam.sim.BeamServices
 import beam.sim.common.GeoUtilsImpl
 import beam.sim.config.BeamConfig
+import beam.sim.population.{AttributesOfIndividual, HouseholdAttributes}
 import beam.utils.DateUtils
 import beam.utils.TestConfigUtils.testConfig
 import com.typesafe.config.ConfigValueFactory
@@ -66,7 +68,7 @@ class TollRoutingSpec
         ZonedDateTime.parse(beamConfig.beam.routing.baseDate)
       )
     )
-    networkCoordinator = DefaultNetworkCoordinator(beamConfig)
+    networkCoordinator = new DefaultNetworkCoordinator(beamConfig)
     networkCoordinator.loadNetwork()
     networkCoordinator.convertFrequenciesToTrips()
 
@@ -93,7 +95,6 @@ class TollRoutingSpec
     val destination = new Location(0.02005, 0.01995)
 
     "report a toll on a route where the fastest route has tolls" in {
-      val timeValueOfMoney = 0.0 // I don't mind tolls
       val request = RoutingRequest(
         origin,
         destination,
@@ -106,13 +107,22 @@ class TollRoutingSpec
             Modes.BeamMode.CAR,
             asDriver = true
           )
-        )
+        ),
+        attributesOfIndividual = Some(AttributesOfIndividual(
+          HouseholdAttributes.EMPTY,
+          None,
+          true,
+          Vector(BeamMode.CAR),
+          valueOfTime = 10000000.0, // I don't mind tolls at all
+          None,
+          None
+        ))
       )
       router ! request
       val response = expectMsgType[RoutingResponse]
       val carOption = response.itineraries.find(_.tripClassifier == CAR).get
-      assert(carOption.costEstimate == 2.0, "contains three toll links: two specified in OSM, and one in CSV file")
-      assert(carOption.totalTravelTimeInSecs == 288)
+      assert(carOption.costEstimate == 3.0, "contains three toll links: two specified in OSM, and one in CSV file")
+      assert(carOption.totalTravelTimeInSecs == 144)
 
       val earlierRequest = request.copy(departureTime = 2000)
       router ! earlierRequest
@@ -141,11 +151,8 @@ class TollRoutingSpec
       val moreExpensiveResponse = expectMsgType[RoutingResponse]
       val moreExpensiveCarOption = moreExpensiveResponse.itineraries.find(_.tripClassifier == CAR).get
       // the factor in the config only applies to link tolls at the moment, i.e. one of the three paid is 2.0
-      assert(moreExpensiveCarOption.costEstimate == 2.0)
+      assert(moreExpensiveCarOption.costEstimate == 4.0)
 
-      // If 1$ is worth more than 144 seconds to me, I should be sent on the alternative route
-      // (which takes 288 seconds)
-      val higherTimeValueOfMoney = 145.0
       val tollSensitiveRequest = RoutingRequest(
         origin,
         destination,
@@ -158,7 +165,18 @@ class TollRoutingSpec
             Modes.BeamMode.CAR,
             asDriver = true
           )
-        )
+        ),
+        attributesOfIndividual = Some(AttributesOfIndividual(
+          HouseholdAttributes.EMPTY,
+          None,
+          true,
+          Vector(BeamMode.CAR),
+          // If 1$ is worth more than 144 seconds to me, I should be sent on the alternative route
+          // (which takes 288 seconds)
+          valueOfTime = 3600.0 / 145.0,
+          None,
+          None
+        ))
       )
       router ! tollSensitiveRequest
       val tollSensitiveResponse = expectMsgType[RoutingResponse]
