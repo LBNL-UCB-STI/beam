012b1fa2dd6e30793de92def5916d83eb1d6eee6
Colin Sheppard <colinthegaunt@gmail.com>
2018-12-10 10:46:35 -0800

    adjust tests now that we properly ignore person costs if the transit fare is  0

--- src/test/scala/beam/agentsim/agents/PersonAgentSpec.scala
+++ src/test/scala/beam/agentsim/agents/PersonAgentSpec.scala
@@ -541,7 +541,7 @@ class PersonAgentSpec
         ),
         beamVehicleId = tramId,
         asDriver = false,
-        cost = 0.0,
+        cost = 1.0, // $1 fare
         unbecomeDriverOnCompletion = false
       )
 
@@ -722,9 +722,6 @@ class PersonAgentSpec
 
       events.expectMsgType[PersonEntersVehicleEvent]
       events.expectMsgType[PersonCostEvent]
-
-      //Generating 1 event of PersonCost having 0.0 cost in between PersonEntersVehicleEvent & PersonLeavesVehicleEvent
-
       events.expectMsgType[PersonLeavesVehicleEvent]
 
       events.expectMsgType[VehicleEntersTrafficEvent]
