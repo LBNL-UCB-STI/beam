eb29922d649f4007ada96caaaeb8125d6a7c236c
wrashid <waraich.rashid@gmail.com>
2018-12-09 19:39:20 -0800

    adding FreeFlowTravelTime

---
+++ src/main/scala/beam/router/FreeFlowTravelTime.scala
@@ -0,0 +1,12 @@
+package beam.router
+
+import com.typesafe.scalalogging.LazyLogging
+import org.matsim.api.core.v01.network.Link
+import org.matsim.api.core.v01.population.Person
+import org.matsim.core.router.util.TravelTime
+import org.matsim.vehicles.Vehicle
+
+class FreeFlowTravelTime extends TravelTime with LazyLogging {
+  override def getLinkTravelTime(link: Link, time: Double, person: Person, vehicle: Vehicle): Double =
+    link.getLength / link.getFreespeed
+}
