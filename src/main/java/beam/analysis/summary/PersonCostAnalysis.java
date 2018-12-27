868fca18a698ee80473b6b0a5956aa8a51a11397
rajnikant <rk_sharmait@yahoo.com>
2018-12-28 04:51:42 +0530

    add averageTripExpenditure in summary stats

--- src/main/java/beam/analysis/summary/PersonCostAnalysis.java
+++ src/main/java/beam/analysis/summary/PersonCostAnalysis.java
@@ -3,6 +3,7 @@ package beam.analysis.summary;
 import beam.agentsim.events.PersonCostEvent;
 import beam.analysis.IterationSummaryAnalysis;
 import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +11,8 @@ import java.util.Map;
 public class PersonCostAnalysis implements IterationSummaryAnalysis {
     private Map<String,Double> personCostByCostType = new HashMap<>();
     private String[] costTypes = {"Cost","Subsidy","Toll"};
+    int numberOfTrips = 0;
+    double totalCost = 0.0;
 
     @Override
     public void processStats(Event event) {
@@ -27,7 +30,11 @@ public class PersonCostAnalysis implements IterationSummaryAnalysis {
                 }
                 String statType = String.format("total%s_%s", costType, mode);
                 personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
+                totalCost += cost;
             }
+        }
+        if (event instanceof PersonDepartureEvent || event.getEventType().equalsIgnoreCase(PersonDepartureEvent.EVENT_TYPE)){
+            numberOfTrips++;
         }
 
     }
@@ -35,10 +42,13 @@ public class PersonCostAnalysis implements IterationSummaryAnalysis {
     @Override
     public void resetStats() {
         personCostByCostType.clear();
+        numberOfTrips = 0;
+        totalCost = 0;
     }
 
     @Override
     public Map<String, Double> getSummaryStats() {
+        personCostByCostType.put("averageTripExpenditure", totalCost/numberOfTrips);
         return personCostByCostType;
     }
 }
