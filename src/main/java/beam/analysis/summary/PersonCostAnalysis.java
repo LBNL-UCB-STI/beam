06452d7c27cd4a774db3f4f6686b83ab79e46c0f
Zeeshan Bilal <zeeshan.bilal@hotmail.com>
2018-12-28 23:31:39 +0500

    summary stats missing keys (#1066)
    * Fixed #1047 fixed missing modes in cost analysis
    
    * miles and hours travled against all vehicle types
    
    * feedback incorporated
    
    * merge issue fixed
    
    * fmt
    
    * rename

--- src/main/java/beam/analysis/summary/PersonCostAnalysis.java
+++ src/main/java/beam/analysis/summary/PersonCostAnalysis.java
@@ -2,43 +2,56 @@ package beam.analysis.summary;
 
 import beam.agentsim.events.PersonCostEvent;
 import beam.analysis.IterationSummaryAnalysis;
+import beam.router.Modes;
 import org.matsim.api.core.v01.events.Event;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class PersonCostAnalysis implements IterationSummaryAnalysis {
-    private Map<String,Double> personCostByCostType = new HashMap<>();
-    private String[] costTypes = {"Cost","Subsidy","Toll"};
+  private Map<String, Double> personCostByCostType = new HashMap<>();
+  private String[] costTypes = {"Cost", "Subsidy", "Toll"};
 
-    @Override
-    public void processStats(Event event) {
-        if (event instanceof PersonCostEvent || event.getEventType().equalsIgnoreCase(PersonCostEvent.EVENT_TYPE)){
-            Map<String, String> attributes = event.getAttributes();
-            String mode = attributes.get(PersonCostEvent.ATTRIBUTE_MODE);
-            Double cost = 0.0;
-            for(String costType : costTypes){
-                if(costType.equals("Cost")){
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_NET_COST));
-                }else if(costType.equals("Subsidy")){
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_SUBSIDY));
-                }else if(costType.equals("Toll")) {
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_TOLL_COST));
-                }
-                String statType = String.format("total%s_%s", costType, mode);
-                personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
-            }
+  @Override
+  public void processStats(Event event) {
+    if (event instanceof PersonCostEvent || event.getEventType().equalsIgnoreCase(PersonCostEvent.EVENT_TYPE)) {
+      Map<String, String> attributes = event.getAttributes();
+      String mode = attributes.get(PersonCostEvent.ATTRIBUTE_MODE);
+      Double cost = 0.0;
+      for (String costType : costTypes) {
+        switch (costType) {
+          case "Cost":
+            cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_NET_COST));
+            break;
+          case "Subsidy":
+            cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_SUBSIDY));
+            break;
+          case "Toll":
+            cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_TOLL_COST));
+            break;
         }
-
+        String statType = String.format("total%s_%s", costType, mode);
+        personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
+      }
     }
+  }
 
-    @Override
-    public void resetStats() {
-        personCostByCostType.clear();
-    }
+  @Override
+  public void resetStats() {
+    personCostByCostType.clear();
+  }
 
-    @Override
-    public Map<String, Double> getSummaryStats() {
-        return personCostByCostType;
-    }
+  @Override
+  public Map<String, Double> getSummaryStats() {
+    Modes.BeamMode$.MODULE$.allModes().foreach(mode -> {
+      Double cost = 0.0;
+      for (String costType : costTypes) {
+        String statType = String.format("total%s_%s", costType, mode.value());
+        personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
+      }
+      return null;
+    });
+
+    return personCostByCostType;
+  }
 }
