624d95c622dc43092e3cd997dd5fc8e30f550a09
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-12-28 06:03:53 +0500

    fixed warnings

--- src/main/java/beam/analysis/summary/PersonCostAnalysis.java
+++ src/main/java/beam/analysis/summary/PersonCostAnalysis.java
@@ -9,31 +9,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PersonCostAnalysis implements IterationSummaryAnalysis {
-    private Map<String,Double> personCostByCostType = new HashMap<>();
-    private String[] costTypes = {"Cost","Subsidy","Toll"};
+    private Map<String, Double> personCostByCostType = new HashMap<>();
+    private String[] costTypes = {"Cost", "Subsidy", "Toll"};
     private int numberOfTrips = 0;
-    double totalNetCost = 0.0;
+    private double totalNetCost = 0.0;
 
     @Override
     public void processStats(Event event) {
-        if (event instanceof PersonCostEvent || event.getEventType().equalsIgnoreCase(PersonCostEvent.EVENT_TYPE)){
+        if (event instanceof PersonCostEvent || event.getEventType().equalsIgnoreCase(PersonCostEvent.EVENT_TYPE)) {
             Map<String, String> attributes = event.getAttributes();
             String mode = attributes.get(PersonCostEvent.ATTRIBUTE_MODE);
             Double cost = 0.0;
-            for(String costType : costTypes){
-                if(costType.equals("Cost")){
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_NET_COST));
-                    totalNetCost += cost;
-                }else if(costType.equals("Subsidy")){
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_SUBSIDY));
-                }else if(costType.equals("Toll")) {
-                    cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_TOLL_COST));
+            for (String costType : costTypes) {
+                switch (costType) {
+                    case "Cost":
+                        cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_NET_COST));
+                        totalNetCost += cost;
+                        break;
+                    case "Subsidy":
+                        cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_SUBSIDY));
+                        break;
+                    case "Toll":
+                        cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_TOLL_COST));
+                        break;
                 }
                 String statType = String.format("total%s_%s", costType, mode);
                 personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
             }
         }
-        if (event instanceof PersonDepartureEvent || event.getEventType().equalsIgnoreCase(PersonDepartureEvent.EVENT_TYPE)){
+        if (event instanceof PersonDepartureEvent || event.getEventType().equalsIgnoreCase(PersonDepartureEvent.EVENT_TYPE)) {
             numberOfTrips++;
         }
 
@@ -48,7 +52,7 @@ public class PersonCostAnalysis implements IterationSummaryAnalysis {
 
     @Override
     public Map<String, Double> getSummaryStats() {
-        personCostByCostType.put("averageTripExpenditure", totalNetCost /numberOfTrips);
+        personCostByCostType.put("averageTripExpenditure", totalNetCost / numberOfTrips);
         return personCostByCostType;
     }
 }
