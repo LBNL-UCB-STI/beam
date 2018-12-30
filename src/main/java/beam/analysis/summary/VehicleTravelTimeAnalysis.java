c16754570de966a096db80306f19e45603cf2a53
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2018-12-30 16:59:12 +0500

    fixed averageVehicleDelay

--- src/main/java/beam/analysis/summary/VehicleTravelTimeAnalysis.java
+++ src/main/java/beam/analysis/summary/VehicleTravelTimeAnalysis.java
@@ -3,10 +3,13 @@ package beam.analysis.summary;
 import beam.agentsim.events.PathTraversalEvent;
 import beam.analysis.IterationSummaryAnalysis;
 import beam.physsim.jdeqsim.AgentSimToPhysSimPlanConverter;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.ListUtils;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.events.ActivityStartEvent;
 import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.events.PersonLeavesVehicleEvent;
 import org.matsim.api.core.v01.network.Link;
 
@@ -24,12 +27,13 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
     private double averageVehicleDelayWork = 0.0;
     private double averageVehicleDelayHome = 0.0;
     private double averageVehicleDelaySecondary = 0.0;
-    private Map<String, Double> vehicleIdDelay = new HashMap<>();
     private Map<String, Double> personIdDelay = new HashMap<>();
+    private Map<String, List<String>> personsByVehicleIds = new HashMap<>();
     private static final String work = "Work";
     private static final String home = "Home";
     private long numOfTimesBusTaken = 0;
     private Set<String> buses = new HashSet<>();
+
 
     public VehicleTravelTimeAnalysis(Scenario scenario) {
         this.scenario = scenario;
@@ -37,7 +41,13 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
 
     @Override
     public void processStats(Event event) {
-        if (event instanceof PathTraversalEvent || event.getEventType().equalsIgnoreCase(PathTraversalEvent.EVENT_TYPE)) {
+        if (event instanceof PersonEntersVehicleEvent || event.getEventType().equalsIgnoreCase(PersonEntersVehicleEvent.EVENT_TYPE)) {
+            Map<String, String> eventAttributes = event.getAttributes();
+            String vehicleId = eventAttributes.get(PersonLeavesVehicleEvent.ATTRIBUTE_VEHICLE);
+            String personId = eventAttributes.get(PersonLeavesVehicleEvent.ATTRIBUTE_PERSON);
+
+            personsByVehicleIds.merge(vehicleId, Lists.newArrayList(personId), ListUtils::union);
+        } else if (event instanceof PathTraversalEvent || event.getEventType().equalsIgnoreCase(PathTraversalEvent.EVENT_TYPE)) {
             Map<String, String> eventAttributes = event.getAttributes();
             String mode = eventAttributes.get(PathTraversalEvent.ATTRIBUTE_MODE);
             double travelDurationInSec = (Double.parseDouble(eventAttributes.get(PathTraversalEvent.ATTRIBUTE_ARRIVAL_TIME)) -
@@ -45,7 +55,7 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
             int numOfPassengers = Integer.parseInt(eventAttributes.get(PathTraversalEvent.ATTRIBUTE_NUM_PASS));
             int seatingCapacity = Integer.parseInt(eventAttributes.get(PathTraversalEvent.ATTRIBUTE_SEATING_CAPACITY));
 
-            secondsTraveledByVehicleType.merge(mode, travelDurationInSec, (d1, d2) -> d1 + d2);
+            secondsTraveledByVehicleType.merge(mode, travelDurationInSec, Double::sum);
 
             if (AgentSimToPhysSimPlanConverter.isPhyssimMode(mode)) {
 
@@ -64,11 +74,14 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
                     }
                 }
                 if (travelDurationInSec > freeFlowDuration) { //discarding negative values
-                    if(AgentSimToPhysSimPlanConverter.CAR.equalsIgnoreCase(mode)){
-                        String vehicleID = eventAttributes.get(PathTraversalEvent.ATTRIBUTE_VEHICLE_ID);
-                        double averageVehicleDelay = numOfPassengers * (travelDurationInSec - freeFlowDuration);
-                        vehicleIdDelay.put(vehicleID, averageVehicleDelay);
+
+                    String vehicleID = eventAttributes.get(PathTraversalEvent.ATTRIBUTE_VEHICLE_ID);
+                    double averageVehicleDelay = travelDurationInSec - freeFlowDuration;
+
+                    if(personsByVehicleIds.containsKey(vehicleID)) {
+                        personsByVehicleIds.get(vehicleID).forEach(personId -> personIdDelay.merge(personId, averageVehicleDelay, Double::sum));
                     }
+
                     totalVehicleTrafficDelay += (travelDurationInSec - freeFlowDuration);
                 }
             }
@@ -86,12 +99,7 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
             if(buses.contains(vehicleId)) {
                 numOfTimesBusTaken++;
             }
-
-            if (vehicleIdDelay.containsKey(vehicleId)) {
-                String personID = eventAttributes.get(PersonLeavesVehicleEvent.ATTRIBUTE_PERSON);
-                personIdDelay.put(personID, vehicleIdDelay.get(vehicleId));
-                vehicleIdDelay.remove(vehicleId);
-            }
+            personsByVehicleIds.remove(vehicleId);
         } else if (event instanceof ActivityStartEvent || event.getEventType().equalsIgnoreCase(ActivityStartEvent.EVENT_TYPE)) {
             Map<String, String> eventAttributes = event.getAttributes();
             String personId = eventAttributes.get(ActivityStartEvent.ATTRIBUTE_PERSON);
@@ -126,7 +134,7 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
         averageVehicleDelayHome = 0.0;
         averageVehicleDelaySecondary = 0.0;
         secondsTraveledByVehicleType.clear();
-        vehicleIdDelay.clear();
+        personsByVehicleIds.clear();
         personIdDelay.clear();
         buses.clear();
     }
@@ -134,7 +142,7 @@ public class VehicleTravelTimeAnalysis implements IterationSummaryAnalysis {
     @Override
     public Map<String, Double> getSummaryStats() {
         Map<String, Double> summaryStats = secondsTraveledByVehicleType.entrySet().stream().collect(Collectors.toMap(
-                e -> "vehicleHoursTraveled_" + e.getKey(),
+                e -> "vehicleHoursTraveled_" +  e.getKey(),
                 e -> e.getValue() / 3600.0
         ));
 
