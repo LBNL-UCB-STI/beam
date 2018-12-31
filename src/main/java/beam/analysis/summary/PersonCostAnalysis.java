067a827d993a52e09497517c39b7cc64a9acdb49
rajnikant <rajnikantsh@users.noreply.github.com>
2019-01-01 01:23:45 +0530

    add misc summary stats (#1085)
    * add misc summary stats
    
    * add null check and change return syntax
    
    * refactoring
    
    * bug fix: wrong field used
    
    * refactor: reuse code
    
    * add totalHoursOfVehicleTrafficDelay in summary stats.
    
    * add summary stats for averagePTWaitingTime
    
    * add bus crowding in summary stats
    
    * reset bus crowding
    
    * add averageTripExpenditure in summary stats
    
    * fixed cost aggregation
    
    * fixed PTCrowding (bus)
    
    * renamed to totalNetCost
    
    * fixed warnings
    
    * reset ptWaiting against personId
    
    * fixed average and update labels
    
    * hoursTraveled should be renamed to travelDurationInSec, as this is not in hours
    
    * remove / 3600 for busCrowding
    
    * fixed hard coding of modes
    
    * fixed hard coding of modes
    
    * rename _person
    
    * fixed warnings and format
    
    * create summary stats for average vehicle delay for motorized leg. (#1095)
    
    * fixed numOfTimesBusTaken in busCrowding
    
    * changed buses list to set
    
    * fixed averageVehicleDelay
    
    * fixed delay stat variable name
    
    * fixed delay stats calculation
    
    * fix n/0 issue
    
    * fix totalVehicles for averageDelays

--- src/main/java/beam/analysis/summary/PersonCostAnalysis.java
+++ src/main/java/beam/analysis/summary/PersonCostAnalysis.java
@@ -4,13 +4,16 @@ import beam.agentsim.events.PersonCostEvent;
 import beam.analysis.IterationSummaryAnalysis;
 import beam.router.Modes;
 import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class PersonCostAnalysis implements IterationSummaryAnalysis {
-  private Map<String, Double> personCostByCostType = new HashMap<>();
+  private Map<String,Double> personCostByCostType = new HashMap<>();
   private String[] costTypes = {"Cost", "Incentive", "Toll"};
+  private int numberOfTrips = 0;
+  private double totalNetCost = 0.0;
 
   @Override
   public void processStats(Event event) {
@@ -22,6 +25,7 @@ public class PersonCostAnalysis implements IterationSummaryAnalysis {
         switch (costType) {
           case "Cost":
             cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_NET_COST));
+            totalNetCost += cost;
             break;
           case "Incentive":
             cost = Double.parseDouble(attributes.get(PersonCostEvent.ATTRIBUTE_INCENTIVE));
@@ -34,15 +38,21 @@ public class PersonCostAnalysis implements IterationSummaryAnalysis {
         personCostByCostType.merge(statType, cost, (d1, d2) -> d1 + d2);
       }
     }
+    if (event instanceof PersonDepartureEvent || event.getEventType().equalsIgnoreCase(PersonDepartureEvent.EVENT_TYPE)) {
+      numberOfTrips++;
+    }
   }
 
   @Override
   public void resetStats() {
     personCostByCostType.clear();
+    numberOfTrips = 0;
+    totalNetCost = 0;
   }
 
   @Override
   public Map<String, Double> getSummaryStats() {
+    personCostByCostType.put("averageTripExpenditure", totalNetCost / numberOfTrips);
     Modes.BeamMode$.MODULE$.allModes().foreach(mode -> {
       Double cost = 0.0;
       for (String costType : costTypes) {
