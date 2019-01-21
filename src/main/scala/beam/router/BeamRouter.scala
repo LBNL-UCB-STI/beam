b47e4cb03b47cf3fd3028320bc9ead37ab6ddff1
Artavazd Balaian <reasyu@gmail.com>
2019-01-22 01:14:50 +0700

    Applied `scalafmt`

--- src/main/scala/beam/router/BeamRouter.scala
+++ src/main/scala/beam/router/BeamRouter.scala
@@ -5,7 +5,18 @@ import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 import akka.actor.Status.{Status, Success}
-import akka.actor.{Actor, ActorLogging, ActorRef, Address, Cancellable, ExtendedActorSystem, Props, RelativeActorPath, RootActorPath, Stash}
+import akka.actor.{
+  Actor,
+  ActorLogging,
+  ActorRef,
+  Address,
+  Cancellable,
+  ExtendedActorSystem,
+  Props,
+  RelativeActorPath,
+  RootActorPath,
+  Stash
+}
 import akka.cluster.ClusterEvent._
 import akka.cluster.{Cluster, Member, MemberStatus}
 import akka.pattern._
@@ -470,8 +481,7 @@ object BeamRouter {
   case class RoutingResponse(
     itineraries: Seq[EmbodiedBeamTrip],
     requestId: Int
-  ) {
-  }
+  ) {}
 
   object RoutingResponse {
     val dummyRoutingResponse = Some(RoutingResponse(Vector(), java.util.UUID.randomUUID().hashCode()))
