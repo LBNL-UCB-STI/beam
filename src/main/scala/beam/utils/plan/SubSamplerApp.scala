14208da31844d5baf1f7dfa47e271a8adf9ffdff
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2019-02-02 23:06:11 +0500

    simple plan sub sampler

--- src/main/scala/beam/utils/plan/SubSamplerApp.scala
+++ src/main/scala/beam/utils/plan/SubSamplerApp.scala
@@ -16,10 +16,8 @@ import scala.collection.mutable
 import scala.util.{Random, Try}
 
 object SubSamplerApp extends App {
-
-/*  val samplingApproach="Simple_Random_Sampling" // || "StratifiedSampling"
-
-  val simpleRandomSampling = "Simple_Random_Sampling"*/
+  val SIMPLE_RANDOM_SAMPLING = "simple"
+  val STRATIFIED_SAMPLING = "stratified"
 
   var vehicles: mutable.HashMap[String, java.util.Map[String, String]] = _
 
@@ -32,7 +30,7 @@ object SubSamplerApp extends App {
     conf.households().setInputFile(s"$sampleDir/households.xml.gz")
     conf.households().setInputHouseholdAttributesFile(s"$sampleDir/householdAttributes.xml.gz")
 
-//    conf.vehicles().setVehiclesFile(s"$sampleDir/vehicles.csv.gz")
+    //    conf.vehicles().setVehiclesFile(s"$sampleDir/vehicles.csv.gz")
     vehicles = BeamVehicleUtils.readCsvFileByLine(
       s"$sampleDir/vehicles.csv.gz",
       mutable.HashMap[String, java.util.Map[String, String]]()
@@ -44,56 +42,55 @@ object SubSamplerApp extends App {
     conf
   }
 
-/*
 
-  def getHouseholdSimpleRandomSample(scenario: Scenario, sampleSize: Int) = {
-    val hhIds = sc.getHouseholds.getHouseholds.keySet().asScala
+  def getSimpleRandomSampleOfHouseholds(scenario: Scenario, sampleSize: Int): List[Set[Id[Household]]] = {
+    val randomizedHHIds = Random.shuffle(scenario.getHouseholds.getHouseholds.keySet().asScala)
 
-    val randomizedHHIds = Random.shuffle(hhIds)
+    var numberOfAgentsInSample = 0
+    val selectedHouseholds = randomizedHHIds.takeWhile { hhId =>
 
-    var numberOfAgentsInSample=0
-    var selectedHouseholds=randomizedHHIds.takeWhile{ hhId =>
+      numberOfAgentsInSample += scenario.getHouseholds.getHouseholds.get(hhId).getMemberIds.size()
 
-      numberOfAgentsInSample+=sc.getHouseholds.getHouseholds.get(hhId).getMemberIds.size()
-
-      numberOfAgentsInSample<sampleSize
+      numberOfAgentsInSample < sampleSize
     }
 
-    List(selectedHouseholds)
+    List(selectedHouseholds.toSet)
   }
 
-
-
-  def createHouseholdSpatialClusters(scenario: Scenario, households:  List[Set[Id[Household]]], numberOfClusters:Int) ={
-    // TODO: return multiple sets of same size clusters (spatial co-location)
-
-    // TODO: use grid if cluster hard
-
-
-
-    //
-
-    households
+  def getStratifiedSampleOfHousholds(scenario: Scenario, sampleSize: Int): List[Set[Id[Household]]] = {
+    List(scenario.getHouseholds.getHouseholds.keySet().asScala.toSet)
   }
 
-  def splitByDistance(scenario: Scenario, households:  List[Set[Id[Household]]], numberOfClusters:Int)= {
-    // calculate distance travelled per day for all household members per day -> d_sum
-    // sort households by d_sum
-    // split into equal size groups: numberOfClusters
+  /*
 
-    households
-  }
+    def createHouseholdSpatialClusters(scenario: Scenario, households:  List[Set[Id[Household]]], numberOfClusters:Int) ={
+      // TODO: return multiple sets of same size clusters (spatial co-location)
 
-
-  def splitByIncome()
+      // TODO: use grid if cluster hard
 
 
 
+      //
 
-  def splitByAge
-*/
+      households
+    }
+
+    def splitByDistance(scenario: Scenario, households:  List[Set[Id[Household]]], numberOfClusters:Int)= {
+      // calculate distance travelled per day for all household members per day -> d_sum
+      // sort households by d_sum
+      // split into equal size groups: numberOfClusters
+
+      households
+    }
 
 
+    def splitByIncome()
+
+
+
+
+    def splitByAge
+  */
 
 
 
@@ -103,13 +100,15 @@ object SubSamplerApp extends App {
     val sc: Scenario = ScenarioUtils.loadScenario(conf)
 
     val hhIds = sc.getHouseholds.getHouseholds.keySet().asScala
+    val hhIsSampled = if(samplingApproach == SIMPLE_RANDOM_SAMPLING) {
+      getSimpleRandomSampleOfHouseholds(sc, sampleSize)
+    } else if(samplingApproach == STRATIFIED_SAMPLING) {
+      getStratifiedSampleOfHousholds(sc, sampleSize)
+    } else {
+      throw new IllegalArgumentException(s"$samplingApproach is not a valid sampling approach.")
+    }
 
-    /*if (samplingApproach == simpleRandomSampling){
-      //     val hhIdsToRemove = Random.shuffle(hhIds).takeRight(Math.max(hhIds.size - sampleSize, 0))
-    } else if (samplingApproach==stratifiedSample){
-
-    } else error*/
-
+    val hhIdsToRemove = hhIds.diff(hhIsSampled.flatten.toSet)
 
     hhIdsToRemove.foreach(id => {
       val hh = sc.getHouseholds.getHouseholds.remove(id)
@@ -131,7 +130,7 @@ object SubSamplerApp extends App {
     new HouseholdsWriterV10(sc.getHouseholds).writeFile(s"$outDir/households.xml.gz")
     new PopulationWriter(sc.getPopulation).write(s"$outDir/population.xml.gz")
     PopulationWriterCSV(sc.getPopulation).write(s"$outDir/population.csv.gz")
-//    new VehicleWriterV1(sc.getVehicles).writeFile(s"$outDir/vehicles.csv.gz")
+    //    new VehicleWriterV1(sc.getVehicles).writeFile(s"$outDir/vehicles.csv.gz")
     new ObjectAttributesXmlWriter(sc.getHouseholds.getHouseholdAttributes)
       .writeFile(s"$outDir/householdAttributes.xml.gz")
     new ObjectAttributesXmlWriter(sc.getPopulation.getPersonAttributes)
@@ -158,6 +157,7 @@ test/input/sf-light/sample/1.5k
   val sampleDir = args(0)
   val sampleSize = Try(args(1).toInt).getOrElse(1000)
   val outDir = args(2)
+  val samplingApproach = args(3).toLowerCase()
 
   val sampler = SubSamplerApp
   val conf = getConfig(sampleDir)
