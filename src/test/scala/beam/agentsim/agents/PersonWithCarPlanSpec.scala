c86a6d335c4c5199ff6fc40e88e0c1be828ef516
Justin Pihony <justin.pihony@gmail.com>
2019-02-04 14:11:36 -0500

    maybe

--- src/test/scala/beam/agentsim/agents/PersonWithCarPlanSpec.scala
+++ src/test/scala/beam/agentsim/agents/PersonWithCarPlanSpec.scala
@@ -2,7 +2,7 @@ package beam.agentsim.agents
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Acd
+import akka.actor.{Ac
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import akka.util.Timeout
 import beam.agentsim.agents.PersonTestUtil._
