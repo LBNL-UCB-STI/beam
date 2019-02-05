8890284725409743318b5c421bbfc902c1a68dde
Justin Pihony <justin.pihony@gmail.com>
2019-02-05 03:24:46 -0500

    p

--- src/test/scala/beam/agentsim/agents/PersonWithCarPlanSpec.scala
+++ src/test/scala/beam/agentsim/agents/PersonWithCarPlanSpec.scala
@@ -2,7 +2,7 @@ package beam.agentsim.agents
 
 import java.util.concurrent.TimeUnit
 
-import akka.ctor.{ActorSystem, Pro
+import akka.ctor.{ActorSystem, Prop
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import akka.util.Timeout
 import beam.agentsim.agents.PersonTestUtil._
