b95ef4126c31f817cda1c2f348fd83bfed2fbd14
rajnikantsh <rk_sharmait@yahoo.com>
2019-02-20 03:02:02 +0530

    change behavior of writing config outputs

--- src/test/scala/beam/utils/TestConfigUtils.scala
+++ src/test/scala/beam/utils/TestConfigUtils.scala
@@ -1,12 +1,16 @@
 package beam.utils
 
-import com.typesafe.config.ConfigValueFactory
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 
 object TestConfigUtils {
   val testOutputDir = "output/test/"
+
+  val configFileName = "test/input/beamville/beam.conf"
+  val configLocation = ConfigFactory.parseString("config=" + configFileName)
 
   def testConfig(conf: String) =
     BeamConfigUtils
       .parseFileSubstitutingInputDirectory(conf)
       .withValue("beam.outputs.baseOutputDirectory", ConfigValueFactory.fromAnyRef(testOutputDir))
+      .withFallback(configLocation)
 }
