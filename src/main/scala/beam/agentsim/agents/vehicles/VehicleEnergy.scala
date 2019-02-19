be3d83969b591102ee42110892c30c5fb7a88bc3
Justin Pihony <justin.pihony@gmail.com>
2019-02-18 21:57:59 -0500

    Everything working - needs integrated

--- src/main/scala/beam/agentsim/agents/vehicles/VehicleEnergy.scala
+++ src/main/scala/beam/agentsim/agents/vehicles/VehicleEnergy.scala
@@ -2,81 +2,104 @@ package beam.agentsim.agents.vehicles
 
 import beam.sim.common.Range
 import beam.sim.config.BeamConfig
-import com.univocity.parsers.csv.{CsvParserSettings, CsvParser}
+import com.univocity.parsers.csv.{CsvParser, CsvParserSettings}
 import com.univocity.parsers.common.record.Record
 import java.io.File
 import org.slf4j.LoggerFactory
 import scala.collection.mutable
 import scala.collection.JavaConverters._
 
-class VehicleEnergyCSVReader(config: BeamConfig){
-  def getRecordsFrom(csvFile: File): Iterable[Record] = {
-    val settings = new CsvParserSettings()
-    settings.detectFormatAutomatically()
-    val parser = new CsvParser(settings)
-    val csvLocation = config.beam.agentsim.populationAdjustment
-    parser.iterateRecords(new File(csvLocation)).asScala
+class VehicleEnergyCsvReader(config: BeamConfig) {
+
+  def getRecordsFrom(csvFile: File, csvParser: CsvParser): Iterable[Record] = {
+    val csvLocation = config.beam.agentsim.agents.vehicles.vehicleEnergyFile
+    csvParser.iterateRecords(new File(csvLocation)).asScala
   }
 }
 
-case class TravelData(speed:Int, gradePercent: Int, numberOfLanes: Int)
-class VehicleEnergy(recordIterator: => Iterable[Record]) {
+case class TravelData(speedInMilesPerHour: Int, gradePercent: Int, numberOfLanes: Int)
+
+class VehicleEnergy(recordsIterableUsing: CsvParser => Iterable[Record]) {
   private lazy val log = LoggerFactory.getLogger(this.getClass)
 
   type ConsumptionRateFilter = Map[Range, Map[Range, Map[Range, Float]]] //speed->(gradePercent->(numberOfLanes->rate))
   private lazy val consumptionRateFilter = loadConsumptionRatesFromCSV
 
   def getRateUsing(travelData: TravelData, fallBack: => Float): Float = {
-    val TravelData(speed, gradePercent, numberOfLanes) = travelData
+    val TravelData(speedInMilesPerHour, gradePercent, numberOfLanes) = travelData
     //Future performance improvement could be to better index the bins so could fuzzily jump straight to it
     //instead of having to iterate
     val filteredRates = consumptionRateFilter
-      .collect{case (speedBin, restOfFilter) if speedBin.has(speed) => restOfFilter}.flatten
-      .collect{case (gradePercentBin, restOfFilter) if gradePercentBin.has(gradePercent) => restOfFilter}.flatten
-      .collect{case (numberOfLanesBin, rate) if numberOfLanesBin.has(numberOfLanes) => rate}
+      .collect {
+        case (speedInMilesPerHourBin, restOfFilter) if speedInMilesPerHourBin.has(speedInMilesPerHour) =>
+          restOfFilter
+      }
+      .flatten
+      .collect { case (gradePercentBin, restOfFilter) if gradePercentBin.has(gradePercent) => restOfFilter }
+      .flatten
+      .collect { case (numberOfLanesBin, rate) if numberOfLanesBin.has(numberOfLanes) => rate }
 
     val ratesSize = filteredRates.size
-    if(ratesSize > 1) log.warn("More than one ({}) rate was found using {}. " +
-      "The first will be used, but the data should be reviewed for range overlap.", ratesSize, travelData)
+    if (ratesSize > 1)
+      log.warn(
+        "More than one ({}) rate was found using {}. " +
+        "The first will be used, but the data should be reviewed for range overlap.",
+        ratesSize,
+        travelData
+      )
     filteredRates.headOption.getOrElse(fallBack)
   }
 
   private def loadConsumptionRatesFromCSV: ConsumptionRateFilter = {
     val currentRateFilter = mutable.Map.empty[Range, mutable.Map[Range, mutable.Map[Range, Float]]]
-    recordIterator.foreach(csvRecord => {
-      val speedBin = Range(csvRecord.getString(0).replace(",",":").replace(" ",""))
-      val gradePercentBin = Range(csvRecord.getString(1).replace(",",":").replace(" ",""))
-      val numberOfLanesBin = Range(csvRecord.getString(2).replace(",",":").replace(" ",""))
-      val rateStr = csvRecord.getString(5)
-      println(csvRecord.getValues.toList)
-      println(rateStr)
-      val rate = rateStr.toFloat
-      currentRateFilter.get(speedBin) match {
+    val settings = new CsvParserSettings()
+    settings.detectFormatAutomatically()
+    val csvParser = new CsvParser(settings)
+    recordsIterableUsing(csvParser).foreach(csvRecord => {
+      val speedInMilesPerHourBin = convertRecordStringToRange(csvRecord.getString(0))
+      val gradePercentBin = convertRecordStringToRange(csvRecord.getString(1))
+      val numberOfLanesBin = convertRecordStringToRange(csvRecord.getString(2))
+      val rate = csvRecord.getFloat(5)
+      if (rate == null)
+        throw new Exception(
+          s"Record $csvRecord does not contain a valid rate. " +
+          "Erroring early to bring attention and get it fixed."
+        )
+
+      currentRateFilter.get(speedInMilesPerHourBin) match {
         case Some(gradePercentFilter) => {
           gradePercentFilter.get(gradePercentBin) match {
             case Some(numberOfLanesFilter) => {
               numberOfLanesFilter.get(numberOfLanesBin) match {
                 case Some(initialRate) =>
-                  println(initialRate)
-                  println(rate)
-                  log.error("Two rates found for the same bin combination: " +
-                    "Speed Bin = {}; Grade Percent Bin = {}; Number of Lanes Bin = {}. " +
-                    "Keeping initial rate of {} and ignoring new rate of {}.",
-                    speedBin.toString, gradePercentBin.toString, numberOfLanesBin.toString, initialRate.toString, rate.toString)
+                  log.error(
+                    "Two rates found for the same bin combination: " +
+                    "Speed In Miles Per Hour Bin = {}; Grade Percent Bin = {}; Number of Lanes Bin = {}. " +
+                    s"Keeping initial rate of $initialRate and ignoring new rate of $rate.",
+                    speedInMilesPerHourBin,
+                    gradePercentBin,
+                    numberOfLanesBin
+                  )
                 case None => numberOfLanesFilter += numberOfLanesBin -> rate
               }
             }
             case None => gradePercentFilter += gradePercentBin -> mutable.Map(numberOfLanesBin -> rate)
           }
         }
-        case None => currentRateFilter += speedBin -> mutable.Map(gradePercentBin -> mutable.Map(numberOfLanesBin -> rate))
+        case None =>
+          currentRateFilter += speedInMilesPerHourBin ->
+          mutable.Map(gradePercentBin -> mutable.Map(numberOfLanesBin -> rate))
       }
     })
-    println(currentRateFilter)
-    currentRateFilter.toMap.map{
-      case (speedBin, gradePercentMap) => speedBin -> gradePercentMap.toMap.map{
-        case (gradePercentBin, lanesMap) => gradePercentBin -> lanesMap.toMap
-      }
+    currentRateFilter.toMap.map {
+      case (speedInMilesPerHourBin, gradePercentMap) =>
+        speedInMilesPerHourBin -> gradePercentMap.toMap.map {
+          case (gradePercentBin, lanesMap) => gradePercentBin -> lanesMap.toMap
+        }
     }
+  }
+
+  private def convertRecordStringToRange(recordString: String) = {
+    Range(recordString.replace(",", ":").replace(" ", ""))
   }
 }
--- src/test/scala/beam/agentsim/agents/vehicles/VehicleEnergyTest.scala
+++ src/test/scala/beam/agentsim/agents/vehicles/VehicleEnergyTest.scala
@@ -1,32 +1,63 @@
 package beam.agentsim.agents.vehicles
 
-import com.univocity.parsers.csv.{CsvParser, CsvParserSettings}
-import java.io.{ByteArrayInputStream}
-
+import com.univocity.parsers.csv.CsvParser
+import java.io.ByteArrayInputStream
 import org.scalatest.mockito.MockitoSugar
-import org.mockito.Mockito._
-import org.scalatest.{FunSpecLike, Matchers, _}
+import org.scalatest._
 import scala.collection.JavaConverters._
 
-class VehicleEnergyTest extends FunSpecLike
-  with BeforeAndAfterAll
-  with Matchers
-  with MockitoSugar {
+class VehicleEnergyTest extends FunSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  val floatAllowedDiscrepancy = 0.001
 
-  describe("A VehicleEnergy") {
-    it("should") {
-      lazy val baseString =
-        """"(1, 5]","(-8, -6]","(0, 1]","6.362255966","0.423485439","6.656215049"
-          |"(1, 5],"(-6, -5]","(0, 1]","2.566056334","0.189079865","7.368500149"""".stripMargin //TODO: Mixed quotes needed
-      val settings = new CsvParserSettings()
-      settings.detectFormatAutomatically()
-      val parser = new CsvParser(settings)
-      val input = parser.iterateRecords(new ByteArrayInputStream(baseString.getBytes)).asScala
-      val energy = new VehicleEnergy(input)
-      val rate = energy.getRateUsing(TravelData(2,-6,1), 1)
-      Math.abs(rate - 6.656215) should be < 0.001
-      //val passengerSchedule: PassengerSchedule = PassengerSchedule()
-      //passengerSchedule.schedule.size should be(0)
+  describe("A VehicleEnergy with the same speed and number of lanes") {
+    it("should return the rate that fits the grade percent") {
+      lazy val vehicleEnergyString =
+        """"(1, 5]","(-8, -6]","(0, 1]",6.362255966266373,0.42348543910738917,6.656215049390844
+          |"(1, 5]","(-6, -5]","(0, 1]",2.566056334197673,0.18907986480158595,7.368500148719666""".stripMargin
+      val recordsRetriever = createRecordsIterableRetrieverFrom(vehicleEnergyString)
+      val energy = new VehicleEnergy(recordsRetriever)
+      val rate = energy.getRateUsing(TravelData(2, -6, 1), 1)
+      Math.abs(rate - 6.656215049390844) should be < floatAllowedDiscrepancy
     }
+  }
+
+  describe("A VehicleEnergy with the same speed and grade percent") {
+    it("should return the rate that fits the number of lanes") {
+      lazy val vehicleEnergyString =
+        """"(1, 5]","(-8, -6]","(3, 4]",30.17397314735461,2.423751926047644,8.032591247467646
+          |"(1, 5]","(-8, -6]","(4, 10]",44.557817527020944,3.683582791215134,8.266973105182542""".stripMargin
+      val recordsRetriever = createRecordsIterableRetrieverFrom(vehicleEnergyString)
+      val energy = new VehicleEnergy(recordsRetriever)
+      val rate = energy.getRateUsing(TravelData(2, -6, 4), 1)
+      Math.abs(rate - 8.032591247467646) should be < floatAllowedDiscrepancy
+    }
+  }
+
+  describe("A VehicleEnergy with the same grade percent and number of lanes") {
+    it("should return the rate that fits the speed") {
+      lazy val vehicleEnergyString =
+        """"(1, 5]","(-8, -6]","(3, 4]",30.17397314735461,2.423751926047644,8.032591247467646
+          |"(5, 10]","(-8, -6]","(3, 4]",44.557817527020944,3.683582791215134,8.266973105182542""".stripMargin
+      val recordsRetriever = createRecordsIterableRetrieverFrom(vehicleEnergyString)
+      val energy = new VehicleEnergy(recordsRetriever)
+      val rate = energy.getRateUsing(TravelData(2, -6, 4), 1)
+      Math.abs(rate - 8.032591247467646) should be < floatAllowedDiscrepancy
+    }
+  }
+
+  describe("A VehicleEnergy that doesn't match anything") {
+    it("should return the fallback rate") {
+      lazy val vehicleEnergyString =
+        """"(1, 5]","(-8, -6]","(3, 4]",30.17397314735461,2.423751926047644,8.032591247467646
+          |"(5, 10]","(-8, -6]","(3, 4]",44.557817527020944,3.683582791215134,8.266973105182542""".stripMargin
+      val recordsRetriever = createRecordsIterableRetrieverFrom(vehicleEnergyString)
+      val energy = new VehicleEnergy(recordsRetriever)
+      val rate = energy.getRateUsing(TravelData(12, -6, 4), 1.05f)
+      Math.abs(rate - 1.05) should be < floatAllowedDiscrepancy
+    }
+  }
+
+  private def createRecordsIterableRetrieverFrom(vehicleEnergyString: String) = { (csvParser: CsvParser) =>
+    csvParser.iterateRecords(new ByteArrayInputStream(vehicleEnergyString.getBytes)).asScala
   }
 }
