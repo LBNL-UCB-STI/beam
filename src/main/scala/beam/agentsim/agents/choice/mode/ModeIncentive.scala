633024067814cdbaf97db6f2c414562a875a92dd
Sid Feygin <sid.feygin@gmail.com>
2019-02-26 11:06:36 -0800

    More descriptive

--- src/main/scala/beam/agentsim/agents/choice/mode/ModeIncentive.scala
+++ src/main/scala/beam/agentsim/agents/choice/mode/ModeIncentive.scala
@@ -62,7 +62,7 @@ object ModeIncentive {
 
     def apply(mode: String, age: String, income: String, amount: String): Incentive = new Incentive(
       if (BeamMode.withValueOpt(mode).isEmpty) {
-        logger.error("ValueError: Mode %s is not a supported BEAM mode!".format(mode))
+        logger.error("Error in ModeIncentivesInput.csv: Mode %s is not a supported BEAM mode!".format(mode))
         System.exit(1)
         throw new RuntimeException()
       }
