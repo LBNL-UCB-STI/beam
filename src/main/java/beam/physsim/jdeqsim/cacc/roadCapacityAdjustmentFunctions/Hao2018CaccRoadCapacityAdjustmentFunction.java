81b41eedb7e43c578e10852ff8bd99cb628307d9
wrashid <waraich.rashid@gmail.com>
2019-02-27 16:16:20 -0600

    add road capacity adjustment function

---
+++ src/main/java/beam/physsim/jdeqsim/cacc/roadCapacityAdjustmentFunctions/Hao2018CaccRoadCapacityAdjustmentFunction.java
@@ -0,0 +1,22 @@
+package beam.physsim.jdeqsim.cacc.roadCapacityAdjustmentFunctions;
+
+import beam.physsim.jdeqsim.cacc.roadCapacityAdjustmentFunctions.RoadCapacityAdjustmentFunction;
+import org.matsim.api.core.v01.network.Link;
+
+
+/*
+
+CACC regression function derived from (Figure 8, Simulation):
+
+Liu, Hao, et al. "Modeling impacts of Cooperative Adaptive Cruise Control on mixed traffic flow
+in multi-lane freeway facilities." Transportation Research Part C: Emerging Technologies 95 (2018): 261-279.
+
+ */
+
+public class Hao2018CaccRoadCapacityAdjustmentFunction implements RoadCapacityAdjustmentFunction {
+
+    public double getCapacityWithCACC(Link link, double fractionCACCOnRoad){
+        double initialCapacity=link.getFlowCapacityPerSec();
+        return (2152.777778*fractionCACCOnRoad*fractionCACCOnRoad*fractionCACCOnRoad - 764.8809524*fractionCACCOnRoad*fractionCACCOnRoad + 456.1507937*fractionCACCOnRoad + 1949.047619)/1949.047619*initialCapacity;
+    }
+}
