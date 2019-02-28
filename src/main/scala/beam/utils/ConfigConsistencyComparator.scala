c37b63a1c7334c079d914045fd8d749041d42b9c
Carlos <ccaldas@gmail.com>
2019-02-28 17:07:08 +0000

    fixed suffix name

--- src/main/scala/beam/utils/ConfigConsistencyComparator.scala
+++ src/main/scala/beam/utils/ConfigConsistencyComparator.scala
@@ -116,7 +116,7 @@ object ConfigConsistencyComparator extends LazyLogging {
       .entrySet()
       .asScala
       .map(entry => (entry.getKey, resolve(entry.getKey, entry.getValue)))
-      .filter { case (key, value) => key.endsWith(".file") && !new File(value).isFile }
+      .filter { case (key, value) => key.toLowerCase.endsWith("filename") && !new File(value).isFile }
       .toSeq
   }
 
