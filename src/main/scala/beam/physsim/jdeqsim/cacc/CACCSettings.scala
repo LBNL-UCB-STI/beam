79aa4bbe2d607070d9f86862fbc40c47d04014e7
wrashid <waraich.rashid@gmail.com>
2019-02-28 10:44:46 -0600

    add cacc settings

---
+++ src/main/scala/beam/physsim/jdeqsim/cacc/CACCSettings.scala
@@ -0,0 +1,5 @@
+package beam.physsim.jdeqsim.cacc
+
+import beam.physsim.jdeqsim.cacc.roadCapacityAdjustmentFunctions.RoadCapacityAdjustmentFunction
+
+case class CACCSettings(isCACCVehicle: java.util.Map[String, java.lang.Boolean], roadCapacityAdjustmentFunction: RoadCapacityAdjustmentFunction)
