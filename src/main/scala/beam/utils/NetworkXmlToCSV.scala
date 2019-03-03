888599d83530c537edf29763293b95fdc30fa2b1
rajnikantsh <rk_sharmait@yahoo.com>
2019-03-03 13:45:55 +0530

    refactor/bug fix: beam/utils/NetworkXmlToCSV

--- src/main/scala/beam/utils/NetworkXmlToCSV.scala
+++ src/main/scala/beam/utils/NetworkXmlToCSV.scala
@@ -1,42 +1,75 @@
 package beam.utils
 
 import java.io.{File, PrintWriter}
+import scala.collection.mutable
 
 object NetworkXmlToCSV {
 
-  def main(args: Array[String]): Unit = {
-    xmlParser(
-      "/home/rajnikant/IdeaProjects/beam/test/input/beamville/physsim-network.xml",
-      "/home/rajnikant/IdeaProjects/beam/test/input/beamville/physsim-network.csv",
-      "\t"
-    )
-  }
+  def networkXmlParser(
+    path: String,
+    delimiter: String,
+    nodeOutput: String,
+    linkOutput: String,
+    mergeOutput: String
+  ): Unit = {
 
-  def xmlParser(path: String, output: String, delimiter: String): Unit = {
-    val nodeHeader = List("@id", "@x", "@y")
-    val linkAttribute =
-      List("@id", "@from", "@to", "@length", "@freespeed", "@capacity", "@permlanes", "@oneway", "@modes")
-    val linkHeader = linkAttribute
-      .map(_.replace("@", "link_"))
-      .mkString(delimiter) + delimiter + "@attributeOrigId" + delimiter + "@attributeOrigType"
-
-    val writer = new PrintWriter(new File(output))
     val physimElement = scala.xml.XML.loadFile(path)
 
-    writer.write(nodeHeader.map(_.replace("@", "node_")).mkString(delimiter) + "\n")
-    (physimElement \ "nodes").foreach { nodes =>
-      (nodes \ "node").foreach { node =>
-        val row = nodeHeader.map(node \ _).map(_.text).mkString(delimiter)
-        writer.write(row + "\n")
-      }
+    val nodeMap: mutable.Map[String, (_, _)] = mutable.Map()
+    val nodeWriter = new PrintWriter(new File(nodeOutput))
+    val nodeHeader = List("node_id", "node_x", "node_y")
+    nodeWriter.write(nodeHeader.mkString(delimiter) + "\n")
+    (physimElement \ "nodes" \ "node").foreach { node =>
+      val id = (node \ "@id").text
+      val x = (node \ "@x").text
+      val y = (node \ "@y").text
+      nodeMap += id -> (x, y)
+      val row = id + delimiter + x + delimiter + y
+      nodeWriter.write(row + "\n")
     }
-    writer.write(linkHeader + "\n")
-    (physimElement \ "links").foreach { links =>
-      (links \ "link").foreach { link =>
-        val row = linkAttribute.map(link \ _).map(_.text).mkString(delimiter) + delimiter +
-        (link \ "attributes").map(attributes => (attributes \ "attribute").map(_.text).mkString(delimiter)).mkString
-        writer.write(row + "\n")
-      }
+    nodeWriter.close()
+
+    val linkWriter = new PrintWriter(new File(linkOutput))
+    val linkAttribute =
+      List("@id", "@from", "@to", "@length", "@freespeed", "@capacity", "@permlanes", "@oneway", "@modes")
+
+    val linkHeader = linkAttribute
+      .map(_.replace("@", "link_"))
+      .mkString(delimiter) + delimiter + "attributeOrigId" + delimiter + "attributeOrigType"
+
+    linkWriter.write(linkHeader + "\n")
+    (physimElement \ "links" \ "link").foreach { link =>
+      val row = linkAttribute.map(link \ _).map(_.text) ++ (link \ "attributes" \ "attribute").map(_.text)
+      linkWriter.write(row.mkString(delimiter) + "\n")
     }
+    linkWriter.close()
+
+    val mergeWriter = new PrintWriter(new File(mergeOutput))
+    val mergeHeader = List("link_id", "from_node", "to_node", "from_x", "from_y", "to_x", "to_y")
+
+    mergeWriter.write(mergeHeader.mkString(delimiter) + "\n")
+    (physimElement \ "links" \ "link").foreach { link =>
+      val id = (link \ "@id").text
+      val from = (link \ "@from").text
+      val to = (link \ "@to").text
+      val fromCoord = nodeMap(from)
+      val toCoord = nodeMap(to)
+      val row = new StringBuffer(id)
+      row
+        .append(delimiter)
+        .append(from)
+        .append(delimiter)
+        .append(to)
+        .append(delimiter)
+        .append(fromCoord._1)
+        .append(delimiter)
+        .append(fromCoord._2)
+        .append(delimiter)
+        .append(toCoord._1)
+        .append(delimiter)
+        .append(toCoord._2)
+      mergeWriter.write(row.toString + "\n")
+    }
+    mergeWriter.close()
   }
 }
