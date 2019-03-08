24cd76c6847b340dc4d5af441e50fa6c78f592e7
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2019-03-08 12:06:13 -0800

    second test for MultinomialLogit

--- src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionParam.scala
+++ src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionParam.scala
@@ -4,7 +4,7 @@ package beam.agentsim.agents.choice.logit
   * One utility parameter of a [[UtilityFunction]] to evaluate the utility of an [[Alternative]]
   *
   * @param paramId the unique parameter (e.g. costs)
-  * @param paramType the type of the parameter as [[UtilityFunctionParamType]] (e.g. [[UtilityFunctionParamType.Intercept]])
+  * @param paramType the type of the parameter as [[UtilityFunctionParamType]] (e.g. [[UtilityFunctionParamType.Multiplier]])
   * @param paramValue the coefficient value
   * @tparam T
   */
--- src/test/scala/beam/agentsim/agents/choice/logit/MultinomialLogitSpec.scala
+++ src/test/scala/beam/agentsim/agents/choice/logit/MultinomialLogitSpec.scala
@@ -12,7 +12,7 @@ import scala.util.Random
   */
 class MultinomialLogitSpec extends WordSpecLike with Matchers with BeamHelper {
 
-  "An MNL Model" must {
+  "An MNL Model with standard data" must {
     val utilityFunctions = Vector(
       UtilityFunction[String, String](
         "car",
@@ -61,4 +61,72 @@ class MultinomialLogitSpec extends WordSpecLike with Matchers with BeamHelper {
       samps.count(_.alternativeId.equals("walk")) > 50 should be(true)
     }
   }
+
+  "An MNL Model with arbitrary data" must {
+
+    // the alternatives as objects
+    sealed trait Mode
+
+    object Car extends Mode
+    object Walk extends Mode
+
+    object Common extends Mode
+
+    // the function parameters as objects
+    sealed trait FunctionParam
+
+    object Cost extends FunctionParam
+    object Time extends FunctionParam
+
+    case object Intercept extends FunctionParam
+
+    val utilityFunctions = Vector(
+      UtilityFunction[Mode, FunctionParam](
+        Car,
+        Set(UtilityFunctionParam(Intercept, UtilityFunctionParamType("intercept"), 3.0))
+      ),
+      UtilityFunction[Mode, FunctionParam](
+        Walk,
+        Set(UtilityFunctionParam(Intercept, UtilityFunctionParamType("intercept"), 4.0))
+      )
+    )
+
+    val common = Some(
+      UtilityFunction[Mode, FunctionParam](
+        Common,
+        Set(
+          UtilityFunctionParam(Cost, UtilityFunctionParamType("multiplier"), -0.01),
+          UtilityFunctionParam(Time, UtilityFunctionParamType("multiplier"), -0.02)
+        )
+      )
+    )
+
+    val mnl = MultinomialLogit(
+      utilityFunctions,
+      common
+    )
+    val rand = new Random()
+    val alts = Vector(
+      Alternative[FunctionParam, Mode](Car, Map(Cost  -> 30.0, Time -> 50.0)),
+      Alternative[FunctionParam, Mode](Walk, Map(Cost -> 0.0, Time  -> 40.0))
+    )
+
+    "should evaluate utility functions as expected" in {
+      val util = mnl.getUtilityOfAlternative(alts(0))
+      ((util - 1.7).abs < 0.000000001) should be(true)
+    }
+    "should evaluate expected max utility as expected" in {
+      val util = mnl.getExpectedMaximumUtility(alts)
+      Math.abs(util - 3.401413) < 0.00001 should be(true)
+    }
+    "should sample higher probability alternatives more often" in {
+      // With these inputs, we expect "walk" ~81% of the time, which translates to an almost certainty that majority
+      // will be walk with 100 trials (p-val 3.00491e-12)
+
+      val samps = for (i <- 1 until 100) yield mnl.sampleAlternative(alts, rand).get
+
+      samps.count(_.alternativeId.equals(Walk)) > 50 should be(true)
+    }
+  }
+
 }
