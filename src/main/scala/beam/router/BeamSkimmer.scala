25bb4b501809be88c78a5321f9e23ea6a69718b5
Carlos <ccaldas@gmail.com>
2019-03-09 00:38:53 +0000

    transformed var to val. removed 1 warning

--- src/main/scala/beam/router/BeamSkimmer.scala
+++ src/main/scala/beam/router/BeamSkimmer.scala
@@ -34,7 +34,7 @@ class BeamSkimmer @Inject()() extends IterationEndsListener {
   // The OD/Mode/Time Matrix
   private var previousSkims: TrieMap[(Int, BeamMode, Id[TAZ], Id[TAZ]), SkimInternal] = TrieMap()
   private var skims: TrieMap[(Int, BeamMode, Id[TAZ], Id[TAZ]), SkimInternal] = TrieMap()
-  private var modalAverage: TrieMap[BeamMode, SkimInternal] = TrieMap()
+  private val modalAverage: TrieMap[BeamMode, SkimInternal] = TrieMap()
 
   def getTimeDistanceAndCost(
     origin: Location,
