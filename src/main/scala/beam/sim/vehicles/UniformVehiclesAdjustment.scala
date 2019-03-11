5546508e9a31a2ae8f97c838dec7d777a091aec9
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2019-03-11 11:43:48 +0500

    reverted uniform adjustment

--- src/main/scala/beam/sim/vehicles/UniformVehiclesAdjustment.scala
+++ src/main/scala/beam/sim/vehicles/UniformVehiclesAdjustment.scala
@@ -7,8 +7,6 @@ import beam.sim.BeamServices
 import org.matsim.api.core.v01.Coord
 import probability_monad.Distribution
 
-import scala.collection.mutable
-
 case class UniformVehiclesAdjustment(beamServices: BeamServices) extends VehiclesAdjustment {
   val randUnif = Distribution.uniform
   val probabilities = randUnif.sample(beamServices.vehicleTypes.size)
@@ -36,8 +34,7 @@ case class UniformVehiclesAdjustment(beamServices: BeamServices) extends Vehicle
     val result = Range(0, numVehicles).map { i =>
       val newRand = randUnif.get
       vehicleTypesAndProbabilitiesByCategory(vehicleCategory).find(_._2 >= newRand).get._1
-    }.toBuffer
-    result.append(BeamVehicleType.defaultBicycleBeamVehicleType)
-    result.toList
+    }.toList
+    result
   }
 }
