c3845ec9969e64edf8a0e9ab26f40d0eed873f38
Artavazd Balaian <reasyu@gmail.com>
2019-03-31 21:42:49 +0700

    Applied `scalafmt`

--- src/main/scala/beam/utils/LinkIdsToGpx.scala
+++ src/main/scala/beam/utils/LinkIdsToGpx.scala
@@ -6,8 +6,9 @@ import org.matsim.core.network.NetworkUtils
 import org.matsim.core.network.io.MatsimNetworkReader
 
 object LinkIdsToGpx {
+
   def main(args: Array[String]): Unit = {
-    assert (args.length == 3)
+    assert(args.length == 3)
     // Input for the tool is path to network and path to link ids file (comma separated link ids in one line)
     val pathToNetwork = args(0)
     val pathToLinkIds = args(1)
@@ -24,18 +25,17 @@ object LinkIdsToGpx {
       override def localCRS: String = "epsg:26910"
     }
 
-
     val linkId2WgsCoord = scala.io.Source.fromFile(pathToLinkIds).getLines().flatMap { linkIds =>
       linkIds.split(",").map { linkIdStr =>
         val link = links.get(Id.createLinkId(linkIdStr))
-        val loc  = link.asInstanceOf[BasicLocation[Link]]
+        val loc = link.asInstanceOf[BasicLocation[Link]]
         val utmCoord = loc.getCoord
         val wgsCoord = geoUtils.utm2Wgs.transform(utmCoord)
         linkIdStr -> wgsCoord
       }
     }
-    val gpxPoints = linkId2WgsCoord.map { case (linkId, wgsCoord) => GpxPoint(linkId, wgsCoord)}.toIterable
-    GpxWriter.write(pathToGpx,  gpxPoints)
+    val gpxPoints = linkId2WgsCoord.map { case (linkId, wgsCoord) => GpxPoint(linkId, wgsCoord) }.toIterable
+    GpxWriter.write(pathToGpx, gpxPoints)
     println(s"$pathToGpx is written.")
   }
 }
