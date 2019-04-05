c8b9aa92c7cec5b9a50ab734ac991b178605774e
Artyom Boyko <artyom.boyko@gmail.com>
2019-04-05 18:51:51 +0200

    refactor zipRootAndLastIterationAndUploadToS3.sh

--- src/main/bash/zipRootAndLastIterationAndUploadToS3.sh
+++ src/main/bash/zipRootAndLastIterationAndUploadToS3.sh
@@ -5,9 +5,11 @@ if [ "$#" -ne 2 ]; then
     exit 1;
 fi
 
-filename=$(basename $1)
+filename=$1
+path=$(find output -maxdepth 2 -type d -name $1 -print -quit)
 
 zip -r "$filename.zip" . -x "output/*"
-zip -ur "$filename.zip" $1/ITERS/it.$2
+zip -ur "$filename.zip" $path/ITERS/it.$2
 
 aws --region "us-east-2" s3 cp "$filename.zip" s3://beam-outputs/
+echo "S3 URL: https://s3.us-east-2.amazonaws.com/beam-outputs/$filename.zip";
