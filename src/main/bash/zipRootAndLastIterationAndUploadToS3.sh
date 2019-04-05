d7193d93e201c91de8fc8b9e1e723677549f7667
Artyom Boyko <artyom.boyko@gmail.com>
2019-04-05 19:02:51 +0200

    fix zipRootAndLastIterationAndUploadToS3.sh

--- src/main/bash/zipRootAndLastIterationAndUploadToS3.sh
+++ src/main/bash/zipRootAndLastIterationAndUploadToS3.sh
@@ -6,10 +6,9 @@ if [ "$#" -ne 2 ]; then
 fi
 
 filename=$1
-path=$(find output -maxdepth 2 -type d -name $1 -print -quit)
 
-zip -r "$filename.zip" . -x "output/*"
-zip -ur "$filename.zip" $path/ITERS/it.$2
+zip -r "$filename.zip" $filename -x "$filename/ITERS/*"
+zip -ur "$filename.zip" $filename/ITERS/it.$2
 
 aws --region "us-east-2" s3 cp "$filename.zip" s3://beam-outputs/
 echo "S3 URL: https://s3.us-east-2.amazonaws.com/beam-outputs/$filename.zip";
