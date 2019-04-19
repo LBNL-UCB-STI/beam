4cd11d34c7a8333b11ad8274e5ad5c4f9ccdc541
Sid Feygin <sid.feygin@gmail.com>
2019-04-18 23:23:34 -0700

    fix build

--- src/main/java/beam/analysis/summary/VehicleMilesTraveledAnalysis.java
+++ src/main/java/beam/analysis/summary/VehicleMilesTraveledAnalysis.java
@@ -26,29 +26,13 @@ public class VehicleMilesTraveledAnalysis implements IterationSummaryAnalysis {
             String vehicleType = pte.vehicleType();
             double lengthInMeters = pte.legLength();
 
-            milesTraveledByVehicleType.merge(vehicleType, lengthInMeters, Double::sum);
-            if (isMotorizedMode(vehicleType)) {
-                milesTraveledByVehicleType.merge("total", lengthInMeters, Double::sum);
+            milesTraveledByVehicleType.merge(vehicleType, lengthInMeters, (d1, d2) -> d1 + d2);
+            if (!vehicleType.equalsIgnoreCase(BeamVehicleType.defaultHumanBodyBeamVehicleType().toString())) {
+                milesTraveledByVehicleType.merge("total", lengthInMeters, (d1, d2) -> d1 + d2);
             }
 
         }
     }
-
-    private boolean isMotorizedMode(String vehicleType){
-        return !vehicleType.equalsIgnoreCase(BeamVehicleType.defaultHumanBodyBeamVehicleType().id().toString()) && !vehicleType.equalsIgnoreCase(BeamVehicleType.defaultBikeBeamVehicleType().id().toString());
-    }
-
-    private String getTitle(String key){
-        String prefix;
-        if (key.contains("total")){
-            prefix= "motorizedVehicleMilesTraveled_";
-        } else {
-            prefix= "vehicleMilesTraveled_";
-        }
-
-        return prefix + key;
-    }
-
     @Override
     public void resetStats() {
         milesTraveledByVehicleType.clear();
@@ -57,11 +41,11 @@ public class VehicleMilesTraveledAnalysis implements IterationSummaryAnalysis {
     @Override
     public Map<String, Double> getSummaryStats() {
         Map<String, Double> result = milesTraveledByVehicleType.entrySet().stream().collect(Collectors.toMap(
-                e -> getTitle(e.getKey()),
+                e -> "motorizedVehicleMilesTraveled_" + e.getKey(),
                 e -> e.getValue() * 0.000621371192 // unit conversion from meters to miles
         ));
 
-        vehicleTypes.foreach(vt -> result.merge("vehicleMilesTraveled_" + vt.toString(),0D, Double::sum));
+        vehicleTypes.foreach(vt -> result.merge("vehicleMilesTraveled_" + vt.toString(),0D, (d1, d2) -> d1 + d2));
 
         return result;
     }
