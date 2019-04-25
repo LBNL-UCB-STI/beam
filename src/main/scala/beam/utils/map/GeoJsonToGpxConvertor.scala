2d78ecc5d316b8564e7085701ea2f6677702902f
Artavazd Balaian <reasyu@gmail.com>
2019-04-25 20:40:58 +0700

    Tools to visualize TAZs

--- src/main/scala/beam/utils/map/GeoJsonToGpxConvertor.scala
+++ src/main/scala/beam/utils/map/GeoJsonToGpxConvertor.scala
@@ -2,29 +2,41 @@ package beam.utils.map
 
 import beam.utils.GeoJsonReader
 import com.typesafe.scalalogging.LazyLogging
-import com.vividsolutions.jts.geom.Geometry
+import com.vividsolutions.jts.geom.{Coordinate, Envelope, Geometry}
 import org.matsim.api.core.v01.Coord
 import org.opengis.feature.Feature
 import org.opengis.feature.simple.SimpleFeature
 
 object GeoJsonToGpxConvertor extends LazyLogging {
+  def renderEnvelope(points: Array[GpxPoint], outputPath: String): Unit = {
+    val envelope = new Envelope(new Coordinate(points.head.wgsCoord.getX, points.head.wgsCoord.getY))
+    points.foreach { p =>
+      envelope.expandToInclude(p.wgsCoord.getX, p.wgsCoord.getY)
+    }
+    new EnvelopeToGpx().render(envelope, None, outputPath)
+  }
 
-  def convert(inputPath: String, outputPath: String): Unit = {
-    val start = System.currentTimeMillis()
-    val mapper: (Feature => GpxPoint) = (feature: Feature) => {
+  def readGeoJson(inputPath: String): Array[GpxPoint] = {
+    val mapper: Feature => GpxPoint = (feature: Feature) => {
       val movementId = feature.getProperty("MOVEMENT_ID").getValue.toString
       val centroid = feature.asInstanceOf[SimpleFeature].getDefaultGeometry.asInstanceOf[Geometry].getCentroid
       val wgsCoord = new Coord(centroid.getX, centroid.getY)
       GpxPoint(movementId, wgsCoord)
     }
-    val points = GeoJsonReader.read(inputPath, mapper)
-    GpxWriter.write(outputPath, points)
-    val end = System.currentTimeMillis()
-    logger.info(s"Converted '$inputPath' to '$outputPath' in ${end - start} ms")
+    GeoJsonReader.read(inputPath, mapper)
   }
 
   def main(args: Array[String]): Unit = {
-    convert("""C:\temp\movement_data\san_francisco_censustracts.json""", "san_francisco_censustracts.gpx")
-    convert("""C:\temp\movement_data\san_francisco_taz.json""", "san_francisco_taz.gpx")
+    {
+      val censusTractsPoints = readGeoJson("""C:\temp\movement_data\san_francisco_censustracts.json""")
+      GpxWriter.write("san_francisco_censustracts.gpx", censusTractsPoints)
+      renderEnvelope(censusTractsPoints, "san_francisco_censustracts_envelope.gpx")
+    }
+
+    {
+      val tazPoints = readGeoJson("""C:\temp\movement_data\san_francisco_taz.json""")
+      GpxWriter.write("san_francisco_taz.gpx", tazPoints)
+      renderEnvelope(tazPoints, "san_francisco_taz_envelope.gpx")
+    }
   }
 }
---
+++ src/main/scala/beam/utils/map/TazTreeMapToGpx.scala
@@ -0,0 +1,28 @@
+package beam.utils.map
+
+import beam.agentsim.infrastructure.TAZTreeMap
+import beam.sim.BeamServices
+import beam.sim.config.BeamConfig
+import beam.utils.map.R5NetworkPlayground.prepareConfig
+import com.google.common.io.Files
+import com.typesafe.scalalogging.LazyLogging
+
+object TazTreeMapToGpx extends LazyLogging {
+  def main(args: Array[String]): Unit = {
+    val (_, cfg) = prepareConfig(args, true)
+    val beamConfig = BeamConfig(cfg)
+    val geoUtils = new beam.sim.common.GeoUtils {
+      override def localCRS: String = "epsg:26910"
+    }
+    val tazTreeMap: TAZTreeMap = BeamServices.getTazTreeMap(beamConfig.beam.agentsim.taz.filePath)
+
+    val tazPoints = tazTreeMap.getTAZs.map { taz =>
+      GpxPoint(taz.tazId.toString, geoUtils.utm2Wgs(taz.coord))
+    }.toArray
+
+    val fileName = Files.getNameWithoutExtension(beamConfig.beam.agentsim.taz.filePath)
+    GpxWriter.write(fileName + ".gpx", tazPoints)
+
+    GeoJsonToGpxConvertor.renderEnvelope(tazPoints, fileName + "_envelope.gpx")
+  }
+}
