dbb604158bf7f0b3e1d7007af892f18ad312f8a2
Artavazd Balaian <reasyu@gmail.com>
2019-04-30 00:09:15 +0700

    - Added `src/main/resources` to resources - Added `logback_prod.xml` for PROD - Set `logback.xml` in `RunBeam` if it is not set - Added `log-path_IS_UNDEFINED` to gitignore

--- src/main/scala/beam/sim/RunBeam.scala
+++ src/main/scala/beam/sim/RunBeam.scala
@@ -1,6 +1,11 @@
 package beam.sim
 
+import ch.qos.logback.classic.util.ContextInitializer
+
 object RunBeam extends BeamHelper {
+  val isLogbackConfigSet = Option(System.getProperty(ContextInitializer.CONFIG_FILE_PROPERTY)).isDefined
+  if (!isLogbackConfigSet)
+    System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "logback.xml")
 
   def main(args: Array[String]): Unit = {
 
