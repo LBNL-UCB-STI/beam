d459fbb37dbbad06479a5b961009cf94e88895b8
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2019-05-02 18:42:05 +0200

    test refactoring / renaming

--- src/test/scala/beam/agentsim/agents/choice/logit/MultinomialLogitSpec.scala
+++ src/test/scala/beam/agentsim/agents/choice/logit/MultinomialLogitSpec.scala
@@ -1,65 +1,57 @@
 package beam.agentsim.agents.choice.logit
 
-import beam.sim.BeamHelper
+import scala.util.Random
 import org.scalatest.{Matchers, WordSpecLike}
 
+class MultinomialLogitSpec extends WordSpecLike with Matchers {
 
-// todo test with objects / types && token --> arbitrary
+  "An MNL Model with standard data" must {
+    val utilityFunctions = Map(
+      "car"  -> Map("intercept" -> UtilityFunctionOperation.Intercept(3.0)),
+      "walk" -> Map("intercept" -> UtilityFunctionOperation.Intercept(4.0))
+    )
 
-/**
-  * BEAM
-  */
-class MultinomialLogitSpec extends WordSpecLike with Matchers with BeamHelper {
-//
-//  "An MNL Model with standard data" must {
-//    val utilityFunctions = Vector(
-//      UtilityFunction[String, String](
-//        "car",
-//        Set(UtilityFunctionParam("intercept", UtilityFunctionParamType("intercept"), 3.0))
-//      ),
-//      UtilityFunction[String, String](
-//        "walk",
-//        Set(UtilityFunctionParam("intercept", UtilityFunctionParamType("intercept"), 4.0))
-//      )
-//    )
-//
-//    val common = Some(
-//      UtilityFunction[String, String](
-//        "COMMON",
-//        Set(
-//          UtilityFunctionParam("cost", UtilityFunctionParamType("multiplier"), -0.01),
-//          UtilityFunctionParam("time", UtilityFunctionParamType("multiplier"), -0.02)
-//        )
-//      )
-//    )
-//
-//    val mnl = MultinomialLogit(
-//      utilityFunctions,
-//      common
-//    )
-//    val rand = new Random()
-//    val alts = Vector(
-//      Alternative("car", Map("cost"  -> 30.0, "time" -> 50.0)),
-//      Alternative("walk", Map("cost" -> 0.0, "time"  -> 40.0))
-//    )
-//
-//    "should evaluate utility functions as expected" in {
-//      val util = mnl.getUtilityOfAlternative(alts(0))
-//      ((util - 1.7).abs < 0.000000001) should be(true)
-//    }
-//    "should evaluate expected max utility as expected" in {
-//      val util = mnl.getExpectedMaximumUtility(alts)
-//      Math.abs(util - 3.401413) < 0.00001 should be(true)
-//    }
-//    "should sample higher probability alternatives more often" in {
-//      // With these inputs, we expect "walk" ~81% of the time, which translates to an almost certainty that majority
-//      // will be walk with 100 trials (p-val 3.00491e-12)
-//
-//      val samps = for (i <- 1 until 100) yield mnl.sampleAlternative(alts, rand).get
-//
-//      samps.count(_.alternativeId.equals("walk")) > 50 should be(true)
-//    }
-//  }
+    val common = Map(
+      "cost" -> UtilityFunctionOperation.Multiplier(-0.01),
+      "time" -> UtilityFunctionOperation.Multiplier(-0.02)
+    )
+
+    val mnl = new MultinomialLogit(utilityFunctions, common)
+
+    val alts = Map(
+      "car"  -> Map("cost" -> 30.0, "time" -> 50.0),
+      "walk" -> Map("cost" -> 0.0, "time"  -> 40.0)
+    )
+
+    "should evaluate utility functions as expected" in {
+      mnl.getUtilityOfAlternative(alts.head._1, alts.head._2) match {
+        case None => fail()
+        case Some(util) =>
+          (util - 1.7).abs should be < 0.000000001
+      }
+
+    }
+    "should evaluate expected max utility as expected" in {
+      val util = mnl.getExpectedMaximumUtility(alts)
+      Math.abs(util.get - 3.401413) < 0.00001 should be(true)
+    }
+
+    "should sample higher probability alternatives more often" in {
+      // With these inputs, we expect "walk" ~81% of the time, which translates to an almost certainty that majority
+      // will be walk with 100 trials (p-val 3.00491e-12)
+
+      val sampleSize = 100
+      val rand = new Random()
+
+      val samples: Seq[String] = for {
+        _ <- 1 until sampleSize
+        result <- mnl.sampleAlternative(alts, rand)
+      } yield result.alternativeType
+
+      samples.count{_ == "walk"} > (sampleSize / 2) // 50% or more should be walk
+
+    }
+
 //
 //  "An MNL Model with arbitrary data" must {
 //
@@ -126,6 +118,5 @@ class MultinomialLogitSpec extends WordSpecLike with Matchers with BeamHelper {
 //
 //      samps.count(_.alternativeId.equals(Walk)) > 50 should be(true)
 //    }
-//  }
-
+  }
 }
