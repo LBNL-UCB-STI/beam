a5babb53f1946121c5f69511ea71f126e258818a
rajnikantsh <rk_sharmait@yahoo.com>
2019-05-04 05:22:45 +0530

    Generate vehicle count csv on level

--- src/main/scala/beam/utils/OutlinkLevelCalculator.scala
+++ src/main/scala/beam/utils/OutlinkLevelCalculator.scala
@@ -1,7 +1,7 @@
 package beam.utils
 import java.io.{File, PrintWriter}
-
 import java.util
+
 import org.matsim.api.core.v01.network.Link
 import org.matsim.core.network.NetworkUtils
 import org.matsim.core.network.io.NetworkReaderMatsimV2
@@ -15,10 +15,10 @@ object OutlinkLevelCalculator {
   def main(args: Array[String]): Unit = {
     val pathToEventXml = args(0)
     val pathToNetworkXml = args(1)
-    val pathToOutputFile = args(2)
-    val level = args(4).toInt
+    val level = args(2).toInt
+    val outputFile = args(3)
     val delimiter = args(4)
-    eventXmlParser(pathToEventXml, pathToNetworkXml, level, pathToOutputFile, delimiter)
+    eventXmlParser(pathToEventXml, pathToNetworkXml, level, outputFile, delimiter)
   }
 
   def initializeNetworkLinks(networkXml: String): util.Map[Id[Link], _ <: Link] = {
@@ -28,56 +28,81 @@ object OutlinkLevelCalculator {
     network.getLinks
   }
 
-  def eventXmlParser(eventXml: String, networkXml: String, level: Int, outputPath: String, delimeter: String): Unit = {
+  def eventXmlParser(eventXml: String, networkXml: String, level: Int, outputPath: String, delimiter: String): Unit = {
 
     val networkLinks = initializeNetworkLinks(networkXml)
 
     val physsimEventElement = scala.xml.XML.loadFile(eventXml)
-    val linkVehicleCount: mutable.Map[String, Int] = mutable.Map[String, Int]()
+    val linkVehicleCount: mutable.Map[Int, Int] = mutable.Map[Int, Int]()
     (physsimEventElement \ "event").foreach { event =>
-      val link = (event \ "@link").text
+      val link = (event \ "@link").text.toInt
       (event \ "@type").text match {
-        case "departure" => linkVehicleCount.put(link, linkVehicleCount.getOrElse(link, 0) + 1)
-        case "arrival"   => linkVehicleCount.put(link, linkVehicleCount.getOrElse(link, 0) - 1)
-        case _           =>
+        case "entered link" => linkVehicleCount.put(link, linkVehicleCount.getOrElse(link, 0) + 1)
+        case "wait2link"    => linkVehicleCount.put(link, linkVehicleCount.getOrElse(link, 0) + 1)
+        case "left link"    => linkVehicleCount.put(link, linkVehicleCount.getOrElse(link, 0) - 1)
+
+        case _ =>
       }
     }
 
     val nodeWriter = new PrintWriter(new File(outputPath))
-    nodeWriter.write("linkId" + delimeter + "travelTimeDuration" + delimeter + "vehOnRoad" + delimeter + "length\n")
-    linkVehicleCount foreach {
-      case (link, count) =>
-        val linkDetails = networkLinks.get(Id.create(link, classOf[Link]))
+
+    val rowOutLink = linkVehicleCount map { linkCount =>
+      val link = linkCount._1
+      val count = linkCount._2
+      val linkDetails = networkLinks.get(Id.create(link, classOf[Link]))
+      val row = new StringBuffer()
+        .append(link)
+        .append(delimiter)
+        .append(linkDetails.getLength / linkDetails.getFreespeed)
+        .append(delimiter)
+        .append(count)
+        .append(delimiter)
+        .append(linkDetails.getLength)
+
+      (row.toString, getOutlink(linkDetails, level, Set()))
+    }
+
+    val maxCount = rowOutLink.values.map(_.size).max
+    val headerItem = new StringBuffer()
+      .append("linkId")
+      .append(delimiter)
+      .append("travelTimeDuration")
+      .append(delimiter)
+      .append("vehOnRoad")
+      .append(delimiter)
+      .append("length")
+
+    for (i <- 1 to maxCount) {
+      headerItem.append(delimiter).append("outLink" + i + "_vehOnRoad")
+    }
+
+    headerItem.append("\n")
+    nodeWriter.write(headerItem.toString)
+
+    rowOutLink foreach {
+      case (first, second) =>
+        val requiredDelimeter = maxCount - second.size
         val row = new StringBuffer()
-          .append(link)
-          .append(delimeter)
-          .append(linkDetails.getLength / linkDetails.getFreespeed)
-          .append(delimeter)
-          .append(count)
-          .append(delimeter)
-          .append(linkDetails.getLength)
-          .append("\n")
-
-        val outLink = getOutlink(linkDetails, level, Set())
-
-        println(
-          linkDetails.getId + "-->" + linkVehicleCount(linkDetails.getId.toString) + " -> " + outLink
-            .map(id => (id, linkVehicleCount.getOrElse(id.toString, 0)))
-            .toMap
-        )
-
+        row.append(first)
+        second.foreach(rowItem => row.append(delimiter).append(linkVehicleCount.getOrElse(rowItem, 0)))
+        for (i <- 0 until requiredDelimeter) {
+          row.append(delimiter)
+        }
+        row.append("\n")
         nodeWriter.write(row.toString)
     }
+
     nodeWriter.close()
   }
 
-  def getOutlink(link: Link, level: Int, list: Set[String]): Set[String] = {
+  def getOutlink(link: Link, level: Int, list: Set[Int]): Set[Int] = {
     level match {
       case 0 =>
         list
       case _ =>
         val outLink = link.getToNode.getOutLinks.asScala
-        val out = list ++ outLink.keys.map(_.toString)
+        val out = list ++ outLink.keys.map(_.toString.toInt)
         outLink.values.flatMap(getOutlink(_, level - 1, out)).toSet
     }
   }
