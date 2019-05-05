3b92011cba3308d646711f6c35f97eaf01931b15
Artavazd Balaian <reasyu@gmail.com>
2019-05-05 14:47:52 +0700

    Fixed PR review

--- src/main/scala/beam/sim/RunBeam.scala
+++ src/main/scala/beam/sim/RunBeam.scala
@@ -3,8 +3,8 @@ package beam.sim
 import ch.qos.logback.classic.util.ContextInitializer
 
 object RunBeam extends BeamHelper {
-  val isLogbackConfigSet = Option(System.getProperty(ContextInitializer.CONFIG_FILE_PROPERTY)).isDefined
-  if (!isLogbackConfigSet)
+  val logbackConfigFile = Option(System.getProperty(ContextInitializer.CONFIG_FILE_PROPERTY))
+  if (logbackConfigFile.isEmpty)
     System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "logback.xml")
 
   def main(args: Array[String]): Unit = {
