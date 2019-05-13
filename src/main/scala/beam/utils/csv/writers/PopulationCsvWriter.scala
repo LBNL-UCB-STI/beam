5733b462dca7d6411a3bd8b41508cac7fd5375cb
Carlos Caldas <ccaldas@gmail.com>
2019-05-13 21:32:54 +0100

    Fixed householdId from population

--- src/main/scala/beam/utils/csv/writers/PopulationCsvWriter.scala
+++ src/main/scala/beam/utils/csv/writers/PopulationCsvWriter.scala
@@ -1,7 +1,9 @@
 package beam.utils.csv.writers
 
 import beam.sim.population.AttributesOfIndividual
-import org.matsim.api.core.v01.Scenario
+import org.matsim.api.core.v01.population.Person
+import org.matsim.api.core.v01.{Id, Scenario}
+import org.matsim.households.Household
 import org.matsim.utils.objectattributes.ObjectAttributes
 
 import scala.collection.JavaConverters._
@@ -12,18 +14,28 @@ object PopulationCsvWriter extends ScenarioCsvWriter {
     Seq("personId", "age", "isFemale", "householdId", "houseHoldRank", "excludedModes")
 
   override def contentIterator(scenario: Scenario): Iterator[String] = {
-    val personAttrib: ObjectAttributes = scenario.getPopulation.getPersonAttributes
+    val personIdToHouseHoldId: Map[Id[Person], Id[Household]] = scenario.getHouseholds.getHouseholds.values().asScala
+      .flatMap{h =>
+        h.getMemberIds.asScala.map(idPerson => idPerson -> h.getId)
+      }.toMap
+
+    val personAttributes: ObjectAttributes = scenario.getPopulation.getPersonAttributes
 
     scenario.getPopulation.getPersons.values().asScala.toIterator.map { person =>
       val customAttributes: AttributesOfIndividual =
         person.getCustomAttributes.get("beam-attributes").asInstanceOf[AttributesOfIndividual]
+
+      val excludedModes = personAttributes.getAttribute(person.getId.toString, "excluded-modes").toString
+        .replaceAll(",", ArrayItemSeparator)
+        .split(ArrayItemSeparator)
+        .mkString(ArrayStartString, ArrayItemSeparator, ArrayEndString)
+
       val values = Seq(
         person.getId.toString,
         customAttributes.age.getOrElse(""),
         !customAttributes.isMale,
-        customAttributes.householdAttributes.householdId,
-        personAttrib.getAttribute(person.getId.toString, "rank"),
-        personAttrib.getAttribute(person.getId.toString, "excluded-modes")
+        personIdToHouseHoldId(person.getId),
+        excludedModes
       )
       values.mkString("", FieldSeparator, LineSeparator)
     }
