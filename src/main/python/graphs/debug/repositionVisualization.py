1c0f752b41d44f9b7fa85122be686ec52b8447fe
rajnikantsh <rk_sharmait@yahoo.com>
2019-05-16 23:34:23 +0530

    draw only 100 images

--- src/main/python/graphs/debug/repositionVisualization.py
+++ src/main/python/graphs/debug/repositionVisualization.py
@@ -8,6 +8,7 @@ def inter(s):
 
 def generateGraph(path, quad_output_file, repositioning_file):
 
+    max_draw_fig_count = 100
     output_path = path+'/tmp/'
     os.mkdir(output_path)
     plt.figure(figsize=(20,12))
@@ -39,8 +40,13 @@ def generateGraph(path, quad_output_file, repositioning_file):
         existing_values.append((float(row[1]),float(row[2]),float(row[3]),float(row[4])))
         coord_output[row[0]] = existing_values
 
-
-    for time,points in coord_output.items():
+    fig_drawn = 0
+    for count,item in enumerate(coord_output.items()):
+        time = item[0]
+        points = item[1]
+        fig_drawn = count
+        if fig_drawn >= max_draw_fig_count:
+            break
         x1 = []
         y1 = []
         x2 = []
@@ -67,8 +73,11 @@ def generateGraph(path, quad_output_file, repositioning_file):
         plt.savefig(output_path+inter('file_{time}.png'))
         plt.cla()
 
-
-    for time,points in quad_data.items():
+    for count,item in enumerate(quad_data.items(), fig_drawn):
+        if count >= max_draw_fig_count:
+            break
+        time = item[0]
+        points = item[1]
         x1 = []
         y1 = []
         for point in points:
