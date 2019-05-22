8a1759069c67c00a37190738c45e3989b9063bb3
Artavazd Balaian <reasyu@gmail.com>
2019-05-22 23:55:20 +0700

    Segmenting activities

---
+++ src/main/scala/beam/utils/ActivitySegment.scala
@@ -0,0 +1,67 @@
+package beam.utils
+
+import beam.router.BeamRouter.Location
+import com.typesafe.scalalogging.LazyLogging
+import org.matsim.api.core.v01.{Coord, Scenario}
+import org.matsim.api.core.v01.population.Activity
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConverters._
+
+class ActivitySegment(val scenario: Scenario, val binSize: Int) extends LazyLogging {
+  import ActivitySegment._
+  private val activities = scenario.getPopulation.getPersons.values.asScala.flatMap { person =>
+    person.getSelectedPlan.getPlanElements.asScala.collect {
+      case act: Activity if act.getEndTime != Double.NegativeInfinity=>
+        act
+    }
+  }.toArray.sortBy(x => x.getEndTime)
+
+  private val emptyArr: Array[Coord] = Array.empty
+
+  private val maxIdx: Int = activities.last.getEndTime.toInt / binSize
+  private val arr: Array[Array[Coord]] = build(activities, binSize)
+
+  val vector = arr.filter(x => x != null).map(x => x.toVector).toVector
+  println(vector)
+
+  def getCoords(time: Double): IndexedSeq[Coord] = {
+    val idx = time.toInt / binSize
+    if (idx > maxIdx) {
+      logger.warn(s"Cant find bucket at time: $time, idx: $idx, maxIdx: $maxIdx")
+      emptyArr
+    }
+    else {
+      val r: Array[Location] = Option(arr(idx)).getOrElse(emptyArr)
+      r
+    }
+  }
+}
+object ActivitySegment {
+  def build(sorted: Array[Activity], binSize: Int): Array[Array[Coord]] = {
+    val minTime = sorted.head.getEndTime
+    val maxTime = sorted.last.getEndTime
+    val maxIdx: Int = maxTime.toInt / binSize
+    val arr: Array[Array[Coord]] = Array.ofDim(maxIdx + 1)
+
+    val buf = new ArrayBuffer[Coord]
+    var i: Int = 0
+    var j: Int = minTime.toInt / binSize
+    var s = minTime
+    while (i < sorted.length) {
+      val act = sorted(i)
+      val time = act.getEndTime
+      if (time - s > binSize) {
+        arr.update(j, buf.toArray)
+        buf.clear()
+        j += 1
+        s = time
+      }
+      else {
+        buf += act.getCoord
+      }
+      i += 1
+    }
+    arr
+  }
+}
