36e2d02be0d243bac0659f625b3ac600797e404b
Carlos Caldas <ccaldas@gmail.com>
2019-06-03 17:29:20 +0100

    added column valueOfTime

--- src/main/scala/beam/utils/csv/writers/PopulationCsvWriter.scala
+++ src/main/scala/beam/utils/csv/writers/PopulationCsvWriter.scala
@@ -11,7 +11,7 @@ import scala.collection.JavaConverters._
 object PopulationCsvWriter extends ScenarioCsvWriter {
 
   override protected val fields: Seq[String] =
-    Seq("personId", "age", "isFemale", "householdId", "householdRank", "excludedModes")
+    Seq("personId", "age", "isFemale", "householdId", "householdRank", "excludedModes", "valueOfTime")
 
   override def contentIterator(scenario: Scenario): Iterator[String] = {
     val personIdToHouseHoldId: Map[Id[Person], Id[Household]] = scenario.getHouseholds.getHouseholds
@@ -46,7 +46,8 @@ object PopulationCsvWriter extends ScenarioCsvWriter {
         !customAttributes.isMale,
         personIdToHouseHoldId.get(person.getId).map(_.toString).getOrElse(""),
         String.valueOf(personAttributes.getAttribute(person.getId.toString, "rank")),
-        excludedModes
+        excludedModes,
+        Option(personAttributes.getAttribute(person.getId.toString, "valueOfTime")).getOrElse("")
       )
       values.mkString("", FieldSeparator, LineSeparator)
     }
