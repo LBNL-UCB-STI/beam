18f1015c94f3965154c3e9106a78ee347a6ee324
Carlos Caldas <ccaldas@gmail.com>
2019-06-16 20:19:37 +0100

    undo superfluous change

--- src/main/scala/beam/sim/population/PopulationAttributes.scala
+++ src/main/scala/beam/sim/population/PopulationAttributes.scala
@@ -25,7 +25,7 @@ case class AttributesOfIndividual(
   availableModes: Seq[BeamMode],
   valueOfTime: Double,
   age: Option[Int],
-  income: Option[Double],
+  income: Option[Double]
 ) extends PopulationAttributes {
   lazy val hasModalityStyle: Boolean = modalityStyle.nonEmpty
 
