eeccbf403a3927b6e3496ed7714d65bcd7147e21
Justin Pihony <justin.pihony@gmail.com>
2019-06-17 04:08:13 -0400

    Fix test

--- src/test/scala/beam/agentsim/infrastructure/ZonalParkingManagerSpec.scala
+++ src/test/scala/beam/agentsim/infrastructure/ZonalParkingManagerSpec.scala
@@ -14,6 +14,7 @@ import beam.agentsim.infrastructure.taz.{TAZ, TAZTreeMap}
 import beam.sim.common.{GeoUtils, GeoUtilsImpl}
 import beam.sim.config.BeamConfig
 import beam.utils.TestConfigUtils.testConfig
+import beam.agentsim.agents.choice.logit._
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 import com.vividsolutions.jts.geom.Envelope
@@ -74,8 +75,18 @@ class ZonalParkingManagerSpec
           new Random(randomSeed)
         )
       } {
+        val beta1 = 1
+        val beta2 = 1
+        val beta3 = 0.001
+        val commonUtilityParams: Map[String, UtilityFunctionOperation] = Map(
+          "energyPriceFactor" -> UtilityFunctionOperation("multiplier", -beta1),
+          "distanceFactor"    -> UtilityFunctionOperation("multiplier", -beta2),
+          "installedCapacity" -> UtilityFunctionOperation("multiplier", -beta3)
+        )
+        import beam.agentsim.infrastructure.parking.ParkingZoneSearch
+        val mnl = new MultinomialLogit[ParkingZoneSearch.ParkingAlternative, String](Map.empty, commonUtilityParams)
 
-        val inquiry = ParkingInquiry(coordCenterOfUTM, "work", 0.0, None, 0.0)
+        val inquiry = ParkingInquiry(coordCenterOfUTM, "work", 0.0, mnl, 0.0)
         val expectedStall: ParkingStall = ParkingStall.lastResortStall(boundingBox, new Random(randomSeed))
 
         zonalParkingManager ! inquiry
