5b3102ccaed560c4031710eea95bca13aa6ef180
Nikolay Ilin <irishwithaxe@gmail.com>
2019-06-24 22:10:03 +0300

    junk file removed

--- src/main/scala/beam/utils/beamToVia/ViaLeftLinkEvent.scala
+++
@@ -1,9 +0,0 @@
-package beam.utils.beamToVia
-
-case class ViaLeftLinkEvent42(time: Double,
-                            vehicle: String,
-                            link: Int) extends ViaEvent {
-
-  def toXml: scala.xml.Elem =
-      <event time={time.toString} type="left link" vehicle={vehicle} link={link.toString} />
-}
