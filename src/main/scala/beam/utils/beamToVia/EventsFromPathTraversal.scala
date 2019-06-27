ffab5d0e5e74aafe34cd9415a4e0a1d5f2a8f39c
Nikolay Ilin <irishwithaxe@gmail.com>
2019-06-27 20:12:57 +0300

    added sampling for vehicle types

--- src/main/scala/beam/utils/beamToVia/EventsFromPathTraversal.scala
+++ src/main/scala/beam/utils/beamToVia/EventsFromPathTraversal.scala
@@ -6,6 +6,16 @@ object EventsFromPathTraversal extends App {
   //val sourcePath = "D:/Work/BEAM/Via-beamville/0.events.xml"
   val sourcePath = "D:/Work/BEAM/Via-fs-light/2.events.csv"
 
-  val config = RunConfig.withoutFiltering(sourcePath)
+  val config = RunConfig.filterVehicles(
+    sourcePath,
+    Seq(
+      VehicleSample("BUS-DEFAULT", 0.01),
+      VehicleSample("BODY-TYPE-DEFAULT", 0.1),
+      VehicleSample("CABLE_CAR-DEFAULT", 0.1),
+      VehicleSample("TRAM-DEFAULT", 0.1),
+      VehicleSample("Car", 0.1),
+      VehicleSample("SUBWAY-DEFAULT", 0.1)
+    )
+  )
   TransformAllPathTraversal.transformAndWrite(config)
 }
--- src/main/scala/beam/utils/beamToVia/TransformAllPathTraversal.scala
+++ src/main/scala/beam/utils/beamToVia/TransformAllPathTraversal.scala
@@ -1,6 +1,8 @@
 package beam.utils.beamToVia
 
-import beam.utils.beamToVia.beamEvent.BeamEvent
+import beam.utils.beamToVia.beamEvent.{BeamEvent, BeamPathTraversal}
+
+import scala.collection.mutable
 
 object TransformAllPathTraversal {
 
@@ -9,8 +11,47 @@ object TransformAllPathTraversal {
       .fromFile(runConfig.beamEventsPath)
       .getOrElse(Seq.empty[BeamEvent])
 
-    val (pathLinkEvents, typeToIdSeq) = EventsTransformer.transform(events)
+    val selectedIds = collectIds(events, runConfig.vehicleSampling)
+
+    val selectedEvents = events.collect {
+      case event: BeamPathTraversal if selectedIds.contains(event.vehicleId) => event
+    }
+
+    val (pathLinkEvents, typeToIdSeq) = EventsTransformer.transform(selectedEvents)
     Writer.writeViaEvents(pathLinkEvents, runConfig.viaEventsPath)
     Writer.writeViaIdFile(typeToIdSeq, runConfig.viaIdGoupsFilePath)
+  }
+
+  def collectIds(events: Traversable[BeamEvent], rules: Seq[VehicleSample]): mutable.HashSet[String] = {
+    val allVehicles = events.foldLeft(mutable.Map.empty[String, mutable.HashSet[String]])((vehicles, event) => {
+      event match {
+        case pte: BeamPathTraversal =>
+          vehicles.get(pte.vehicleType) match {
+            case Some(map) => map += pte.vehicleId
+            case None      => vehicles(pte.vehicleType) = mutable.HashSet(pte.vehicleId)
+          }
+        case _ =>
+      }
+      vehicles
+    })
+
+    val selectedIds = rules.foldLeft(mutable.HashSet.empty[String])((selected, rule) => {
+      allVehicles.get(rule.vehicleType) match {
+        case Some(vehicleIds) =>
+          vehicleIds.foreach(id => {
+            if (rule.percentage >= 1.0 || rule.percentage >= Math.random()) selected += id
+          })
+        case None =>
+      }
+
+      selected
+    })
+
+    val filteredTypes = mutable.HashSet(rules.map(_.vehicleType): _*)
+    allVehicles.foldLeft(selectedIds)((selected, vehiclesGroup) => {
+      val (vehicleType: String, ids) = vehiclesGroup
+      if (!filteredTypes.contains(vehicleType)) selectedIds ++= ids
+      selected
+    })
   }
 }
