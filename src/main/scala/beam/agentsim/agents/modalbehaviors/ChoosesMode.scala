470cfbf670bd03cf37af331107c655f1ac295386
rajnikantsh <rk_sharmait@yahoo.com>
2019-07-03 03:39:55 +0530

    change bike_transit mode as per drive transit

--- src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
@@ -90,7 +90,7 @@ trait ChoosesMode {
               _,
               _,
               _,
-              None | Some(CAR | BIKE | DRIVE_TRANSIT),
+              None | Some(CAR | BIKE | DRIVE_TRANSIT | BIKE_TRANSIT),
               _,
               _,
               _,
@@ -184,7 +184,7 @@ trait ChoosesMode {
           case None | Some(CAR | BIKE) =>
             // In these cases, a personal vehicle will be involved
             newlyAvailableBeamVehicles
-          case Some(DRIVE_TRANSIT) =>
+          case Some(DRIVE_TRANSIT | BIKE_TRANSIT) =>
             val tour = _experiencedBeamPlan.getTourContaining(nextAct)
             val tripIndex = tour.tripIndexOfElement(nextAct)
             if (tripIndex == 0 || tripIndex == tour.trips.size - 1) {
@@ -350,8 +350,8 @@ trait ChoosesMode {
               responsePlaceholders =
                 makeResponsePlaceholders(boundingBox, withRouting = true, withParking = mode == CAR)
           }
-        case Some(DRIVE_TRANSIT) =>
-          val LastTripIndex = currentTour(choosesModeData.personData).trips.size - 1
+        case mode @ Some(DRIVE_TRANSIT | BIKE_TRANSIT) =>
+          val lastTripIndex = currentTour(choosesModeData.personData).trips.size - 1
           (
             currentTour(choosesModeData.personData).tripIndexOfElement(nextAct),
             choosesModeData.personData.currentTourPersonalVehicle
@@ -360,13 +360,16 @@ trait ChoosesMode {
               // We use our car if we are not replanning, otherwise we end up doing a walk transit (catch-all below)
               // we do not send parking inquiry here, instead we wait for drive_transit route to come back and we use
               // actual location of transit station
+
+              val r5Mode = mode.get
+
               makeRequestWith(
                 withTransit = true,
-                filterStreetVehiclesForQuery(newlyAvailableBeamVehicles.map(_.streetVehicle), CAR) :+ bodyStreetVehicle,
+                filterStreetVehiclesForQuery(newlyAvailableBeamVehicles.map(_.streetVehicle), r5Mode) :+ bodyStreetVehicle,
                 withParking = false
               )
               responsePlaceholders = makeResponsePlaceholders(boundingBox, withRouting = true, withParking = false)
-            case (LastTripIndex, Some(currentTourPersonalVehicle)) =>
+            case (lastTripIndex, Some(currentTourPersonalVehicle)) =>
               // At the end of the tour, only drive home a vehicle that we have also taken away from there.
               parkingRequestId = makeRequestWith(
                 withTransit = true,
@@ -399,9 +402,6 @@ trait ChoosesMode {
         case Some(RIDE_HAIL_TRANSIT) =>
           responsePlaceholders = makeResponsePlaceholders(boundingBox, withRideHailTransit = true)
           requestId = makeRideHailTransitRoutingRequest(bodyStreetVehicle)
-        case Some(BIKE_TRANSIT) =>
-          responsePlaceholders = makeResponsePlaceholders(boundingBox, withRouting = true)
-          makeRequestWith(withTransit = true, Vector(bodyStreetVehicle), withParking = false)
         case Some(m) =>
           logDebug(m.toString)
       }
@@ -872,14 +872,14 @@ trait ChoosesMode {
       )
 
       val filteredItinerariesForChoice = (choosesModeData.personData.currentTourMode match {
-        case Some(DRIVE_TRANSIT) =>
+        case mode @ Some(DRIVE_TRANSIT | BIKE_TRANSIT) =>
           val LastTripIndex = currentTour(choosesModeData.personData).trips.size - 1
           (
             currentTour(choosesModeData.personData).tripIndexOfElement(nextAct),
             personData.hasDeparted
           ) match {
             case (0 | LastTripIndex, false) =>
-              combinedItinerariesForChoice.filter(_.tripClassifier == DRIVE_TRANSIT)
+              combinedItinerariesForChoice.filter(_.tripClassifier == mode.get)
             case _ =>
               combinedItinerariesForChoice.filter(
                 trip => trip.tripClassifier == WALK_TRANSIT || trip.tripClassifier == RIDE_HAIL_TRANSIT
