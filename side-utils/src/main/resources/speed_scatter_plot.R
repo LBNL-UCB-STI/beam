84e30adeaf18f5ac847026157c0dfb90b011b09f
Kirill Mitin <kirill.mitin@btsdigital.kz>
2019-07-09 15:10:22 +0600

    Scatterplot creation

--- side-utils/src/main/resources/speed_scatter_plot.R
+++ side-utils/src/main/resources/speed_scatter_plot.R
@@ -3,4 +3,21 @@
 # Created by: kirmit
 # Created on: 2019-07-09
 
+speedData <- read.csv("compare_sf-light_simple_2.csv")
 
+# Give the chart file a name.
+png(file = "scatterplot.png", width = 3064, height = 2048, units = "px", pointsize = 48)
+
+plot(speedData$speedMedian ~ speedData$speedBeam,
+    data = speedData,
+    xlab = "Matsim Speed",
+    axes = FALSE,
+    ylab = "Uber Avg Speed",
+    main = "Beam vs Uber"
+)
+axis(side = 1, at = seq(0, 50, by = 2))
+axis(side = 2, at = seq(0, 80, by = 5))
+abline(lm(speedData$speedBeam ~ speedData$speedMedian), col = "red")
+
+# Save the file.
+dev.off()
