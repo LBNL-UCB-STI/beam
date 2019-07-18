cfcb1f8a152c89d7145da73c371edf72b8ae2903
Kirill Mitin <kirmit13@gmail.com>
2019-07-18 08:10:51 +0600

    Diff scatterplot

--- side-utils/src/main/resources/speed_data.R
+++ side-utils/src/main/resources/speed_data.R
@@ -2,11 +2,28 @@
 # Objective : TODO
 # Created by: kirill.mitin
 # Created on: 2019-07-15
-keep <- c("osmId","speedBeam","speedMedian","speedAvg")
-speedData <- read.csv("speed/speed_data_compare_2.csv")
-sdata <- speedData[complete.cases(speedData[keep]), ]
-cd <- (sdata[4]- sdata[2])
-cd1 <- cd[complete.cases(cd), ]
+#library(ggplot2)
 
-print(nrow(sdata))
-print(length(cd1))
+keep <- c("osmId", "speedBeam", "speedMedian", "speedAvg")
+speedData <- read.csv("speed/osm_way_segm.csv")
+sdata <- speedData[complete.cases(speedData[keep]),]
+
+cd <- transform(sdata, diff = speedBeam - speedAvg)
+
+#ggplot(cd, aes(x = diff, x = maxDev)) + geom_point()
+#ggsave("diff_plot.png", width = 16, height = 9, dpi = 300)
+
+png(file = "diff_scatterplot.png", width = 3064, height = 2048, units = "px", pointsize = 48)
+
+plot(cd$maxDev ~ cd$diff,
+data = sdata,
+xlab = "Diff",
+axes = FALSE,
+ylab = "Observations",
+main = "Beam vs Uber"
+)
+axis(side = 1, at = seq(-40, 40, by = 2))
+axis(side = 2, at = seq(-50, 1000, by = 20))
+
+# Save the file.
+dev.off()
