f702065e96fc0ca05b21d723d18a4f8084aa1622
Rob Fitzgerald <robfitzgerald@gmail.com>
2019-07-25 17:13:43 -0600

    fixed test for 100% available parking. was incorrectly assuming a TAZ selection.

--- src/test/scala/beam/agentsim/infrastructure/parking/ParkingZoneSearchSpec.scala
+++ src/test/scala/beam/agentsim/infrastructure/parking/ParkingZoneSearchSpec.scala
@@ -35,43 +35,41 @@ class ParkingZoneSearchSpec extends WordSpec with Matchers {
       }
     }
 
-    // todo JH talk with RJF if the fact, that both parking zones can provide a parking stall @ the same location
-    //  is an intended behavior (which would mean that they are somehow overlapping?!)
-    //  How is "nearest" TAZ defined? By it's center? then the calculation in ParkingZoneSearch might be wrong
-    //    "search for parking with full availability" should {
-    //      "find a spot in the nearest TAZ with full availability which places the stall exactly at the driver's destination" in new ParkingZoneSearchSpec.SimpleParkingAlternatives {
-    //        val result: Option[ParkingZoneSearch.ParkingSearchResult] = ParkingZoneSearch.find(
-    //          destinationNearTazB,
-    //          valueOfTime = 1.0,
-    //          parkingDuration = 0.0, // ignore pricing ranking
-    //          ParkingInquiry.simpleDistanceEqualUtilityFunction,
-    //          tazsInProblem,
-    //          Seq(ParkingType.Public),
-    //          parkingSearchTree,
-    //          parkingZones,
-    //          ParkingZoneSearchSpec.mockGeoUtils.distUTMInMeters,
-    //          ParkingZoneSearchSpec.random
-    //        )
-    //
-    //        result match {
-    //          case None => fail()
-    //          case Some(
-    //              ParkingZoneSearch.ParkingSearchResult(taz, parkingType, parkingZone, stallCoord, utilityOfAlternative)
-    //              ) =>
-    //            // the TAZ selected should be TAZ B
-    //            taz should equal(tazB)
-    //
-    //            // since everything is equal, either TAZ should work out, but
-    //            utilityOfAlternative should equal(0)
-    //
-    //            // these should be consistent with the configuration of this scenario
-    //            parkingType should equal(ParkingType.Public)
-    //
-    //            // since availability is 18/18 = 1.0, sample location should equal destination coordinate
-    //            stallCoord should equal(destinationNearTazB)
-    //        }
-    //      }
-    //    }
+    "search for parking with full availability" should {
+      "find a spot which places the stall exactly at the driver's destination" in new ParkingZoneSearchSpec.SimpleParkingAlternatives {
+        val result: Option[ParkingZoneSearch.ParkingSearchResult] = ParkingZoneSearch.find(
+          destinationNearTazB,
+          valueOfTime = 1.0,
+          parkingDuration = 0.0, // ignore pricing ranking
+          ParkingInquiry.simpleDistanceEqualUtilityFunction,
+          tazsInProblem,
+          Seq(ParkingType.Public),
+          parkingSearchTree,
+          parkingZones,
+          ParkingZoneSearchSpec.mockGeoUtils.distUTMInMeters,
+          ParkingZoneSearchSpec.random,
+          true,
+          true
+        )
+
+        result match {
+          case None => fail()
+          case Some(
+              ParkingZoneSearch.ParkingSearchResult(taz, parkingType, parkingZone, stallCoord, utilityOfAlternative)
+              ) =>
+            // due to 100% availability at both stalls, the taz could be either TAZ
+
+            // since everything is equal, either TAZ should work out, but
+            utilityOfAlternative should equal(0)
+
+            // these should be consistent with the configuration of this scenario
+            parkingType should equal(ParkingType.Public)
+
+            // since availability is 18/18 = 1.0, sample location should equal destination coordinate
+            stallCoord should equal(destinationNearTazB)
+        }
+      }
+    }
 
     "search for parking exactly between two TAZs finds some availability at one TAZ" should {
       "find a spot near their destination but with some variance due to the availability of parking" in new ParkingZoneSearchSpec.SimpleParkingAlternatives {
