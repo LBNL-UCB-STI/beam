4b4e42047be7a4d21a5d601cea873d6877be6247
Nikolay Ilin <irishwithaxe@gmail.com>
2019-07-31 19:17:12 +0300

    added visualizations 33, 34

--- src/main/scala/beam/utils/beamToVia/apps/FindIdsInCircles.scala
+++ src/main/scala/beam/utils/beamToVia/apps/FindIdsInCircles.scala
@@ -1,14 +1,13 @@
 package beam.utils.beamToVia.apps
 
+import beam.utils.beamToVia.beamEvent.{BeamEvent, BeamPathTraversal}
 import beam.utils.beamToVia.{BeamEventsReader, LinkCoordinate, Point, Writer}
-import beam.utils.beamToVia.beamEvent.{BeamEvent, BeamPathTraversal, BeamPersonEntersVehicle}
-import beam.utils.beamToVia.beamEventsFilter.MutableSamplingFilter
 
 import scala.collection.mutable
 
 object FindIdsInCircles extends App {
-  val sourcePath = "D:/Work/BEAM/visualizations/v2.it20.events.bridge_cap_5000.half.csv"
-  val outputPath = "D:/Work/BEAM/visualizations/v2.it20.events.bridge_cap_5000.half_in_SF"
+  val sourcePath = "D:/Work/BEAM/visualizations/v34.it20.events.csv"
+  val baseOutputPath = "D:/Work/BEAM/visualizations/v34.it20.events.in_SF"
   val networkPath = "D:/Work/BEAM/visualizations/physSimNetwork.xml"
 
   val networkXml = xml.XML.loadFile(networkPath)
@@ -34,46 +33,26 @@ object FindIdsInCircles extends App {
       case (links, (linkId, _)) => links += linkId
     }
 
-/*  object CircleFilter extends MutableSamplingFilter {
+  class CircleAccumulator() {
     var interestingVehicles = mutable.HashSet.empty[String]
-    val empty = Seq.empty[BeamEvent]
-    override def filter(event: BeamEvent): Seq[BeamEvent] = event match {
-      case pte: BeamPathTraversal =>
-        if (pte.linkIds.exists(interestingLinks.contains)) {
-          interestingVehicles += pte.vehicleId
-          Seq(pte)
-        } else {
-          empty
-        }
 
-      case _ => Seq(event)
+    def process(event: BeamEvent): Unit = event match {
+      case pte: BeamPathTraversal if pte.linkIds.exists(interestingLinks.contains) =>
+        interestingVehicles += pte.vehicleId
+
+      case _ =>
     }
   }
 
-  val events = BeamEventsReader
-    .fromFileWithFilter(sourcePath, CircleFilter)
-    .getOrElse(Seq.empty[BeamEvent])
+  val vehiclesInCircle = BeamEventsReader
+    .fromFileFoldLeft[CircleAccumulator](sourcePath, new CircleAccumulator(), (acc, event) => {
+      acc.process(event)
+      acc
+    })
+    .getOrElse(new CircleAccumulator())
+    .interestingVehicles
 
-  case class Accumulator(
-    persons: mutable.HashSet[String] = mutable.HashSet.empty[String]
-  )
-
-  def vehicleSelected(vehicleId: String): Boolean = CircleFilter.interestingVehicles.contains(vehicleId)
-
-  val accumulator = events.foldLeft(Accumulator())((acc, event) => {
-    event match {
-      case pev: BeamPersonEntersVehicle if vehicleSelected(pev.vehicleId) => acc.persons += pev.personId
-      case _                                                              =>
-    }
-
-    acc
-  })
-
-  val personsPath = outputPath + ".persons.txt"
-  Writer.writeSeqOfString(accumulator.persons, personsPath)
-  Console.println("persons written into " + personsPath)
-
-  val vehiclesPath = outputPath + ".vehicles.txt"
-  Writer.writeSeqOfString(CircleFilter.interestingVehicles, vehiclesPath)
-  Console.println("vehicles written into " + vehiclesPath)*/
+  val vehiclesPath = baseOutputPath + ".vehicles.txt"
+  Writer.writeSeqOfString(vehiclesInCircle, vehiclesPath)
+  Console.println("vehicles written into " + vehiclesPath)
 }
---
+++ src/main/scala/beam/utils/beamToVia/appsForVisualizations/visualization_33.scala
@@ -0,0 +1,64 @@
+package beam.utils.beamToVia.appsForVisualizations
+
+import beam.utils.beamToVia.{EventsProcessor, HashSetReader, Writer}
+import beam.utils.beamToVia.beamEvent.BeamPathTraversal
+import beam.utils.beamToVia.beamEventsFilter.{MutablePopulationFilter, MutableSamplingFilter, MutableVehiclesFilter, PopulationSample, VehicleSample}
+import beam.utils.beamToVia.viaEvent.ViaEvent
+
+object visualization_33 extends App {
+  val beamEventsFilePath = "D:/Work/BEAM/visualizations/v33.it50.events.csv"
+  val vehiclesInCircleFilePath = "D:/Work/BEAM/visualizations/v33.it50.events.in_SF.vehicles.txt"
+
+  val idPrefix = ""
+  val sampleSize = 0.3
+
+  val viaOutputBaseFilePath = "D:/Work/BEAM/visualizations/v33.it50.events.popSize" + sampleSize
+  val viaEventsFile = viaOutputBaseFilePath + ".via.xml"
+  val viaIdsFile = viaOutputBaseFilePath + ".ids.txt"
+
+  val vehiclesInCircle = HashSetReader.fromFile(vehiclesInCircleFilePath)
+
+  val filter: MutableSamplingFilter = MutableVehiclesFilter.withListOfInclude(
+    vehiclesInCircle,
+    Seq(
+      VehicleSample("RH_PHEV-Car_L1", sampleSize),
+      VehicleSample("RH_HEV-Car_L1", sampleSize),
+      VehicleSample("RH_Conventional-Truck_L1", sampleSize),
+      VehicleSample("RH_BEV-Car_L1", sampleSize),
+      VehicleSample("PHEV-Car_L1", sampleSize),
+      VehicleSample("HEV-Car_L1", sampleSize),
+      VehicleSample("Conventional-Truck_L1", sampleSize),
+      VehicleSample("Conventional-Car_L1", sampleSize),
+      VehicleSample("CAR", sampleSize),
+      VehicleSample("BUS-WH", sampleSize),
+      VehicleSample("BUS-WC", sampleSize),
+      VehicleSample("BUS-VTA", sampleSize),
+      VehicleSample("BUS-VN", sampleSize),
+      VehicleSample("BUS-VC", sampleSize),
+      VehicleSample("BUS-SR", sampleSize),
+      VehicleSample("BUS-RV", sampleSize),
+      VehicleSample("BUS-PE", sampleSize),
+      VehicleSample("BUS-MA", sampleSize),
+      VehicleSample("BUS-GG", sampleSize),
+      VehicleSample("BUS-DEFAULT", sampleSize),
+      VehicleSample("BUS-DE", sampleSize),
+      VehicleSample("BUS-CT", sampleSize),
+      VehicleSample("BUS-CM", sampleSize),
+      VehicleSample("BUS-CC", sampleSize),
+      VehicleSample("BUS-AC", sampleSize),
+      VehicleSample("BEV-Car_L1", sampleSize)
+    ),
+    0
+  )
+
+  def vehicleType(pte: BeamPathTraversal): String =
+    pte.mode + "_" + pte.vehicleType + "_P%03d".format(pte.numberOfPassengers)
+
+  def vehicleId(pte: BeamPathTraversal): String =
+    idPrefix + vehicleType(pte) + "__" + pte.vehicleId
+
+  val (vehiclesEvents, _) = EventsProcessor.readWithFilter(beamEventsFilePath, filter)
+  val (events, typeToId) = EventsProcessor.transformPathTraversals(vehiclesEvents, vehicleId, vehicleType)
+
+  Writer.writeViaEventsQueue[ViaEvent](events, _.toXml.toString, viaEventsFile)
+  Writer.writeViaIdFile(typeToId, viaIdsFile)}
