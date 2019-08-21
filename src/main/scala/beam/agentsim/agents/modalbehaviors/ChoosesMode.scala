e1b15d2bf516edff199ebd7c799fbb8e7e27bd53
Colin Sheppard <colinthegaunt@gmail.com>
2019-08-21 08:27:54 -0700

    change park protocol to allow release of stall when fast charging complete

--- src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
@@ -86,7 +86,7 @@ trait ChoosesMode {
       nextStateData match {
         // If I am already on a tour in a vehicle, only that vehicle is available to me
         case ChoosesModeData(
-            BasePersonData(_, _, _, _, _, Some(vehicle), _, _, _, _, _),
+            BasePersonData(_, _, _, _, _, Some(vehicle), _, _, _, _, _, _),
             _,
             _,
             _,
@@ -115,6 +115,7 @@ trait ChoosesMode {
               _,
               _,
               None | Some(CAR | BIKE | DRIVE_TRANSIT),
+              _,
               _,
               _,
               _,
