642e0efcd014104b7846c8d21a8be6a5a9aee938
Justin Pihony <justin.pihony@gmail.com>
2019-08-21 00:30:12 -0400

    Format

--- src/main/scala/beam/analysis/AnalysisProcessor.scala
+++ src/main/scala/beam/analysis/AnalysisProcessor.scala
@@ -7,13 +7,14 @@ import java.nio.charset.StandardCharsets
 import beam.utils.logging.ExponentialLazyLogging
 
 object AnalysisProcessor extends ExponentialLazyLogging {
+
   def iterationEndBulkAnalysisOutput_Async(currentIterationNumber: Int) = {
     fireAndForgetPythonScript("src/main/python/events_analysis/drive_to_transit.py")
     fireAndForgetPythonScript("src/main/python/events_analysis/pool_metrics.py")
   }
 
   def fireAndForgetPythonScript(scriptPath: String, args: String*) = {
-    val processBuilder = new NuProcessBuilder((Array("py", scriptPath) ++ args):_*)
+    val processBuilder = new NuProcessBuilder((Array("py", scriptPath) ++ args): _*)
     val processHandler = new ProcessHandler(source = "Python Script: " + scriptPath)
     processBuilder.setProcessListener(processHandler)
     processBuilder.start()
@@ -21,12 +22,13 @@ object AnalysisProcessor extends ExponentialLazyLogging {
 }
 
 class ProcessHandler(var nuProcess: NuProcess = null, source: String)
-  extends NuAbstractProcessHandler with ExponentialLazyLogging {
+    extends NuAbstractProcessHandler
+    with ExponentialLazyLogging {
   override def onStart(nuProcess: NuProcess) = {
     this.nuProcess = nuProcess
   }
 
-  override def onStdout(buffer: ByteBuffer, closed: Boolean)= {
+  override def onStdout(buffer: ByteBuffer, closed: Boolean) = {
     val output = StandardCharsets.UTF_8.decode(buffer).toString
     logger.info(s"Process Handler Stdout for $source: $output")
     println(s"Process Handler Stdout for $source: $output")
