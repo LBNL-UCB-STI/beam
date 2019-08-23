d5d1ee793ba6d07e97114a41fd217f7c8c4976f4
Nikolay Ilin <irishwithaxe@gmail.com>
2019-08-24 01:31:28 +0300

    FastHouseholdCAVSchedulingSpec format fix

--- src/test/scala/beam/agentsim/agents/household/FastHouseholdCAVSchedulingSpec.scala
+++ src/test/scala/beam/agentsim/agents/household/FastHouseholdCAVSchedulingSpec.scala
@@ -6,12 +6,11 @@ import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
 import beam.agentsim.agents.vehicles.EnergyEconomyAttributes.Powertrain
 import beam.agentsim.agents.vehicles.{BeamVehicle, BeamVehicleType}
-import beam.agentsim.agents.{Dropoff, Pickup}
 import beam.router.BeamSkimmer
-import beam.sim.{BeamHelper, BeamServicesImpl}
 import beam.sim.common.GeoUtilsImpl
 import beam.sim.config.{BeamConfig, MatSimBeamConfigBuilder}
-import beam.utils.{SimRunnerForTest, TestConfigUtils}
+import beam.sim.{BeamHelper, BeamServicesImpl}
+import beam.utils.TestConfigUtils
 import beam.utils.TestConfigUtils.testConfig
 import com.typesafe.config.ConfigFactory
 import org.matsim.api.core.v01.population.{Activity, Person, Plan, Population}
@@ -26,9 +25,9 @@ import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
 
 import scala.collection.immutable.List
-import scala.collection.{JavaConverters, mutable}
+import scala.collection.{mutable, JavaConverters}
 import scala.concurrent.ExecutionContext
-/*
+
 class FastHouseholdCAVSchedulingSpec
     extends TestKit(
       ActorSystem(
@@ -318,4 +317,3 @@ class FastHouseholdCAVSchedulingSpec
     (sc.getPopulation, household)
   }
 }
- */
