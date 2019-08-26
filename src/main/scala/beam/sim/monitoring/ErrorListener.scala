bf358af78f954dc6307a01030b2b1a93287de701
Colin Sheppard <colinthegaunt@gmail.com>
2019-08-25 23:41:19 -0700

    emergency fix, was leading to stuckness

--- src/main/scala/beam/sim/monitoring/ErrorListener.scala
+++ src/main/scala/beam/sim/monitoring/ErrorListener.scala
@@ -38,8 +38,9 @@ class ErrorListener() extends Actor with ActorLogging {
           d.sender ! ReservationResponse(Left(DriverNotFoundError))
         case _: RemovePassengerFromTrip =>
         // Can be safely skipped
-        case TriggerWithId(EndRefuelSessionTrigger(_, _, _, _), _) =>
-        // Can be safely skipped, happens when a person drives away before the charging session is over
+        case TriggerWithId(EndRefuelSessionTrigger(_, _, _, _), triggerId) =>
+        // Can be safely skipped, happens when a person ends the day before the charging session is over
+          d.sender ! CompletionNotice(triggerId)
         case TriggerWithId(trigger, triggerId) =>
           log.warning("Trigger id {} sent to dead letters: {}", triggerId, trigger)
           d.sender ! CompletionNotice(triggerId)
