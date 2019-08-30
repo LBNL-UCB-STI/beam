2296d9311cbceb5e0cd2b0e7097079eff3a8856e
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2019-08-30 22:40:11 +0200

    first structure of ParkingUtilityEvent

---
+++ src/main/scala/beam/agentsim/events/ParkingUtilityEvent.scala
@@ -0,0 +1,87 @@
+package beam.agentsim.events
+
+import java.util
+
+import beam.agentsim.agents.vehicles.BeamVehicleType
+import beam.agentsim.infrastructure.charging.ChargingPointType
+import beam.agentsim.infrastructure.parking.ParkingType
+import org.matsim.api.core.v01.Id
+import org.matsim.api.core.v01.events.Event
+import org.matsim.api.core.v01.population.Person
+import org.matsim.core.api.internal.HasPersonId
+import org.matsim.vehicles.Vehicle
+
+class ParkingUtilityEvent(tick: Double,
+                          driverId: Id[Person],
+                          vehId: Id[Vehicle],
+                          vehicleType: BeamVehicleType,
+                          activityDuration: Double,
+                          numStallsSeen: Int,
+                          numStallsSampled: Int,
+                          selectedStallPrice: Double,
+                          selectedStallParkingType: ParkingType,
+                          selectedStallChargingPointType: ChargingPointType,
+                          selectedStallMnlRangeAnxiety: Double,
+                          selectedStallMnlParkingPrice: Double,
+                          selectedStallMnlDistance: Double,
+                          selectedStallMnlResidential: Double
+                         )
+  extends Event(tick)
+    with HasPersonId
+    with ScalaEvent {
+
+  import ParkingUtilityEvent._
+
+  override def getEventType: String = EVENT_TYPE
+
+  override def getPersonId: Id[Person] = driverId
+
+
+  override def getAttributes: util.Map[String, String] = {
+    val attributes = super.getAttributes
+    attributes.put(ATTRIBUTE_DRIVER_ID, driverId.toString)
+    attributes.put(ATTRIBUTE_VEHICLE_ID, vehId.toString)
+    attributes.put(ATTRIBUTE_VEHICLE_ENGINE_TYPE, vehicleType.toString)
+    //    attributes.put(ATTRIBUTE_ACTIVITY_TYPE) todo
+    attributes.put(ATTRIBUTE_ACTIVITY_DURATION, activityDuration.toString)
+    attributes.put(ATTRIBUTE_NUM_STALLS_SEEN, numStallsSeen.toString)
+    attributes.put(ATTRIBUTE_NUM_STALLS_SAMPLED, numStallsSampled.toString)
+    //    attributes.put(ATTRIBUTE_SAMPLED_STALLS_CHARGING_TYPES_DISTRIBUTION) todo
+    //    attributes.put(ATTRIBUTE_SAMPLED_STALLS_PARKING_TYPES_DISTRIBUTION) todo
+    //    attributes.put(ATTRIBUTE_SAMPLED_STALLS_COSTS_DISTRIBUTION) todo
+    attributes.put(ATTRIBUTE_SELECTED_STALL_PRICE, selectedStallPrice.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_PARKING_TYPE, selectedStallParkingType.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_CHARGING_POINT_TYPE, selectedStallChargingPointType.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_MNL_RANGE_ANXIETY, selectedStallMnlRangeAnxiety.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_MNL_PARKING_PRICE, selectedStallMnlParkingPrice.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_MNL_DISTANCE, selectedStallMnlDistance.toString)
+    attributes.put(ATTRIBUTE_SELECTED_STALL_MNL_RESIDENTIAL, selectedStallMnlResidential.toString)
+
+    attributes
+  }
+
+
+}
+
+object ParkingUtilityEvent {
+  val EVENT_TYPE: String = "ParkingUtilityEvent"
+  val ATTRIBUTE_DRIVER_ID: String = "driver"
+  val ATTRIBUTE_VEHICLE_ID: String = "vehicle"
+  val ATTRIBUTE_VEHICLE_ENGINE_TYPE: String = "vehicleType"
+  val ATTRIBUTE_ACTIVITY_TYPE: String = "activityType"
+  val ATTRIBUTE_ACTIVITY_DURATION: String = "activityDuration"
+  val ATTRIBUTE_NUM_STALLS_SEEN: String = "numStallsSeen"
+  val ATTRIBUTE_NUM_STALLS_SAMPLED: String = "numStallsSampled"
+  val ATTRIBUTE_SAMPLED_STALLS_CHARGING_TYPES_DISTRIBUTION: String = "sampledStallsChargingTypesDistribution"
+  val ATTRIBUTE_SAMPLED_STALLS_PARKING_TYPES_DISTRIBUTION: String = "sampledStallsParkingTypesDistribution"
+  val ATTRIBUTE_SAMPLED_STALLS_COSTS_DISTRIBUTION: String = "sampledStallsCostsDistribution"
+  val ATTRIBUTE_SELECTED_STALL_PRICE: String = "selectedStallPrice"
+  val ATTRIBUTE_SELECTED_STALL_PARKING_TYPE: String = "selectedStallParkingType"
+  val ATTRIBUTE_SELECTED_STALL_CHARGING_POINT_TYPE: String = "selectedStallChargingPointType"
+  val ATTRIBUTE_SELECTED_STALL_MNL_RANGE_ANXIETY: String = "selectedStallMnlRangeAnxiety"
+  val ATTRIBUTE_SELECTED_STALL_MNL_PARKING_PRICE: String = "selectedStallMnlParkingPrice"
+  val ATTRIBUTE_SELECTED_STALL_MNL_DISTANCE: String = "selectedStallMnlDistance"
+  val ATTRIBUTE_SELECTED_STALL_MNL_RESIDENTIAL: String = "selectedStallMnlResidential"
+
+}
+
