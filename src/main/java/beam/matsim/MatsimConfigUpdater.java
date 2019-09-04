a9d6f9bbc6d81d310734807cf08207c0668b77d9
Nikolay Ilin <irishwithaxe@gmail.com>
2019-09-04 18:31:47 +0300

    added control over linkstats.txt.gz file writing

---
+++ src/main/java/beam/matsim/MatsimConfigUpdater.java
@@ -0,0 +1,36 @@
+package beam.matsim;
+
+import beam.sim.BeamConfigChangesObservable;
+import beam.sim.config.BeamConfig;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.matsim.core.config.groups.ControlerConfigGroup;
+import org.matsim.core.config.groups.LinkStatsConfigGroup;
+import scala.Tuple2;
+
+import java.util.Observable;
+import java.util.Observer;
+
+@Singleton
+public class MatsimConfigUpdater implements Observer {
+    private ControlerConfigGroup controlerConfigGroup;
+    private LinkStatsConfigGroup linkStatsConfigGroup;
+
+    @Inject
+    MatsimConfigUpdater(BeamConfigChangesObservable beamConfigChangesObservable, LinkStatsConfigGroup linkStatsConfigGroup, ControlerConfigGroup controlerConfigGroup) {
+        this.controlerConfigGroup = controlerConfigGroup;
+        this.linkStatsConfigGroup = linkStatsConfigGroup;
+
+        beamConfigChangesObservable.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        Tuple2 t = (Tuple2) o;
+        BeamConfig beamConfig = (BeamConfig) t._2;
+
+        controlerConfigGroup.setWritePlansInterval(beamConfig.beam().physsim().writePlansInterval());
+        linkStatsConfigGroup.setWriteLinkStatsInterval(beamConfig.matsim().modules().linkStats().writeLinkStatsInterval());
+
+    }
+}
