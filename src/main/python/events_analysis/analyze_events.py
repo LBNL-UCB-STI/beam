f957a3fc8cfbd147ba92011e5bf316edfb5c0e2d
Zach Needell <zaneedell@lbl.gov>
2019-09-10 16:44:18 -0700

    process vehicle locations

--- src/main/python/events_analysis/analyze_events.py
+++ src/main/python/events_analysis/analyze_events.py
@@ -2,17 +2,140 @@ import sys
 import json
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 import required_module_installer as required
+import matplotlib.pyplot as plt
+import collections
+
+
+def assignVehicleDayToLocationMatrix(day, timeBins, keys):
+    timeUtilization = np.zeros((np.size(timeBins),1+max(keys.values())))
+    distanceUtilization = np.zeros((np.size(timeBins),1+max(keys.values())))
+    isRH = np.any(day['isRH'].values == True)
+    if isRH:
+        if np.any(day['isCAV'].values == True):
+            timeUtilization[:,keys['idle']] += 1
+        else:
+            timeUtilization[:,keys['offline']] += 1
+    else:
+        timeUtilization[:,keys['parked']] += 1
+    for idx, event in day.iterrows():
+        lastEvent = (idx == (len(day.index) - 1))
+        if lastEvent:
+            chargingNext = False
+            pickupNext = False
+        else:
+            chargingDirectlyNext = (day.iloc[idx + 1]['type'] == 'RefuelSessionEvent')
+            if (idx == (len(day.index) - 2)):
+                chargingOneAfter = False
+            else:
+                chargingOneAfter = (day.iloc[idx + 1]['type'] == 'ParkEvent') & (day.iloc[idx + 2]['type'] == 'RefuelSessionEvent')
+            chargingNext = chargingDirectlyNext | chargingOneAfter
+            pickupNext = (day.iloc[idx + 1]['type'] == 'PathTraversal') & (day.iloc[idx + 1]['numPassengers'] >= 1)
+        eventCharacteristics = classifyEventLocation(event, lastEvent, chargingNext, pickupNext, isRH)
+
+        afterEventStart = (timeBins >= eventCharacteristics['start'])
+        timeUtilization[afterEventStart,:] = 0.0
+
+        duringEvent =  afterEventStart & (timeBins < eventCharacteristics['end'])
+        timeUtilization[duringEvent,keys[eventCharacteristics['type']]] += 1.0
+
+        if (event['type'] == 'PathTraversal'):
+            #firstIndex = np.argmax(afterEventStart)
+            if np.sum(duringEvent) > 0:
+                meanDistancePerTime = event['length']/np.sum(duringEvent)
+                distanceUtilization[duringEvent,keys[eventCharacteristics['type']]] += meanDistancePerTime/1609.34
+            else:
+                firstIndex = np.argmax(afterEventStart)
+                distanceUtilization[firstIndex,keys[eventCharacteristics['type']]] += event['length']/1609.34
+
+        if 'next-type' in eventCharacteristics:
+            afterEventEnd = (timeBins >= eventCharacteristics['end'])
+            timeUtilization[afterEventEnd,keys[eventCharacteristics['next-type']]] += 1.0
+    return (timeUtilization, distanceUtilization)
+
+def classifyEventLocation(event, lastEvent, chargingNext, pickupNext, isRH):
+    if event.type == 'PathTraversal':
+        if isRH == True:
+            if event.numPassengers >= 1:
+                if lastEvent:
+                    if event.isCAV == True:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-full', 'next-type': 'idle'}
+                    else:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-full', 'next-type': 'offline'}
+                else:
+                    if chargingNext:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-full', 'next-type': 'queuing'}
+                    else:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-full', 'next-type': 'idle'}
+            else:
+                if lastEvent:
+                    if event.isCAV:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-reposition', 'next-type': 'idle'}
+                    else:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-reposition', 'next-type': 'offline'}
+                else:
+                    if chargingNext:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-tocharger', 'next-type': 'queuing'}
+                    elif pickupNext:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-topickup', 'next-type': 'idle'}
+                    else:
+                        return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-reposition', 'next-type': 'idle'}
+        else:
+            if chargingNext:
+                return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-tocharger', 'next-type': 'queuing'}
+            else:
+                return {'start': event.departureTime, 'end': event.arrivalTime, 'type': 'driving-full', 'next-type': 'queuing'}
+    elif event.type == 'RefuelSessionEvent':
+        if isRH == True:
+            if event.primaryFuelLevel < 0.0:
+                return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'offline'}
+                #return {'start': event.time - event.duration, 'end': 30*3600, 'type': 'offline'}
+            else:
+                if lastEvent:
+                    if event.isCAV == True:
+                        return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'idle'}
+                    else:
+                        return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'offline'}
+                else:
+                    return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'idle'}
+        else:
+            if event.primaryFuelLevel < 0.0:
+                return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'parked'}
+                #return {'start': event.time - event.duration, 'end': 30*3600, 'type': 'offline'}
+            else:
+                return {'start': event.time, 'end': event.time + event.duration, 'type': 'charging', 'next-type': 'parked'}
+    elif event.type == 'ParkEvent':
+        if isRH == True:
+            return {'start': event.time, 'end': 30*3600, 'type': 'idle'}
+        else:
+            return {'start': event.time, 'end': 30*3600, 'type': 'parked'}
 
 def get_pooling_metrics(filename):
     data = pd.read_csv(filename, sep=",", index_col=None, header=0)
+
+
+    relevantEvents = data.loc[((data['type'] == 'RefuelSessionEvent') & (data['fuel'] > 0)) | (data['type'] == 'PathTraversal') | (data['type'] == 'ParkEvent')].dropna(how='all', axis=1)
+    cars = set(relevantEvents.loc[(relevantEvents['type'] == 'PathTraversal') & (relevantEvents['mode'] == 'car'), 'vehicle'])
+
+    relevantEvents = relevantEvents.loc[relevantEvents['vehicle'].isin(cars), :]
+
+    relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','isRH'] = relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','vehicle'].str.contains('rideHail')
+    relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','isCAV'] = relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','vehicleType'].str.contains('L5')
+
+    relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','time'] = relevantEvents.loc[relevantEvents['type'] == 'PathTraversal','departureTime']  - 0.5
+    relevantEvents.loc[relevantEvents['type'] == 'RefuelSessionEvent','time'] = relevantEvents.loc[relevantEvents['type'] == 'RefuelSessionEvent','time']  - relevantEvents.loc[relevantEvents['type'] == 'RefuelSessionEvent','duration'] + 0.5
+
+    relevantEvents = relevantEvents.sort_values(by='time')
     modeChoice = data.loc[data['type'] == 'ModeChoice'].dropna(how='all', axis=1)
-    pathTraversal = data.loc[data['type'] == 'PathTraversal'].dropna(how='all', axis=1)
+
     ride_hail_mc = modeChoice[modeChoice['mode'].str.startswith('ride_hail')]
     ride_hail_mc_users = set(ride_hail_mc['person'])
     data2 = data[(data['type'].isin(['PathTraversal']) & data['vehicle'].str.startswith('rideHailVehicle')) |
                  (data['type'].isin(['ModeChoice', 'PersonEntersVehicle', 'PersonLeavesVehicle']) &
                   data['person'].isin(ride_hail_mc_users))]
+
+
     del data
     count_of_multi_passenger_pool_trips = 0
     count_of_one_passenger_pool_trips = 0
@@ -121,6 +244,116 @@ def get_pooling_metrics(filename):
         "unmatched_per_ride_hail_requests": unmatched_per_ride_hail_requests,
         "deadheading_per_ride_hail_trips": deadheading_per_ride_hail_trips
     }
+
+    resolutionInSeconds = 300
+    timeBins = np.arange(start=0,stop=30*3600,step=resolutionInSeconds)
+
+    keys = collections.OrderedDict({'driving-full':0,
+                                'driving-reposition':1,
+                                'driving-topickup':2,
+                                'driving-tocharger':3,
+                                'queuing':4,
+                                'charging':5,
+                                'idle':6,
+                                'offline':7,
+                                'parked':8})
+
+
+    electricVehicleSet = set(relevantEvents.loc[relevantEvents['vehicleType'].str.startswith('ev') == True, 'vehicle'])
+    pluginHybridVehicleSet = set(relevantEvents.loc[relevantEvents['vehicleType'].str.startswith('phev') == True, 'vehicle'])
+    electricVehicles = relevantEvents.loc[relevantEvents['vehicle'].isin(electricVehicleSet),:].groupby('vehicle')
+
+
+    ridehailVehicleSet = set(relevantEvents.loc[relevantEvents.isRH == True, 'vehicle'])
+    ridehailVehicles = relevantEvents.loc[relevantEvents['vehicle'].isin(ridehailVehicleSet),:].groupby('vehicle')
+
+    ridehailElectricVehicles = relevantEvents.loc[relevantEvents['vehicle'].isin(ridehailVehicleSet) & relevantEvents['vehicle'].isin(electricVehicleSet),:].groupby('vehicle')
+    ridehailConventionalVehicles = relevantEvents.loc[relevantEvents['vehicle'].isin(ridehailVehicleSet) & ~relevantEvents['vehicle'].isin(electricVehicleSet),:].groupby('vehicle')
+    personalElectricVehicles = relevantEvents.loc[relevantEvents['vehicle'].isin(cars) & ~relevantEvents['vehicle'].isin(ridehailVehicleSet) & relevantEvents['vehicle'].isin(electricVehicleSet.union(pluginHybridVehicleSet)),:].groupby('vehicle')
+
+    timeUtilizationPV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    distanceUtilizationPV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    for vehicle, day in personalElectricVehicles:
+        now = day.reset_index(drop=True)
+        (timeUtilizationVehicle,distanceUtilizationVehicle) = assignVehicleDayToLocationMatrix(now, timeBins, keys)
+        timeUtilizationPV += timeUtilizationVehicle
+        distanceUtilizationPV += distanceUtilizationVehicle
+
+    timeUtilizationEV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    distanceUtilizationEV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    for vehicle, day in ridehailElectricVehicles:
+        now = day.reset_index(drop=True)
+        (timeUtilizationVehicle,distanceUtilizationVehicle) = assignVehicleDayToLocationMatrix(now, timeBins, keys)
+        timeUtilizationEV += timeUtilizationVehicle
+        distanceUtilizationEV += distanceUtilizationVehicle
+
+    timeUtilizationCV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    distanceUtilizationCV = np.zeros((np.size(timeBins),1+max(keys.values())))
+    for vehicle, day in ridehailConventionalVehicles:
+        now = day.reset_index(drop=True)
+        (timeUtilizationVehicle,distanceUtilizationVehicle) = assignVehicleDayToLocationMatrix(now, timeBins, keys)
+        timeUtilizationCV += timeUtilizationVehicle
+        distanceUtilizationCV += distanceUtilizationVehicle
+
+
+
+    key_names = list(keys.keys())
+    utilizationOutput = dict()
+    timeUtilizationEVsum = np.sum(timeUtilizationEV, axis=0)
+    distanceUtilizationEVsum = np.sum(distanceUtilizationEV, axis=0)
+    timeUtilizationCVsum = np.sum(timeUtilizationCV, axis=0)
+    distanceUtilizationCVsum = np.sum(distanceUtilizationCV, axis=0)
+    timeUtilizationPVsum = np.sum(timeUtilizationPV, axis=0)
+    distanceUtilizationPVsum = np.sum(distanceUtilizationPV, axis=0)
+    for idx, key in enumerate(key_names):
+        utilizationOutput['time-RH-EV-'+key] = timeUtilizationEVsum[idx]/timeUtilizationEV.shape[0]
+    for idx, key in enumerate(key_names[:4]):
+        utilizationOutput['miles-RH-EV-'+key] = distanceUtilizationEVsum[idx]
+    for idx, key in enumerate(key_names):
+        utilizationOutput['time-RH-CV-'+key] = timeUtilizationCVsum[idx]/timeUtilizationCV.shape[0]
+    for idx, key in enumerate(key_names[:4]):
+        utilizationOutput['miles-RH-CV-'+key] = distanceUtilizationCVsum[idx]
+    for idx, key in enumerate(key_names):
+        utilizationOutput['time-Personal-EV-'+key] = timeUtilizationPVsum[idx]/timeUtilizationPV.shape[0]
+    for idx, key in enumerate(key_names[:4]):
+        utilizationOutput['miles-Personal-EV-'+key] = distanceUtilizationPVsum[idx]
+
+    collected = pd.DataFrame(utilizationOutput, index=[filename.rsplit('/', 1)[0]])
+    print(collected)
+    print(utilizationOutput)
+    collected.to_csv("{}/utilization_stats.csv".format(filename.rsplit('/', 1)[0]))
+
+
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(14,10), sharex=True,gridspec_kw={'hspace': 0.06, 'wspace': 0.15})
+    ax1.stackplot(timeBins/3600,np.transpose(timeUtilizationEV))
+    ax2.stackplot(timeBins/3600,np.transpose(timeUtilizationCV))
+    ax3.stackplot(timeBins/3600,np.transpose(distanceUtilizationEV)*12./1000)
+    ax4.stackplot(timeBins/3600,np.transpose(distanceUtilizationCV)*12./1000)
+
+    ax1.set_title('BEV Ridehail')
+    ax2.set_title('Other Ridehail')
+
+    ax3.legend(('driving-full','driving-reposition','driving-pickup','driving-charge','queuing','charging','idle','offline'))
+    ax3.set_xlabel('Hour of Day')
+    ax4.set_xlabel('Hour of Day')
+    ax3.set_ylabel('Distance per hour (1000 mi)')
+    ax1.set_ylabel('Vehicles')
+
+    plt.savefig("{}/ridehail_utilization.png".format(filename.rsplit('/', 1)[0]), transparent=True, facecolor='white' )
+    plt.close(f)
+    f, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,6), sharex=True,gridspec_kw={'hspace': 0.06, 'wspace': 0.15})
+    ax1.stackplot(timeBins/3600,np.transpose(timeUtilizationPV[:,[0,3,4,5,8]]))
+    ax2.stackplot(timeBins/3600,np.transpose(distanceUtilizationPV[:,[0,3,4,5,8]])*12./1000)
+    ax1.set_title('Personal EVs')
+    ax1.set_ylabel('Vehicles')
+    ax2.set_xlabel('Hour of Day')
+    ax2.set_ylabel('Distance per hour (1000 mi)')
+    ax2.legend(('driving-full','driving-charge','queuing','charging','parked'))
+    plt.savefig("{}/personal_utilization.png".format(filename.rsplit('/', 1)[0]), transparent=True, facecolor='white' )
+    plt.close(f)
+
+    pathTraversal = relevantEvents.loc[relevantEvents['type'] == 'PathTraversal'].dropna(how='all', axis=1)
+    del relevantEvents
 
     pathTraversal['miles'] = pathTraversal['length'] / 1609.34
     pathTraversal['gallons'] = (pathTraversal['primaryFuel'] + pathTraversal['secondaryFuel']) * 8.3141841e-9
