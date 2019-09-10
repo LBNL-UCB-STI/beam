e078f88097194cfe4ce34b954c0e0efaf15abb54
Colin Sheppard <colinthegaunt@gmail.com>
2019-09-10 04:48:56 -0700

    catch the full trigger

--- src/main/scala/beam/agentsim/agents/modalbehaviors/DrivesVehicle.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/DrivesVehicle.scala
@@ -436,7 +436,7 @@ trait DrivesVehicle[T <: DrivingData] extends BeamAgent[T] with Stash {
         data.currentLegPassengerScheduleIndex
       )
 
-    case ev @ Event(StartRefuelSessionTrigger(_), _) =>
+    case ev @ Event(TriggerWithId(StartRefuelSessionTrigger(_),_), _) =>
       log.debug("state(DrivesVehicle.Driving): {}", ev)
       stash()
       stay
@@ -575,7 +575,7 @@ trait DrivesVehicle[T <: DrivingData] extends BeamAgent[T] with Stash {
       log.debug("state(DrivesVehicle.DrivingInterrupted): {}", ev)
       stash()
       stay
-    case ev @ Event(StartRefuelSessionTrigger(_), _) =>
+    case ev @ Event(TriggerWithId(StartRefuelSessionTrigger(_),_), _) =>
       log.debug("state(DrivesVehicle.DrivingInterrupted): {}", ev)
       stash()
       stay
