e6409c925e3dbff7c4256ee204632da9b3864201
Colin Sheppard <colinthegaunt@gmail.com>
2019-09-21 18:56:40 -0700

    change assertion to warning because rare

--- src/main/scala/beam/agentsim/agents/PersonAgent.scala
+++ src/main/scala/beam/agentsim/agents/PersonAgent.scala
@@ -669,7 +669,7 @@ class PersonAgent(
         )
         if (currentBeamVehicle != body) {
           if (currentBeamVehicle.beamVehicleType.vehicleCategory != Bike) {
-            assert(currentBeamVehicle.stall.isDefined)
+            if(currentBeamVehicle.stall.isEmpty)logWarn("Expected currentBeamVehicle.stall to be defined.")
           }
           if (!currentBeamVehicle.mustBeDrivenHome) {
             // Is a shared vehicle. Give it up.
