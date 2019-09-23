56131e6334c91b61a819284e86df7f66f5ecef4c
wrashid <waraich.rashid@gmail.com>
2019-09-23 01:32:10 -0500

    adding pmt and vmt per capita

--- src/main/python/smart/smartplots2_run.py
+++ src/main/python/smart/smartplots2_run.py
@@ -10,7 +10,7 @@ plt_setup_smart2 = {
     'scenarios_year': [2010, 2025, 2025, 2025, 2025, 2040, 2040],
 
     'plot_size': (5, 4.5),
-    'bottom_labels': ['Base', 'Sharing is Caring', 'Technology Takeover', "All About Me"],
+    'bottom_labels': ['Base\n2010', 'Sharing is Caring\n2025', 'Technology Takeover\n2025', "All About Me\n2040"],
     'top_labels': ["Base", "BAU", "VTO", "BAU", "VTO", "BAU", "VTO"],
 }
 
@@ -22,8 +22,10 @@ smartplots2_setup.tableSummary(plt_setup_smart2, output_folder)
 
 smartplots2_setup.pltLdvRhOccupancy(plt_setup_smart2, output_folder)
 smartplots2_setup.pltModeSplitInPMT(plt_setup_smart2, output_folder)
+smartplots2_setup.pltModeSplitInPMTPerCapita(plt_setup_smart2, output_folder)
 smartplots2_setup.pltLdvTechnologySplitInVMT(plt_setup_smart2, output_folder)
 smartplots2_setup.pltModeSplitInVMT(plt_setup_smart2, output_folder)
+smartplots2_setup.pltModeSplitInVMTPerCapita(plt_setup_smart2, output_folder)
 smartplots2_setup.pltRHEmptyPooled(plt_setup_smart2, output_folder)
 smartplots2_setup.pltLdvRhOccupancyByVMT(plt_setup_smart2, output_folder)
 
@@ -37,7 +39,7 @@ plt_setup_smart2_base = {
     'scenarios_year': [2010, 2025, 2025, 2025, 2025, 2025, 2025, 2040, 2040, 2040, 2040],
 
     'plot_size': (7.5, 4.5),
-    'bottom_labels': ['Base', 'Base Short', 'Sharing is Caring', 'Technology Takeover', "Base Long", "All About Meter"],
+    'bottom_labels': ['Base\n2010', 'Base Short\n2025', 'Sharing is Caring\n2025', 'Technology Takeover\n2025', "Base Long\n2040", "All About Me\n2040"],
     'top_labels': ["Base", "BAU", "VTO", "BAU", "VTO", "BAU", "VTO", "BAU", "VTO", "BAU", "VTO"],
 }
 
