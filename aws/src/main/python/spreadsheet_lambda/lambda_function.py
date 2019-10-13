88193f12caece7d4b69f04bfb553c541959cd79a
Kirill Mitin <kirmit13@gmail.com>
2019-10-14 02:14:43 +0600

    #2211 Script collect runs
    - Add and create file

--- aws/src/main/python/spreadsheet_lambda/lambda_function.py
+++ aws/src/main/python/spreadsheet_lambda/lambda_function.py
@@ -1,26 +1,147 @@
 # coding=utf-8
-import time
-import json
-import boto3
+from __future__ import print_function
 import pickle
 import os.path
-from botocore.errorfactory import ClientError
-from __future__ import print_function
+import boto3
+import time
+import os
+from datetime import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from botocore.errorfactory import ClientError
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
+SHEET_NAME_PREFIX = 'Run BEAM instances and results'
+INSTANCE_OPERATIONS = ['start', 'stop', 'terminate']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+def create_spreadsheet(sheet_api, title):
+    spreadsheet = {
+        'properties': {
+            'title': title
+        }
+    }
+    spreadsheet = sheet_api.create(body=spreadsheet,
+                                   fields='spreadsheetId').execute()
+    sheet_id = spreadsheet.get('spreadsheetId')
+    print('Spreadsheet ID: {0}'.format(sheet_id))
+    body = {
+        'requests': [
+            {
+                'updateSheetProperties': {
+                    'properties': {
+                        'sheetId': '0',
+                        'title': 'BEAM Instances',
+                        "gridProperties": {
+                            "frozenRowCount": 1
+                        }
+                    },
+                    'fields': '(title,gridProperties.frozenRowCount)'
+                }
+            },
+            {
+                "updateDimensionProperties": {
+                    "range": {
+                        "sheetId": 0,
+                        "dimension": "COLUMNS",
+                        "startIndex": 0,
+                        "endIndex": 5
+                    },
+                    "properties": {
+                        "pixelSize": 160
+                    },
+                    "fields": "pixelSize"
+                }
+            },
+            {
+                "repeatCell": {
+                    "range": {
+                        "sheetId": 0,
+                        "startRowIndex": 0,
+                        "endRowIndex": 1
+                    },
+                    "cell": {
+                        "userEnteredFormat": {
+                            "backgroundColor": {
+                                "red": 0.5,
+                                "green": 0.5,
+                                "blue": 0.5
+                            },
+                            "horizontalAlignment": "CENTER",
+                            "textFormat": {
+                                "foregroundColor": {
+                                    "red": 1.0,
+                                    "green": 1.0,
+                                    "blue": 1.0
+                                },
+                                "fontSize": 10,
+                                "bold": 'true'
+                            }
+                        }
+                    },
+                    "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
+                }
+            }
+        ]
+    }
+    sheet_api.batchUpdate(spreadsheetId=sheet_id, body=body).execute()
+    header = {
+        'values': [
+            ["Run Name", "Date", "Branch", "EC2 Instance", "S3 Link"]
+        ]
+    }
+    sheet_api.values().append(
+        spreadsheetId=sheet_id, range="BEAM Instances!A1:E1",
+        valueInputOption="USER_ENTERED", insertDataOption='OVERWRITE', body=header).execute()
+
+    empty = {
+        'values': [
+            []
+        ]
+    }
+    sheet_api.values().append(
+        spreadsheetId=sheet_id, range="BEAM Instances!A2:E2",
+        valueInputOption="USER_ENTERED", insertDataOption='OVERWRITE', body=empty).execute()
+
+    return sheet_id
+
+
+def find_actual_sheet(creds):
+    datem = datetime.now().strftime('%h.%Y')
+    drive_service = build('drive', 'v3', credentials=creds)
+    page_token = None
+    response = drive_service.files().list(
+        q="mimeType='application/vnd.google-apps.spreadsheet' and name='{0} {1}'".format(SHEET_NAME_PREFIX, datem),
+        spaces='drive',
+        fields='nextPageToken, files(id, name)',
+        pageToken=page_token).execute()
+    items = response.get('files', [])
+    if not items:
+        return False
+    else:
+        return True
+
+
+def add_row(sheet_id, row_data, sheet_api):
+    empty = {
+        'values': [
+            [
+                row_data.get('name'),
+                row_data.get('date'),
+                row_data.get('branch'),
+                row_data.get('instance'),
+                row_data.get('s3_link')
+            ]
+        ]
+    }
+    sheet_api.values().append(
+        spreadsheetId=sheet_id, range="BEAM Instances!A2:E2",
+        valueInputOption="USER_ENTERED", insertDataOption='OVERWRITE', body=empty).execute()
+
+
+def load_creds():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -39,23 +160,37 @@ def main():
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return creds
 
+
+def lambda_handler(event, context):
+    command_id = event.get('command', 'deploy') # deploy | start | stop | terminate | log
+
+    if command_id == 'deploy':
+        return 'deploy_handler(event)'
+
+    if command_id in INSTANCE_OPERATIONS:
+        return 'instance_handler(event)'
+
+    return "Operation {command} not supported, please specify one of the supported operations (deploy | start | stop | terminate | log). ".format(command=command_id)
+
+
+def main():
+    creds = load_creds()
     service = build('sheets', 'v4', credentials=creds)
-
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+    datem = datetime.now().strftime('%h.%Y')
+    sheet_id = create_spreadsheet(sheet, '{0} {1}'.format(SHEET_NAME_PREFIX, datem))
+    for i in range(10):
+        add_row(sheet_id, {
+            'name': 'test_name',
+            'date': datetime.now().replace(microsecond=0).isoformat(),
+            'branch': 'test_branch',
+            'instance': 'i13123qw',
+            's3_link': 'https://www.googleapis.com/auth/drive.file'
+        }, sheet)
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
 
 if __name__ == '__main__':
     main()
-
