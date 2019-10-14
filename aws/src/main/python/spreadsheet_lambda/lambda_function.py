5fd0dab7c434be2a95073c1c2f2ed1401b1079f1
Kirill Mitin <kirmit13@gmail.com>
2019-10-14 23:26:43 +0600

    #2211   - add start/stop instances

--- aws/src/main/python/spreadsheet_lambda/lambda_function.py
+++ aws/src/main/python/spreadsheet_lambda/lambda_function.py
@@ -14,7 +14,15 @@ from botocore.errorfactory import ClientError
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
 SHEET_NAME_PREFIX = 'Run BEAM instances and results'
-INSTANCE_OPERATIONS = ['start', 'stop', 'terminate']
+INSTANCE_OPERATIONS = ['start', 'stop']
+REGIONS = ['us-east-1', 'us-east-2', 'us-west-2']
+
+ec2 = None
+
+
+def init_ec2(region):
+    global ec2
+    ec2 = boto3.client('ec2', region_name=region)
 
 
 def create_spreadsheet(sheet_api, title):
@@ -163,16 +171,83 @@ def load_creds():
     return creds
 
 
-def lambda_handler(event, context):
-    command_id = event.get('command', 'deploy') # deploy | start | stop | terminate | log
+def create_handler(event):
+    datem = datetime.now().strftime('%h.%Y')
+    return create_spreadsheet(event.get('spreadsheet_id'), '{0} {1}'.format(SHEET_NAME_PREFIX, datem))
 
-    if command_id == 'deploy':
-        return 'deploy_handler(event)'
+
+def check_instance_id(instance_ids):
+    for reservation in ec2.describe_instances()['Reservations']:
+        for instance in reservation['Instances']:
+            if instance['InstanceId'] in instance_ids:
+                instance_ids.remove(instance['InstanceId'])
+    return instance_ids
+
+
+def start_instance(instance_ids):
+    return ec2.start_instances(InstanceIds=instance_ids)
+
+
+def stop_instance(instance_ids):
+    return ec2.stop_instances(InstanceIds=instance_ids)
+
+
+def get_dns(instance_id):
+    host = None
+    while host is None:
+        time.sleep(2)
+        instances = ec2.describe_instances(InstanceIds=[instance_id])
+        for r in instances['Reservations']:
+            for i in r['Instances']:
+                dns = i['PublicDnsName']
+                if dns != '':
+                    host = dns
+    return host
+
+
+def instance_handler(event):
+    region = event.get('region', os.environ['REGION'])
+    instance_ids = event.get('instance_ids')
+    command_id = event.get('command')
+    system_instances = os.environ['SYSTEM_INSTANCES']
+
+    if region not in REGIONS:
+        return "Unable to {command} instance(s), {region} region not supported.".format(command=command_id,
+                                                                                        region=region)
+
+    init_ec2(region)
+    system_instances = system_instances.split(',')
+    instance_ids = instance_ids.split(',')
+    invalid_ids = check_instance_id(list(instance_ids))
+    valid_ids = [item for item in instance_ids if item not in invalid_ids]
+    allowed_ids = [item for item in valid_ids if item not in system_instances]
+
+    if command_id == 'start':
+        ec2.start_instances(InstanceIds=allowed_ids)
+        return "Started instance(s) {insts}.".format(
+            insts=', '.join([': '.join(inst) for inst in zip(allowed_ids, list(map(get_dns, allowed_ids)))]))
+
+    if command_id == 'stop':
+        ec2.stop_instances(InstanceIds=allowed_ids)
+
+    return "Instantiated {command} request for instance(s) [ {ids} ]".format(command=command_id,
+                                                                             ids=",".join(allowed_ids))
+
+
+def lambda_handler(event, context):
+    command_id = event.get('command', 'create')  # start | stop | add | create
+
+    if command_id == 'create':
+        return create_handler(event)
+
+    if command_id == INSTANCE_OPERATIONS:
+        return instance_handler(event)
 
     if command_id in INSTANCE_OPERATIONS:
         return 'instance_handler(event)'
 
-    return "Operation {command} not supported, please specify one of the supported operations (deploy | start | stop | terminate | log). ".format(command=command_id)
+    return "Operation {command} not supported, please specify one of the supported operations (deploy | start | stop | terminate | log). ".format(
+        command=command_id)
 
 
 def main():
