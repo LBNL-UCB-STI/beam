fd9b91c429a17923d3dd495781ea3e934fc8280a
Colin Sheppard <colinthegaunt@gmail.com>
2019-11-03 12:16:41 -0800

    processing mep results

---
+++ src/main/R/mep.R
@@ -0,0 +1,44 @@
+library(stringr)
+
+setwd('/Users/critter/Dropbox/ucb/vto/beam-all/beam') # for development and debugging
+source('./src/main/R/beam-utilities.R')
+
+mep.mode.colors <- data.table(rbind(mode.colors,data.frame(key='Bike',color='peach',color.hex='#f2b272')))
+mep.mode.colors[,name:=c('tnc','drive','walk','transit','','bike')]
+
+scen.codes <- c('Base1'='2010_base',
+                'Base2'='2025_base_short_bau',
+                'Base3'='2025_base_short_vto',
+                'Base5'='2040_base_long_bau',
+                'Base6'='2040_base_long_vto',
+                'A2'='2025_a_bau',
+                'A3'='2025_a_vto',
+                'B5'='2040_b_bau',
+                'B6'='2040_b_vto',
+                'C5'='2040_c_bau',
+                'C6'='2040_c_vto')
+
+mep.dir <- '/Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/mep/'
+
+mep.subs <- grep('old',list.dirs(mep.dir,recursive=F,full.names=F),value=T,invert=T)
+
+mep.sub<-mep.subs[1]
+
+mep <- list()
+for(mep.sub in mep.subs){
+  the.file <- grep('overall_mep.csv',list.files(pp(mep.dir,mep.sub)),value=T)
+  the.scen <- str_split(mep.sub,'beam_nov2_')[[1]][2]
+
+  tmp <- data.table(read.csv(pp(mep.dir,mep.sub,'/',the.file)))
+  tmp[,scen:=names(scen.codes)[scen.codes == the.scen]]
+  mep[[length(mep)+1]] <- tmp
+}
+mep <- na.omit(rbindlist(mep))
+mep[,col:=mep.mode.colors$color.hex[match(type,mep.mode.colors$name)]]
+mep[,type.polished:=mep.mode.colors$key[match(type,mep.mode.colors$name)]]
+mep[,scen:=factor(scen,names(scen.codes))]
+
+ggplot(mep[type!='mep'],aes(x=scen,y=overall_mep,fill=type))+geom_bar(stat='identity')+theme_bw()+
+  scale_fill_manual(values=as.character(mep.mode.colors$color.hex[match(sort(u(mep[type!='mep']$type)),mep.mode.colors$name)]),labels=as.character(mep.mode.colors$key[match(sort(u(mep[type!='mep']$type)),mep.mode.colors$name)])) + labs(x='',y='Mobility Energy Productivity',fill='Mode')
+  
+
---
+++ src/main/bash/mep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_a_bau/beam_2010_a_bau_mep/mep -o /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_a_bau/beam_2010_a_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_a_vto/beam_2010_a_vto_mep/mep -o /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_a_vto/beam_2010_a_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_b_bau/beam_2010_b_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_b_vto/beam_2010_b_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_base/beam_2010_base_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_c_bau/beam_2010_c_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2010_c_vto/beam_2010_c_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2025_a_bau/beam_2025_a_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2025_a_vto/beam_2025_a_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2025_base/beam_2025_base_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2025_base_short_bau/beam_2025_base_short_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2025_base_short_vto/beam_2025_base_short_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_b_bau/beam_2040_b_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_b_vto/beam_2040_b_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_base/beam_2040_base_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_base_long_bau/beam_2040_base_long_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_base_long_vto/beam_2040_base_long_vto_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_c_bau/beam_2040_c_bau_mep/mep
+shp2csv.R -c /Users/critter/Dropbox/ucb/vto/smart-mobility/final-results/beam/beam_2040_c_vto/beam_2040_c_vto_mep/mep
