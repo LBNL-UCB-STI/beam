9ff5e1f59d1fd256b47087185998866ebbc8a69d
irishwithaxe <irishwithaxe@gmail.com>
2019-11-27 00:39:59 +0300

    added Trip Distance per iteration graph

--- src/main/scala/beam/sim/metrics/SimulationMetricCollector.scala
+++ src/main/scala/beam/sim/metrics/SimulationMetricCollector.scala
@@ -18,8 +18,7 @@ import scala.util.control.NonFatal
 
 object SimulationMetricCollector {
   val defaultMetricValueName: String = "count"
-
-  case class SimulationTime(time: Int) extends AnyVal
+  case class SimulationTime(seconds: Int) extends AnyVal
 }
 
 trait SimulationMetricCollector {
@@ -72,12 +71,12 @@ trait SimulationMetricCollector {
 
   def writeIterationjava(
     metricName: String,
-    time: Int,
+    seconds: Int,
     metricValue: Double = 1.0,
     level: MetricLevel,
     tags: java.util.Map[String, String] = Collections.EMPTY_MAP.asInstanceOf[java.util.Map[String, String]]
   ): Unit = {
-    write(metricName, SimulationTime(time), Map(defaultMetricValueName -> metricValue), tags.asScala.toMap, level)
+    write(metricName, SimulationTime(seconds), Map(defaultMetricValueName -> metricValue), tags.asScala.toMap, level)
   }
 
   def increment(name: String, time: SimulationTime, level: MetricLevel): Unit = {
@@ -151,7 +150,7 @@ class InfluxDbSimulationMetricCollector @Inject()(beamCfg: BeamConfig)
     if (isRightLevel(level)) {
       val rawPoint = Point
         .measurement(metricName)
-        .time(influxTime(metricName, time.time), TimeUnit.NANOSECONDS)
+        .time(influxTime(metricName, time.seconds), TimeUnit.NANOSECONDS)
 
       val withFields = values.foldLeft(rawPoint) {
         case (p, (n, v)) => p.addField(n, v)
