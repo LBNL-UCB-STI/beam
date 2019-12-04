998f7da4157fff98888ff97355642954124b5601
irishwithaxe <irishwithaxe@gmail.com>
2019-12-04 23:11:11 +0300

    +beam displaying logs during run +8g max ram for beam run

--- workshop/run-beam.sh
+++ workshop/run-beam.sh
@@ -9,4 +9,5 @@ docker run \
 	--mount source=$output,destination=/output,type=bind \
 	--link docker-influxdb-grafana:metrics \
 	--net beam_default \
-	beammodel/beam:workshop --config=$config
+  --env JAVA_OPTS='-Xmx8g -Xms4g -Dlogback.configurationFile=logback.xml' \
+  beammodel/beam:workshop --config=$config
