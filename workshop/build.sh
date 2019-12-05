1658fd058383972340119b62797b0409c97b8210
irishwithaxe <irishwithaxe@gmail.com>
2019-12-05 18:47:30 +0300

    +copy windows scripts to workshop folder

--- workshop/build.sh
+++ workshop/build.sh
@@ -5,6 +5,8 @@ mkdir beam
 cp -r ../test/input beam/input
 cp -r ../metrics2.0/* beam/
 cp run-beam.sh beam/
+cp run-beam.cmd beam/
+cp clear-data.cmd beam/
 cp metrics-for-docker-container.conf beam/input/common/metrics.conf
 mkdir beam/output
 chmod -R 777 beam
