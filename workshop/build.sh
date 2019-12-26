1266a0d47c77473b6572c5bb89e135738948fec2
irishwithaxe <irishwithaxe@gmail.com>
2019-12-26 18:51:04 +0300

    copying all `input` folder into workshop

--- workshop/build.sh
+++ workshop/build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 rm -rf beam*
-mkdir -p beam/input
-cp -r ../test/input/common beam/input/common
-cp -r ../test/input/beamville beam/input/beamville
-cp -r ../test/input/sf-light* beam/input
+mkdir beam
+mkdir beam/output
+cp -r ../test/input beam/input
 cp -r ../metrics2.0/* beam/
 cp run-beam.* beam/
 cp docker-pull.* beam/
 cp clear-data.cmd beam/
 cp metrics-for-docker-container.conf beam/input/common/metrics.conf
-mkdir beam/output
 chmod -R 777 beam
 chown -R $USER:$USER beam
 cd beam
 ./clear-data.sh
-
 
 if [[ $1 == 'pack' ]]
 then
