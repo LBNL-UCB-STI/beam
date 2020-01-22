585404ec97574d6a415b15b664e3b1ce9a2dfd91
haitam <10712736+htmlrb@users.noreply.github.com>
2020-01-21 16:46:43 -0800

    fix

--- src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
@@ -179,24 +179,24 @@ class PoolingAlonsoMora(val rideHailManager: RideHailManager)
         rideHailManager.log
           .debug("%%%%% Requests: {}", spatialPoolCustomerReqs.values().asScala.map(_.toString).mkString("\n"))
       }
-//      val alg =
-//        new VehicleCentricMatchingForRideHail(
-//          spatialPoolCustomerReqs,
-//          availVehicles,
-//          rideHailManager.beamServices
-//        )
+      val alg =
+        new VehicleCentricMatchingForRideHail(
+          spatialPoolCustomerReqs,
+          availVehicles,
+          rideHailManager.beamServices
+        )
 //      val alg =
 //        new AsyncAlonsoMoraAlgForRideHail(
 //          spatialPoolCustomerReqs,
 //          availVehicles,
 //          rideHailManager.beamServices
 //        )
-      val alg =
-        new AlonsoMoraPoolingAlgForRideHail(
-          spatialPoolCustomerReqs,
-          availVehicles,
-          rideHailManager.beamServices
-        )
+//      val alg =
+//        new AlonsoMoraPoolingAlgForRideHail(
+//          spatialPoolCustomerReqs,
+//          availVehicles,
+//          rideHailManager.beamServices
+//        )
       import scala.concurrent.duration._
       val assignment = try {
         Await.result(alg.matchAndAssign(tick), atMost = 2.minutes)
