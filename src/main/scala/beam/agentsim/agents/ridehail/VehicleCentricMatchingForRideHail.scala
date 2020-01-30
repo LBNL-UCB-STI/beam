aa375ba0fc1593e9f951ecac7f0a6d19192ac463
haitam <10712736+htmlrb@users.noreply.github.com>
2020-01-30 11:53:33 -0800

    k-1

--- src/main/scala/beam/agentsim/agents/ridehail/VehicleCentricMatchingForRideHail.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/VehicleCentricMatchingForRideHail.scala
@@ -85,7 +85,7 @@ class VehicleCentricMatchingForRideHail(
     val numPassengers = v.getFreeSeats
     for (k <- 2 to numPassengers) {
       val tripsWithKPassengers = mutable.ListBuffer.empty[(RideHailTrip, Double)]
-      val solutionSizePerPool = Math.pow(solutionSpaceSizePerVehicle, k)/2
+      val solutionSizePerPool = Math.pow(solutionSpaceSizePerVehicle, k-1)
       potentialTrips.zipWithIndex.foreach {
         case ((t1, _), pt1_index) =>
           potentialTrips
