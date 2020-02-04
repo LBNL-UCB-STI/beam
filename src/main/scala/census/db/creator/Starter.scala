2f325ed1404bebd052fcc1b5766797e572cd2e60
SHaaD94 <eugenzuy@gmail.com>
2020-02-04 23:31:26 +0700

    nearly final

--- src/main/scala/census/db/creator/Starter.scala
+++ src/main/scala/census/db/creator/Starter.scala
@@ -1,21 +1,50 @@
 package census.db.creator
 import java.nio.file.Paths
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import census.db.creator.config.Hardcoded
+import census.db.creator.database.DataRepoImpl
 import census.db.creator.service.fileDownloader.FileDownloadService
+import census.db.creator.service.shape.ShapefileRepo
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object Starter extends App {
-//  val repo: DataRepository = new DataRepoImpl(Hardcoded.config)
-//  val shape = "/Users/e.zuykin/Downloads/tl_2019_01_tract/tl_2019_01_tract.shp"
-//  val features = new ShapefileRepo(shape).getFeatures()
-
-//  repo.save(features)
+  private implicit val system = ActorSystem()
+  private implicit val materializer = ActorMaterializer()
+  private implicit val executionContext = system.dispatcher
 
   new java.io.File(Hardcoded.config.workingDir).mkdirs()
   new java.io.File(Paths.get(Hardcoded.config.workingDir, "zips").toString).mkdirs()
   new java.io.File(Paths.get(Hardcoded.config.workingDir, "shapes").toString).mkdirs()
 
-  new FileDownloadService(Hardcoded.config).downloadZipFiles()
+  for {
+    fileFutures <- new FileDownloadService(Hardcoded.config).downloadZipFiles()
+  } yield {
+    val futures = fileFutures.map { shape =>
+      shape
+        .map(x => x -> new ShapefileRepo(x).getFeatures())
+        .map {
+          case (sh, features) =>
+            val repo = new DataRepoImpl(Hardcoded.config)
+            repo.save(features)
+            repo.close()
+            sh
+        }
+        .map(sh => println(s"processed shape $sh"))
 
-  println()
+    }
+    Future
+      .sequence(futures)
+      .onComplete {
+        case Success(_) =>
+          println("Everything processed successfully")
+          System.exit(0)
+        case Failure(exception) =>
+          exception.printStackTrace()
+          System.exit(1)
+      }
+  }
 }
--- src/main/scala/census/db/creator/service/fileDownloader/FileDownloadService.scala
+++ src/main/scala/census/db/creator/service/fileDownloader/FileDownloadService.scala
@@ -1,54 +1,76 @@
 package census.db.creator.service.fileDownloader
-import java.io.{File, FileOutputStream}
+import java.io.{File, FileInputStream, FileOutputStream}
 import java.nio.file.Paths
+import java.util.zip.ZipInputStream
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpRequest
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.FileIO
 import akka.util.ByteString
 import census.db.creator.config.Config
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Failure
 
-class FileDownloadService(val config: Config) {
-  private implicit val system = ActorSystem()
-  private implicit val materializer = ActorMaterializer()
+class FileDownloadService(val config: Config)(
+  implicit val system: ActorSystem,
+  implicit val materializer : ActorMaterializer,
+  implicit val executionContext: ExecutionContext
+) {
   // needed for the future flatMap/onComplete in the end
-  private implicit val executionContext = system.dispatcher
-
-  def downloadZipFiles(): Future[Seq[String]] = {
+  def downloadZipFiles(): Future[Seq[Future[String]]] = {
     for {
       filesToDownloads <- getFileNames()
-      downloadedFiles  <- Future.traverse(filesToDownloads)(downloadFile)
-      unzippedFiles    <- Future.traverse(downloadedFiles)(unzipFile)
     } yield {
-      unzippedFiles
+      filesToDownloads
+        .map(config.censusUrl + _)
+        .map(downloadFile)
+        .map(x => x.flatMap(unzipFile))
     }
   }
 
   private def downloadFile(url: String): Future[String] = {
     val fileName = url.split("/").last
-    val downloadedZipFile = config.workingDir
     val downloadedFilesDir = "zips"
-    val resultFileName = Paths.get(downloadedZipFile, downloadedFilesDir, fileName).toString
-    val writer = new FileOutputStream(new File(resultFileName))
-    for {
+    val resultFileName = Paths.get(config.workingDir, downloadedFilesDir, fileName)
+    new File(resultFileName.toString).delete()
+    (for {
       zipFileStream <- Http().singleRequest(HttpRequest(uri = url))
-      _ <- zipFileStream.entity.dataBytes.runForeach { x =>
-        writer.write(x.asByteBuffer.array())
-        Done
-      }
+      _             <- zipFileStream.entity.withoutSizeLimit().dataBytes.runWith(FileIO.toPath(resultFileName))
     } yield {
-      writer.flush()
       println(s"Downloaded $resultFileName")
-      resultFileName
+      resultFileName.toString
+    }).andThen {
+      case Failure(exception) =>
+        exception.printStackTrace()
+        throw exception
     }
   }
 
   private def unzipFile(path: String): Future[String] = {
-    Future.successful(path)
+    val fileName = path.split("/").last
+    val shapefilesDir = "shapes"
+
+    val resultDir = Paths.get(config.workingDir, shapefilesDir).toString
+    var shapeFile: String = ""
+    Future {
+      val zis = new ZipInputStream(new FileInputStream(path))
+      Stream.continually(zis.getNextEntry).takeWhile(_ != null).foreach { file =>
+        val fileName = Paths.get(resultDir, file.getName).toString
+        if (fileName.endsWith(".shp"))
+          shapeFile = fileName
+        new File(fileName).delete()
+        val fout = new FileOutputStream(fileName)
+        val buffer = new Array[Byte](1024)
+        Stream.continually(zis.read(buffer)).takeWhile(_ != -1).foreach(fout.write(buffer, 0, _))
+        zis.closeEntry()
+      }
+      zis.close()
+      if (shapeFile.isEmpty) throw new RuntimeException(s"no shapefile! for file $path")
+      shapeFile
+    }
   }
 
   private def getFileNames() = {
