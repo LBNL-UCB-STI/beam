a2a87d8e773c3f8e3f364a0bce7aa4c4395b5102
SHaaD94 <eugenzuy@gmail.com>
2020-02-05 22:35:23 +0700

    1

--- src/main/scala/census/db/creator/service/actors/DownloadActor.scala
+++ src/main/scala/census/db/creator/service/actors/DownloadActor.scala
@@ -1,6 +1,7 @@
 package census.db.creator.service.actors
 import java.io.File
 import java.nio.file.Paths
+import java.util.concurrent.Semaphore
 
 import akka.actor.{Actor, ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
@@ -9,7 +10,7 @@ import akka.stream.Materializer
 import akka.stream.scaladsl.FileIO
 import census.db.creator.config.Config
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext}
 import scala.util.Failure
 
 case class DownloadMessage(url: String)
@@ -20,16 +21,22 @@ class DownloadActor(config: Config, unzipActor: ActorRef)(
 ) extends Actor {
   private implicit val actorSystem: ActorSystem = context.system
 
+  //TODO limit downloading files count in some other way
+  private val semaphore = new Semaphore(20)
+
   override def receive: Receive = {
     case DownloadMessage(url) =>
       val fileName = url.split("/").last
       val downloadedFilesDir = "zips"
       val resultFileName = Paths.get(config.workingDir, downloadedFilesDir, fileName)
       new File(resultFileName.toString).delete()
-      (for {
+
+      semaphore.acquire()
+      val future = (for {
         zipFileStream <- Http().singleRequest(HttpRequest(uri = url))
         _             <- zipFileStream.entity.withoutSizeLimit().dataBytes.runWith(FileIO.toPath(resultFileName))
       } yield {
+        semaphore.release()
         println(s"Downloaded $fileName")
         unzipActor ! UnzipMessage(resultFileName.toString)
       }).andThen {
@@ -37,6 +44,9 @@ class DownloadActor(config: Config, unzipActor: ActorRef)(
           exception.printStackTrace()
           throw exception
       }
+
+      import scala.concurrent.duration._
+      Await.result(future, 10.minutes)
   }
 
 }
--- src/main/scala/census/db/creator/service/actors/TazSavingActor.scala
+++ src/main/scala/census/db/creator/service/actors/TazSavingActor.scala
@@ -1,5 +1,7 @@
 package census.db.creator.service.actors
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import akka.actor.{Actor, ActorSystem}
 import census.db.creator.config.Config
 import census.db.creator.database.TazInfoRepoImpl
@@ -12,11 +14,15 @@ case class TazBatchMessage(tazBatch: Seq[TazInfo])
 class TazSavingActor(config: Config)(private implicit val executionContext: ExecutionContext) extends Actor {
   private implicit val actorSystem: ActorSystem = context.system
 
+  private val counter = new AtomicInteger(0)
+
   override def receive: Receive = {
     case TazBatchMessage(features) =>
       val repo = new TazInfoRepoImpl(config)
       repo.save(features)
       repo.close()
+
+      println(s"Processed ${counter.incrementAndGet()} files")
   }
 
 }
