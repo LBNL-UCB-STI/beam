65d6dc92903a728047558e27473ad06eef5ba81d
Artavazd Balaian <reasyu@gmail.com>
2020-02-10 23:25:28 +0700

    Move to `PointGenerator` to be able to cherry-pick

---
+++ src/main/scala/beam/taz/PointGenerator.scala
@@ -0,0 +1,40 @@
+package beam.taz
+
+import com.vividsolutions.jts.geom.{Geometry, GeometryFactory, PrecisionModel}
+import com.vividsolutions.jts.shape.random.RandomPointsInGridBuilder
+import org.matsim.api.core.v01.Coord
+
+import scala.annotation.tailrec
+
+trait PointGenerator {
+  def generate(geometry: Geometry, nPoints: Int): Seq[Coord]
+}
+
+object RandomPointsInGridGenerator extends PointGenerator {
+  private val projection: Int = 4326
+  private val geometryFactory: GeometryFactory = new GeometryFactory(new PrecisionModel(), projection)
+
+  override def generate(geometry: Geometry, nPoints: Int): Seq[Coord] = {
+    val pointsBuilder = new RandomPointsInGridBuilder(geometryFactory)
+    pointsBuilder.setExtent(geometry.getEnvelopeInternal)
+
+    @tailrec
+    def generate(n: Int): Seq[Coord] = {
+      pointsBuilder.setNumPoints(n)
+      val multiPoint = pointsBuilder.getGeometry
+      val points = (0 until multiPoint.getNumGeometries)
+        .map(multiPoint.getGeometryN)
+        .filter(geometry.contains)
+        .map { x =>
+          val coord = x.getCoordinate
+          new Coord(coord.getOrdinate(0), coord.getOrdinate(1))
+        }
+
+      if (points.size >= nPoints) return points.take(nPoints)
+
+      generate((n * 1.1).toInt)
+    }
+
+    generate((nPoints * 1.1).toInt)
+  }
+}
--- src/main/scala/beam/taz/TazCoordinateGenerator.scala
+++ src/main/scala/beam/taz/TazCoordinateGenerator.scala
@@ -1,44 +1,27 @@
 package beam.taz
-import census.db.creator.GeometryUtil
 import census.db.creator.database.TazRepository
 import census.db.creator.domain.TazInfo
 import com.vividsolutions.jts.geom._
-import com.vividsolutions.jts.shape.random.RandomPointsInGridBuilder
+import org.matsim.api.core.v01.Coord
 
-import scala.annotation.tailrec
 import scala.collection.concurrent.TrieMap
 
 trait TazCoordinateGenerator {
-  def generate(geoId: String, number: Int): Seq[Coordinate]
+  def generate(geoId: String, number: Int): Seq[Coord]
 }
 
 class TazCoordinateGeneratorImpl(private val osmService: OsmService, private val tazRepo: TazRepository)
     extends TazCoordinateGenerator {
   private val tazCache = TrieMap.empty[String, TazInfo]
 
-  override def generate(geoId: String, number: Int): Seq[Coordinate] = {
+  override def generate(geoId: String, number: Int): Seq[Coord] = {
     val taz = tazCache.getOrElseUpdate(geoId, {
       val tazs = tazRepo.query(Some(geoId))
       require(tazs.size == 1, { s"Failed to find TAZ for geoid $geoId" })
       tazs.head
     })
 
-    val pointsBuilder = new RandomPointsInGridBuilder(GeometryUtil.geometryFactory)
-    pointsBuilder.setExtent(taz.preparedGeometry.getGeometry.getEnvelopeInternal)
-
-    @tailrec def generate(n: Int): Seq[Coordinate] = {
-      pointsBuilder.setNumPoints(n)
-      val multiPoint = pointsBuilder.getGeometry
-      val points = (0 until multiPoint.getNumGeometries)
-        .map(multiPoint.getGeometryN)
-        .filter(taz.preparedGeometry.contains)
-        .map(_.getCoordinate)
-
-      if (points.size >= number) return points.take(number)
-
-      generate((n * 1.1).toInt)
-    }
-
-    generate((number * 1.1).toInt)
+    val geometry: Geometry = taz.preparedGeometry.getGeometry
+    RandomPointsInGridGenerator.generate(geometry, number)
   }
 }
