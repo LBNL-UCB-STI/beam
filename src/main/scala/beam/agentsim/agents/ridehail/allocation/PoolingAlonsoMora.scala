f06f7c45339198df4ed3a8c1fed88ee5d1110170
SHaaD94 <eugenzuy@gmail.com>
2020-02-29 20:08:56 +0700

    return default pooling algo

--- src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
@@ -180,7 +180,7 @@ class PoolingAlonsoMora(val rideHailManager: RideHailManager)
           .debug("%%%%% Requests: {}", spatialPoolCustomerReqs.values().asScala.map(_.toString).mkString("\n"))
       }
       val alg =
-        new AlonsoMoraPoolingAlgForRideHail(
+        new VehicleCentricMatchingForRideHail(
           spatialPoolCustomerReqs,
           availVehicles,
           rideHailManager.beamServices
