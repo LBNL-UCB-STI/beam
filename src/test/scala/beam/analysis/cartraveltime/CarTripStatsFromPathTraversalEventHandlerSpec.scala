60cca8191ed5909ab33b377dfd99845063b9d46e
Artavazd Balaian <reasyu@gmail.com>
2020-03-21 01:00:10 +0700

    Added regression for iteration files

--- src/test/scala/beam/analysis/cartraveltime/CarTripStatsFromPathTraversalEventHandlerSpec.scala
+++ src/test/scala/beam/analysis/cartraveltime/CarTripStatsFromPathTraversalEventHandlerSpec.scala
@@ -1,5 +1,6 @@
 package beam.analysis.cartraveltime
 import beam.agentsim.agents.GenericEventsSpec
+import org.scalatest.Assertion
 import org.scalatest.Matchers._
 
 import scala.reflect.io.File
@@ -21,10 +22,19 @@ class CarTripStatsFromPathTraversalEventHandlerSpec extends GenericEventsSpec {
       checkFileExistenceInRoot("CarTravelDistance.csv")
       checkFileExistenceInRoot("CarSpeed.csv")
       checkFileExistenceInRoot("FreeFlowCarSpeed.csv")
+
+      // If those start to fail, someone changed vehicle types for beamville or `CarTripStatsFromPathTraversalEventHandler` is changed
+      checkFileExistenceInIterFolder("ridehail.CarRideStats.csv.gz", 0)
+      checkFileExistenceInIterFolder("personal.CarRideStats.csv.gz", 0)
+      checkFileExistenceInIterFolder("cav.CarRideStats.csv.gz", 0)
     }
   }
 
-  private def checkFileExistenceInRoot(file: String) = {
+  private def checkFileExistenceInRoot(file: String): Assertion = {
     File(beamServices.matsimServices.getControlerIO.getOutputFilename(file)).isFile shouldEqual true
+  }
+
+  private def checkFileExistenceInIterFolder(file: String, iteration: Int): Assertion = {
+    File(beamServices.matsimServices.getControlerIO.getIterationFilename(iteration, file)).isFile shouldEqual true
   }
 }
