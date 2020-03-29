51f7f5b9131e7917c3a01fa40a662830f9b4caeb
Artavazd Balaian <reasyu@gmail.com>
2020-03-29 18:11:31 +0700

    - Compute routes for all modes - Get the right itinerary by finding with the right `tripClassifier` - Pass `attributesOfIndividual` to the routing request

--- src/main/scala/beam/router/skim/PeakSkimCreator.scala
+++ src/main/scala/beam/router/skim/PeakSkimCreator.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import beam.agentsim.agents.modalbehaviors.ModeChoiceCalculator
 import beam.agentsim.agents.vehicles.VehicleProtocol.StreetVehicle
-import beam.agentsim.agents.vehicles.{BeamVehicleType, VehicleCategory}
+import beam.agentsim.agents.vehicles.{BeamVehicle, BeamVehicleType, VehicleCategory}
 import beam.agentsim.events.SpaceTime
 import beam.agentsim.infrastructure.taz.TAZ
 import beam.router.BeamRouter.RoutingRequest
@@ -15,7 +15,7 @@ import beam.router.r5.{R5Wrapper, WorkerParameters}
 import beam.sim.BeamServices
 import beam.sim.config.BeamConfig
 import beam.sim.population.{AttributesOfIndividual, HouseholdAttributes, PopulationAdjustment}
-import beam.utils.ProfilingUtils
+import beam.utils.{DebugLib, ProfilingUtils}
 import com.typesafe.scalalogging.StrictLogging
 import org.matsim.api.core.v01.{Coord, Id, Scenario}
 import org.matsim.core.controler.events.IterationEndsEvent
@@ -40,7 +40,12 @@ class PeakSkimCreator(val beamServices: BeamServices, val config: BeamConfig, va
   private val dummyBodyVehicleType: BeamVehicleType =
     beamServices.beamScenario.vehicleTypes.values.find(theType => theType.vehicleCategory == VehicleCategory.Body).get
 
+  private val dummyBikeVehicleType: BeamVehicleType =
+    beamServices.beamScenario.vehicleTypes.values.find(theType => theType.vehicleCategory == VehicleCategory.Bike).get
+
   private val tazs: Array[TAZ] = beamServices.beamScenario.tazTreeMap.getTAZs.toArray.sortBy(_.tazId.toString)
+
+  private val beamModes: Array[BeamMode] = Array(BeamMode.CAR, BeamMode.BIKE, BeamMode.WALK_TRANSIT)
 
   def write(iteration: Int): Unit = {
     try {
@@ -76,23 +81,33 @@ class PeakSkimCreator(val beamServices: BeamServices, val config: BeamConfig, va
     val failedRoutes = new AtomicInteger(0)
     val emptyItineraries = new AtomicInteger(0)
     val skimEvents = ProfilingUtils.timed("Creating skim events", logger.info(_)) {
+      // The most outer loop will be executed in parallel `.par`
       tazs.par
         .flatMap { srcTaz =>
           tazs.map { dstTaz =>
-            val routingReq: RoutingRequest = createRoutingRequest(BeamMode.CAR, requestTime, srcTaz, dstTaz)
-            Try(r5Wrapper.calcRoute(routingReq)) match {
-              case Failure(ex) =>
-                failedRoutes.getAndIncrement()
-                None
-              case Success(response) =>
-                response.itineraries.headOption match {
-                  case Some(tripItin) =>
-                    val event: ODSkimmerEvent = createSkimEvent(requestTime, tripItin)
-                    Some(event)
-                  case None =>
-                    emptyItineraries.getAndIncrement()
-                    None
-                }
+            beamModes.flatMap { beamMode =>
+              val routingReq: RoutingRequest = createRoutingRequest(beamMode, requestTime, srcTaz, dstTaz)
+              Try(r5Wrapper.calcRoute(routingReq)) match {
+                case Failure(ex) =>
+                  failedRoutes.getAndIncrement()
+                  None
+                case Success(response) =>
+                  val maybeRightTrip = response.itineraries.find(trip => trip.tripClassifier == beamMode)
+                  maybeRightTrip match {
+                    case Some(trip) =>
+                      val maybeEvent = try {
+                        Some(createSkimEvent(beamMode, requestTime, trip))
+                      } catch {
+                        case NonFatal(ex) =>
+                          logger.error(s"Can't create skim event: ${ex.getMessage}", ex)
+                          None
+                      }
+                      maybeEvent
+                    case None =>
+                      emptyItineraries.getAndIncrement()
+                      None
+                  }
+              }
             }
           }
         }
@@ -105,27 +120,32 @@ class PeakSkimCreator(val beamServices: BeamServices, val config: BeamConfig, va
     skimmer
   }
 
-  private def createSkimEvent(requestTime: Int, tripItin: EmbodiedBeamTrip): ODSkimmerEvent = {
-    val actualLegs = tripItin.legs
-    val theTrip = EmbodiedBeamTrip(
-      EmbodiedBeamLeg.dummyLegAt(
-        start = actualLegs.head.beamLeg.startTime,
-        vehicleId = Id.createVehicleId("dummy-body"),
-        isLastLeg = false,
-        location = actualLegs.head.beamLeg.travelPath.startPoint.loc,
-        mode = WALK,
-        vehicleTypeId = dummyBodyVehicleType.id
-      ) +:
-      actualLegs :+
-      EmbodiedBeamLeg.dummyLegAt(
-        start = actualLegs.last.beamLeg.endTime,
-        vehicleId = Id.createVehicleId("dummy-body"),
-        isLastLeg = true,
-        location = actualLegs.last.beamLeg.travelPath.endPoint.loc,
-        mode = WALK,
-        vehicleTypeId = dummyBodyVehicleType.id
+  private def createSkimEvent(beamMode: BeamMode, requestTime: Int, trip: EmbodiedBeamTrip): ODSkimmerEvent = {
+    // In case of CAR AND BIKE we have to create two dummy legs: walk to the CAR in the beginning and walk when CAR has arrived
+    val theTrip = if (beamMode == BeamMode.CAR || beamMode == BeamMode.BIKE) {
+      val actualLegs = trip.legs
+      EmbodiedBeamTrip(
+        EmbodiedBeamLeg.dummyLegAt(
+          start = actualLegs.head.beamLeg.startTime,
+          vehicleId = Id.createVehicleId("dummy-body"),
+          isLastLeg = false,
+          location = actualLegs.head.beamLeg.travelPath.startPoint.loc,
+          mode = WALK,
+          vehicleTypeId = dummyBodyVehicleType.id
+        ) +:
+        actualLegs :+
+        EmbodiedBeamLeg.dummyLegAt(
+          start = actualLegs.last.beamLeg.endTime,
+          vehicleId = Id.createVehicleId("dummy-body"),
+          isLastLeg = true,
+          location = actualLegs.last.beamLeg.travelPath.endPoint.loc,
+          mode = WALK,
+          vehicleTypeId = dummyBodyVehicleType.id
+        )
       )
-    )
+    } else {
+      trip
+    }
     val generalizedTime =
       modeChoiceCalculator.getGeneralizedTimeOfTrip(theTrip, Some(dummyPersonAttributes), None)
     val generalizedCost = modeChoiceCalculator.getNonTimeCost(theTrip) + dummyPersonAttributes.getVOT(generalizedTime)
@@ -148,13 +168,34 @@ class PeakSkimCreator(val beamServices: BeamServices, val config: BeamConfig, va
   }
 
   private def createRoutingRequest(mode: BeamMode, requestTime: Int, srcTaz: TAZ, dstTaz: TAZ): RoutingRequest = {
-    val dummyStreetVehicle = StreetVehicle(
-      Id.createVehicleId("dummy-car-for-skim-observations"),
-      dummyCarVehicleType.id,
-      new SpaceTime(srcTaz.coord, requestTime),
-      mode,
-      asDriver = true
-    )
+    val streetVehicle: StreetVehicle = mode match {
+      case BeamMode.CAR =>
+        StreetVehicle(
+          Id.createVehicleId("dummy-car-for-skim-observations"),
+          dummyCarVehicleType.id,
+          new SpaceTime(srcTaz.coord, requestTime),
+          mode,
+          asDriver = true
+        )
+      case BeamMode.BIKE =>
+        StreetVehicle(
+          Id.createVehicleId("dummy-bike-for-skim-observations"),
+          dummyBikeVehicleType.id,
+          new SpaceTime(srcTaz.coord, requestTime),
+          mode,
+          asDriver = true
+        )
+      case BeamMode.WALK_TRANSIT =>
+        StreetVehicle(
+          Id.createVehicleId("dummy-body-for-skim-observations"),
+          dummyBodyVehicleType.id,
+          new SpaceTime(srcTaz.coord, requestTime),
+          WALK,
+          asDriver = true
+        )
+      case x =>
+        throw new IllegalArgumentException(s"Get mode ${x}, but don't know what to do with it.")
+    }
     val srcCoord = if (srcTaz.tazId.equals(dstTaz.tazId)) {
       new Coord(srcTaz.coord.getX + Math.sqrt(srcTaz.areaInSquareMeters) / 3.0, srcTaz.coord.getY)
     } else {
@@ -172,8 +213,9 @@ class PeakSkimCreator(val beamServices: BeamServices, val config: BeamConfig, va
       originUTM = srcCoord,
       destinationUTM = dstCoord,
       departureTime = requestTime,
-      withTransit = mode == BeamMode.BUS,
-      streetVehicles = Vector(dummyStreetVehicle)
+      withTransit = mode == BeamMode.WALK_TRANSIT,
+      streetVehicles = Vector(streetVehicle),
+      attributesOfIndividual = Some(dummyPersonAttributes)
     )
     routingReq
   }
