b46aef2ee19a997ad49a0ddc27087119fdd5b483
SHaaD94 <eugenzuy@gmail.com>
2020-03-31 00:13:33 +0700

    reading lat long from graph

--- src/main/java/beam/physsim/routingTool/GraphReader.scala
+++ src/main/java/beam/physsim/routingTool/GraphReader.scala
@@ -1,19 +1,62 @@
 package beam.physsim.routingTool
-import java.io.{DataInputStream, File}
-import java.nio.ByteBuffer
+import java.io.File
+import java.nio.{ByteBuffer, ByteOrder}
 
 import com.google.common.io.Files
 
-object GraphReader extends App{
+import scala.collection.mutable
+
+object GraphReader extends App {
   val graphBinary = new File("/tmp/rt/beamville_graph.gr.bin")
 
-  val source = Files.asByteSource(graphBinary)
+  val source = Files.asByteSource(graphBinary).openStream()
 
+  val vertices = readInt()
+  val edges = readInt()
 
+  val outEdgesFirst = (0 until vertices).map(_ => readInt())
 
-  val stream = new DataInputStream(source.openStream())
+  val outEdgesEnd = (0 until edges).map(_ => readInt())
 
-  print(stream.readInt())
-  print(1)
+  val numOfAttributes = readInt()
 
+  (0 until numOfAttributes).foreach { _ =>
+    val attributeName = readString()
+    val size = readInt()
+
+    if (attributeName == "lat_lng") {
+      readInt() // skip list size
+      val index2LatLng = (0 until vertices)
+        .map(i => i -> (readInt() / 1000000.0 -> readInt()/1000000.0))
+      println()
+    } else {
+      source.skip(size)
+    }
+  }
+
+  throw new RuntimeException("lat_lng attribute not found in graph")
+
+  println()
+
+  //------------------------
+  private def readString(): String = {
+    val b = mutable.ArrayBuffer[Byte]()
+    Stream
+      .continually(source.read())
+      .takeWhile(_ != -1)
+      .map(_.toByte)
+      .takeWhile(_ != '\u0000')
+      .foreach(b += _)
+    new String(b.toArray)
+  }
+
+  private def readInt(): Int = {
+    readLittleEndianBuffer(4).getInt
+  }
+
+  private def readLittleEndianBuffer(size: Int): ByteBuffer = {
+    val bytes = new Array[Byte](size)
+    source.read(bytes)
+    ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN)
+  }
 }
--- src/main/java/beam/physsim/routingTool/RoutingToolWrapper.scala
+++ src/main/java/beam/physsim/routingTool/RoutingToolWrapper.scala
@@ -58,7 +58,7 @@ class InternalRTWrapper (private val pbfPath : String) extends RoutingToolWrappe
         | $toolDockerImage
         | $createODPairs
         | -g $graphPathWithExtension
-        | -n 100
+        | -n 1000
         | -o $odPairsFile -d 10 15 20 25 30 -geom
       """.stripMargin.replace("\n", ""))
 
