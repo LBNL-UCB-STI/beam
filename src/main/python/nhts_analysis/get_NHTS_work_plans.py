5705b553653184bf90d091e0ae98ac0df60a0cca
Zach Needell <zaneedell@lbl.gov>
2020-04-10 14:42:41 -0700

    get intercepts

--- src/main/python/nhts_analysis/get_NHTS_work_plans.py
+++ src/main/python/nhts_analysis/get_NHTS_work_plans.py
@@ -12,19 +12,43 @@ persons_all = pd.read_csv('Data/NHTS/perpub.csv')
 
 #%%
 
+
+activities = {1:'Home',2:'Home',3:'Work',8:'Work',11:'Shopping',12:'Shopping',13:'Meal',15:'SocRec',16:'SocRec',17:'SocRec',19:'SocRec'}
+
 def getActivities(trips):
     locations = np.append(trips.WHYFROM.values,trips.WHYTO.values[-1])
+    locations = [activities.get(val,'Other') for val in locations]
     startTimes = np.append([0], trips.endHour.values)
     endTimes = np.append(trips.startHour.values, [24])
     durations = endTimes - startTimes
-    return pd.DataFrame({'location':locations,'startTime':startTimes,'endTime':endTimes,'duration':durations})
+    weights = np.append(trips.WTTRDFIN.values, [trips.WTTRDFIN.values[0]])
+    return pd.DataFrame({'location':locations,'startTime':startTimes,'endTime':endTimes,'duration':durations,'weight':weights})
 
+
+def getIntercepts(activities):
+    activities.reset_index(inplace=True)
+    locations = ['Other','Shopping','Meal','SocRec']
+    intercepts = dict()
+    nPeople = activities['UniquePID'].nunique()
+    for location in locations:
+        counts, bins = np.histogram(activities.loc[activities.location == location,'startTime'],range(26), weights = activities.loc[activities.location == location,'weight'])
+        counts = counts / nPeople /3000
+        counts[counts < 0.1] = 0.0
+        intercepts[location] = counts
+    return pd.DataFrame(intercepts, columns=locations)
+    
+
+def getParams(activities):
+    activities.reset_index(inplace=True)
+    locations = ['Other','Shopping','Meal','SocRec']
+    intercepts = dict()
+    nPeople = activities['UniquePID'].nunique()
 #%%
 for cbsa in ['12420']:#persons_all.HH_CBSA.unique():
     trips = trips_all.loc[(trips_all['HH_CBSA'] == cbsa) , :]
 
 
-    valid = (trips.TRPMILES > 0) & (trips.TDWKND == 2) & (trips.TRPTRANS != 19)
+    valid = (trips.TRPMILES > 0) & (trips.TDWKND == 2) & (trips.TRPTRANS != 19) & (trips.ENDTIME > trips.STRTTIME)
     
     
     
@@ -35,40 +59,12 @@ for cbsa in ['12420']:#persons_all.HH_CBSA.unique():
     trips['toWork'] = (trips.WHYTO == 3) | (trips.WHYTO == 4)
     trips['fromWork'] = (trips.WHYFROM == 3) | (trips.WHYFROM == 4)
     
-    
-    workInvolved = (trips.TRIPPURP == 'HBW')
-    workTrips = trips.loc[workInvolved, :]
+    out = trips.groupby('UniquePID').apply(getActivities)
 
-    persons = persons_all.loc[(persons_all['HH_CBSA'] == cbsa), :]
-    
-    valid = (persons.TRAVDAY > 1) & (persons.TRAVDAY < 7)
-    
-    persons = persons.loc[valid,:]
-    persons['UniquePID'] = persons.HOUSEID * 100 + persons.PERSONID
-    
-    workPIDs = set(workTrips.UniquePID)
-    workerTrips = trips.loc[trips['UniquePID'].isin(workPIDs),:]
-
-    starts = workerTrips.loc[workerTrips.toWork].groupby('UniquePID')['startHour'].agg('first')
-    ends = workerTrips.loc[workerTrips.fromWork].groupby('UniquePID')['startHour'].agg('last')
-    
-    workers = persons.loc[persons['UniquePID'].isin(workPIDs),:].set_index('UniquePID')
-    toMerge = pd.concat([starts,ends],axis=1,join='inner')
-    toMerge.columns = ['startWork','endWork']
-    
-    workers = workers.merge(toMerge,left_index=True, right_index=True)
-    workers['workDuration'] = workers['endWork'] - workers['startWork']
-    
-    workers['startTimeIndex'] = pd.cut(workers.startWork,np.arange(4,21,0.25))
-    workers['durationIndex'] = pd.cut(workers.workDuration,np.arange(0,15,0.25))
-    
-    binProb = workers.groupby(['startTimeIndex','durationIndex']).agg({'WTPERFIN':'sum'}).fillna(0)
-    binProb = binProb / binProb.sum()
-    
-    mat = binProb.unstack()
-    mat2 = pd.DataFrame(scipy.ndimage.filters.gaussian_filter(mat.values,[1.0,1.0]), index = mat.index, columns = mat.columns)
-    binProb = mat2.stack().rename(columns={'WTPERFIN':'probability'})
-    binProb.to_csv('outputs/work_activities_'+cbsa+'.csv')
+    intercepts = getIntercepts(out)
+    # mat2 = pd.DataFrame(scipy.ndimage.filters.gaussian_filter(mat.values,[1.0,1.0]), index = mat.index, columns = mat.columns)
+    # binProb = mat2.stack().rename(columns={'WTPERFIN':'probability'})
+    # binProb.to_csv('outputs/work_activities_'+cbsa+'.csv')
 
 
 #%%
