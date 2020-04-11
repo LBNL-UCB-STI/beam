28bc88e864ef8050adbe341eef31e4a427e7ad64
Carlos Caldas <ccaldas@gmail.com>
2020-04-11 07:14:43 -0300

    renamed arguments

--- src/main/scala/beam/agentsim/infrastructure/geozone/GeoZone.scala
+++ src/main/scala/beam/agentsim/infrastructure/geozone/GeoZone.scala
@@ -4,15 +4,15 @@ import scala.collection.parallel.ParSet
 
 import H3Wrapper.geoToH3Address
 
-class GeoZone(points: ParSet[WgsCoordinate]) {
+class GeoZone(coordinates: ParSet[WgsCoordinate]) {
 
-  def this(points: Set[WgsCoordinate]) {
-    this(points.par)
+  def this(coordinates: Set[WgsCoordinate]) {
+    this(coordinates.par)
   }
 
   def includeBoundBoxPoints: GeoZone = {
-    val newPoints = WgsRectangle.from(points).coordinates
-    val allPoints = points ++ newPoints
+    val newPoints = WgsRectangle.from(coordinates).coordinates
+    val allPoints = coordinates ++ newPoints
     new GeoZone(allPoints)
   }
 
@@ -20,7 +20,7 @@ class GeoZone(points: ParSet[WgsCoordinate]) {
     expectedNumberOfBuckets: Int,
     initialResolution: Int = 2
   ): TopDownEqualDemandsGeoZoneHexGenerator = {
-    val allContent = GeoZone.generateContent(points, initialResolution)
+    val allContent = GeoZone.generateContent(coordinates, initialResolution)
     val allHexagons: IndexedSeq[HexagonLeaf] = allContent.map {
       case (index, points) => HexagonLeaf(index, points)
     }.toIndexedSeq
