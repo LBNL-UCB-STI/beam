75773a4cd7e33b0b0bc21ea6b4aa1155e9b0cec4
Carlos Caldas <ccaldas@gmail.com>
2020-04-11 07:13:19 -0300

    replace custom function to use GenericCsvReader

--- src/main/scala/beam/agentsim/infrastructure/geozone/GeoZoneUtil.scala
+++ src/main/scala/beam/agentsim/infrastructure/geozone/GeoZoneUtil.scala
@@ -1,44 +1,35 @@
 package beam.agentsim.infrastructure.geozone
 
-import java.io.{FileInputStream, InputStreamReader}
+import java.io.Closeable
 import java.nio.file.Path
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
+import beam.utils.csv.GenericCsvReader
 import com.typesafe.scalalogging.LazyLogging
 import com.uber.h3core.util.GeoCoord
 import com.vividsolutions.jts.geom.{GeometryFactory, Polygon}
 import org.matsim.core.utils.geometry.geotools.MGC
 import org.matsim.core.utils.gis.{PolygonFeatureFactory, ShapeFileWriter}
-import org.supercsv.io.{CsvMapReader, ICsvMapReader}
-import org.supercsv.prefs.CsvPreference
 
 object GeoZoneUtil extends LazyLogging {
 
   def readWgsCoordinatesFromCsv(relativePath: Path): Set[WgsCoordinate] = {
-    var mapReader: ICsvMapReader = null
-    val res = ArrayBuffer[WgsCoordinate]()
+    val (iter: Iterator[WgsCoordinate], toClose: Closeable) =
+      GenericCsvReader.readAs[WgsCoordinate](relativePath.toString, toWgsCoordinate, _ => true)
     try {
-      mapReader = new CsvMapReader(readerFromFile(relativePath), CsvPreference.STANDARD_PREFERENCE)
-      val header = mapReader.getHeader(true)
-      var line: java.util.Map[String, String] = mapReader.read(header: _*)
-      while (null != line) {
-        val coordX = line.get("longitude").toDouble
-        val coordY = line.get("latitude").toDouble
-        res.append(WgsCoordinate(coordY, coordX))
-        line = mapReader.read(header: _*)
-      }
+      iter.toSet
     } finally {
-      if (null != mapReader)
-        mapReader.close()
+      toClose.close()
     }
-    res.toSet
   }
 
-  private def readerFromFile(filePath: Path): java.io.Reader = {
-    new InputStreamReader(new FileInputStream(filePath.toFile))
+  def toWgsCoordinate(rec: java.util.Map[String, String]): WgsCoordinate = {
+    WgsCoordinate(
+      latitude = rec.get("latitude").toDouble,
+      longitude = rec.get("longitude").toDouble
+    )
   }
 
   def writeToShapeFile(filePath: Path, content: GeoZoneSummary): Unit = {
