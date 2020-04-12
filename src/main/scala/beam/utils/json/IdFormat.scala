940d3b902e0e5e171a12ad8a9aee6ecc2791286d
Artavazd Balaian <reasyu@gmail.com>
2020-04-13 01:52:26 +0700

    Make sure `null` is handled

--- src/main/scala/beam/utils/json/IdFormat.scala
+++ src/main/scala/beam/utils/json/IdFormat.scala
@@ -7,7 +7,9 @@ import org.matsim.api.core.v01.Id
 import scala.reflect.ClassTag
 
 class IdFormat[T](implicit val ct: ClassTag[T]) extends Encoder[Id[T]] with Decoder[Id[T]] {
-  override def apply(o: Id[T]): Json = Json.fromString(o.toString)
+  override def apply(o: Id[T]): Json = {
+    Option(o).map(id => Json.fromString(id.toString)).getOrElse(Json.Null)
+  }
 
   override def apply(c: HCursor): Result[Id[T]] = {
     c.as[String].map { id =>
