d349f249a0677966847c7ad9d74c76ac8bc3c9a4
wrashid <sfA1kewo>
2020-04-18 13:52:38 -0500

    adding write out road capacities shapefile

--- src/main/scala/beam/utils/scripts/austin_network/LinkReader.scala
+++ src/main/scala/beam/utils/scripts/austin_network/LinkReader.scala
@@ -1,12 +1,19 @@
 package beam.utils.scripts.austin_network
 
+import beam.sim.common.GeoUtils
 import org.matsim.api.core.v01.{Coord, Id}
 import org.matsim.api.core.v01.network.Link
+import org.matsim.core.utils.geometry.geotools.MGC
+import org.matsim.core.utils.gis.{PointFeatureFactory, ShapeFileWriter}
+import org.opengis.feature.simple.SimpleFeature
+import scala.collection.JavaConverters._
+
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 case class LinkDetails(
   linkChainId: Id[Link],
   length: Double,
-  speed: Double,
+  speedInMetersPerSecond: Double,
   capacity: Int,
   lanes: Int,
   geometry: List[Coord]
@@ -16,8 +23,8 @@ object LinkReader {
 
   val skipLine = 6 // 5 for information and 1 for header
   def main(args: Array[String]): Unit = {
-    val detailFilePath = "/home/rajnikant/Desktop/austin-data/austin.2015_regional_am.public.linkdetails.csv"
-    val publicLinkPath = "/home/rajnikant/Desktop/austin-data/austin.2015_regional_am.public.links.csv"
+    val detailFilePath = "E:\\work\\austin\\austin.2015_regional_am.public.linkdetails.csv"
+    val publicLinkPath = "E:\\work\\austin\\austin.2015_regional_am.public.links.csv"
     val publicLinkSource = Source.fromFile(publicLinkPath)
     val publicLink = publicLinkSource.getLines().drop(skipLine).map(linkGeometry).toMap
     publicLinkSource.close()
@@ -25,9 +32,39 @@ object LinkReader {
     val detailLinkSource = Source.fromFile(detailFilePath)
     val linkDetails = detailLinkSource.getLines().drop(skipLine).map(linkDetail(publicLink))
 
+    createShapeFile(linkDetails,"E:\\work\\austin\\capacityAustin.shp")
+
     linkDetails.foreach(linkDetails => println(linkDetails))
     detailLinkSource.close()
   }
+
+  def createShapeFile(linkDetails: Iterator[LinkDetails],shapeFileOutputPath:String)={
+    val features = ArrayBuffer[SimpleFeature]()
+
+    val pointf: PointFeatureFactory = new PointFeatureFactory.Builder()
+      .setCrs(MGC.getCRS("EPSG:4326"))
+      .setName("nodes")
+      .addAttribute("Resolution", classOf[java.lang.Integer])
+      .addAttribute("capacity", classOf[java.lang.Integer])
+      .addAttribute("lanes", classOf[java.lang.Integer])
+      .addAttribute("speedInMPS", classOf[java.lang.Double])
+      .create()
+
+    linkDetails.foreach{ link =>
+      link.geometry.foreach{ wsgCoord=>
+        val coord=new com.vividsolutions.jts.geom.Coordinate(wsgCoord.getY, wsgCoord.getX)
+        val feature=pointf.createPoint(coord)
+        feature.setAttribute("capacity",link.capacity)
+        feature.setAttribute("lanes",link.lanes)
+        feature.setAttribute("speedInMPS",link.speedInMetersPerSecond)
+        features+=feature
+      }
+    }
+
+    ShapeFileWriter.writeGeometries(features.asJava,shapeFileOutputPath)
+  }
+
+
 
   def linkDetail(linkGeometry: Map[Id[Link], List[Coord]])(row: String): LinkDetails = {
     val tokens = row.replaceAll("\"", "").split(",")
@@ -35,7 +72,7 @@ object LinkReader {
     LinkDetails(
       linkId,
       tokens(4).toDouble,
-      tokens(5).toDouble,
+      tokens(5).toDouble*60*0.44704,
       tokens(6).toDouble.toInt,
       tokens(7).toDouble.toInt,
       linkGeometry(linkId)
