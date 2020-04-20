ec50bc8bb0e02365d7f6e4bf17de0671261905bf
wrashid <sfA1kewo>
2020-04-20 18:48:17 -0500

    refactoring: VisualizeVolumeStations

--- src/main/scala/beam/utils/scripts/austin_network/VisualizeVolumeStations.scala
+++ src/main/scala/beam/utils/scripts/austin_network/VisualizeVolumeStations.scala
@@ -11,37 +11,34 @@ import scala.collection.JavaConverters._
 
 object VisualizeVolumeStations {
   def main(args: Array[String]): Unit = {
-    //generateTrafficDetectorsShapeFile
+    generateTrafficDetectorsShapeFile
     generateTrafficSensorsShapeFile
   }
 
-  private def generateTrafficSensorsShapeFile = {
-    val trafficDetectors = getLines("E:\\work\\austin\\Travel_Sensors.csv")
-
-    val coords = trafficDetectors.drop(1).map { line =>
-      val tempColumns = line.split("\",")
-      val lastCol = tempColumns(tempColumns.length - 1).split(",")
-      val wgsCoordinate = new Coord(lastCol(3).toDouble, lastCol(2).toDouble)
-      wgsCoordinate
-    }
-
-    createShapeFile(coords, "E:\\work\\austin\\Travel_Sensors.shp")
+  private def generateTrafficSensorsShapeFile() = {
+    generateTrafficCountsFile("E:\\work\\austin\\Travel_Sensors.csv",
+      2, 3, "E:\\work\\austin\\Travel_Sensors.shp")
   }
 
   private def generateTrafficDetectorsShapeFile = {
-    val trafficDetectors = getLines("E:\\work\\austin\\Traffic_Detectors.csv")
+    generateTrafficCountsFile("E:\\work\\austin\\Traffic_Detectors.csv",
+      0, 1, "E:\\work\\austin\\Traffic_Detectors.shp")
+  }
+
+  private def generateTrafficCountsFile(trafficCountsFile: String, indexLat: Int, indexLong: Int, outputShapeFileName: String) = {
+    val trafficDetectors = getLines(trafficCountsFile)
 
     val coords = trafficDetectors.drop(1).map { line =>
       val tempColumns = line.split("\",")
       val lastCol = tempColumns(tempColumns.length - 1).split(",")
-      val wgsCoordinate = new Coord(lastCol(1).toDouble, lastCol(0).toDouble)
+      val wgsCoordinate = new Coord(lastCol(indexLong).toDouble, lastCol(indexLat).toDouble)
       wgsCoordinate
     }
 
-    createShapeFile(coords, "E:\\work\\austin\\Traffic_Detectors.shp")
+    createShapeFile(coords, outputShapeFileName)
   }
 
-  def createShapeFile(coords: Vector[Coord], shapeFileOutputPath:String)={
+  def createShapeFile(coords: Vector[Coord], shapeFileOutputPath: String) = {
     val features = ArrayBuffer[SimpleFeature]()
 
     val pointf: PointFeatureFactory = new PointFeatureFactory.Builder()
@@ -49,16 +46,16 @@ object VisualizeVolumeStations {
       .setName("nodes")
       .create()
 
-    coords.foreach{ wsgCoord =>
-        val coord=new com.vividsolutions.jts.geom.Coordinate(wsgCoord.getX, wsgCoord.getY)
-        val feature=pointf.createPoint(coord)
-        features+=feature
+    coords.foreach { wsgCoord =>
+      val coord = new com.vividsolutions.jts.geom.Coordinate(wsgCoord.getX, wsgCoord.getY)
+      val feature = pointf.createPoint(coord)
+      features += feature
     }
 
-    ShapeFileWriter.writeGeometries(features.asJava,shapeFileOutputPath)
+    ShapeFileWriter.writeGeometries(features.asJava, shapeFileOutputPath)
   }
 
-  def getLines(filePath:String): Vector[String] ={
+  def getLines(filePath: String): Vector[String] = {
     val trafficDetectorsFilePath = Source.fromFile(filePath)
     var lines = trafficDetectorsFilePath.getLines.toVector
     trafficDetectorsFilePath.close
