e6d3c689d69ac2e4b771254212835faaf1ea81bd
Carlos Caldas <ccaldas@gmail.com>
2020-04-21 17:22:19 -0300

    read indexes file

---
+++ src/main/scala/beam/agentsim/infrastructure/geozone/taz/GeoIndexFileReader.scala
@@ -0,0 +1,19 @@
+package beam.agentsim.infrastructure.geozone.taz
+
+import java.nio.file.Path
+
+import scala.io.Source
+
+import beam.agentsim.infrastructure.geozone.GeoIndex
+import beam.utils.FileUtils.using
+
+object GeoIndexFileReader {
+
+  def readIndexes(path: Path): Set[GeoIndex] = {
+    using(Source.fromFile(path.toFile)) { source =>
+      val allLines = source.getLines().toList
+      allLines.flatMap(GeoIndex.tryCreate).toSet
+    }
+  }
+
+}
