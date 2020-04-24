997949b1740c1a967471ee29720e300bac73de9b
Carlos Caldas <ccaldas@gmail.com>
2020-04-24 13:31:46 -0300

    applied scalafmt

--- src/main/scala/beam/utils/map/SequenceUtil.scala
+++ src/main/scala/beam/utils/map/SequenceUtil.scala
@@ -13,9 +13,12 @@ object SequenceUtil {
 
   def groupBy[K, V](pairs: ParSeq[(K, V)]): Map[K, Set[V]] = {
     val groupedByKey = pairs.groupBy(_._1)
-    groupedByKey.mapValues { pairs: ParSeq[(K, V)] =>
-      pairs.map(_._2).toSet.seq
-    }.seq.toMap
+    groupedByKey
+      .mapValues { pairs: ParSeq[(K, V)] =>
+        pairs.map(_._2).toSet.seq
+      }
+      .seq
+      .toMap
   }
 
 }
