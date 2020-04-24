d7ba7c4919d498524127ab0db7ad92813917ea2d
Artavazd Balaian <reasyu@gmail.com>
2020-04-25 04:54:02 +0700

    BugFix: Free flow travel time computation

--- src/main/scala/beam/analysis/cartraveltime/CarTripStatsFromPathTraversalEventHandler.scala
+++ src/main/scala/beam/analysis/cartraveltime/CarTripStatsFromPathTraversalEventHandler.scala
@@ -496,7 +496,8 @@ object CarTripStatsFromPathTraversalEventHandler extends LazyLogging {
           ((driving.arrivalTime - driving.departureTime) + (parking.arrivalTime - parking.departureTime)).toDouble
         // add the computed travel time to the list of travel times tracked during the hour
         val length = driving.legLength + parking.legLength
-        val linkIds = (driving.linkIds ++ parking.linkIds).map(lid => networkHelper.getLinkUnsafe(lid))
+        // We start driving in the very end of the first link => so we we didn't actually travel that link, so we should drop it for both driving and parking
+        val linkIds = (driving.linkIds.drop(1) ++ parking.linkIds.drop(1)).map(lid => networkHelper.getLinkUnsafe(lid))
         val freeFlowTravelTime: Double = calcFreeFlowDuration(freeFlowTravelTimeCalc, linkIds)
         val startCoordWGS = new Coord(driving.startX, driving.startY)
         val endCoordWGS = new Coord(parking.endX, parking.endY)
