df0a59165652d00988ebbd5aca7aed0a26682004
wrashid <wrashid>
2020-04-25 18:41:18 -0500

    link assignment complete

--- src/main/scala/beam/utils/scripts/austin_network/MapPhysSimToTrafficDetectors.scala
+++ src/main/scala/beam/utils/scripts/austin_network/MapPhysSimToTrafficDetectors.scala
@@ -1,18 +1,27 @@
 package beam.utils.scripts.austin_network
 
-import beam.utils.scripts.austin_network.VisualizeVolumeStations.generateTrafficCountsFile
+
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
 
 object MapPhysSimToTrafficDetectors {
 
   def main(args: Array[String]): Unit = {
+    import AustinUtils._
     val splitSizeInMeters=1
     val trafficDetectors=VisualizeVolumeStations.getCoordinatesTrafficCountsFile("E:\\work\\austin\\Traffic_Detectors.csv",0,1)
-    val physsimNetwork=AustinUtils.getPhysSimNetwork("C:\\Users\\owner\\IdeaProjects\\beam\\output\\austin\\austin-prod-1k-activities__2020-04-25_07-21-44_tmc\\output_network.xml.gz")
+    val physsimNetwork=getPhysSimNetwork("C:\\Users\\owner\\IdeaProjects\\beam\\output\\austin\\austin-prod-1k-activities__2020-04-25_07-21-44_tmc\\output_network.xml.gz")
 
-    val physSimDataPoints=physsimNetwork.getLinks.values().asScala.toVector.map{ link =>
+    val physSimDataPoints=physsimNetwork.getLinks.values().asScala.toVector.flatMap{ link =>
       DataVector(DataId(link.toString),link.getFromNode.getCoord,link.getToNode.getCoord,false).produceSpeedDataPointFromSpeedVector(splitSizeInMeters)
     }
+
+    val quadTree=getQuadTree(physSimDataPoints)
+    val trafficDataPoints=trafficDetectors.map{ case (id,coord) =>
+      DataPoint(id,coord,ArrayBuffer.empty)
+    }
+
+    assignDataPointsToQuadTree(quadTree,trafficDataPoints)
 
 
 
