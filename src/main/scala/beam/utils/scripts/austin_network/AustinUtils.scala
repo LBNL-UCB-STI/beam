2cd3504acc1252dd096ff86beaa79e319da6abd1
wrashid <wrashid>
2020-04-25 18:40:29 -0500

    add quadTree

--- src/main/scala/beam/utils/scripts/austin_network/AustinUtils.scala
+++ src/main/scala/beam/utils/scripts/austin_network/AustinUtils.scala
@@ -1,16 +1,19 @@
 package beam.utils.scripts.austin_network
 
 import beam.sim.common.GeoUtils
+import beam.utils.Statistics
 import beam.utils.matsim_conversion.ShapeUtils.QuadTreeBounds
 import beam.utils.scripts.austin_network.AustinUtils.getGeoUtils
 import org.matsim.api.core.v01.{Coord, Id}
-import org.matsim.api.core.v01.network.Link
+import org.matsim.api.core.v01.network.{Link, Network}
 import org.matsim.core.network.NetworkUtils
 import org.matsim.core.network.io.NetworkReaderMatsimV2
+import org.matsim.core.utils.collections.QuadTree
 
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 //TODO: push some of this out to more general library
 object AustinUtils {
@@ -49,10 +52,50 @@ object AustinUtils {
     QuadTreeBounds(minX, minY, maxX, maxY)
   }
 
+  def getQuadTree(dataPoints: Vector[DataPoint]) = {
+    val quadTreeBounds = getQuadTreeBounds(dataPoints.map(_.coord))
+    val quadTree: QuadTree[DataPoint] =
+      new QuadTree[DataPoint](quadTreeBounds.minx, quadTreeBounds.miny, quadTreeBounds.maxx, quadTreeBounds.maxy)
+    dataPoints.foreach { dataPoint =>
+      quadTree.put(dataPoint.coord.getX, dataPoint.coord.getY, dataPoint)
+    }
+    quadTree
+  }
 
 
+  def assignDataPointsToQuadTree(
+                                  quadTree: QuadTree[DataPoint],
+                                  dataPoints: scala.Vector[DataPoint]
+                                ) = {
+    val closestPhysSimPointMap = dataPoints.par.map { dataPoint =>
+      (dataPoint -> quadTree.getClosest(
+        dataPoint.coord.getX,
+        dataPoint.coord.getY
+      ))
+    }.toMap
+    dataPoints.foreach { dataPoint =>
+      val closestPhysSimNetworkPoint = closestPhysSimPointMap.get(dataPoint).get
+      closestPhysSimNetworkPoint.closestAttractedDataPoint += dataPoint
+    }
+  }
 
 
+  def getBothDirectionsOfSelectedLinks(links: Vector[Link]): Vector[Link] = {
+    val linkSet = mutable.Set()[Link]
+
+    links.foreach { link =>
+      linkSet += link
+      getOppositeLink(link).foreach(oppLink => linkSet += oppLink)
+    }
+
+    linkSet.toVector
+  }
+
+  def getOppositeLink(link: Link): Option[Link] = {
+    val inLinks = link.getFromNode.getInLinks.values()
+    val outLinks = link.getToNode.getOutLinks.values()
+    inLinks.asScala.toVector.find(linkId => outLinks.contains(linkId))
+  }
 
 }
 
@@ -80,10 +123,11 @@ case class DataVector(linkId: DataId, startCoord: Coord, endCoord: Coord, isWGS:
 }
 
 case class DataPoint(
-                      linkId: DataId,
+                      id: DataId,
                       coord: Coord,
                       closestAttractedDataPoint: ArrayBuffer[DataPoint]
                     )
+
 case class DataId(id: String) {
   def getLinkId = {
     Id.createLinkId(id)
