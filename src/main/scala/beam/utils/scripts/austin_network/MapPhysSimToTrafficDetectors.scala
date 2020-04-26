927a7703f78882883642f30e039bfafcd45b94e4
wrashid <wrashid>
2020-04-25 22:17:42 -0500

    refactor

--- src/main/scala/beam/utils/scripts/austin_network/MapPhysSimToTrafficDetectors.scala
+++ src/main/scala/beam/utils/scripts/austin_network/MapPhysSimToTrafficDetectors.scala
@@ -2,25 +2,37 @@ package beam.utils.scripts.austin_network
 
 
 import com.typesafe.scalalogging.LazyLogging
+import org.matsim.api.core.v01.Coord
+import org.matsim.api.core.v01.network.Network
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
 object MapPhysSimToTrafficDetectors {
 
+  val log=new Logging()
   def main(args: Array[String]): Unit = {
 
     import AustinUtils._
-    val log=new Logging()
+
     log.info("start")
     val splitSizeInMeters = 10
-    val trafficDetectors = VisualizeVolumeStations.getCoordinatesTrafficCountsFile("E:\\work\\austin\\Traffic_Detectors.csv", 0, 1)
+
     val physsimNetwork = getPhysSimNetwork("C:\\Users\\owner\\IdeaProjects\\beam\\output\\austin\\austin-prod-1k-activities__2020-04-25_07-21-44_tmc\\output_network.xml.gz")
     val outputFileName="E:\\work\\austin\\physSimNetworkLinkIdsWithTrafficDetectors.csv"
 
     log.info("data read")
+    val linkIdsBothDirections: Vector[String] = getPhysSimNetworkIdsWithTrafficDectors(splitSizeInMeters, physsimNetwork)
+
+    writeFile(linkIdsBothDirections,outputFileName,Some("linkId"))
+
+  }
+
+  private def getPhysSimNetworkIdsWithTrafficDectors(splitSizeInMeters: Int, physsimNetwork: Network) = {
+    import AustinUtils._
+    val trafficDetectors = VisualizeVolumeStations.getCoordinatesTrafficCountsFile("E:\\work\\austin\\Traffic_Detectors.csv", 0, 1)
     val physSimDataPoints = physsimNetwork.getLinks.values().asScala.toVector.flatMap { link =>
-      DataVector(DataId(link.toString), link.getFromNode.getCoord, link.getToNode.getCoord, false).produceSpeedDataPointFromSpeedVector(splitSizeInMeters)
+      DataVector(DataId(link.getId.toString), link.getFromNode.getCoord, link.getToNode.getCoord, false).produceSpeedDataPointFromSpeedVector(splitSizeInMeters)
     }
 
     log.info("physSimDataPoints complete")
@@ -34,11 +46,11 @@ object MapPhysSimToTrafficDetectors {
     assignDataPointsToQuadTree(quadTree, trafficDataPoints)
     log.info("assignDataPointsToQuadTree complete")
 
-    val links=physSimDataPoints.filter(dataPoint => dataPoint.closestAttractedDataPoint.nonEmpty).map(dataPoint => physsimNetwork.getLinks.get(dataPoint.id.getLinkId))
-    val linkIds=AustinUtils.getBothDirectionsOfSelectedLinks(links).map(_.getId.toString).toVector
+    val link = physsimNetwork.getLinks.get(DataId("1").getLinkId)
 
-
-    writeFile(linkIds,outputFileName,Some("linkId"))
+    val links = physSimDataPoints.filter(dataPoint => dataPoint.closestAttractedDataPoint.nonEmpty)
+    val linkIdsWithTrafficDetectors = links.map(dataPoint => physsimNetwork.getLinks.get(dataPoint.id.getLinkId))
+    val linkIdsBothDirections = AustinUtils.getBothDirectionsOfSelectedLinks(linkIdsWithTrafficDetectors).map(_.getId.toString).toVector
+    linkIdsBothDirections
   }
-
 }
