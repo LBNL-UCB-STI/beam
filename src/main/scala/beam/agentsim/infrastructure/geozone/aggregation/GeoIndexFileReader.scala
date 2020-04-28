5e1c304693f6a6d7277153f3ef34b4ea090de883
Carlos Caldas <ccaldas@gmail.com>
2020-04-28 13:36:13 -0300

    added script to write shape file from indexes

--- src/main/scala/beam/agentsim/infrastructure/geozone/aggregation/GeoIndexFileReader.scala
+++ src/main/scala/beam/agentsim/infrastructure/geozone/aggregation/GeoIndexFileReader.scala
@@ -9,9 +9,9 @@ import beam.utils.FileUtils.using
 
 object GeoIndexFileReader {
 
-  def readIndexes(path: Path): Set[GeoIndex] = {
+  def readIndexes(path: Path, skipLines: Int = 1): Set[GeoIndex] = {
     using(Source.fromFile(path.toFile)) { source =>
-      val allLines = source.getLines().toList
+      val allLines = source.getLines().drop(skipLines).toList
       allLines.flatMap(GeoIndex.tryCreate).toSet
     }
   }
---
+++ src/main/scala/beam/utils/shapefiles/WriteShapeFile.scala
@@ -0,0 +1,25 @@
+package beam.utils.shapefiles
+
+import java.nio.file.{Files, Paths}
+
+import beam.agentsim.infrastructure.geozone.aggregation.GeoIndexFileReader
+import beam.agentsim.infrastructure.geozone.GeoZoneUtil
+
+object WriteShapeFile extends App {
+  if (args.length != 2) {
+    val msg =
+      """You should specify the outputFile as parameter:
+         |sbt "runMain beam.utils.shapefiles.WriteShapeFile <inputFileGeoIndexFile> <outputFile>"
+         |""".stripMargin
+    println(msg)
+  } else {
+    val indexesFile = Paths.get(args(0))
+    val outputPath = Paths.get(args(1))
+    if (Files.isRegularFile(indexesFile)) {
+      val indexes = GeoIndexFileReader.readIndexes(indexesFile)
+      GeoZoneUtil.writeToShapeFile(outputPath, indexes)
+    } else {
+      System.err.println(s"The file [$indexesFile] does not exist.")
+    }
+  }
+}
