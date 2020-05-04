e11c92694040e3b106da803dbf513ba9f08e5d6a
danteform <danteform@gmail.com>
2020-05-04 18:39:23 +0500

    - Test to show StreetRouter behavior;

---
+++ src/test/scala/beam/router/R5RouterSpec.scala
@@ -0,0 +1,186 @@
+package beam.router
+
+import java.io.File
+import java.time.OffsetDateTime
+
+import beam.utils.TestConfigUtils.testConfig
+import com.conveyal.r5.analyst.fare.SimpleInRoutingFareCalculator
+import com.conveyal.r5.profile.{ProfileRequest, StreetMode, StreetPath}
+import com.conveyal.r5.streets._
+import com.conveyal.r5.transit.TransportNetwork
+import org.scalatest.{Assertion, FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+import scala.util.Try
+
+class R5RouterSpec extends FlatSpec with Matchers {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private val transportNetwork = TransportNetwork.fromDirectory(new File("test/input/sf-light/r5"))
+
+  private val mode = EdgeStore.EdgeFlag.ALLOWS_CAR
+  private val streetMode = StreetMode.CAR
+
+  private val startEdge1 = edgeById(20109) //This link doesn't have mode ALLOWS_CAR
+  private val stopEdge1 = edgeById(66763)
+
+  private val stopEdge2 = edgeById(46525) //This link doesn't have mode ALLOWS_CAR
+
+  it should "not fail with start edge" in {
+    val path = routeV1(startEdge1, stopEdge1)
+    check(path)
+  }
+
+  it should "not fail with stop edge" in {
+    val path = routeV1(startEdge1, stopEdge2)
+    check(path)
+  }
+
+  it should "not fail with start edge with different setOrigin" in {
+    val path = routeV2(startEdge1, stopEdge1)
+    check(path)
+  }
+
+  private def check(path: Iterable[EdgeStore#Edge]): Assertion = {
+    val checkFlag = path.forall(_.getFlag(mode))
+    if (!checkFlag) {
+      path.map(e => e.getEdgeIndex + " - " + e.getFlags).foreach(msg => logger.error("Failed with stop: {}", msg))
+    }
+    checkFlag shouldBe true
+  }
+
+  private def routeV1(startEdge: Option[EdgeStore#Edge], stopEdge: Option[EdgeStore#Edge]): Iterable[EdgeStore#Edge] = {
+    val result = for {
+      startVertex <- startEdge.flatMap(edge => vertexById(edge.getFromVertex))
+      stopVertex  <- stopEdge.flatMap(edge => vertexById(edge.getToVertex))
+      streetPath  <- calculateRouteV1(startVertex, stopVertex)
+    } yield streetPath
+
+    result.map(_.getEdges.asScala).getOrElse(Iterable.empty[Integer]).flatMap(eid => edgeById(eid))
+  }
+
+  private def calculateRouteV1(startVertex: VertexStore#Vertex, stopVertex: VertexStore#Vertex): Option[StreetPath] = {
+    val streetRouter = createStreetRouter(startVertex, stopVertex)
+
+    streetRouter.setOrigin(startVertex.getLat, startVertex.getLon)
+    streetRouter.setDestination(stopVertex.getLat, stopVertex.getLon)
+    streetRouter.route()
+
+    Option(streetRouter.getState(streetRouter.getDestinationSplit)).map { lastState =>
+      new StreetPath(lastState, transportNetwork, false)
+    }
+  }
+
+  private def routeV2(startEdge: Option[EdgeStore#Edge], stopEdge: Option[EdgeStore#Edge]): Iterable[EdgeStore#Edge] = {
+    val result = for {
+      startSplit <- startEdge.flatMap(splitByEdge)
+      stopSplit  <- stopEdge.flatMap(splitByEdge)
+      streetPath  <- calculateRouteV2(startSplit, stopSplit)
+    } yield streetPath
+
+    result.map(_.getEdges.asScala).getOrElse(Iterable.empty[Integer]).flatMap(eid => edgeById(eid))
+  }
+
+  private def calculateRouteV2(startSplit: Split, stopSplit: Split): Option[StreetPath] = {
+    val startVertexOpt = vertexById(startSplit.vertex0)
+    val stopVertexOpt = vertexById(stopSplit.vertex1)
+    val streetRouterOpt = for {
+      startVertex <- startVertexOpt
+      stopVertex  <- stopVertexOpt
+    } yield {
+      val streetRouter = createStreetRouter(startVertex, stopVertex)
+      streetRouter.setOrigin(startVertex.index)
+      streetRouter.setDestination(stopSplit)
+      streetRouter.route()
+
+      streetRouter
+    }
+
+    streetRouterOpt.map(_.getState(stopSplit)).map { lastState =>
+      new StreetPath(lastState, transportNetwork, false)
+    }
+  }
+
+  private def splitByEdge(edge: EdgeStore#Edge): Option[Split] = vertexById(edge.getFromVertex).flatMap{
+    vertex =>
+    Option(Split.find(
+      vertex.getLat,
+      vertex.getLon,
+      StreetLayer.LINK_RADIUS_METERS,
+      transportNetwork.streetLayer,
+      streetMode
+    ))
+  }
+
+  private def edgeById(edgeId: Int): Option[EdgeStore#Edge] = {
+    Try(transportNetwork.streetLayer.edgeStore.getCursor(edgeId)).toOption
+  }
+
+  private def vertexById(vertexId: Int): Option[VertexStore#Vertex] = {
+    Try(transportNetwork.streetLayer.edgeStore.vertexStore.getCursor(vertexId)).toOption
+  }
+
+  private def createStreetRouter(startVertex: VertexStore#Vertex, stopVertex: VertexStore#Vertex): StreetRouter = {
+    val streetRouter = new StreetRouter(
+      transportNetwork.streetLayer,
+      travelTimeCalculator(),
+      turnCostCalculator,
+      travelCostCalculator()
+    )
+
+    val profileRequest = createProfileRequest(startVertex, stopVertex)
+
+    streetRouter.profileRequest = profileRequest
+    streetRouter.streetMode = streetMode
+
+    streetRouter
+  }
+
+  private val turnCostCalculator: TurnCostCalculator =
+    new TurnCostCalculator(transportNetwork.streetLayer, true) {
+      override def computeTurnCost(fromEdge: Int, toEdge: Int, streetMode: StreetMode): Int = 0
+    }
+  private def travelCostCalculator(): TravelCostCalculator =
+    (_: EdgeStore#Edge, _: Int, traversalTimeSeconds: Float) => traversalTimeSeconds
+
+  private def travelTimeCalculator(): TravelTimeCalculator = { (_: EdgeStore#Edge, _: Int, _: StreetMode, _) =>
+    10
+  }
+
+  private def createProfileRequest(startVertex: VertexStore#Vertex, stopVertex: VertexStore#Vertex) = {
+    val profileRequest = new ProfileRequest()
+    // Warning: carSpeed is not used for link traversal (rather, the OSM travel time model is used),
+    // but for R5-internal bushwhacking from network to coordinate, AND ALSO for the A* remaining weight heuristic,
+    // which means that this value must be an over(!)estimation, otherwise we will miss optimal routes,
+    // particularly in the presence of tolls.
+    profileRequest.carSpeed = 60f
+    profileRequest.maxWalkTime = 30
+    profileRequest.maxCarTime = 30
+    profileRequest.maxBikeTime = 30
+    // Maximum number of transit segments. This was previously hardcoded as 4 in R5, now it is a parameter
+    // that defaults to 8 unless I reset it here. It is directly related to the amount of work the
+    // transit router has to do.
+    profileRequest.maxRides = 4
+    profileRequest.streetTime = 2 * 60
+    profileRequest.maxTripDurationMinutes = 4 * 60
+    profileRequest.wheelchair = false
+    profileRequest.bikeTrafficStress = 4
+    profileRequest.zoneId = transportNetwork.getTimeZone
+    profileRequest.monteCarloDraws = 10
+    profileRequest.date = OffsetDateTime.parse("2017-09-22T00:00:00-07:00").toLocalDate
+    // Doesn't calculate any fares, is just a no-op placeholder
+    profileRequest.inRoutingFareCalculator = new SimpleInRoutingFareCalculator
+    profileRequest.suboptimalMinutes = 0
+
+    profileRequest.fromLon = startVertex.getLon
+    profileRequest.fromLat = startVertex.getLat
+    profileRequest.toLon = stopVertex.getLon
+    profileRequest.toLat = stopVertex.getLat
+    profileRequest.fromTime = 1500
+    profileRequest.toTime = profileRequest.fromTime + 61
+
+    profileRequest
+  }
+}
