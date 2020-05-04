f7f8452116f9959111268756621d67eec4abf29b
Carlos Caldas <ccaldas@gmail.com>
2020-05-04 13:43:15 -0300

    renamed GeoIndex to H3Index

--- src/main/scala/beam/agentsim/infrastructure/geozone/GeoZoneSummary.scala
+++ src/main/scala/beam/agentsim/infrastructure/geozone/GeoZoneSummary.scala
@@ -2,4 +2,4 @@ package beam.agentsim.infrastructure.geozone
 
 case class GeoZoneSummary(items: Seq[GeoZoneSummaryItem])
 
-case class GeoZoneSummaryItem(index: GeoIndex, size: Int)
+case class GeoZoneSummaryItem(index: H3Index, size: Int)
