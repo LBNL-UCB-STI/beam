d58107d451c21d3ee372ca5de1680f1e260abaa8
Carlos Caldas <ccaldas@gmail.com>
2020-05-07 18:56:57 -0300

    applied scalafmt

--- src/main/scala/beam/utils/hereapi/HerePath.scala
+++ src/main/scala/beam/utils/hereapi/HerePath.scala
@@ -2,4 +2,4 @@ package beam.utils.hereapi
 
 import beam.agentsim.infrastructure.geozone.WgsCoordinate
 
-case class HerePath (coordinates: Seq[WgsCoordinate], spans: Seq[HereSpan])
+case class HerePath(coordinates: Seq[WgsCoordinate], spans: Seq[HereSpan])
