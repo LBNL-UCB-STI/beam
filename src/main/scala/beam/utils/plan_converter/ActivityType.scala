09557da3768a76df836da600fb59510ced18a596
danteform <danteform@gmail.com>
2020-05-08 19:27:11 +0500

    - Plans reader;

---
+++ src/main/scala/beam/utils/plan_converter/ActivityType.scala
@@ -0,0 +1,15 @@
+package beam.utils.plan_converter
+
+import scala.annotation.switch
+
+sealed trait ActivityType
+case object Activity extends ActivityType
+case object Leg extends ActivityType
+
+object ActivityType {
+
+  def determineActivity(activityName: String): ActivityType = (activityName: @switch) match {
+    case "activity" => Activity
+    case "leg"      => Leg
+  }
+}
--- src/main/scala/beam/utils/plan_converter/Transformer.scala
+++ src/main/scala/beam/utils/plan_converter/Transformer.scala
@@ -2,11 +2,12 @@ package beam.utils.plan_converter
 
 trait Transformer[T] {
 
-  def getIfNotNull(rec: java.util.Map[String, String], column: String): String = {
-    val v = rec.get(column)
-    assert(v != null, s"Value in column '$column' is null")
-    v
-  }
+  def getIfNotNull(rec: java.util.Map[String, String], column: String): String =
+    getOptional(rec, column).getOrElse(
+      throw new java.lang.AssertionError(s"assertion failed: Value in column '$column' is null")
+    )
+
+  def getOptional(rec: java.util.Map[String, String], column: String): Option[String] = Option(rec.get(column))
 
   def transform(m: java.util.Map[String, String]): T
 }
