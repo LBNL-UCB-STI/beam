785cce4eeb9a7a47d287a2fd12ce0c67e8f9b9f2
danteform <danteform@gmail.com>
2020-05-08 19:03:15 +0500

    - Extract entities;

---
+++ src/main/scala/beam/utils/plan_converter/TripElement.scala
@@ -0,0 +1,16 @@
+package beam.utils.plan_converter
+
+case class TripElement(tripId: Int, personId: Int, householdId: Int, depart: Double, trip_mode: String)
+
+object TripElement extends Transformer[TripElement] {
+
+  def transform(rec: java.util.Map[String, String]): TripElement = {
+    val tripId = getIfNotNull(rec, "trip_id").toInt
+    val personId = getIfNotNull(rec, "person_id").toInt
+    val householdId = getIfNotNull(rec, "household_id").toInt
+    val depart = getIfNotNull(rec, "depart").toDouble
+    val tripMode = getIfNotNull(rec, "trip_mode")
+
+    TripElement(tripId, personId, householdId, depart, tripMode)
+  }
+}
