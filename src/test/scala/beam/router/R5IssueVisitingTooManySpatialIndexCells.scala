0a883176efbb8424cf8710d26407eeeaa8be03a7
Artavazd Balaian <reasyu@gmail.com>
2020-05-09 02:07:06 +0700

    Use findSplit explicitly

--- src/test/scala/beam/router/R5IssueVisitingTooManySpatialIndexCells.scala
+++ src/test/scala/beam/router/R5IssueVisitingTooManySpatialIndexCells.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import beam.router.BeamRouter.Location
 import beam.sim.common.GeoUtils
+import com.conveyal.r5.profile.StreetMode
 import com.conveyal.r5.transit.TransportNetwork
 import org.matsim.api.core.v01.Coord
 import org.scalatest.{FlatSpec, Matchers}
@@ -17,15 +18,13 @@ class R5IssueVisitingTooManySpatialIndexCells extends FlatSpec with Matchers {
   val geo = new GeoUtilsTestImpl("epsg:26910")
   val dir = new File("test/test-resources/R5-detroit-square")
   val transportNetwork: TransportNetwork = TransportNetwork.fromDirectory(dir)
-  val location: Location = new Coord(-83.40673203295833, 42.41642536832911)
-  val coord = new Coord()
+  val streetLayer = transportNetwork.streetLayer
+
+  // val location: Location = new Coord(-83.140673203295833, 42.378642536832911)
+  val location: Location = new Coord(-83.40862361941298, 42.41622317907699)
 
   it should "be able to snap to R5 edge" in {
-    val coord = geo.snapToR5Edge(
-      transportNetwork.streetLayer,
-      geo.utm2Wgs(location)
-    )
-
-    coord shouldBe equal(coord)
+    val split = streetLayer.findSplit(location.getY, location.getX, 10000, StreetMode.WALK)
+    require(split != null)
   }
 }
