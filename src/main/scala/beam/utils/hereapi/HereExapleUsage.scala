0a28a023ed3dc064534e019c3b36f98ccd3c045b
Carlos Caldas <ccaldas@gmail.com>
2020-05-11 18:49:39 -0300

    extracted findSegments to a service

--- src/main/scala/beam/utils/hereapi/HereExapleUsage.scala
+++ src/main/scala/beam/utils/hereapi/HereExapleUsage.scala
@@ -1,26 +1,18 @@
 package beam.utils.hereapi
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
 import beam.agentsim.infrastructure.geozone.WgsCoordinate
-import beam.utils.FileUtils
 
-object HereMain extends App {
+object HereExapleUsage extends App {
   if (args.length != 3) {
     println("Expected arguments: [API-KEY] [ORIGIN] [DESTINATION]")
     println("Example: KqkuBonCHeDLytZwdGfKcUH9N287H-lOdqu 37.705687,-122.461096 37.724113,-122.447652")
     System.exit(1)
   }
+  val apiKey = args(0)
   val originCoordinate = toWgsCoordinate(args(1))
   val destinationCoordinate = toWgsCoordinate(args(2))
-  println(originCoordinate, destinationCoordinate)
 
-  val result = FileUtils.using(new HereAdapter(args(0))) { adapter =>
-    val service = new HereService(adapter)
-    val segFuture = service.findSegments(origin = originCoordinate, destination = destinationCoordinate)
-    Await.result(segFuture, Duration("5 seconds"))
-  }
+  val result: Seq[HereSegment] = HereService.findSegments(apiKey, originCoordinate, destinationCoordinate)
 
   println(result.mkString(System.lineSeparator()))
 
