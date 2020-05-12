2b91df46aba26d05185e40ffce0c38590dd594ca
danteform <danteform@gmail.com>
2020-05-12 20:20:01 +0500

    - Refactoring of readers; - Added person reader;

--- src/main/scala/beam/utils/plan_converter/reader/Reader.scala
+++ src/main/scala/beam/utils/plan_converter/reader/Reader.scala
@@ -1,7 +1,5 @@
 package beam.utils.plan_converter.reader
 
 trait Reader[T] extends AutoCloseable {
-
   def iterator(): Iterator[T]
-
 }
--- src/main/scala/beam/utils/plan_converter/reader/TripReader.scala
+++ src/main/scala/beam/utils/plan_converter/reader/TripReader.scala
@@ -1,21 +1,8 @@
 package beam.utils.plan_converter.reader
 
-import beam.utils.FileUtils
+import beam.utils.plan_converter.Transformer
 import beam.utils.plan_converter.entities.TripElement
-import org.supercsv.io.CsvMapReader
-import org.supercsv.prefs.CsvPreference
 
-class TripReader(path: String) extends Reader[TripElement]{
-
-  private val csvMapReader = new CsvMapReader(FileUtils.readerFromFile(path), CsvPreference.STANDARD_PREFERENCE)
-
-  override def iterator(): Iterator[TripElement] = {
-      val header = csvMapReader.getHeader(true)
-      Iterator
-        .continually(csvMapReader.read(header: _*))
-        .takeWhile(data => data != null)
-        .map(TripElement.transform)
-  }
-
-  override def close(): Unit = csvMapReader.close()
+class TripReader(path: String) extends BaseCsvReader[TripElement](path) {
+  override val transformer: Transformer[TripElement] = TripElement
 }
