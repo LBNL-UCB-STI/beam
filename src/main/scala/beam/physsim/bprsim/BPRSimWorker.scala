7b26412cad79ee18fa682b4149b2d4729bad6d8e
Dmitry Openkov <dimaopen@gmail.com>
2020-05-12 19:00:06 +0300

    Use a single collection for storing SimEvents. Using scala PriorityQueue with synchronization instead of java.util.concurrent.PriorityBlockingQueue.

--- src/main/scala/beam/physsim/bprsim/BPRSimWorker.scala
+++ src/main/scala/beam/physsim/bprsim/BPRSimWorker.scala
@@ -2,7 +2,6 @@ package beam.physsim.bprsim
 
 import java.util.Comparator
 
-import com.typesafe.scalalogging.StrictLogging
 import org.matsim.api.core.v01.network.Link
 import org.matsim.api.core.v01.{Id, Scenario}
 
@@ -14,80 +13,48 @@ import scala.collection.mutable
   *
   * @author Dmitry Openkov
   */
-class BPRSimWorker(scenario: Scenario, config: BPRSimConfig, val myLinks: Set[Id[Link]], coordinator: Coordinator)
-    extends StrictLogging {
-  private val queue =
-    new java.util.concurrent.PriorityBlockingQueue[SimEvent](2048, BPRSimWorker.javaSimEventComparator)
-  private val toBeProcessed = mutable.PriorityQueue.empty[SimEvent](BPRSimulation.simEventOrdering)
+class BPRSimWorker(scenario: Scenario, config: BPRSimConfig, val myLinks: Set[Id[Link]], coordinator: Coordinator) {
+  private val queue = mutable.PriorityQueue.empty[SimEvent](BPRSimulation.simEventOrdering)
   private val params = BPRSimParams(config, new VolumeCalculator)
 
-  def init(tillTime: Double): Unit = {
+  def init(): Unit = {
     val persons = scenario.getPopulation.getPersons.values().asScala
     persons
       .map(person => BPRSimulation.startingEvent(person, firstAct => myLinks.contains(firstAct.getLinkId)))
       .flatMap(_.iterator)
-      .foreach(se => acceptSimEvent(se, tillTime))
+      .foreach(se => acceptSimEvent(se))
   }
 
-  def startingTime: Double = {
-    val persons = scenario.getPopulation.getPersons.values().asScala
-    val eventTimes = persons
-      .map(person => BPRSimulation.startingEvent(person, firstAct => myLinks.contains(firstAct.getLinkId)))
-      .flatMap(_.iterator)
-      .map(_.time)
-    if (eventTimes.isEmpty) Double.MaxValue else eventTimes.min
-  }
-
-  def minTime: Double =
-    toBeProcessed.headOption
+  def minTime: Double = {
+    queue.headOption
       .map(_.time)
       .getOrElse(Double.MaxValue)
-
-  def moveToQueue(tillTime: Double): Int = {
-    @tailrec
-    def moveToQueue(tillTime: Double, counter: Int): Int = {
-      toBeProcessed.headOption match {
-        case Some(se) =>
-          if (se.time <= tillTime) {
-            queue.put(toBeProcessed.dequeue())
-            moveToQueue(tillTime, counter + 1)
-          } else {
-            counter
-          }
-        case None => counter
-      }
-    }
-
-    moveToQueue(tillTime, 0)
   }
 
   def processQueuedEvents(workers: Map[Id[Link], BPRSimWorker], tillTime: Double): Int = {
-    logger.debug(s"Messages in queue = ${queue.size()}, in toBeProcessed = ${toBeProcessed.size}")
     @tailrec
     def processQueuedEvents(workers: Map[Id[Link], BPRSimWorker], tillTime: Double, counter: Int): Int = {
-      val simEvent = queue.poll()
-      if (simEvent == null) {
+      val seOption = queue.headOption
+      if (seOption.isEmpty || seOption.get.time > tillTime) {
         counter
       } else {
-        simEvent.execute(scenario, params) match {
-          case (events, simEvent) =>
-            events.foreach(coordinator.processEvent)
-            simEvent.foreach { event =>
-              workers(event.linkId).acceptSimEvent(event, tillTime)
-            }
-        }
+        val simEvent = queue.synchronized(queue.dequeue())
+        val (events, maybeSimEvent) = simEvent.execute(scenario, params)
+        events.foreach(coordinator.processEvent)
+        for {
+          se <- maybeSimEvent
+        } workers(se.linkId).acceptSimEvent(se)
+
         processQueuedEvents(workers, tillTime: Double, counter + 1)
       }
-
     }
+
     processQueuedEvents(workers, tillTime, 0)
   }
 
-  def acceptSimEvent(simEvent: SimEvent, tillTime: Double) = {
-    if (simEvent.time <= tillTime) {
-      queue.put(simEvent)
-    } else if (simEvent.time < config.simEndTime) {
-      toBeProcessed += simEvent
+  def acceptSimEvent(simEvent: SimEvent): Unit = {
+    if (simEvent.time <= config.simEndTime) {
+      queue.synchronized(queue += simEvent)
     }
   }
 }
