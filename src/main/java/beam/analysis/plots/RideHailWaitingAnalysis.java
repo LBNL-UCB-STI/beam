89754db7dd7364a893af9e92e5addfa4d3dd3aeb
wrashid <wrashid>
2020-05-23 18:02:55 -0500

    fmt

--- src/main/java/beam/analysis/plots/RideHailWaitingAnalysis.java
+++ src/main/java/beam/analysis/plots/RideHailWaitingAnalysis.java
@@ -247,7 +247,7 @@ public class RideHailWaitingAnalysis implements GraphAnalysis, IterationSummaryA
                 rideHailWaitingIndividualStat.personId = pId;
                 rideHailWaitingIndividualStat.vehicleId = vehicleId;
                 rideHailWaitingIndividualStat.waitingTime = difference;
-                rideHailWaitingIndividualStat.modeChoice=modeChoiceEvent.mode;
+                rideHailWaitingIndividualStat.modeChoice = modeChoiceEvent.mode;
                 rideHailWaitingIndividualStatList.add(rideHailWaitingIndividualStat);
 
 
@@ -312,7 +312,7 @@ public class RideHailWaitingAnalysis implements GraphAnalysis, IterationSummaryA
                 String line = rideHailWaitingIndividualStat.time + "," +
                         rideHailWaitingIndividualStat.personId + "," +
                         rideHailWaitingIndividualStat.vehicleId + "," +
-                        rideHailWaitingIndividualStat.waitingTime+ "," +
+                        rideHailWaitingIndividualStat.waitingTime + "," +
                         rideHailWaitingIndividualStat.modeChoice;
 
                 out.write(line);
