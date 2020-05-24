eadb2aaa81e977c1f23b732c0e0073d3264244a3
Carlos Caldas <ccaldas@gmail.com>
2020-05-24 06:55:32 -0300

    Using google polyline decoder from google-maps-services-java

--- src/main/scala/beam/utils/mapsapi/googleapi/GooglePolylineDecoder.scala
+++ src/main/scala/beam/utils/mapsapi/googleapi/GooglePolylineDecoder.scala
@@ -1,52 +1,17 @@
 package beam.utils.mapsapi.googleapi
 
-import scala.collection.mutable.ArrayBuffer
-
 import beam.agentsim.infrastructure.geozone.WgsCoordinate
+import com.google.maps.internal.PolylineEncoding
+import com.google.maps.model.LatLng
+
+import scala.collection.JavaConverters._
 
 object GooglePolylineDecoder {
-  private val accuracy: Double = 1e-5
-  private val accuracyDigits: Int = 5
-  private val questionMarkPlusOne = 64
 
   def decode(polyline: String): Seq[WgsCoordinate] = {
-    val result = new ArrayBuffer[WgsCoordinate]
-    var index: Int = 0
-    var lat: Int = 0
-    var long: Int = 0
-    while (index < polyline.length) {
-      val (indexInc1, latitudeInc) = calculateIndexAndCoordinateIncrements(index, polyline)
-      index += indexInc1
-      lat += latitudeInc
-
-      val (indexInc2, longitudeInc) = calculateIndexAndCoordinateIncrements(index, polyline)
-      index += indexInc2
-      long += longitudeInc
-
-      val wgsCoordinate = WgsCoordinate(latitude = lat * accuracy, longitude = long * accuracy)
-      result.append(wgsCoordinate)
+    PolylineEncoding.decode(polyline).asScala.map{point: LatLng =>
+      WgsCoordinate(latitude = point.lat, longitude = point.lng)
     }
-    result.toIndexedSeq
-  }
-
-  @inline
-  private def coordinateIncrement(result: Int): Int = {
-    if ((result & 1) != 0) ~(result >> 1)
-    else result >> 1
-  }
-
-  private def calculateIndexAndCoordinateIncrements(initialIndex: Int, polyline: String): (Int, Int) = {
-    var currentResult: Int = 1
-    var shift: Int = 0
-    var currentCharacter: Int = 0
-    var indexIncrement = 0
-    do {
-      indexIncrement += 1
-      currentCharacter = polyline.charAt(initialIndex + indexIncrement - 1) - questionMarkPlusOne
-      currentResult += currentCharacter << shift
-      shift += accuracyDigits
-    } while (currentCharacter >= 31)
-    (indexIncrement, coordinateIncrement(currentResult))
   }
 
 }
