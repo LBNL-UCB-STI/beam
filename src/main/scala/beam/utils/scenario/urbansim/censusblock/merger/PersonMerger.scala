c98ba452b8842044592ff93502bb8948198f5438
Artavazd Balaian <reasyu@gmail.com>
2020-06-03 14:12:54 +0700

    Return default 0 income if can't compute income

--- src/main/scala/beam/utils/scenario/urbansim/censusblock/merger/PersonMerger.scala
+++ src/main/scala/beam/utils/scenario/urbansim/censusblock/merger/PersonMerger.scala
@@ -9,9 +9,7 @@ class PersonMerger(inputHousehold: Map[String, InputHousehold]) extends Merger[I
 
   private def inputToOutput(inputPersonInfo: InputPersonInfo): PersonInfo = {
     val inputIncome = inputHousehold(inputPersonInfo.householdId).income
-    val income = PopulationAdjustment.incomeToValueOfTime(inputIncome).getOrElse {
-      throw new IllegalStateException(s"Can't compute income with input value income $inputIncome")
-    }
+    val income = PopulationAdjustment.incomeToValueOfTime(inputIncome).getOrElse(0d)
 
     PersonInfo(
       PersonId(inputPersonInfo.personId),
