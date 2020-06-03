73d94905357fe20f0589d3cacd67474513436da2
Artavazd Balaian <reasyu@gmail.com>
2020-06-03 12:53:11 +0700

    Respect modes again

--- src/main/scala/beam/utils/scenario/urbansim/censusblock/entities/TripElement.scala
+++ src/main/scala/beam/utils/scenario/urbansim/censusblock/entities/TripElement.scala
@@ -11,8 +11,7 @@ object TripElement extends EntityTransformer[TripElement] {
     val personId = getIfNotNull(rec, "person_id")
     val householdId = getIfNotNull(rec, "household_id")
     val depart = getIfNotNull(rec, "depart").toDouble
-    // FIXME: Uncomment me once experiment for Austin with cleared modes is over OR ready to merge
-    val tripMode = "" // getIfNotNull(rec, "trip_mode")
+    val tripMode = getIfNotNull(rec, "trip_mode")
 
     TripElement(tripId, personId, householdId, depart, tripMode)
   }
