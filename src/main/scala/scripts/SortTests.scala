6f489173267d2aeea0ecddf9a6f804ed52ec6cc0
Carlos Caldas <ccaldas@gmail.com>
2020-06-12 13:53:18 -0300

    added sort tests possibility

---
+++ src/main/scala/scripts/SortTests.scala
@@ -0,0 +1,46 @@
+package scripts
+
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration._
+
+object SortTests extends App {
+  val minutePattern = "([0-9]+?) minute( )??".r
+  val secondPattern = "([0-9]+?) second( )?".r
+  val millisPattern = "([0-9]+?) millisecond( )?".r
+  val descriptionPattern = "(.*?)\\([0-9]+? .*?\\)??".r
+
+  def parseLine(text: String): (Duration, String) = {
+    val minutes = minutePattern.findAllMatchIn(text)
+      .toList
+      .headOption
+      .map(x => Duration(x.group(1).toInt, TimeUnit.MINUTES))
+      .getOrElse(Duration.Zero)
+    val seconds = secondPattern.findAllMatchIn(text)
+      .toList
+      .headOption
+      .map(x => Duration(x.group(1).toInt, TimeUnit.SECONDS))
+      .getOrElse(Duration.Zero)
+    val millis = millisPattern.findAllMatchIn(text)
+      .toList
+      .headOption
+      .map(x => Duration(x.group(1).toInt, TimeUnit.MILLISECONDS))
+      .getOrElse(Duration.Zero)
+
+    val description = descriptionPattern.findAllMatchIn(text)
+      .toList
+      .headOption
+      .map(_.group(1).trim)
+      .getOrElse("could not extract from: " + text)
+
+    (minutes + seconds + millis, description)
+  }
+
+  val source = scala.io.Source.fromFile(args(0))
+  val allLines = try source.getLines.toList finally source.close()
+
+  val tests = allLines.filter(l => l.toLowerCase.contains("second") || l.toLowerCase.contains("minute"))
+  val testsSortedDesc = tests.map(parseLine).sortWith(_._1 > _._1)
+
+  println(testsSortedDesc.mkString("\n"))
+}
