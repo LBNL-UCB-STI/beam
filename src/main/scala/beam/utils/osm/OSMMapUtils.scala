82cb24962040b46eacb2dd91f9458790f46aad22
irishwithaxe <irishwithaxe@gmail.com>
2020-06-26 18:24:57 +0300

    first approach to simplify network

--- src/main/scala/beam/utils/osm/OSMMapUtils.scala
+++ src/main/scala/beam/utils/osm/OSMMapUtils.scala
@@ -1,13 +1,21 @@
 package beam.utils.osm
 
+import java.lang
+
+import beam.sim.common.GeoUtils
 import beam.utils.csv.CsvWriter
-import com.conveyal.osmlib.{OSM, Way}
+import com.conveyal.osmlib.{Node, OSM, Way}
+import org.matsim.api.core.v01.Coord
 
 import scala.collection.mutable
 import scala.collection.JavaConverters._
 import scala.util.Random
 
 object OSMMapUtils {
+
+  private val geo: GeoUtils = new GeoUtils {
+    override def localCRS: String = "epsg:2808"
+  }
 
   def readMap(filePath: String): OSM = {
     val osm = new OSM(null)
@@ -20,36 +28,138 @@ object OSMMapUtils {
 
     println(s"count of nodes is: ${osm.nodes.size()}")
     println(s"count of ways is: $totalWays")
+    println(s"count of relations is: ${osm.relations.size()}")
   }
 
-  def writeTagToWay(osm: OSM, outputCSVFilePath: String): Unit = {
-    val tagValToWay = mutable.Map.empty[String, Int]
-    def plusTagWay(tagVal: String): Unit = {
-      tagValToWay.get(tagVal) match {
-        case Some(count) => tagValToWay(tagVal) = count + 1
-        case None        => tagValToWay(tagVal) = 1
-      }
+  def getNodeCoord(osm: OSM, nodeId: Long): Coord = {
+    val node: Node = osm.nodes.get(nodeId)
+    new Coord(node.getLon, node.getLat)
+  }
+
+  def getDistance(osm: OSM, nodeId1: Long, nodeId2: Long): Double = {
+    val coord1 = getNodeCoord(osm, nodeId1)
+    val coord2 = getNodeCoord(osm, nodeId2)
+    geo.distLatLon2Meters(coord1, coord2)
+  }
+
+  def getWayLen(osm: OSM, way: Way): Double = {
+    val (_, distance) = way.nodes.tail.foldLeft((way.nodes.head, 0.0)) {
+      case ((node1, totalDistance), node2) => (node2, getDistance(osm, node1, node2) + totalDistance)
     }
 
-    osm.ways.asScala.foreach {
-      case (_, way) =>
-        if (way.tags == null) plusTagWay("empty-tag")
-        else way.tags.asScala.foreach(osmTag => plusTagWay(s"${osmTag.key}+${osmTag.value}"))
-    }
+    distance
+  }
 
+  def writeOSMInfo(osm: OSM, outputCSVFilePath: String): Unit = {
     val csvWriter = new CsvWriter(
       outputCSVFilePath,
       Vector(
-        "tag+value",
-        "wayCnt",
+        "wayId",
+        "nodesCount",
+        "wayLength"
       )
     )
 
-    tagValToWay.toSeq.sortBy(_._2).foreach {
-      case (tag, cnt) => csvWriter.writeRow(IndexedSeq("\"" + tag.replace(',', '.') + "\"", cnt))
+    osm.ways.asScala.foreach {
+      case (wayId, way) => csvWriter.writeRow(IndexedSeq(wayId, way.nodes.length, getWayLen(osm, way)))
     }
 
     csvWriter.close()
+  }
+
+  def removeShortWays(osm: OSM, minDistanceInMeters: Double): OSM = {
+    var nextNodeId: Long = osm.nodes.asScala.keys.max + 1
+    class NodeGroup(val nodesIds: Set[Long]) {
+      val nodes: Map[lang.Long, Node] = osm.nodes.asScala.filter { case (nodeId, _) => nodesIds.contains(nodeId) }.toMap
+
+      def getCenterNode: Node = {
+        val centerLat: Double = {
+          val minLat: Double = nodes.map(_._2.getLat).min
+          val maxLat: Double = nodes.map(_._2.getLat).max
+          (minLat + maxLat) / 2.0
+        }
+
+        val centerLon: Double = {
+          val minLon: Double = nodes.map(_._2.getLon).min
+          val maxLon: Double = nodes.map(_._2.getLon).max
+          (minLon + maxLon) / 2.0
+        }
+
+        new Node(centerLat, centerLon)
+      }
+
+      val centerNodeId: Long = {
+        val newId = nextNodeId
+        nextNodeId += 1
+        newId
+      }
+    }
+
+    val shortWays = osm.ways.asScala.filter {
+      case (_, way) => getWayLen(osm, way) < minDistanceInMeters
+    }.toArray
+
+    println(s"got ${shortWays.length} ways with len less than $minDistanceInMeters")
+
+    val waysToRemove = mutable.HashMap.empty[Long, Way]
+    val affectedNodes = mutable.HashMap.empty[Long, NodeGroup]
+    val groupedNodes = mutable.ListBuffer.empty[NodeGroup]
+
+    shortWays.foreach {
+      case (wayId, way) if !way.nodes.exists(affectedNodes.contains) =>
+        waysToRemove(wayId) = way
+        val group = new NodeGroup(way.nodes.toSet)
+        groupedNodes += group
+        way.nodes.foreach(nodeId => affectedNodes(nodeId) = group)
+      case _ =>
+    }
+
+    val copy = new OSM(null)
+    copy.writeBegin()
+
+    val secondsSinceEpoch: Long = System.currentTimeMillis / 1000
+    copy.setReplicationTimestamp(secondsSinceEpoch)
+
+    osm.ways.asScala.foreach {
+      case (wayId, way) if !waysToRemove.contains(wayId) =>
+        if (way.nodes.exists(affectedNodes.contains)) {
+          val newNodes = mutable.ListBuffer.empty[Long]
+          way.nodes
+            .map { nodeId =>
+              affectedNodes.get(nodeId) match {
+                case None        => nodeId
+                case Some(group) => group.centerNodeId
+              }
+            }
+            .foreach { nodeId =>
+              newNodes.lastOption match {
+                case None                             => newNodes += nodeId
+                case Some(lastId) if nodeId != lastId => newNodes += nodeId
+                case _                                =>
+              }
+            }
+          way.nodes = newNodes.toArray
+          copy.writeWay(wayId, way)
+        } else {
+          copy.writeWay(wayId, way)
+        }
+      case _ =>
+    }
+
+    osm.nodes.asScala.foreach {
+      case (nodeId, node) if !affectedNodes.contains(nodeId) => copy.writeNode(nodeId, node)
+      case _                                                 =>
+    }
+
+    groupedNodes.foreach { nodeGroup =>
+      copy.writeNode(nodeGroup.centerNodeId, nodeGroup.getCenterNode)
+    }
+
+    osm.relations.asScala.foreach {
+      case (relationId, relation) => copy.writeRelation(relationId, relation)
+    }
+
+    copy
   }
 
   def copyWithChosenWays(osm: OSM, isChosen: Way => Boolean): OSM = {
@@ -82,36 +192,41 @@ object OSMMapUtils {
 
   def main(args: Array[String]): Unit = {
 
-    //    showInfo("/mnt/data/work/beam/beam/test/input/detroit/r5-light/detroit-cut.osm.pbf")
+    def printInfo(filePath: String): Unit = {
+      println(s"map: $filePath")
+      val map = readMap(filePath)
+      showInfo(map)
+    }
 
-    val mapPath = "/mnt/data/work/beam/detroit-scenario-data/detroit-big.osm.pbf"
-    val cutMapPath = "/mnt/data/work/beam/detroit-scenario-data/detroit-big-without-residential.osm.pbf"
+//    printInfo("/mnt/data/work/beam/detroit-scenario-data/detroit-big-filtered.osm.pbf")
+//    printInfo("/mnt/data/work/beam/detroit-scenario-data/detroit-big-cut-0.8.osm.pbf")
+//    printInfo("/mnt/data/work/beam/detroit-scenario-data/detroit-big.osm.pbf")
+
+    val mapPath = "/mnt/data/work/beam/detroit-scenario-data/detroit-tiny.osm.pbf"
+    val cutMapPath = "/mnt/data/work/beam/detroit-scenario-data/detroit-tiny-simplified.osm.pbf"
     val map = readMap(mapPath)
     showInfo(map)
 
-    val random = new Random()
-    val map_cut = copyWithChosenWays(map, _ => random.nextDouble() < 0.4)
-    showInfo(map_cut)
-    map_cut.writeToFile(cutMapPath)
+    val copy = removeShortWays(map, 30.0)
+    showInfo(copy)
+    copy.writeToFile(cutMapPath)
 
-//    showInfo("/mnt/data/work/beam/beam/test/input/detroit/r5/detroit-big.osm.pbf")
-//    showInfo("/mnt/data/work/beam/beam/test/input/texas/r5-prod/texas-six-counties-simplified.osm.pbf")
-
-//    for ((_, relation) <- osm.relations.asScala) {
-//      if (relation.tags != null)
-//        for (tag <- relation.tags.asScala) {
-//          tags.get(tag.key) match {
-//            case Some(vals) => vals += tag.value
-//            case None       => tags(tag.key) = mutable.Set(tag.value)
-//          }
-//        }
+//    writeOSMInfo(map, "/home/nikolay/.jupyter-files/detroit.tiny.stats.csv")
+//    val random = new Random()
 //
-////      for (member <- relation.members.asScala) {
-////                if (member.`type` eq OSMEntity.Type.NODE) assertTrue(osm.relationsByNode.contains(Fun.t2(member.id, id)))
-////                else if (member.`type` eq OSMEntity.Type.WAY) assertTrue(osm.relationsByWay.contains(Fun.t2(member.id, id)))
-////                else if (member.`type` eq OSMEntity.Type.RELATION) assertTrue(osm.relationsByRelation.contains(Fun.t2(member.id, id)))
-////      }
+//    // highway=residential
+//    def choosePercentOfResidentialAndFootway(way: Way): Boolean = {
+//      if (way.tags == null || !way.tags.asScala.exists(
+//            tag => tag.key == "highway" && (tag.value == "residential" || tag.value == "footway")
+//          )) {
+//        true
+//      } else {
+//        random.nextDouble() < 0.6
+//      }
 //    }
-//    println(tags.size)
+//
+//    val map_cut = copyWithChosenWays(map, choosePercentOfResidentialAndFootway)
+//    showInfo(map_cut)
+//    map_cut.writeToFile(cutMapPath)
   }
 }
