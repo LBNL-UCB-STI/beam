40fe53ec6330a3b48c9fc212e4b1719e0500863d
Illia Tkachuk <tkachuk.i.m@gmail.com>
2020-07-06 17:52:58 +0200

    #2787 - Added scaladoc for non trivial CSV parsing

--- src/main/scala/beam/router/r5/FrequencyAdjustingNetworkCoordinator.scala
+++ src/main/scala/beam/router/r5/FrequencyAdjustingNetworkCoordinator.scala
@@ -35,6 +35,21 @@ case class FrequencyAdjustingNetworkCoordinator(beamConfig: BeamConfig) extends 
     scenario
   }
 
+  /** Loads CSV in the following format:
+    * {{{
+    *   trip_id,start_time,end_time,headway_secs,exact_times
+    *   bus:B1-EAST-1,06:00:00,22:00:00,150,
+    *   train:R2-NORTH-1,07:00:00,23:00:00,300,
+    *   ...
+    * }}}
+    * where "bus:", "train:" etc. prefixes for tripId are the names of GTFS feeds,
+    * everything else have the same format as <a href="https://developers.google.com/transit/gtfs/reference#frequenciestxt">frequencies.txt</a>
+    * <br/>
+    * Therefore part "bus:B1" will be considered as "routeId", whole thing "bus:B1-EAST-1" - as a tripId
+    * in the [[com.conveyal.r5.transit.TransportNetwork#transitLayer]]
+    *
+    * @return set of FrequencyAdjustment objects
+    */
   def loadFrequencyAdjustments(): Set[FrequencyAdjustment] =
     CsvFileUtils
       .readCsvFileByLineToList(beamConfig.beam.agentsim.scenarios.frequencyAdjustmentFile) { row =>
