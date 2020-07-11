c5170d1443bc7ee8ebf965a2dad09601f77646a4
Artavazd Balaian <reasyu@gmail.com>
2020-07-12 02:29:29 +0700

    Fix `BeamLegOrdering` by actually comparing all (but not `mode`) fields of `BeamLeg`

--- src/main/scala/beam/agentsim/events/SpaceTime.scala
+++ src/main/scala/beam/agentsim/events/SpaceTime.scala
@@ -4,7 +4,20 @@ import beam.utils.MathUtils
 import io.circe.{Encoder, Json}
 import org.matsim.api.core.v01.Coord
 
-case class SpaceTime(loc: Coord, time: Int)
+case class SpaceTime(loc: Coord, time: Int) extends Ordered[SpaceTime] {
+  import scala.math.Ordered.orderingToOrdered
+
+  override def compare(that: SpaceTime): Int = {
+    // FIXME Heap allocations, if it is hot place, replace it by good old if else
+    val thisCord = (loc.getX, loc.getY)
+    val thatCord = (that.loc.getX, that.loc.getY)
+    val r = thisCord.compareTo(thatCord)
+    if (r != 0) r
+    else {
+      time.compareTo(that.time)
+    }
+  }
+}
 
 object SpaceTime {
   def apply(x: Double, y: Double, time: Int): SpaceTime = SpaceTime(new Coord(x, y), time)
