bd766d484abad8f9a6905bfd2b50406a62377488
Dmitry Openkov <dimaopen@gmail.com>
2020-07-13 15:10:35 +0300

    TransitCrowding: get average of the last 2 iterations

--- src/main/scala/beam/router/skim/TransitCrowdingSkims.scala
+++ src/main/scala/beam/router/skim/TransitCrowdingSkims.scala
@@ -1,7 +1,10 @@
 package beam.router.skim
 
+import java.math.RoundingMode
+
 import beam.router.model.EmbodiedBeamTrip
 import beam.router.skim.TransitCrowdingSkimmer.{TransitCrowdingSkimmerInternal, TransitCrowdingSkimmerKey}
+import com.google.common.math.IntMath
 import org.apache.commons.math3.stat.descriptive.rank.Percentile
 import org.matsim.api.core.v01.Id
 import org.matsim.vehicles.Vehicle
@@ -41,6 +44,36 @@ class TransitCrowdingSkims extends AbstractSkimmerReadOnly {
 
   private def getSkimValue(vehicleId: Id[Vehicle], fromStopIdx: Int): Option[TransitCrowdingSkimmerInternal] = {
     val key = TransitCrowdingSkimmerKey(vehicleId, fromStopIdx)
+    if (pastSkims.size >= 2) {
+      average(
+        pastSkims(0).get(key).asInstanceOf[Option[TransitCrowdingSkimmerInternal]],
+        pastSkims(1).get(key).asInstanceOf[Option[TransitCrowdingSkimmerInternal]]
+      ).orElse(getFromPastOrAggregated(key))
+    } else
+      getFromPastOrAggregated(key)
+  }
+
+  private def average(
+    first: Option[TransitCrowdingSkimmerInternal],
+    second: Option[TransitCrowdingSkimmerInternal]
+  ): Option[TransitCrowdingSkimmerInternal] = {
+    (first, second) match {
+      case (s @ Some(_), None) => s
+      case (None, s @ Some(_)) => s
+      case (None, None)        => None
+      case (Some(prev), Some(current)) =>
+        Some(
+          TransitCrowdingSkimmerInternal(
+            numberOfPassengers =
+              IntMath.divide(prev.numberOfPassengers + current.numberOfPassengers, 2, RoundingMode.HALF_UP),
+            capacity = IntMath.divide(prev.capacity + current.capacity, 2, RoundingMode.HALF_UP),
+            iterations = 2
+          )
+        )
+    }
+  }
+
+  private def getFromPastOrAggregated(key: TransitCrowdingSkimmerKey): Option[TransitCrowdingSkimmerInternal] = {
     pastSkims.headOption
       .map(_.get(key))
       .getOrElse(aggregatedSkim.get(key))
