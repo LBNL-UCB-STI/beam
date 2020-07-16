3f4c9d91edf441e9de27d7ad8dc295265c67e0b0
Dmitry Openkov <dimaopen@gmail.com>
2020-07-16 21:05:52 +0300

    Run ModeIterationPlanCleaner at BeamMobsim

--- src/main/scala/beam/replanning/ModeIterationPlanCleaner.scala
+++ src/main/scala/beam/replanning/ModeIterationPlanCleaner.scala
@@ -4,24 +4,20 @@ import com.google.inject.Inject
 import com.typesafe.scalalogging.LazyLogging
 import org.matsim.api.core.v01.Scenario
 import org.matsim.api.core.v01.population._
-import org.matsim.core.controler.events.IterationStartsEvent
-import org.matsim.core.controler.listener.IterationStartsListener
 
 import scala.collection.JavaConverters._
 
-class ModeIterationPlanCleaner @Inject()(config: BeamConfig, scenario: Scenario)
-    extends IterationStartsListener
-    with LazyLogging {
+class ModeIterationPlanCleaner @Inject()(config: BeamConfig, scenario: Scenario) extends LazyLogging {
 
   private val clearModeIteration: Int = config.beam.replanning.clearModes.iteration
   private val clearModes: Set[String] =
     config.beam.replanning.clearModes.modes.getOrElse(List.empty).map(_.toLowerCase).toSet
 
-  override def notifyIterationStarts(event: IterationStartsEvent): Unit = {
-    if (clearModes.nonEmpty && (event.getIteration == clearModeIteration
-        || event.getIteration > clearModeIteration
+  def clearModesAccordingToStrategy(iteration: Int): Unit = {
+    if (clearModes.nonEmpty && (iteration == clearModeIteration
+        || iteration > clearModeIteration
         && "AtBeginningAndAllSubsequentIterations".equalsIgnoreCase(config.beam.replanning.clearModes.strategy))) {
-      logger.debug("Cleaning modes {} for iteration {}", clearModes.mkString("(", ", ", ")"), event.getIteration)
+      logger.debug("Cleaning modes {} for iteration {}", clearModes.mkString("(", ", ", ")"), iteration)
       replanModesForPopulation(scenario.getPopulation)
     }
   }
