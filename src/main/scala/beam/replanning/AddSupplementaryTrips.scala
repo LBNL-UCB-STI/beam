f33ed337735f72e16229fe9b302ac4da0823f3f1
rajnikant <rk_sharmait@yahoo.com>
2020-07-22 03:47:38 +0530

    remove old plan

--- src/main/scala/beam/replanning/AddSupplementaryTrips.scala
+++ src/main/scala/beam/replanning/AddSupplementaryTrips.scala
@@ -32,6 +32,7 @@ class AddSupplementaryTrips @Inject()(beamConfig: BeamConfig) extends PlansStrat
       AttributesUtils.copyAttributesFromTo(person.getSelectedPlan, newPlan)
 
       if (newPlan.getPlanElements.size > 1) {
+        person.getPlans.remove(person.getSelectedPlan)
         person.addPlan(newPlan)
         person.setSelectedPlan(newPlan)
       }
@@ -86,7 +87,7 @@ class AddSupplementaryTrips @Inject()(beamConfig: BeamConfig) extends PlansStrat
     activity: Activity
   ): List[Activity] = {
     val startTime = if (activity.getStartTime > 0) { activity.getStartTime } else { 0 }
-    val endTime = if (activity.getEndTime > 0) { activity.getEndTime } else { 3600 * 24 }
+    val endTime = if (activity.getEndTime > 0) { activity.getEndTime } else { 3600 * 30 }
 
     val newStartTime = (endTime - startTime) / 2 - 1 + startTime
     val newEndTime = (endTime - startTime) / 2 + 1 + startTime
