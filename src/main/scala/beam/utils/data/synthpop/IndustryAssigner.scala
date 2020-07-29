d74076b2f18a02c51cf8fee247c805a68ea52649
Artavazd Balaian <reasyu@gmail.com>
2020-07-29 21:48:39 +0700

    Fix the path

--- src/main/scala/beam/utils/data/synthpop/IndustryAssigner.scala
+++ src/main/scala/beam/utils/data/synthpop/IndustryAssigner.scala
@@ -18,7 +18,7 @@ object IndustryAssigner {
   def main(args: Array[String]): Unit = {
     require(args.length == 2, "Expected two args: 1) path to CTPP 2) Path to plans")
     val pathToCTPP: String = args(0) // "d:/Work/beam/CTPP/"
-    val pathToPlans: String = args(1) // "D:/Work/beam/NewYork/results_07-10-2020_22-13-14/plans.csv.gz"
+    val pathToPlans: String = args(1) // "C:/repos/beam/test/input/newyork/generic_scenario/4040k-NY-related/plans.csv.gz"
     val databaseInfo = CTPPDatabaseInfo(PathToData(pathToCTPP), Set("36", "34"))
 
     val odList = new IndustryTableReader(databaseInfo, ResidenceToWorkplaceFlowGeography.`TAZ To TAZ`).read()
