c4315ea778fb2c96f1ab4a6fa78212e279f61865
Dmitry Ogurtsov <dogurtsov@gmail.com>
2020-08-03 22:24:35 +0700

    add beam graphhopper

--- src/main/scala/beam/router/graphhopper/BeamWeighting.scala
+++ src/main/scala/beam/router/graphhopper/BeamWeighting.scala
@@ -1,40 +1,33 @@
 package beam.router.graphhopper
 
 import com.graphhopper.routing.util.FlagEncoder
-import com.graphhopper.routing.weighting.FastestWeighting
+import com.graphhopper.routing.weighting.{FastestWeighting, TurnCostProvider}
 import com.graphhopper.util.{EdgeIteratorState, FetchMode}
 import com.typesafe.scalalogging.LazyLogging
 import org.locationtech.jts.geom.Coordinate
 import org.matsim.api.core.v01.Coord
 import org.matsim.api.core.v01.network.Link
 
-class BeamWeighting(flagEncoder: FlagEncoder, wayId2TravelTime: Map[Long, Double])
-    extends FastestWeighting(flagEncoder)
-    with LazyLogging {
+class BeamWeighting(flagEncoder: FlagEncoder, turnCostProvider: TurnCostProvider, wayId2TravelTime: Map[Long, Double])
+  extends FastestWeighting(flagEncoder, turnCostProvider) with LazyLogging {
 
   override def getMinWeight(distance: Double): Double = super.getMinWeight(distance)
 
-  override def calcEdgeMillis(edgeState: EdgeIteratorState, reverse: Boolean): Long =
-    super.calcEdgeMillis(edgeState, reverse)
-
-  override def calcEdgeWeight(edgeState: EdgeIteratorState, reverse: Boolean): Double = {
-    val nodeGeom = edgeState.fetchWayGeometry(FetchMode.ALL).toLineString(false)
-
-    val time = for {
-      link <- wayId2TravelTime.get(
-        toCoord(nodeGeom.getCoordinateN(0)),
-        toCoord(nodeGeom.getCoordinateN(nodeGeom.getNumPoints - 1))
-      )
-      travelTime <- link2TravelTime.get(link)
-    } yield {
-      travelTime
-    }
-
-    time.getOrElse {
-      logger.info("FUCK!")
-      super.calcEdgeWeight(edgeState, reverse)
+  override def calcEdgeMillis(edgeState: EdgeIteratorState, reverse: Boolean): Long = {
+    calcEdgeTime(edgeState, reverse) match {
+      case Some(value) => (1000 * value).toLong
+      case None => super.calcEdgeMillis(edgeState, reverse)
     }
   }
 
-  private def toCoord(coord: Coordinate): Coord = new Coord(coord.x, coord.y)
+  override def calcEdgeWeight(edgeState: EdgeIteratorState, reverse: Boolean): Double = {
+    calcEdgeTime(edgeState, reverse).getOrElse(super.calcEdgeWeight(edgeState, reverse))
+  }
+
+  private def calcEdgeTime(edgeState: EdgeIteratorState, reverse: Boolean): Option[Double] = {
+    val edgeId = if (reverse) 2 * edgeState.getEdge else 2 * edgeState.getEdge + 1
+    wayId2TravelTime.get(edgeId)
+  }
+
+  override def getName = "beam"
 }
