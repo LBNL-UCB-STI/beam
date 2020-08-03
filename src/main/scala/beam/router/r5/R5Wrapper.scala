905456f57b9815a2564bd2cf97115449533c986b
Dmitry Ogurtsov <dogurtsov@gmail.com>
2020-08-03 22:35:05 +0700

    fix

--- src/main/scala/beam/router/r5/R5Wrapper.scala
+++ src/main/scala/beam/router/r5/R5Wrapper.scala
@@ -45,6 +45,7 @@ class R5Wrapper(workerParams: R5Parameters, travelTime: TravelTime, travelTimeNo
   private val R5Parameters(
     beamConfig,
     transportNetwork,
+    _,
     vehicleTypes,
     fuelTypePrices,
     ptFares,
