c4d01dec129004706ed13104e7081bf542bbd975
Artavazd Balaian <reasyu@gmail.com>
2020-08-13 23:29:50 +0700

    Show Java version and JVM args

--- src/main/scala/beam/sim/BeamHelper.scala
+++ src/main/scala/beam/sim/BeamHelper.scala
@@ -485,7 +485,8 @@ trait BeamHelper extends LazyLogging {
       beamExecutionConfig.beamConfig,
       beamExecutionConfig.matsimConfig,
     )
-
+    logger.info(s"Java version: ${System.getProperty("java.version")}")
+    logger.info("JVM args: " + java.lang.management.ManagementFactory.getRuntimeMXBean.getInputArguments.asScala.toList.toString())
     logger.info(s"Heap size: ${MathUtils.formatBytes(Runtime.getRuntime.totalMemory())}")
     logger.info(s"Heap max memory: ${MathUtils.formatBytes(Runtime.getRuntime.maxMemory())}")
     logger.info(s"Heap free memory: ${MathUtils.formatBytes(Runtime.getRuntime.freeMemory())}")
