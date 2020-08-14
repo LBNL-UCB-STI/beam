4e85c5c4886929c474ad7002340c215a8c335cf8
Ilya Tkachuk <tkachuk.i.m@gmail.com>
2020-08-14 07:08:28 +0200

    #2845 - Test for ChargingNetworkManager

---
+++ src/test/scala/beam/agentsim/infrastructure/ChargingNetworkManagerSpec.scala
@@ -0,0 +1,55 @@
+package beam.agentsim.infrastructure
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import beam.agentsim.infrastructure.ChargingNetworkManager.PlanningTimeOutTrigger
+import beam.agentsim.scheduler.BeamAgentScheduler.{CompletionNotice, ScheduleTrigger}
+import beam.agentsim.scheduler.Trigger.TriggerWithId
+import beam.sim.config.BeamConfig
+import beam.sim.{BeamHelper, BeamScenario, BeamServices}
+import beam.utils.TestConfigUtils.testConfig
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.scalatestplus.mockito.MockitoSugar
+
+class ChargingNetworkManagerSpec
+    extends TestKit(
+      ActorSystem(
+        "ChargingNetworkManagerSpec",
+        testConfig("test/input/beamville/beam.conf").resolve()
+      )
+    )
+    with WordSpecLike
+    with Matchers
+    with BeamHelper
+    with ImplicitSender
+    with MockitoSugar
+    with BeforeAndAfterAll {
+
+  val beamConfig: BeamConfig = BeamConfig(system.settings.config)
+  val beamServices: BeamServices = mock[BeamServices]
+
+  var beamScenario: BeamScenario = _
+  var chargingNetworkManager: ActorRef = _
+
+  "ChargingNetworkManager" should {
+    beamScenario = loadScenario(beamConfig)
+      .copy(fuelTypePrices = Map().withDefaultValue(0.0)) // Reset fuel prices to 0 so we get pure monetary costs
+
+    chargingNetworkManager = system.actorOf(
+      Props(
+        new ChargingNetworkManager(
+          beamServices,
+          beamScenario.beamConfig,
+          beamScenario.privateVehicles
+        )
+      )
+    )
+    "process trigger event" in {
+      val request = TriggerWithId(PlanningTimeOutTrigger(300), 0)
+      chargingNetworkManager ! request
+
+      val response = expectMsgType[CompletionNotice]
+      response.newTriggers shouldBe Vector(ScheduleTrigger(PlanningTimeOutTrigger(600), chargingNetworkManager))
+    }
+  }
+}
