6602da99d22c3eede12504ad74b23799c38492cd
Artavazd Balaian <reasyu@gmail.com>
2020-08-21 00:22:40 +0700

    More precise types instead of just `Iterable`

--- src/main/scala/beam/utils/data/ctpp/readers/BaseTableReader.scala
+++ src/main/scala/beam/utils/data/ctpp/readers/BaseTableReader.scala
@@ -44,11 +44,11 @@ abstract class BaseTableReader(
     }
   }
 
-  protected def readRaw(): Iterable[CTPPEntry] = {
+  protected def readRaw(): Seq[CTPPEntry] = {
     stateToCsvTablePath.values.flatMap { path =>
       CTPPParser
         .readTable(path, geographyLevelFilter)
-    }
+    }.toSeq
   }
 }
 
