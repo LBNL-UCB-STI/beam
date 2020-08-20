6820dd29e277d326292715b26833f19d9dd6b8ee
Carlos Caldas <ccaldas@gmail.com>
2020-08-20 19:26:25 -0300

    undo change

--- src/main/java/beam/router/r5/R5MnetBuilder.java
+++ src/main/java/beam/router/r5/R5MnetBuilder.java
@@ -35,6 +35,7 @@ public class R5MnetBuilder {
     private final GeotoolsTransformation transform;
     private final String osmFile;
     private final Map<Coord, Id<Node>> coordinateNodes = new HashMap<>();
+    private final BeamConfig.Beam beamConfig;
     private final HighwaySetting highwaySetting;
 
     private int matsimNetworkNodeId = 0;
@@ -45,12 +46,12 @@ public class R5MnetBuilder {
      */
     public R5MnetBuilder(TransportNetwork r5Net, BeamConfig beamConfig, HighwaySetting highwaySetting) {
         this.r5Network = r5Net;
+        this.beamConfig = beamConfig.beam();
         this.highwaySetting = highwaySetting;
 
-        BeamConfig.Beam.Routing routing = beamConfig.beam().routing();
-        osmFile = routing.r5().osmMapdbFile();
-        transform = new GeotoolsTransformation(routing.r5().mNetBuilder().fromCRS(),
-                routing.r5().mNetBuilder().toCRS());
+        osmFile = this.beamConfig.routing().r5().osmMapdbFile();
+        transform = new GeotoolsTransformation(this.beamConfig.routing().r5().mNetBuilder().fromCRS(),
+                this.beamConfig.routing().r5().mNetBuilder().toCRS());
         mNetwork = NetworkUtils.createNetwork();
     }
 
