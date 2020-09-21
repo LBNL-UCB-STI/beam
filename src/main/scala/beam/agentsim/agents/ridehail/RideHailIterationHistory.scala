4b91630452e6098fd7c51a26f64eb898519ce28a
Carlos Caldas <ccaldas@gmail.com>
2020-09-21 17:17:38 -0300

    removed typo

--- src/main/scala/beam/agentsim/agents/ridehail/RideHailIterationHistory.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/RideHailIterationHistory.scala
@@ -5,7 +5,6 @@ import javax.inject.Inject
 import scala.collection.mutable.ArrayBuffer
 
 class RideHailIterationHistory @Inject()() {
-  RideHailManager
   //val rideHailIterationHistory=scala.collection.mutable.ListBuffer( Map[String, ArrayBuffer[Option[RideHailStatsEntry]]])
   // TODO: put in RideHailStats class!
   // create methods in that class, which I need for my programming
