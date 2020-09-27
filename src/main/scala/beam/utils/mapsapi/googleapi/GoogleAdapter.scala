47677d8adeef7018d5a6e052279f096f592bdcc1
Carlos Caldas <ccaldas@gmail.com>
2020-09-27 07:41:17 -0300

    simplified expression

--- src/main/scala/beam/utils/mapsapi/googleapi/GoogleAdapter.scala
+++ src/main/scala/beam/utils/mapsapi/googleapi/GoogleAdapter.scala
@@ -91,10 +91,9 @@ class GoogleAdapter(apiKey: String, outputResponseToFile: Option[Path] = None, a
   }
 
   private def parseRoutes(jsRoutes: Seq[JsValue]): Seq[Route] = {
-    jsRoutes.flatMap { route =>
-      (route \ "legs").as[JsArray].value.headOption match {
-        case Some(value) => Some(parseRoute(value.as[JsObject]))
-        case None        => None
+    jsRoutes.flatMap { route: JsValue =>
+      (route \ "legs").as[JsArray].value.map { value =>
+        parseRoute(value.as[JsObject])
       }
     }
   }
