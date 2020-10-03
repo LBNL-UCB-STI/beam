65b981a946205930d7bea369173c80f595235861
rajnikant <rk_sharmait@yahoo.com>
2020-10-04 00:45:38 +0530

    Fix log sequence issue

--- aws/src/main/python/s3BucketHandler/lambda_function.py
+++ aws/src/main/python/s3BucketHandler/lambda_function.py
@@ -1,12 +1,7 @@
 import os
+import json
 import boto3
 import datetime
-
-lm = boto3.client('lambda')
-#cw = boto3.client('cloudwatch')
-#now = datetime.datetime.now()
-
-DEFAULT_REGION = 'us-east-2'
 
 initscript = (('''#cloud-config
 runcmd:
@@ -16,21 +11,23 @@ runcmd:
   - sudo apt-get install unattended-upgrades
   - sudo dpkg --configure -a
   - sudo apt update
-  - export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
-  - export AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_SECRET
-  - result=$(aws s3 ls s3://$BUCKET/$FOLDER --recursive | awk 'BEGIN {total=0}{total+=$3}END{print total/1024/1024" MB"}')
+  - result=$(aws s3 --region=$REGION ls s3://$BUCKET/$FOLDER --recursive | awk 'BEGIN {total=0}{total+=$3}END{print total/1024/1024" MB"}')
   - echo "invoke cloudwatch for updating matric..."
   - aws logs --region=$REGION create-log-stream --log-group-name S3-Bucket-Log --log-stream-name $BUCKET/$FOLDER
-  - aws logs --region=$REGION describe-log-streams --log-group-name S3-Bucket-Log --order-by=LastEventTime > output.txt
-  - token=$(jq .logStreams[0].uploadSequenceToken output.txt)
-  - temp="${token%\\"}"
-  - temp="${temp#\\"}"
-  - aws logs --region=$REGION put-log-events --log-group-name S3-Bucket-Log --log-stream-name=$BUCKET/$FOLDER --log-events=timestamp=$(($(date +%s%N)/1000000)),message="$result" --sequence-token=$temp
+  - aws logs --region=$REGION describe-log-streams --log-group-name S3-Bucket-Log --log-stream-name-prefix=$BUCKET/$FOLDER > output.txt
+  - token=$(jq -r '.logStreams[] | select(.logStreamName=="$BUCKET/$FOLDER") | .uploadSequenceToken' output.txt)
+  - if [ $token = null ]; then
+  -     aws logs --region=$REGION put-log-events --log-group-name S3-Bucket-Log --log-stream-name=$BUCKET/$FOLDER --log-events=timestamp=$(($(date +%s%N)/1000000)),message="$result"
+  -   else
+  -     aws logs --region=$REGION put-log-events --log-group-name S3-Bucket-Log --log-stream-name=$BUCKET/$FOLDER --log-events=timestamp=$(($(date +%s%N)/1000000)),message="$result" --sequence-token=$token
+  - fi
   - echo "setting up auto shutdown ..."
   - sudo shutdown -h +$SHUTDOWN_WAIT
   - echo "shutdown in $SHUTDOWN_WAIT ..."
 '''))
 
+DEFAULT_REGION = 'us-east-2'
+lm = boto3.client('lambda')
 
 ec2 = None
 
@@ -38,9 +35,9 @@ def init_ec2(region):
     global ec2
     ec2 = boto3.client('ec2',region_name=region)
 
-def startInstance(script, instance_type, shutdown_behaviour):
+def startInstance(script, shutdown_behaviour):
     res = ec2.run_instances(ImageId=os.environ['IMAGE_ID'],
-                            InstanceType=instance_type,
+                            InstanceType=os.environ['INSTANCE_TYPE'],
                             UserData=script,
                             KeyName=os.environ['KEY'],
                             MinCount=1,
@@ -65,13 +62,10 @@ def lambda_handler(event, context):
         return "Bucket Name not provided!!!"
     if prefix is None:
         return "Directory Name not provided!!!"
-    if event.get('aws_access_key') is None or event.get('aws_access_secret') is None:
-        return "AWS credentials not set"
-    instance_type = os.environ['INSTANCE_TYPE']
     shutdown_behaviour = 'terminate'
     shutdown_wait = "10"
     init_ec2(region)
-    script = initscript.replace("$BUCKET", bucket).replace("$FOLDER", prefix).replace('$REGION', region).replace('$SHUTDOWN_WAIT', shutdown_wait).replace('$AWS_ACCESS_KEY', event['aws_access_key']).replace('$AWS_ACCESS_SECRET',event['aws_access_secret'])
+    script = initscript.replace("$BUCKET", bucket).replace("$FOLDER", prefix).replace('$REGION', region).replace('$SHUTDOWN_WAIT', shutdown_wait)
 
-    instance_id = startInstance(script, instance_type, shutdown_behaviour)
+    instance_id = startInstance(script, shutdown_behaviour)
     return instance_id
