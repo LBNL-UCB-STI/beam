a99dbe9620734b247d46e14bc77aca93c8c0e7ca
Artavazd Balaian <reasyu@gmail.com>
2020-10-16 18:57:09 +0700

    Allow to safe way_id via tag parser to the GraphHopper storage

--- src/main/scala/beam/router/graphhopper/GraphHopperOsmUsageExample.scala
+++ src/main/scala/beam/router/graphhopper/GraphHopperOsmUsageExample.scala
@@ -1,13 +1,31 @@
 package beam.router.graphhopper
 
 import com.graphhopper.GHRequest
+import com.graphhopper.routing.util.AllEdgesIterator
+import com.graphhopper.routing.util.parsers.{DefaultTagParserFactory, TagParser, TagParserFactory}
+import com.graphhopper.util.PMap
 
 import scala.collection.JavaConverters._
 
 object GraphHopperOsmUsageExample {
 
   def main(args: Array[String]): Unit = {
-    val gh = GraphHopperWrapper.fromOsm("""test/input/sf-light/r5/sflight_muni.osm.pbf""")
+    val tagParserFactory: TagParserFactory = new TagParserFactory {
+      val default: TagParserFactory = new DefaultTagParserFactory()
+
+      override def create(name: String, configuration: PMap): TagParser = {
+        if (name == "way_id") new WayIdParser()
+        else default.create(name, configuration)
+      }
+    }
+    val gh = GraphHopperWrapper.fromOsm("""test/input/sf-light/r5/sflight_muni.osm.pbf""", Some(tagParserFactory))
+
+    val edgeIdToWayIdConvertor = new EdgeIdToWayIdConvertor(gh.getEncodingManager)
+    val it: AllEdgesIterator = gh.getGraphHopperStorage.getAllEdges
+    while (it.next()) {
+      val wayId = edgeIdToWayIdConvertor.getWayId(it)
+      println(s"${it.getName}: ${wayId}")
+    }
 
     val ghRequest = new GHRequest(37.781534, -122.495603, 37.725107, -122.38193)
       .setProfile("car")
