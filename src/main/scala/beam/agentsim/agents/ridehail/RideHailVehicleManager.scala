1134ca7229c5305b1006eea2a390dd865184e1b0
Artavazd Balaian <reasyu@gmail.com>
2021-01-26 00:24:11 +0700

    Optimization: Use java hashmap with initial capacity to reduce resize overhead with Scala `mutable.HashMap`

--- src/main/scala/beam/agentsim/agents/ridehail/RideHailVehicleManager.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/RideHailVehicleManager.scala
@@ -262,12 +262,14 @@ class RideHailVehicleManager(val rideHailManager: RideHailManager, boundingBox: 
     * any that should be excluded according to the doNotUseInAllocation map.
     * @return HashMap from Id[BeamVehicle] to RideHailAgentLocation
     */
-  def getIdleAndRepositioningVehiclesAndFilterOutExluded: mutable.HashMap[Id[BeamVehicle], RideHailAgentLocation] = {
-    val filteredVehicles: mutable.HashMap[Id[BeamVehicle], RideHailAgentLocation] = collection.mutable.HashMap()
+  def getIdleAndRepositioningVehiclesAndFilterOutExluded: mutable.Map[Id[BeamVehicle], RideHailAgentLocation] = {
+    val repositioningVehicles = getRepositioningVehicles
+    val maxSize = idleRideHailVehicles.size + repositioningVehicles.size
+    val filteredVehicles = new java.util.HashMap[Id[BeamVehicle], RideHailAgentLocation](maxSize)
 
     def addIfNotInAllocation(
-      idleOrRepositioning: mutable.HashMap[Id[BeamVehicle], RideHailVehicleManager.RideHailAgentLocation]
-    ) = {
+                              idleOrRepositioning: mutable.HashMap[Id[BeamVehicle], RideHailVehicleManager.RideHailAgentLocation]
+                            ): Unit = {
       idleOrRepositioning.foreach {
         case (vehicleId, location) =>
           if (!rideHailManager.doNotUseInAllocation.contains(vehicleId)) {
@@ -277,9 +279,9 @@ class RideHailVehicleManager(val rideHailManager: RideHailManager, boundingBox: 
     }
 
     addIfNotInAllocation(idleRideHailVehicles)
-    addIfNotInAllocation(getRepositioningVehicles)
+    addIfNotInAllocation(repositioningVehicles)
 
-    filteredVehicles
+    filteredVehicles.asScala
   }
 
   def getIdleAndRepositioningAndOfflineCAVsAndFilterOutExluded
