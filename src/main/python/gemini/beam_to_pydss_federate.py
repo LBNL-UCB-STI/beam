aa8c2e03c0296ccf28c0944bd44e253e1281638f
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2021-03-19 15:07:53 -0700

    addressing @nikolayilyin comments

--- src/main/python/gemini/beam_to_pydss_federate.py
+++ src/main/python/gemini/beam_to_pydss_federate.py
@@ -130,14 +130,14 @@ def run_beam_to_pydss_federate(station_bus_pairs):
 
 ###############################################################################
 def load_station_bus_pairs():
-    with open('station_bus_pairs.csv', 'r') as sbpfile:
-        station_bus_list = sbpfile.readlines()
+    # with open('station_bus_pairs.csv', 'r') as sbpfile:
+    #     station_bus_list = sbpfile.readlines()
     station_bus_pairs = []
-    for sbp in station_bus_list:
-        pair = sbp.split(',')
-        station_id = pair[0].strip()
-        bus_name = pair[1].strip()
-        station_bus_pairs.append((station_id, bus_name))
+    # for sbp in station_bus_list:
+    #     pair = sbp.split(',')
+    #     station_id = pair[0].strip()
+    #     bus_name = pair[1].strip()
+    #     station_bus_pairs.append((station_id, bus_name))
     return station_bus_pairs
 
 
