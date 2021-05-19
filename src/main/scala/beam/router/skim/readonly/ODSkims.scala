b48a26c64d4cf8cd879c340f4b88060aa9f42026
Dmitry Openkov <dimaopen@gmail.com>
2021-05-19 17:27:42 +0300

    No conversion of pastSkimsInternal; if past skims misses some key return from the aggregated skims.

--- src/main/scala/beam/router/skim/readonly/ODSkims.scala
+++ src/main/scala/beam/router/skim/readonly/ODSkims.scala
@@ -258,15 +258,11 @@ case class ODSkims(beamConfig: BeamConfig, beamScenario: BeamScenario) extends A
   }
 
   private def getSkimValue(time: Int, mode: BeamMode, orig: Id[TAZ], dest: Id[TAZ]): Option[ODSkimmerInternal] = {
-    val res = if (pastSkims.isEmpty) {
-      aggregatedFromPastSkims.get(ODSkimmerKey(timeToBin(time), mode, orig, dest))
-    } else {
-      pastSkims
-        .get(currentIteration - 1)
-        .map(_.get(ODSkimmerKey(timeToBin(time), mode, orig, dest)))
-        .getOrElse(aggregatedFromPastSkims.get(ODSkimmerKey(timeToBin(time), mode, orig, dest)))
-    }
-    res.map(_.asInstanceOf[ODSkimmerInternal])
+    pastSkims
+      .get(currentIteration - 1)
+      .flatMap(_.get(ODSkimmerKey(timeToBin(time), mode, orig, dest)))
+      .orElse(aggregatedFromPastSkims.get(ODSkimmerKey(timeToBin(time), mode, orig, dest)))
+      .asInstanceOf[Option[ODSkimmerInternal]]
   }
 
   def containsValue(
