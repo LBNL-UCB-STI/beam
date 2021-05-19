9e7ba108a35610131925639adbf489fb4fb3c175
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2021-05-18 19:42:13 -0700

    fix python

--- src/main/python/gemini/beam_pydss_broker.py
+++ src/main/python/gemini/beam_pydss_broker.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import time
 import helics as h
+import sys
 
 # helics_recorder beam_recorder.txt --output=recording_output.txt
 # Create broker #
 print("Script started")
-broker = h.helicsCreateBroker("zmq", "", "-f 2 --name=BeamHelicsBroker")
+nbFed = 3
+if len(sys.argv) > 0:
+    nbFed = int(sys.argv[1])
+brokerSetting = "-f {} --name=BeamHelicsBroker".format(nbFed)
+print("broker setting: {}".format(brokerSetting))
+broker = h.helicsCreateBroker("zmq", "", brokerSetting)
 isConnected = h.helicsBrokerIsConnected(broker)
 if isConnected == 1:
     print("Broker created and connected")
--- src/main/python/gemini/beam_pydss_processing.py
+++ src/main/python/gemini/beam_pydss_processing.py
@@ -2,14 +2,14 @@ import os
 
 basefolder1 = "/Users/haitamlaarabi/Documents/Workspace/beam/production-gemini-develop/output/sf-light/"
 basefolder2 = basefolder1+"/sf-light-25k-xml__2021-05-18_16-53-10_sqr"
-beamLog_out = "{}/beamLog.out".format(basefolder2)
-beamLog_out_csv = "{}/beamLog.csv".format(basefolder2)
+beamLog_out = "{}/beamLog2.out".format(basefolder2)
+beamLog_out_csv = "{}/beamLog2.csv".format(basefolder2)
 file1 = open(beamLog_out, 'r')
 Lines = file1.readlines()
 file2 = open(beamLog_out_csv, 'w')
 # Strips the newline character
 for line in Lines:
-    if "DELETE-THIS-" in line or "DELETE-THIS-" in line:
+    if "DELETE-THIS-" in line:
         file2.writelines(line)
         print(line)
 file1.close()
