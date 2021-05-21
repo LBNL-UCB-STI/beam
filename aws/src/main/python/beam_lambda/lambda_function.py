a9809d18cf1441d576b99864afb1e7b12a15163f
irishwithaxe <irishwithaxe@gmail.com>
2021-05-21 17:40:35 +0300

    more logs

--- aws/src/main/python/beam_lambda/lambda_function.py
+++ aws/src/main/python/beam_lambda/lambda_function.py
@@ -11,10 +11,13 @@ HELICS_RUN = '''pip install helics
   -    cd /home/ubuntu/git/beam/src/main/python/gemini
   -    now="$(date +"%Y_%m_%d_%I_%M_%p")"
   -    python beam_pydss_broker.py > output_${now}_broker.log &
+  -    echo "broker started"
   -    sleep 5s
   -    python beam_to_pydss_federate.py > output_${now}_federate.log &
+  -    echo "federate started"
   -    sleep 5s
   -    helics_recorder beam_recorder.txt --output=recording_output.txt > output_${now}_recorder.log &
+  -    echo "recorder started"
   -    sleep 5s
   -    cd /home/ubuntu/git/beam
   -    '''
