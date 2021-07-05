f57607150c07a54cf6f49b7a8914d7f74d24fae1
Alexander Vykhodtsev <avflance@gmail.com>
2021-07-05 17:31:59 +0600

    Clear config system variable so it doesn't get picked up by other tests

--- src/test/scala/beam/router/skim/urbansim/BackgroundSkimsCreatorAppSpec.scala
+++ src/test/scala/beam/router/skim/urbansim/BackgroundSkimsCreatorAppSpec.scala
@@ -2,6 +2,7 @@ package beam.router.skim.urbansim
 
 import beam.router.skim.ActivitySimSkimmer.ExcerptData
 import beam.router.skim.urbansim.BackgroundSkimsCreatorApp.toCsvSkimRow
+import beam.sim.BeamConfigChangesObservable
 import beam.utils.csv.GenericCsvReader
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
@@ -22,16 +23,20 @@ class BackgroundSkimsCreatorAppSpec extends AnyWordSpecLike with Matchers with S
   )
 
   "BackgroundSkimsCreatorApp" should {
+
     "run with parameters" in {
       whenReady(BackgroundSkimsCreatorApp.runWithParams(params)) { _ =>
+        BeamConfigChangesObservable.clear()
         val csv = GenericCsvReader.readAs[ExcerptData](outputPath.toString, toCsvSkimRow, _ => true)._1.toVector
         csv.size shouldBe 3
         csv.count(_.weightedGeneralizedTime > 0) shouldBe 3
         csv.count(_.weightedGeneralizedTime > 10) shouldBe 1
       }
     }
+
     "generate all skims if input is not set" in {
       whenReady(BackgroundSkimsCreatorApp.runWithParams(params.copy(input = None))) { _ =>
+        BeamConfigChangesObservable.clear()
         val csv = GenericCsvReader.readAs[ExcerptData](outputPath.toString, toCsvSkimRow, _ => true)._1.toVector
         csv.size shouldBe 840
         csv.count(_.weightedGeneralizedTime > 0) shouldBe 60
