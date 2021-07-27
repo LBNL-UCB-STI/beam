3d0107465fd9ea2faff3b7747a8acaee20852c2a
Justin Pihony <justin.pihony@gmail.com>
2021-07-26 23:06:58 -0400

    logging change for CI test

--- src/main/scala/beam/agentsim/scheduler/BeamAgentScheduler.scala
+++ src/main/scala/beam/agentsim/scheduler/BeamAgentScheduler.scala
@@ -8,7 +8,7 @@ import beam.agentsim.scheduler.BeamAgentScheduler._
 import beam.agentsim.scheduler.Trigger.TriggerWithId
 import beam.sim.config.BeamConfig
 import beam.utils.StuckFinder
-import beam.utils.logging.{LogActorState, LoggingMessageActor}
+import beam.utils.logging.{ExponentialLazyLogging, LogActorState, LoggingMessageActor}
 import com.google.common.collect.TreeMultimap
 
 import java.util.Comparator
@@ -123,7 +123,8 @@ class BeamAgentScheduler(
   val maxWindow: Int,
   val stuckFinder: StuckFinder
 ) extends LoggingMessageActor
-    with ActorLogging {
+    with ActorLogging
+    with ExponentialLazyLogging {
   // Used to set a limit on the total time to process messages (we want this to be quite large).
   private implicit val timeout: Timeout = Timeout(50000, TimeUnit.SECONDS)
 
@@ -257,6 +258,7 @@ class BeamAgentScheduler(
              |\ttriggerQueue.head=${Option(triggerQueue.peek())}
              |\tawaitingResponse.head=$awaitingToString""".stripMargin
         log.info(logStr)
+        logger.info(logStr)
 
         // if RidehailManager at first position in queue, it is very likely, that we are stuck
         awaitingResponse.values().asScala.take(1).foreach { x =>
