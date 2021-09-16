8458066faa42c1458a9b9bebdf5b0454aa930e6c
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2021-09-16 10:05:27 -0700

    infrastructure with XFC

--- src/main/python/gemini/unlimited_charging_infrastructure.py
+++ src/main/python/gemini/unlimited_charging_infrastructure.py
@@ -2,26 +2,28 @@
 
 headerfile = "taz,parkingType,pricingModel,chargingPointType,numStalls,feeInCents,reservedFor"
 
-with open('gemini_taz_unlimited_parking_plugs_power_150kw.csv', mode='w') as csv_writer:
+with open('gemini_taz_unlimited_parking_plugs_power.csv', mode='w') as csv_writer:
     csv_writer.write(headerfile+"\n")
 
     for x in range(1, 1455):
-        csv_writer.write(f"{x},Residential,Block,NoCharger,9999999,0," + "\n")
-        csv_writer.write(f"{x},Residential,Block,HomeLevel1(1.8|AC),9999999,50," + "\n")
-        csv_writer.write(f"{x},Residential,Block,HomeLevel2(7.2|AC),9999999,200," + "\n")
+        csv_writer.write(f"{x},Residential,Block,NoCharger,9999999,0,Any" + "\n")
+        csv_writer.write(f"{x},Residential,Block,HomeLevel1(1.8|AC),9999999,50,Any" + "\n")
+        csv_writer.write(f"{x},Residential,Block,HomeLevel2(7.2|AC),9999999,200,Any" + "\n")
 
-        csv_writer.write(f"{x},Workplace,Block,NoCharger,9999999,0," + "\n")
-        csv_writer.write(f"{x},Workplace,Block,EVIWorkLevel2(7.2|AC),9999999,200," + "\n")
+        csv_writer.write(f"{x},Workplace,Block,NoCharger,9999999,0,Any" + "\n")
+        csv_writer.write(f"{x},Workplace,Block,EVIWorkLevel2(7.2|AC),9999999,200,Any" + "\n")
 
-        csv_writer.write(f"{x},Public,Block,NoCharger,9999999,0," + "\n")
-        csv_writer.write(f"{x},Public,Block,EVIPublicLevel2(7.2|AC)(7.2|AC),9999999,200," + "\n")
-        csv_writer.write(f"{x},Public,Block,EVIPublicDCFast(150|DC),9999999,7500," + "\n")
+        csv_writer.write(f"{x},Public,Block,NoCharger,9999999,0,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,Custom(7.2|AC)(7.2|AC),9999999,200,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,Custom(150|DC),9999999,7500,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,Custom(250|DC),9999999,15000,Any" + "\n")
 
 
-with open('gemini_depot_unlimited_parking_power_150kw.csv', mode='w') as csv_writer:
+with open('gemini_depot_unlimited_parking_power.csv', mode='w') as csv_writer:
     csv_writer.write(headerfile+"\n")
 
     for x in range(1, 1455):
-        csv_writer.write(f"{x},Public,FlatFee,FCSFast(150.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
+        csv_writer.write(f"{x},Public,FlatFee,Custom(150.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
+        csv_writer.write(f"{x},Public,FlatFee,Custom(250.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
 
 
