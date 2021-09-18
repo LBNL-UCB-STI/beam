9971c8e06134f4382c7ddb29459947de5e4697c2
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2021-09-17 18:34:40 -0700

    calibration batch 1

--- src/main/R/gemini/gemini-processing.R
+++ src/main/R/gemini/gemini-processing.R
@@ -284,3 +284,37 @@ logs[,.(estimatedLoad=sum(estimatedLoad)),by=.(currentTime)] %>%
   geom_bar(stat="identity")
 ggplot(logs) + geom_histogram(aes(estimatedLoad))
 
+
+####
+
+
+parking <- readCsv(pp(workDir, "/gemini_taz_parking_plugs_power_150kw.csv"))
+
+parking[,.(feeInCents=mean(feeInCents)),by=.(parkingType,chargingPointType)]
+
+
+
+#####
+
+eventsFile <- "/2021Aug22-Oakland/BATCH2-Calibration/events-raw/0.events.csv.gz"
+events <- readCsv(pp(workDir, eventsFile))
+
+rse <- events[type=='RefuelSessionEvent']
+
+rse[,.N,by=.(parkingType,chargingPointType)]
+
+rseSum <- rse[,.(fuel=sum(fuel)),by=.(parkingType,chargingPointType)]
+rseSum[,fuelShare:=fuel/sum(fuel)]
+dcfc <- rseSum[chargingPointType=="custom(150.0|DC)"]$fuelShare + rseSum[chargingPointType=="custom(250.0|DC)"]$fuelShare
+publicL2 <- rseSum[chargingPointType=="custom(7.2|AC)"]$fuelShare
+workL2 <- rseSum[chargingPointType=="worklevel2(7.2|AC)"]$fuelShare
+homeL1 <- rseSum[chargingPointType=="homelevel1(1.8|AC)"]$fuelShare
+homeL2 <- rseSum[chargingPointType=="homelevel2(7.2|AC)"]$fuelShare
+print("************************")
+print(pp("DCFC: ",dcfc))
+print(pp("PublicL2: ",publicL2))
+print(pp("WorkL2: ",workL2))
+print(pp("HomeL1: ",homeL1))
+print(pp("HomeL2: ",homeL2))
+
+
--- src/main/python/gemini/unlimited_charging_infrastructure.py
+++ src/main/python/gemini/unlimited_charging_infrastructure.py
@@ -14,16 +14,16 @@ with open('gemini_taz_unlimited_parking_plugs_power.csv', mode='w') as csv_write
         csv_writer.write(f"{x},Workplace,Block,WorkLevel2(7.2|AC),9999999,200,Any" + "\n")
 
         csv_writer.write(f"{x},Public,Block,NoCharger,9999999,0,Any" + "\n")
-        csv_writer.write(f"{x},Public,Block,Custom(7.2|AC),9999999,200,Any" + "\n")
-        csv_writer.write(f"{x},Public,Block,Custom(150|DC),9999999,7500,Any" + "\n")
-        csv_writer.write(f"{x},Public,Block,Custom(250|DC),9999999,15000,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,PublicLevel2(7.2|AC),9999999,200,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,PublicFC(150|DC),9999999,7500,Any" + "\n")
+        csv_writer.write(f"{x},Public,Block,PublicXFC(250|DC),9999999,15000,Any" + "\n")
 
 
 with open('gemini_depot_unlimited_parking_power.csv', mode='w') as csv_writer:
     csv_writer.write(headerfile+"\n")
 
     for x in range(1, 1455):
-        csv_writer.write(f"{x},Public,FlatFee,Custom(150.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
-        csv_writer.write(f"{x},Public,FlatFee,Custom(250.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
+        csv_writer.write(f"{x},Public,FlatFee,DepotFC(150.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
+        csv_writer.write(f"{x},Public,FlatFee,DepotXFC(250.0|DC),9999999,0,ridehail(GlobalRHM)" + "\n")
 
 
