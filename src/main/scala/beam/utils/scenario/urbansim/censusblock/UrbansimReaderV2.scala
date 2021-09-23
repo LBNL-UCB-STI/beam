4714a2983e901108891ab04557c05615cb91969d
Dmitry Openkov <dimaopen@gmail.com>
2021-09-23 18:53:20 +0300

    Removed trips from UrbansimReaderV2

--- src/main/scala/beam/utils/scenario/urbansim/censusblock/UrbansimReaderV2.scala
+++ src/main/scala/beam/utils/scenario/urbansim/censusblock/UrbansimReaderV2.scala
@@ -1,7 +1,7 @@
 package beam.utils.scenario.urbansim.censusblock
 
 import beam.sim.common.GeoUtils
-import beam.utils.scenario.urbansim.censusblock.entities.{InputHousehold, TripElement}
+import beam.utils.scenario.urbansim.censusblock.entities.{InputHousehold}
 import beam.utils.scenario.urbansim.censusblock.merger.{HouseholdMerger, PersonMerger, PlanMerger}
 import beam.utils.scenario.urbansim.censusblock.reader._
 import beam.utils.scenario.{HouseholdInfo, PersonInfo, PlanElement, ScenarioSource}
@@ -50,13 +50,7 @@ class UrbansimReaderV2(
   }
 
   override def getPlans: Iterable[PlanElement] = {
-    logger.info("Reading of the trips...")
-    val tripReader = new TripReader(inputTripsPath)
-    val modes = tripReader
-      .iterator()
-      .map(tripElement => (tripElement.personId, tripElement.depart) -> tripElement.trip_mode)
-      .toMap
-    val merger = new PlanMerger(modes, modeMap)
+    val merger = new PlanMerger(modeMap)
 
     logger.info("Merging modes into plan...")
 
@@ -77,7 +71,6 @@ class UrbansimReaderV2(
     } finally {
       logger.info("Modes merged successfully into plan.")
       planReader.close()
-      tripReader.close()
     }
   }
 
