32658fc68019c1da6004fa3ec8028a7c15b49a38
rajnikant <rk_sharmait@yahoo.com>
2021-09-27 17:20:25 +0530

    add script usage comments and fix detecting stacktrace in last line

--- src/main/python/general_analysis/simulation_health_analysis.py
+++ src/main/python/general_analysis/simulation_health_analysis.py
@@ -2,7 +2,9 @@ import os
 import sys
 from glob import glob
 
-
+# detectors is a dictionary that consist key as health metric and
+# value as lambda function that can detect health metric from line.
+# if we need to add more detector then we have to add that detector in this dictionary
 detectors = {
                 "deadLetter": lambda line: "DeadLetter" in line,
                 "actor died" : lambda line: "terminated unexpectedly" in line,
@@ -10,30 +12,28 @@ detectors = {
                 "error": lambda line: " ERROR " in line, 
                 "stacktrace": lambda line: line.startswith("\tat ")   
             }
-beam_home = sys.argv[1]
-print(beam_home)
-os.chdir(beam_home)
-os.system("./gradlew :run -PappArgs=\"['--config', 'test/input/beamville/beam.conf']\"")
-
-log_file_location = glob(beam_home+"/output/beamville/*/beamLog.out")
-
+beam_home = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd()))))
+log_file_location = glob(beam_home+"/output/*/*/beamLog.out")
+log_file_location.sort(key=lambda x: os.path.getmtime(x), reverse = True)
 with open(log_file_location[0]) as file:
     file = file.readlines()
 
 matric_log = {}
 stacktrace_count = 0
 for line in file:
-    
+    # treating stacktrace detector specially because 1 stacktrace consist multiple lines of stacktrace
     if detectors["stacktrace"](line):
+        if stacktrace_count == 0:
+            matric = matric_log.get("stacktrace", [])
+            matric.append(stacktrace_count)
+            matric_log["stacktrace"] = matric
         stacktrace_count = stacktrace_count + 1
         continue
     elif stacktrace_count > 0:
-        matric = matric_log.get("stacktrace", [])
-        matric.append(stacktrace_count)
-        matric_log["stacktrace"] = matric
         stacktrace_count = 0
         continue
 
+    # iterating each detector and evaluating if line consist that detector
     for key,value in detectors.items():
         if value(line):
             matric = matric_log.get(key, [])
