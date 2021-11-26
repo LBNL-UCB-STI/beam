259f1f309665230ad78abd7dd334fe1d59a5de44
rajnikant <rk_sharmait@yahoo.com>
2021-11-27 02:47:55 +0530

    remove max ram from lambda   function

--- aws/src/main/python/beam_lambda/lambda_function.py
+++ aws/src/main/python/beam_lambda/lambda_function.py
@@ -725,7 +725,7 @@ def deploy_handler(event, context):
     if not is_spot and instance_type not in instance_types:
         return "Unable to start run, {instance_type} instance type not supported.".format(instance_type=instance_type)
 
-    max_ram = event.get('max_ram', calculate_max_ram(instance_type))
+    max_ram = event.get('forced_max_ram', calculate_max_ram(instance_type))
     if shutdown_behaviour not in shutdown_behaviours:
         return "Unable to start run, {shutdown_behaviour} shutdown behaviour not supported.".format(shutdown_behaviour=shutdown_behaviour)
 
