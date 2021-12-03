2b1e2ba49d948ba7cfba5fd15d64837f8188a55f
Xuan-1998 <j503440616@berkeley.edu>
2021-12-02 21:08:21 -0800

    back-format

--- src/main/scala/beam/utils/csv/readers/BeamCsvScenarioReader.scala
+++ src/main/scala/beam/utils/csv/readers/BeamCsvScenarioReader.scala
@@ -68,7 +68,6 @@ object BeamCsvScenarioReader extends BeamScenarioReader with ExponentialLazyLogg
   }
 
   private[readers] def toPlanInfo(rec: java.util.Map[String, String]): PlanElement = {
-    val tripId = getIfNotNull(rec, "tripId")
     val personId = getIfNotNull(rec, "personId")
     val planIndex = getIfNotNull(rec, "planIndex").toInt
     val planElementType = getIfNotNull(rec, "planElementType")
@@ -77,7 +76,7 @@ object BeamCsvScenarioReader extends BeamScenarioReader with ExponentialLazyLogg
     val linkIds =
       Option(rec.get("legRouteLinks")).map(_.split(ArrayItemSeparator).map(_.trim)).getOrElse(Array.empty[String])
     PlanElement(
-      tripId = Option(rec.get("tripId")),
+      tripId = Option(rec.get("trip_id")),
       personId = PersonId(personId),
       planIndex = planIndex,
       planScore = getIfNotNull(rec, "planScore", "0").toDouble,
