14ba8aba4e4346faa718f8e37c7b209f9991d556
Nikolay Ilyin <irishwithaxe@gmail.com>
2021-12-22 20:33:01 +0300

    test for modes in generated plans

--- src/test/scala/beam/sim/WritePlansAndStopSimulationTest.scala
+++ src/test/scala/beam/sim/WritePlansAndStopSimulationTest.scala
@@ -5,21 +5,33 @@ import beam.utils.TestConfigUtils.testConfig
 import beam.utils.csv.GenericCsvReader
 import com.typesafe.config.{Config, ConfigFactory}
 import org.matsim.core.scenario.MutableScenario
+import org.scalatest.AppendedClues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.nio.file.Paths
 
-class WritePlansAndStopSimulationTest extends AnyFlatSpec with Matchers with BeamHelper {
+class WritePlansAndStopSimulationTest extends AnyFlatSpec with Matchers with BeamHelper with AppendedClues {
 
   def readActivitiesTypesFromPlan(pathToPlans: String): Set[String] =
     GenericCsvReader
       .readAsSeq[String](pathToPlans) { row => row.get("activityType") }
       .toSet
 
-//  def readModesFromSelectedPlans(pathToPlans: String): Set[String] = {
-//    Set.empty[String]
-//  }
+  case class LegInfo(personId: String, planIndex: String, legMode: String)
+
+  def readPersonPlanIndexModeFromSelectedPlans(pathToPlans: String): Seq[LegInfo] = {
+    GenericCsvReader
+      .readAsSeq[(String, LegInfo)](pathToPlans) { row =>
+        val person = row.get("personId")
+        val planIndex = row.get("planIndex")
+        val mode = row.get("legMode") // will be either `null` or string value
+        val planElementType = row.get("planElementType") // either "leg" or "activity"
+        (planElementType, LegInfo(person, planIndex, mode))
+      }
+      .filter { case (planElementType, _) => planElementType == "leg" }
+      .map { case (_, legInfo) => legInfo }
+  }
 
   def runSimulationAndCatchException(config: Config): String = {
     val (
@@ -86,9 +98,15 @@ class WritePlansAndStopSimulationTest extends AnyFlatSpec with Matchers with Bea
 
     val plansPath = Paths.get(outputDirectory, "generatedPlans.csv.gz").toFile
     val activitiesTypes = readActivitiesTypesFromPlan(plansPath.getPath)
+    val personPlanIndexMode = readPersonPlanIndexModeFromSelectedPlans(plansPath.getPath)
+
     activitiesTypes.contains("Work") shouldBe true
     activitiesTypes.contains("Home") shouldBe true
     activitiesTypes.size should be > 2
+
+    personPlanIndexMode.foreach { legInfo: LegInfo =>
+      legInfo.legMode shouldBe null withClue s"in plans for person ${legInfo.personId} with planIndex ${legInfo.planIndex}"
+    }
   }
 
   it should "Stop simulation, trow runtime exception and write plans with different activities with modes to the output folder." in {
@@ -109,8 +127,18 @@ class WritePlansAndStopSimulationTest extends AnyFlatSpec with Matchers with Bea
     val plansPath = Paths.get(outputDirectory, "generatedPlans.csv.gz").toFile
 
     val activitiesTypes = readActivitiesTypesFromPlan(plansPath.getPath)
+    val personPlanIndexMode = readPersonPlanIndexModeFromSelectedPlans(plansPath.getPath)
+
     activitiesTypes.contains("Work") shouldBe true
     activitiesTypes.contains("Home") shouldBe true
     activitiesTypes.size should be > 2
+
+    personPlanIndexMode.foreach {
+      case LegInfo(person, "0", legMode) =>
+        legMode should be(null) withClue s"in plans for person $person with planIndex 0"
+      case LegInfo(person, "1", legMode) =>
+        legMode shouldNot be(null) withClue s"in plans for person $person with planIndex 1"
+      case legInfo => throw new NotImplementedError(s"Unexpected legInfo: $legInfo")
+    }
   }
 }
