38f4d6b65d9fdad18fa497a8cebc70f1f9c2f374
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-03-02 22:28:24 +0300

    debugging of age of actors

--- src/main/scala/beam/utils/scenario/UrbanSimScenarioLoader.scala
+++ src/main/scala/beam/utils/scenario/UrbanSimScenarioLoader.scala
@@ -117,20 +117,20 @@ class UrbanSimScenarioLoader(
 
     val householdIds = households.map(_.householdId.id).toSet
 
-    logger.info(s"There are ${persons.size} persons in input plans.")
-    logger.info(s"There are ${persons.map(_.age).toSet.mkString(", ")} possible ages of persons.")
     val minimumAge = beamScenario.beamConfig.beam.agentsim.minimumAgentAgeToIncludeInSimulation
     val personsFilteredByAge = persons.filter(person => person.age >= minimumAge)
+    logger.info(s"There are ${persons.size} persons in input plans.")
+    logger.info(s"There are ${persons.map(_.age).toSet.toSeq.sorted.mkString(", ")} existing ages of persons.")
     logger.info(s"There are ${personsFilteredByAge.size} persons with minimum age of $minimumAge.")
 
     val personsWithPlans = getPersonsWithPlan(personsFilteredByAge, plans)
       .filter(p => householdIds.contains(p.householdId.id))
-    logger.info(s"There are ${personsWithPlans.size} persons with plans")
+    logger.info(s"There are ${personsWithPlans.size} persons with plans.")
 
     val householdIdToPersons: Map[HouseholdId, Iterable[PersonInfo]] = personsWithPlans.groupBy(_.householdId)
 
     val householdsWithMembers = households.filter(household => householdIdToPersons.contains(household.householdId))
-    logger.info(s"There are ${householdsWithMembers.size} non-empty households")
+    logger.info(s"There are ${householdsWithMembers.size} non-empty households.")
 
     logger.info("Applying households...")
     applyHousehold(householdsWithMembers, householdIdToPersons, plans)
