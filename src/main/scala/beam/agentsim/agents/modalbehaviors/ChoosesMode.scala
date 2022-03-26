08e532be977a732da64b391979fce3cba557a396
Zach Needell <zneedell@gmail.com>
2022-03-25 20:35:05 -0700

    fix tripId

--- src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
@@ -1505,12 +1505,11 @@ trait ChoosesMode {
         )
     }
 
-    val tripId = Option(
-      _experiencedBeamPlan
-        .trips(data.personData.currentActivityIndex)
-        .getAttributes
-        .getAttribute("trip_id")
-    ).getOrElse("").toString
+    val tripId = _experiencedBeamPlan.trips
+      .lift(data.personData.currentActivityIndex + 1) match {
+      case Some(trip) => trip.leg.map(_.getAttributes.getAttribute("trip_id").toString).getOrElse("")
+      case None       => ""
+    }
 
     val modeChoiceEvent = new ModeChoiceEvent(
       tick,
