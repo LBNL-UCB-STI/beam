f3af73e34194b68b4a3462a27e97979a684a3e43
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-04-03 12:16:36 +0300

    testing how to work with git

---
+++ jupyter/DEBUG/MATH.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1 + 1
+
+
+# In[2]:
+
+
+2 + 2
+
+
+# In[2]:
+
+
+get_ipython().system('jupyter nbconvert --to python DEBUG.ipynb')
+
+
+# In[ ]:
+
+
+
+
