fd74c6a0842217fdf9c4eb1cb82b85c027f62190
Xuan-1998 <j503440616@berkeley.edu>
2022-04-04 21:07:46 -0700

    cleaning-up

--- test/input/sfbay/experiments/ev-fleet/ev-fleet-qos-S98/runs/batchRunExperiment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-experiment_dir="/home/ubuntu/git/beam/production/application-sfbay/experiments/ev-fleet-qos-S98/runs"
-# search through runs directory and run individual runBeam.sh
-for dir in ${experiment_dir//\\//}/*/; do
-    echo $dir
-    $dir/runBeam.sh $1 > $dir/console.log 2>&1
-    #grep ModeChoice $dir/output/ITERS/it.0/0.events.csv | grep -Eo "ModeChoice,,\w*" | sort | uniq -c
-done
