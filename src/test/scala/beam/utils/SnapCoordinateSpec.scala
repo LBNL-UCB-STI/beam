7bc9881387fbc7df8d5edc8d1b8b138346c44864
Rutvik Patel <heyrutvik@gmail.com>
2022-04-07 11:37:52 +0530

    adds more tests

--- src/test/scala/beam/utils/SnapCoordinateSpec.scala
+++ src/test/scala/beam/utils/SnapCoordinateSpec.scala
@@ -16,6 +16,21 @@ import scala.jdk.CollectionConverters.collectionAsScalaIterableConverter
 
 class SnapCoordinateSpec extends AnyWordSpec with Matchers with BeamHelper {
 
+  /**
+    * see the notes,
+    *
+    * Scenario:
+    * case1: test/test-resources/beam/input/snap-location/scenario/case1/note
+    *   three invalid plans, update population/households accordingly
+    * case2: test/test-resources/beam/input/snap-location/scenario/case2/note
+    *   one invalid household and three invalid plans, update population/households accordingly
+    *
+    * Freight:
+    * ???
+    */
+
+  val pwd: String = System.getenv("PWD")
+
   "scenario plan" should {
     "contain all valid locations" in {
       lazy val config: TypesafeConfig = ConfigFactory
@@ -44,7 +59,7 @@ class SnapCoordinateSpec extends AnyWordSpec with Matchers with BeamHelper {
       )
 
       val dummyCoord = new Coord()
-      val results: Seq[SnapCoordinateUtils.Result] = scenario.getPopulation.getPersons
+      val population: Seq[SnapCoordinateUtils.Result] = scenario.getPopulation.getPersons
         .values()
         .asScala
         .flatMap { person =>
@@ -57,10 +72,151 @@ class SnapCoordinateSpec extends AnyWordSpec with Matchers with BeamHelper {
         }
         .toList
 
-      results.forall {
+      val households: Seq[SnapCoordinateUtils.Result] = scenario.getHouseholds.getHouseholds
+        .values()
+        .asScala
+        .map { household =>
+          val locationX = scenario.getHouseholds.getHouseholdAttributes
+            .getAttribute(household.getId.toString, "homecoordx")
+            .asInstanceOf[Double]
+          val locationY = scenario.getHouseholds.getHouseholdAttributes
+            .getAttribute(household.getId.toString, "homecoordy")
+            .asInstanceOf[Double]
+          val coord = new Coord(locationX, locationY)
+          snapLocationHelper.computeResult(coord)
+        }
+        .toList
+
+      (population ++ households).forall {
         case _: Result.Succeed => true
         case _                 => false
       } shouldBe true
+    }
+
+    "remove/update invalid persons and households [case1 xml input]" in {
+      lazy val config: TypesafeConfig = ConfigFactory
+        .parseString(s"""
+                        |beam.agentsim.agents.plans {
+                        |  inputPlansFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/xml/population.xml"
+                        |  inputPersonAttributesFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/xml/populationAttributes.xml"
+                        |}
+                        |beam.agentsim.agents.households {
+                        |  inputFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/xml/households.xml"
+                        |  inputHouseholdAttributesFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/xml/householdAttributes.xml"
+                        |}
+                        |beam.routing.r5.linkRadiusMeters = 350
+                        |""".stripMargin)
+        .withFallback(testConfig("test/input/beamville/beam.conf"))
+        .resolve()
+
+      val configBuilder = new MatSimBeamConfigBuilder(config)
+      val matsimConfig: Config = configBuilder.buildMatSimConf()
+      matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+      val beamConfig: BeamConfig = BeamConfig(config)
+
+      FileUtils.setConfigOutputFile(beamConfig, matsimConfig)
+
+      val (scenario, _, _) = buildBeamServicesAndScenario(
+        beamConfig,
+        matsimConfig
+      )
+
+      val households = scenario.getHouseholds.getHouseholds.values().asScala.toList
+
+      scenario.getPopulation.getPersons.size() shouldBe 2
+      households.foreach { household =>
+        household.getMemberIds.size() shouldBe 1
+      }
+    }
+
+    "remove/update invalid persons and households [case2 xml input]" in {
+      lazy val config: TypesafeConfig = ConfigFactory
+        .parseString(s"""
+                        |beam.agentsim.agents.plans {
+                        |  inputPlansFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/xml/population.xml"
+                        |  inputPersonAttributesFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/xml/populationAttributes.xml"
+                        |}
+                        |beam.agentsim.agents.households {
+                        |  inputFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/xml/households.xml"
+                        |  inputHouseholdAttributesFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/xml/householdAttributes.xml"
+                        |}
+                        |beam.routing.r5.linkRadiusMeters = 350
+                        |""".stripMargin)
+        .withFallback(testConfig("test/input/beamville/beam.conf"))
+        .resolve()
+
+      val configBuilder = new MatSimBeamConfigBuilder(config)
+      val matsimConfig: Config = configBuilder.buildMatSimConf()
+      matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+      val beamConfig: BeamConfig = BeamConfig(config)
+
+      FileUtils.setConfigOutputFile(beamConfig, matsimConfig)
+
+      val (scenario, _, _) = buildBeamServicesAndScenario(
+        beamConfig,
+        matsimConfig
+      )
+
+      scenario.getPopulation.getPersons.size() shouldBe 1
+      scenario.getHouseholds.getHouseholds.size() shouldBe 1
+    }
+
+    "remove invalid persons and households [case1 csv input]" in {
+      lazy val config: TypesafeConfig = ConfigFactory
+        .parseString(s"""
+                        |beam.agentsim.agents.plans.inputPlansFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/csv/plans.csv"
+                        |beam.agentsim.agents.households.inputFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case1/csv/households.csv"
+                        |beam.routing.r5.linkRadiusMeters = 350
+                        |""".stripMargin)
+        .withFallback(testConfig("test/input/beamville/beam-csv.conf"))
+        .resolve()
+
+      val configBuilder = new MatSimBeamConfigBuilder(config)
+      val matsimConfig: Config = configBuilder.buildMatSimConf()
+      matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+      val beamConfig: BeamConfig = BeamConfig(config)
+
+      FileUtils.setConfigOutputFile(beamConfig, matsimConfig)
+
+      val (scenario, _, _) = buildBeamServicesAndScenario(
+        beamConfig,
+        matsimConfig
+      )
+
+      val households = scenario.getHouseholds.getHouseholds.values().asScala.toList
+
+      scenario.getPopulation.getPersons.size() shouldBe 2
+      households.foreach { household =>
+        household.getMemberIds.size() shouldBe 1
+      }
+    }
+
+    "remove invalid persons and households [case2 csv input]" in {
+      lazy val config: TypesafeConfig = ConfigFactory
+        .parseString(s"""
+                        |beam.agentsim.agents.plans.inputPlansFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/csv/plans.csv"
+                        |beam.agentsim.agents.households.inputFilePath = "$pwd/test/test-resources/beam/input/snap-location/scenario/case2/csv/households.csv"
+                        |beam.routing.r5.linkRadiusMeters = 350
+                        |""".stripMargin)
+        .withFallback(testConfig("test/input/beamville/beam-csv.conf"))
+        .resolve()
+
+      val configBuilder = new MatSimBeamConfigBuilder(config)
+      val matsimConfig: Config = configBuilder.buildMatSimConf()
+      matsimConfig.planCalcScore().setMemorizingExperiencedPlans(true)
+      val beamConfig: BeamConfig = BeamConfig(config)
+
+      FileUtils.setConfigOutputFile(beamConfig, matsimConfig)
+
+      val (scenario, _, _) = buildBeamServicesAndScenario(
+        beamConfig,
+        matsimConfig
+      )
+
+      val households = scenario.getHouseholds.getHouseholds.values().asScala.toList
+
+      scenario.getPopulation.getPersons.size() shouldBe 1
+      scenario.getHouseholds.getHouseholds.size() shouldBe 1
     }
   }
 
