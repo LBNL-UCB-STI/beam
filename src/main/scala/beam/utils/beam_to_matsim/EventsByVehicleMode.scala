8c1efe3d331b3eed3cdc5cdb766eb8da9af74dbb
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-04-20 15:28:19 +0300

    using args

--- src/main/scala/beam/utils/beam_to_matsim/EventsByVehicleMode.scala
+++ src/main/scala/beam/utils/beam_to_matsim/EventsByVehicleMode.scala
@@ -41,7 +41,7 @@ object EventsByVehicleMode extends App {
     "1"
   )
 
-  val inputArgs = inputArgs2
+  val inputArgs = args
 
   if (inputArgs.length == 4) {
     val eventsFile = inputArgs.head
