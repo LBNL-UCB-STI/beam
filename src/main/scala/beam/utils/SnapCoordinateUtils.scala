4c6700195abe7d52c0a7134bbc516f05da0267cb
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2022-04-22 22:07:08 -0700

    fix

--- src/main/scala/beam/utils/SnapCoordinateUtils.scala
+++ src/main/scala/beam/utils/SnapCoordinateUtils.scala
@@ -54,9 +54,6 @@ object SnapCoordinateUtils extends LazyLogging {
 
     def computeResult(planCoord: Coord, isWgs: Boolean = false): SnapCoordinateResult = {
       val coord = if (isWgs) planCoord else geo.utm2Wgs(planCoord)
-      logger.info(
-        s"SnapCoordinateUtils. streetLayer.envelope ${streetLayer.envelope}. coordinate planCoord $planCoord. isWgs $isWgs. coord $coord"
-      )
       if (streetLayer.envelope.contains(coord.getX, coord.getY)) {
         val snapCoordOpt = store.getOrElseUpdate(
           coord,
