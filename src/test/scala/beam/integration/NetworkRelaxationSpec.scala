61d9be5237a070afdc3800f387e28bf62ce7825c
Alexander Vykhodtsev <avflance@gmail.com>
2022-05-17 20:51:11 +0600

    Finishing network relaxation spec

--- src/test/scala/beam/integration/NetworkRelaxationSpec.scala
+++ src/test/scala/beam/integration/NetworkRelaxationSpec.scala
@@ -1,43 +1,29 @@
 package beam.integration
 
 import akka.actor.ActorSystem
-import beam.agentsim.infrastructure.taz.CsvTaz
-import beam.sim.{BeamHelper, BeamServices}
-import beam.sim.common.GeoUtilsImpl
 import beam.sim.config.{BeamConfig, MatSimBeamConfigBuilder}
-import beam.utils.{ConfigConsistencyComparator, FileUtils, InputConsistencyCheck, LoggingUtil}
-import org.scalatest.wordspec.AnyWordSpecLike
-import beam.utils.TestConfigUtils.testConfig
-import beam.utils.csv.CsvWriter
-import beam.utils.matsim_conversion.{ConversionConfig, MatsimConversionTool}
-import beam.utils.scenario.{HouseholdId, HouseholdInfo, VehicleInfo}
-import beam.utils.scenario.generic.writers.CsvPersonInfoWriter.{headers, logger}
-import beam.utils.scenario.urbansim.censusblock.entities.{
-  Activity,
-  Female,
-  InputPersonInfo,
-  InputPlanElement,
-  Leg,
-  Male
-}
+import beam.sim.{BeamHelper, BeamServices}
+import beam.utils.csv.{CsvWriter, GenericCsvReader}
+import beam.utils.scenario.VehicleInfo
+import beam.utils.scenario.urbansim.censusblock.entities._
+import beam.utils.{FileUtils, LoggingUtil}
 import ch.qos.logback.classic.util.ContextInitializer
 import com.google.inject.Injector
 import com.typesafe.config.ConfigFactory
-import org.matsim.api.core.v01.Coord
-import org.matsim.core.network.NetworkUtils
 import org.matsim.core.scenario.MutableScenario
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.matchers.should.Matchers._
 
-import java.io.File
-import java.nio.file.Paths
+import java.io.{Closeable, File}
 import scala.util.Random
 
 class NetworkRelaxationScenarioGenerator {
-  val scenarioDir = new File("test/input/relaxation-network-scenario/urbansim_v2")
+  val scenarioDir = new File("test/input/network-relaxation-scenario/urbansim_v2")
 
   def generateHouseholds(count: Int = 2000) = {
     println(s"Generating $count households")
     val csvWriter = new CsvWriter(
-      scenarioDir + "/households.csv",
+      scenarioDir + "/households.csv.gz",
       Seq("household_id", "income", "cars", "block_id")
     )
     try {
@@ -56,7 +42,7 @@ class NetworkRelaxationScenarioGenerator {
       VehicleInfo(i.toString, "Car", None, i.toString)
     }
     val csvWriter = new CsvWriter(
-      "test/input/relaxation-network-scenario/vehicles.csv",
+      scenarioDir.getPath + "/vehicles.csv.gz",
       Seq("vehicleId", "vehicleTypeId", "stateOfCharge", "householdId")
     )
     try {
@@ -84,7 +70,8 @@ class NetworkRelaxationScenarioGenerator {
         None
       )
     }
-    val csvWriter = new CsvWriter(scenarioDir.getPath + "/persons.csv", Seq("person_id", "household_id", "age", "sex"))
+    val csvWriter =
+      new CsvWriter(scenarioDir.getPath + "/persons.csv.gz", Seq("person_id", "household_id", "age", "sex"))
     try {
       persons.foreach { person =>
         csvWriter.write(
@@ -118,8 +105,6 @@ class NetworkRelaxationScenarioGenerator {
     val plans = for (i <- 1 to count) yield {
       // Home = -0.03763798759, 0.00975476975
       // Work = 0.04874384139, 0.01013286711
-
-      // TODO wgs2utm =
       Seq(
         InputPlanElement(
           tripId = Some(i.toString),
@@ -130,7 +115,7 @@ class NetworkRelaxationScenarioGenerator {
           ActivityType = Some("Home"),
           x = Some(161827.4228835071),
           y = Some(1079.7224574150498),
-          departureTime = Some(8)
+          departureTime = Some(8 + Random.nextDouble() * 4)
         ),
         InputPlanElement(
           tripId = Some(i.toString),
@@ -156,9 +141,7 @@ class NetworkRelaxationScenarioGenerator {
         )
       )
     }
-    println(plans)
-
-    val csvWriter = new CsvWriter(scenarioDir.getPath + "/plans.csv", headers)
+    val csvWriter = new CsvWriter(scenarioDir.getPath + "/plans.csv.gz", headers)
     try {
       plans.flatten.foreach { plan =>
         csvWriter.write(
@@ -179,10 +162,10 @@ class NetworkRelaxationScenarioGenerator {
   }
 
   def generate() = {
-//    generateHouseholds()
+    generateHouseholds()
     generateVehicles()
-//    generatePersons()
-//    generatePlans()
+    generatePersons()
+    generatePlans()
   }
 }
 
@@ -190,37 +173,26 @@ object NetworkRelaxationScenarioGenerator extends App {
   new NetworkRelaxationScenarioGenerator().generate()
 }
 
+case class LinkStatsCsvRow(link: Int, volume: Double)
+
+object LinkStatsCsvRow {
+
+  def toCsvRow(rec: java.util.Map[String, String]): LinkStatsCsvRow =
+    LinkStatsCsvRow(rec.get("link").toInt, rec.get("volume").toDouble)
+}
+
 class NetworkRelaxationSpec extends AnyWordSpecLike with BeamHelper {
+  val lastIteration = 15
   System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "logback-test.xml")
 
   "Network relaxation" should {
     "pass" in {
       val config = ConfigFactory
-        .parseFile(new File("test/input/relaxation-network-scenario/beam.conf"))
+        .parseFile(new File("test/input/network-relaxation-scenario/beam.conf"))
         .resolve()
       val configBuilder = new MatSimBeamConfigBuilder(config)
       val matsimConfig = configBuilder.buildMatSimConf()
       val beamConfig = BeamConfig(config)
-
-      val geoUtils = new GeoUtilsImpl(beamConfig)
-
-      // Home = -0.03763798759, 0.00975476975
-      // Work = 0.04874384139, 0.01013286711
-
-      println(geoUtils.wgs2Utm(new Coord(-0.03763798759, 0.00975476975)))
-      println(geoUtils.wgs2Utm(new Coord(0.04874384139, 0.01013286711)))
-
-      val networkCoordinator = buildNetworkCoordinator(beamConfig)
-      val boundingBox = NetworkUtils.getBoundingBox(networkCoordinator.network.getNodes.values())
-      val minX = boundingBox(0)
-      val maxX = boundingBox(2)
-      val minY = boundingBox(1)
-      val maxY = boundingBox(3)
-
-      val midX = (maxX + minX) / 2
-      val midY = (maxY + minY) / 2
-
-//      println(new Coord(midX, midY))
 
       val (scenarioBuilt, beamScenario, _) = buildBeamServicesAndScenario(beamConfig, matsimConfig)
       val scenario: MutableScenario = scenarioBuilt
@@ -230,13 +202,6 @@ class NetworkRelaxationSpec extends AnyWordSpecLike with BeamHelper {
       val beamServices: BeamServices = buildBeamServices(injector)
 
       LoggingUtil.initLogger(outputDir, beamConfig.beam.logger.keepConsoleAppenderOn)
-      println(s"Beam output directory is: $outputDir")
-      logger.info(ConfigConsistencyComparator.getMessage.getOrElse(""))
-
-      val errors = InputConsistencyCheck.checkConsistency(beamConfig)
-      if (errors.nonEmpty) {
-        logger.error("Input consistency check failed:\n" + errors.mkString("\n"))
-      }
 
       runBeam(
         beamServices,
@@ -245,7 +210,28 @@ class NetworkRelaxationSpec extends AnyWordSpecLike with BeamHelper {
         outputDir,
         true
       )
-      fail
+
+      val linkStats = new File(outputDir, s"ITERS/it.$lastIteration/$lastIteration.linkstats.csv.gz")
+
+      val (iter: Iterator[LinkStatsCsvRow], toClose: Closeable) =
+        GenericCsvReader.readAs[LinkStatsCsvRow](linkStats.toString, LinkStatsCsvRow.toCsvRow, _ => true)
+      val result =
+        try {
+          iter.toVector
+        } finally {
+          toClose.close()
+        }
+
+      val routes = List(
+        Set(0, 26, 24),
+        Set(4, 28, 22),
+        Set(2, 30, 20),
+        Set(6, 32, 8)
+      )
+
+      val sums = routes.map(route => result.filter(row => route.contains(row.link)).map(_.volume).sum)
+
+      sums.foreach(_ should be > 500.0)
     }
   }
 }
