661961e04e16cb60e6732ca3c1f4042a17540a6a
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2022-06-07 01:48:53 -0700

    remove tests

--- src/main/scala/beam/agentsim/infrastructure/ChargingNetworkManagerHelper.scala
+++ src/main/scala/beam/agentsim/infrastructure/ChargingNetworkManagerHelper.scala
@@ -76,13 +76,7 @@ trait ChargingNetworkManagerHelper extends {
   ): Option[ScheduleTrigger] = {
     assume(endTime - startTime >= 0, s"timeInterval should not be negative! startTime $startTime endTime $endTime")
     // Calculate the energy to charge each vehicle connected to the a charging station
-    val endOfMaxSessionTime = chargingEndTimeInSeconds.getOrElse(
-      chargingVehicle.personId, {
-        if (!isVirtualPerson(chargingVehicle.personId) && !chargingVehicle.personId.toString.startsWith("rideHail"))
-          log.error(s"Something is broken. The person ${chargingVehicle.personId} is not tracked by CHM")
-        endTime
-      }
-    )
+    val endOfMaxSessionTime = chargingEndTimeInSeconds.getOrElse(chargingVehicle.personId, endTime)
     val endOfShiftTime = chargingVehicle.chargingShouldEndAt.map(_ - parallelismWindow).getOrElse(endTime)
     val updatedEndTime = Math.min(endOfMaxSessionTime, endOfShiftTime)
     val duration = Math.max(0, updatedEndTime - startTime)
