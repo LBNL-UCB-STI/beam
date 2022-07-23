4f8e925d33556660ddba9eae9fd50c7ad078976d
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-07-23 17:45:00 +0300

    a fix for generated urbansim plans

--- src/main/scala/scripts/GeneratedPlansToUrbansimPlans.scala
+++ src/main/scala/scripts/GeneratedPlansToUrbansimPlans.scala
@@ -15,33 +15,46 @@ object GeneratedPlansToUrbansimPlans {
 //        "epsg:32631"
       )
 
-    val args = if (manual_args.length == 3) manual_args else input_args
+    val args = if (manual_args.nonEmpty) manual_args else input_args
 
     println(s"Current arguments: ${args.mkString(",")}")
-    if (args.length < 3) {
+    if (args.length < 2) {
       println(
-        "Expected following arguments: <path to generated plans> <path to output plans csv> <generated plans crs>"
+        "Expected following arguments: <path to generated plans> <path to output plans csv> [<generated plans crs>]"
       )
     } else {
       val pathToGeneratedPlans = args(0)
       val pathToOutputPlans = args(1)
-      val crsString = args(2)
+      val maybeCRSString = {
+        if (args.length > 2) Some(args(2))
+        else None
+      }
 
       object geo extends GeoUtils {
-        override def localCRS: String = crsString
+        override def localCRS: String = maybeCRSString.getOrElse("")
       }
 
       val (plans, closable) = CsvPlanElementReader.readWithFilter(pathToGeneratedPlans, _ => true)
 
       val plansSelected = plans.filter(p => p.planSelected)
-      val plansUTM = plansSelected.map { planElement =>
-        (planElement.activityLocationX, planElement.activityLocationY) match {
-          case (Some(x), Some(y)) =>
-            val wgsLoc = geo.utm2Wgs(new Coord(x, y))
-            planElement.copy(activityLocationX = Some(wgsLoc.getX), activityLocationY = Some(wgsLoc.getY))
-          case _ => planElement
+      val plansUTM = plansSelected
+        // changing CRS if required
+        .map { planElement =>
+          (planElement.activityLocationX, planElement.activityLocationY) match {
+            case (Some(x), Some(y)) if maybeCRSString.nonEmpty =>
+              val wgsLoc = geo.utm2Wgs(new Coord(x, y))
+              planElement.copy(activityLocationX = Some(wgsLoc.getX), activityLocationY = Some(wgsLoc.getY))
+            case _ => planElement
+          }
         }
-      }
+        // converting departure_time in output plans from seconds to hours
+        .map { planElement =>
+          planElement.activityEndTime match {
+            case Some(departureTimeInSeconds) =>
+              planElement.copy(activityEndTime = Some(departureTimeInSeconds / 3600))
+            case None => planElement
+          }
+        }
 
       UrbansimPlansCsvWriter.toCsvWithHeader(plansUTM, pathToOutputPlans)
       closable.close()
