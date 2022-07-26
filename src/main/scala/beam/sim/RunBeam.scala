fe49b6b55580a18aa703a3d6b22cf41438b7f8b9
GrigoryD <grigory.derzhavets@simrise.tech>
2022-07-26 15:36:40 +0200

    [issue-3523] fix beam doesnt stop when exception occurs

--- src/main/scala/beam/sim/RunBeam.scala
+++ src/main/scala/beam/sim/RunBeam.scala
@@ -1,7 +1,7 @@
 package beam.sim
 
-import beam.utils.MathUtils
 import beam.api.{BeamCustomizationAPI, DefaultAPIImplementation}
+import beam.utils.{DebugLib, FileUtils, MathUtils}
 import ch.qos.logback.classic.util.ContextInitializer
 import org.matsim.core.controler.AbstractModule
 
@@ -23,10 +23,22 @@ object RunBeam extends BeamHelper {
     println(s"Heap size: ${MathUtils.formatBytes(Runtime.getRuntime.totalMemory())}")
     println(s"Heap max memory: ${MathUtils.formatBytes(Runtime.getRuntime.maxMemory())}")
     println(s"Heap free memory: ${MathUtils.formatBytes(Runtime.getRuntime.freeMemory())}")
-
-    runBeamUsing(args, None)
-    logger.info("Exiting BEAM")
-    System.exit(0)
+    //      this try-catch is needed in case an exception is thrown and the application hangs with some threads locked
+    //      e.g. for this method LoggingEventsManager#handleBlocking()
+    try {
+      runBeamUsing(args, None)
+    } catch {
+      case e: Exception =>
+        {
+          val threadDumpFileName = "thread_dump_from_RunBeam.txt.gz"
+          logger.error(s"Exception occurred: {}", e.getMessage)
+          FileUtils.writeToFile(threadDumpFileName,DebugLib.currentThreadsDump().asScala.iterator)
+          logger.info("Thread dump has been saved to the file {}", threadDumpFileName)
+          System.exit(2)
+        }
+        logger.info("Exiting BEAM")
+        System.exit(0)
+    }
   }
 
   def configureDefaultAPI: AbstractModule = {
@@ -38,5 +50,4 @@ object RunBeam extends BeamHelper {
     }
     abstractModule
   }
-
 }
