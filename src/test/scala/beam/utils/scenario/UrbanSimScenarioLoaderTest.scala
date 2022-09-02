228c0016d06088569af044dba86f1af6694282a2
Alvin Lee Jin Wen <alvinlee001@live.com.my>
2022-09-02 17:22:08 +0800

    Integration Test draft for FromFileVehicleAdjustment

--- src/test/scala/beam/utils/scenario/UrbanSimScenarioLoaderTest.scala
+++ src/test/scala/beam/utils/scenario/UrbanSimScenarioLoaderTest.scala
@@ -1,14 +1,22 @@
 package beam.utils.scenario
 
+import beam.agentsim.agents.vehicles.{BeamVehicle, BeamVehicleType}
+import beam.agentsim.agents.vehicles.VehicleCategory.Bike
 import beam.sim.BeamScenario
+import beam.sim.RunBeam.{buildUrbansimV2ScenarioSource, loadScenario}
 import beam.sim.common.GeoUtilsImpl
-import beam.sim.config.BeamConfig
+import beam.sim.config.{BeamConfig, MatSimBeamConfigBuilder}
 import beam.utils.TestConfigUtils.testConfig
-import org.matsim.core.scenario.MutableScenario
+import com.typesafe.config.ConfigFactory
+import org.matsim.api.core.v01.Id
+import org.matsim.core.scenario.{MutableScenario, ScenarioBuilder}
+import org.matsim.households.Household
 import org.mockito.Mockito.{mock, when}
 import org.scalatest.wordspec.AsyncWordSpec
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.matchers.should.Matchers
+
+import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 
 class UrbanSimScenarioLoaderTest extends AsyncWordSpec with Matchers with BeforeAndAfterEach {
   private val mutableScenario = mock(classOf[MutableScenario])
@@ -90,10 +98,44 @@ class UrbanSimScenarioLoaderTest extends AsyncWordSpec with Matchers with Before
 
       assertCarNumbers(res, "1" -> 2, "2" -> 2, "3" -> 2)
     }
+
+  }
+
+  "UrbanSimScenarioLoader with vehicleAdjustmentMethod = STATIC_FROM_FILE "should {
+    "assign vehicles properly in case1" in {
+
+      val matsimConfig = new MatSimBeamConfigBuilder(staticVehiclesConfig).buildMatSimConf()
+      val emptyScenario = ScenarioBuilder(matsimConfig, beamScenario.network).build
+      val staticVehicleBeamConfig = BeamConfig(staticVehiclesConfig)
+      val staticGeoUtils = new GeoUtilsImpl(staticVehicleBeamConfig);
+      when(beamScenario.beamConfig).thenReturn(staticVehicleBeamConfig);
+      lazy val staticBeamScenario: BeamScenario = loadScenario(staticVehicleBeamConfig);
+      val staticScenarioSource = buildUrbansimV2ScenarioSource(staticGeoUtils, staticVehicleBeamConfig);
+      val urbanSimScenario = new UrbanSimScenarioLoader(emptyScenario, staticBeamScenario, staticScenarioSource, geoUtils)
+      urbanSimScenario.loadScenario()
+
+      val vehicleIds = emptyScenario.getHouseholds.getHouseholds.get(Id.create(2, classOf[Household])).getVehicleIds
+      val vehicleMap = staticBeamScenario.privateVehicles.toMap
+      val vehiclesBelongToSpecificHousehold = vehicleIds.map(x => Id.create(x, classOf[BeamVehicle])).flatMap(x => vehicleMap get x);
+      vehiclesBelongToSpecificHousehold.count(x => x.beamVehicleType.id.compareTo(
+        Id.create("Bicycle", classOf[BeamVehicleType])
+      ) == 0) shouldBe 7
+    }
+
   }
 
   override protected def beforeEach(): Unit =
     idIter = Iterator.from(1)
+
+  def staticVehiclesConfig: com.typesafe.config.Config =
+    ConfigFactory
+      .parseString(s"""
+                      |beam.agentsim.simulationName = "beamville-urbansimv2_static"
+                      |beam.agentsim.agents.vehicles.vehiclesFilePath = $${beam.inputDirectory}"/vehicles-test.csv"
+                      |beam.agentsim.agents.vehicles.vehicleAdjustmentMethod = "STATIC_FROM_FILE"
+                    """.stripMargin)
+      .withFallback(testConfig("test/input/beamville/beam-urbansimv2.conf"))
+      .resolve()
 
   private def getConfig(fractionOfInitialVehicleFleet: Double = 1.0) = beamConfigBase.copy(
     matsim = beamConfigBase.matsim
