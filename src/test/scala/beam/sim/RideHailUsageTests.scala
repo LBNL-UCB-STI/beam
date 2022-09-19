be17fd5f5e3cbb44ba3e7feab1d5bd313f97aabd
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-09-19 14:51:47 +0300

    check if replanning and RH usage happens

--- src/test/scala/beam/sim/RideHailUsageTests.scala
+++ src/test/scala/beam/sim/RideHailUsageTests.scala
@@ -1,6 +1,6 @@
 package beam.sim
 
-import beam.agentsim.events.{PathTraversalEvent, RefuelSessionEvent}
+import beam.agentsim.events.{ModeChoiceEvent, PathTraversalEvent, RefuelSessionEvent, ReplanningEvent}
 import beam.integration.IntegrationSpecCommon
 import beam.utils.EventReader.{fromXmlFile, getEventsFilePath}
 import beam.utils.TestConfigUtils.testConfig
@@ -14,7 +14,7 @@ class RideHailUsageTests extends AnyFlatSpec with Matchers with BeamHelper with 
 
   def getRHPathTraversalsWithPassengers(
     events: Seq[Event],
-    minimumNumberOfPassengers: Int = 1
+    minimumNumberOfPassengers: Int
   ): Iterable[PathTraversalEvent] = {
     val pathTraversalEvents = events
       .filter(e => PathTraversalEvent.EVENT_TYPE.equals(e.getEventType))
@@ -25,13 +25,15 @@ class RideHailUsageTests extends AnyFlatSpec with Matchers with BeamHelper with 
     )
   }
 
-  it should "Use RH_BEV_L5 to transfer agents, RH_BEV_L5 should charge with 1 stall." in {
+  it should "Use RH_BEV_L5 to transfer agents" in {
     val automatedRideHailVehicleType = "RH_BEV_L5"
     val config = ConfigFactory
       .parseString(s"""
            |beam.agentsim.lastIteration = 0
            |beam.agentsim.taz.parkingFilePath = $${beam.inputDirectory}"/parking/taz-parking-one-rh-stall.csv"
            |beam.outputs.events.fileOutputFormats = "xml"
+           |beam.agentsim.agents.households.inputFilePath = $${beam.inputDirectory}"/households-no-vehicles.xml"
+           |beam.agentsim.agents.vehicles.replanOnTheFlyWhenHouseholdVehiclesAreNotAvailable = true
          """.stripMargin)
       .withFallback(testConfig("test/input/beamville/beam-withL5.conf"))
       .resolve()
@@ -47,7 +49,7 @@ class RideHailUsageTests extends AnyFlatSpec with Matchers with BeamHelper with 
     val refuelSessionEvents = events.filter(e =>
       RefuelSessionEvent.EVENT_TYPE.equals(e.getEventType) && e.getAttributes.get("vehicle").contains("-L5")
     )
-    refuelSessionEvents.size should be > 0 withClue ", expecting charging events"
+    refuelSessionEvents.size should be > 20 withClue ", expecting about 21 charging events"
     refuelSessionEvents.map(e => e.getAttributes.get(RefuelSessionEvent.ATTRIBUTE_VEHICLE_TYPE)) should contain(
       automatedRideHailVehicleType
     ) withClue f", expecting $automatedRideHailVehicleType to charge"
@@ -66,9 +68,35 @@ class RideHailUsageTests extends AnyFlatSpec with Matchers with BeamHelper with 
 
     refuelSessionEvents.tail.foldLeft(firstEventEndOfCharging) { case (previousEventEndOfCharging, refuelEvent) =>
       val (currentEventTime, currentEventDuration) = getTimeDuration(refuelEvent)
-      previousEventEndOfCharging should be <= currentEventTime
+      previousEventEndOfCharging should be <= currentEventTime withClue ", there is only one RH stall, charging events should go one by one"
       currentEventTime + currentEventDuration
     }
+
+    def isReplanningOrModeChoiceEventForPerson2(event: Event) = {
+      val attributes = event.getAttributes
+      val person = attributes.getOrDefault("person", "")
+      val isModeChoice = ModeChoiceEvent.EVENT_TYPE.equalsIgnoreCase(event.getEventType)
+      val isReplanning = ReplanningEvent.EVENT_TYPE.equalsIgnoreCase(event.getEventType)
+      person == "2" && (isModeChoice || isReplanning)
+    }
+
+    val replanAndModeChoiceForPerson2 = events
+      .filter(e => isReplanningOrModeChoiceEventForPerson2(e))
+      .dropWhile(e => !ReplanningEvent.EVENT_TYPE.equalsIgnoreCase(e.getEventType))
+      .map(_.getAttributes)
+      .take(2)
+
+    replanAndModeChoiceForPerson2(0)
+      .getOrDefault(
+        ReplanningEvent.ATTRIBUTE_REPLANNING_REASON,
+        ""
+      ) shouldBe "HouseholdVehicleNotAvailable CAR" withClue ", expected replanning because there are no household vehicles available"
+
+    replanAndModeChoiceForPerson2(1)
+      .getOrDefault(
+        ModeChoiceEvent.ATTRIBUTE_MODE,
+        ""
+      ) shouldBe "ride_hail" withClue ", expected RH usage after replanning"
   }
 
   it should "Use RH_BEV to transfer agents." in {
