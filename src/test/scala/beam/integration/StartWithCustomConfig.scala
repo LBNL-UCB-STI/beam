01a13314c3c151c77abfd8428bde48ba6519c7cc
Dmitry Openkov <dimaopen@gmail.com>
2022-09-20 13:05:43 +0300

    Added MultipleRideHailManagerSpec

--- src/test/scala/beam/integration/StartWithCustomConfig.scala
+++ src/test/scala/beam/integration/StartWithCustomConfig.scala
@@ -4,6 +4,7 @@ import beam.sim.BeamHelper
 import beam.sim.config.BeamConfig
 import beam.utils.EventReader._
 import com.typesafe.config.{Config, ConfigValueFactory}
+import org.matsim.api.core.v01.events.Event
 
 class StartWithCustomConfig(val config: Config) extends IntegrationSpecCommon with BeamHelper {
 
@@ -18,4 +19,7 @@ class StartWithCustomConfig(val config: Config) extends IntegrationSpecCommon wi
       .groupBy(_.getAttributes.get("mode"))
       .map { case (k, v) => (k, v.size) }
 
+  def events: IndexedSeq[Event] = fromXmlFile(
+    getEventsFilePath(matsimConfig, "events", BeamConfig(config).beam.outputs.events.fileOutputFormats).getPath
+  )
 }
