8059eeeedcab4e5fdf40f0ff99051df5b2a950e7
Justin Pihony <justin.pihony@gmail.com>
2022-09-25 20:36:41 -0400

    Fix compilation

--- src/main/scala/beam/utils/scenario/ScenarioLoaderHelper.scala
+++ src/main/scala/beam/utils/scenario/ScenarioLoaderHelper.scala
@@ -104,7 +104,7 @@ object ScenarioLoaderHelper extends LazyLogging {
     val households: List[Household] = scenario.getHouseholds.getHouseholds.values().asScala.toList
     households.par.foreach { household =>
       val members = household.getMemberIds.asScala.toSet
-      val validMembers = ProfilingUtils.timed(s"Determining valid members from households. Valid People Size: ${validPeople.size} and Members Size: ${members.size}", logger.info) {
+      val validMembers = ProfilingUtils.timed(s"Determining valid members from households. Valid People Size: ${validPeople.size} and Members Size: ${members.size}", x=>logger.info(x)) {
         validPeople.filter(validPerson => members.exists(member => validPerson.equals(member))) //validPeople.intersect(members)
       }
       if (validMembers.isEmpty) {
