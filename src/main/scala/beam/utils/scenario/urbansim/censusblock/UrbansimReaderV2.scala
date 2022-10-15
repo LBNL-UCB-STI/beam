83f01f895d935bfef7f6206729d452798743ab9b
Justin Pihony <justin.pihony@gmail.com>
2022-10-15 01:03:36 -0400

    Parallelize plan reading

--- src/main/scala/beam/utils/scenario/urbansim/censusblock/UrbansimReaderV2.scala
+++ src/main/scala/beam/utils/scenario/urbansim/censusblock/UrbansimReaderV2.scala
@@ -1,5 +1,8 @@
 package beam.utils.scenario.urbansim.censusblock
 
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.Materializer
 import beam.sim.common.GeoUtils
 import beam.utils.scenario.urbansim.censusblock.entities.InputHousehold
 import beam.utils.scenario.urbansim.censusblock.merger.{HouseholdMerger, PersonMerger, PlanMerger}
@@ -7,6 +10,10 @@ import beam.utils.scenario.urbansim.censusblock.reader._
 import beam.utils.scenario.{HouseholdInfo, PersonInfo, PlanElement, ScenarioSource}
 import org.matsim.api.core.v01.Coord
 import org.slf4j.LoggerFactory
+
+import scala.collection.immutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 class UrbansimReaderV2(
   val inputPersonPath: String,
@@ -56,6 +63,41 @@ class UrbansimReaderV2(
     val planReader = new PlanReader(inputPlanPath)
 
     try {
+      val iter: Iterator[PlanElement] = merger.merge(planReader.iterator())
+      import scala.concurrent.ExecutionContext.Implicits.global
+      /*val futures: Iterator[Future[PlanElement]] = iter.map{ plan: PlanElement => Future {
+        if (plan.planElementType == PlanElement.Activity && shouldConvertWgs2Utm) {
+          val utmCoord = geoUtils.wgs2Utm(new Coord(plan.activityLocationX.get, plan.activityLocationY.get))
+          plan.copy(activityLocationX = Some(utmCoord.getX), activityLocationY = Some(utmCoord.getY))
+        } else {
+          plan
+        }
+      }
+      }
+      val x: Future[Iterator[PlanElement]] = Future.sequence(futures)
+*/
+
+
+      implicit val system = ActorSystem()
+
+      import akka.stream.scaladsl.Source
+      val source: Source[PlanElement, NotUsed] = Source.fromIterator(()=>iter).mapAsync(100){ plan: PlanElement => Future {
+        if (plan.planElementType == PlanElement.Activity && shouldConvertWgs2Utm) {
+          val utmCoord = geoUtils.wgs2Utm(new Coord(plan.activityLocationX.get, plan.activityLocationY.get))
+          plan.copy(activityLocationX = Some(utmCoord.getX), activityLocationY = Some(utmCoord.getY))
+        } else {
+          plan
+        }
+      }}
+
+      import akka.stream.scaladsl.Sink
+      val sink = Sink.seq[PlanElement]
+
+      val output: Future[immutable.Seq[PlanElement]] = source.runWith(sink)
+      val result = Await.result(output, Duration.Inf)
+      system.terminate()
+      result.toList
+/*
       merger
         .merge(planReader.iterator())
         .map { plan: PlanElement =>
@@ -67,6 +109,8 @@ class UrbansimReaderV2(
           }
         }
         .toList
+
+ */
     } finally {
       logger.info("Modes merged successfully into plan.")
       planReader.close()
