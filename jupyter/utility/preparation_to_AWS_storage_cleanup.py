88beb3e97d409746ac760d0bc0b9e1b9ea5e55f2
Edward Zhao <edward.zhao@dbschenker.com>
2022-10-22 01:57:09 +0000

    #3611 correct link contains output/*/*

--- jupyter/utility/preparation_to_AWS_storage_cleanup.py
+++ jupyter/utility/preparation_to_AWS_storage_cleanup.py
@@ -13,7 +13,7 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 
-# In[3]:
+# In[6]:
 
 
 ## define a method to check if beam log exists
@@ -25,32 +25,40 @@ def beam_log_exists(path):
     s3url_fixed = path
     if path and ("#output/*/*" not in path):
         s3url_fixed = path.replace("s3.us-east-2.amazonaws.com/beam-outputs/index.html#output","beam-outputs.s3.amazonaws.com/output").strip()
+    else:
+        return False
+    
     beam_log_path = f"{s3url_fixed}/beamLog.out"
+    # print(beam_log_path)
     try:
         r = requests.head(beam_log_path)
         return r.status_code == requests.codes.ok
     except Exception:
         return False
 
-# output_path = " https://s3.us-east-2.amazonaws.com/beam-outputs/index.html#output/sfbay/sfbay-smart-ba1se__2019-10-23_13-18-45"
+# output_path = "https://s3.us-east-2.amazonaws.com/beam-outputs/index.html#output/*/*"
 # print(beam_log_exists(output_path))
 
 
-# In[4]:
+# In[9]:
 
 
 ## reading exported csv
+from datetime import datetime
 
 csv_data = pd.read_csv("../local_files/BEAM Deploy Status and Run Data - BEAM Instances.csv", parse_dates=['Time'])
 
 exist_rows = []
 non_exist_rows = []
+total_row_length=len(csv_data['S3 Url'])
 
 index = 0
 for s3url in csv_data['S3 Url'].copy():
     index=index+1
     if index % 500 == 1:
-        print("read index :" + str(index))
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print('Current Time = %s, %d out of %d' % (current_time, index, total_row_length))
     # if index > 360:
     #     break
 
@@ -77,7 +85,7 @@ for s3url in csv_data['S3 Url'].copy():
 print("found rows:" + str(len(exist_rows)))
 
 
-# In[6]:
+# In[10]:
 
 
 ## sorting the rows which still in S3 bucket
@@ -86,13 +94,13 @@ print("found rows:" + str(len(exist_rows)))
 exist_rows.sort(key=lambda x: x[0])
 
 
-# In[7]:
+# In[11]:
 
 
 ## generate output.csv
-pd.DataFrame(exist_rows).to_csv("../local_files/output.csv", header=["branch", "time", "s3url"], index=None)
+pd.DataFrame(exist_rows).to_csv("../local_files/preparation_to_AWS_storage_cleanup.csv", header=["branch", "time", "s3url"], index=None)
 
-output_data = pd.read_csv("../local_files/output.csv")
+output_data = pd.read_csv("../local_files/preparation_to_AWS_storage_cleanup.csv")
 
 print(output_data.head(10))
 
