13a9b43d8e6099b3cfe85579da7f9bbc77a83bfd
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-10-31 18:00:03 +0300

    tool to read/process events

--- jupyter/agents_events/analyse_events.py
+++ jupyter/agents_events/analyse_events.py
@@ -8,6 +8,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import sys
+import zlib
+
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -15,37 +18,48 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
 
-def try_read_akka_events(base_path, iteration):
+def read_csv(file_path):
+    try:
+        df = pd.read_csv(actor_events_path)
+        return df
+    except Exception as ex:
+        print(f"An exception while reading {actor_events_path}: \n\t{str(ex)}")
+        return pd.DataFrame()
+
+
+def find_and_read_akka_events(base_path, iteration):
     dfs = []
 
     for i in range(0,100):
         actor_events_path = f"{base_path}/ITERS/it.{iteration}/{iteration}.actor_messages_{i}.csv.gz"
         if not os.path.isfile(actor_events_path): 
             break
-        df = pd.read_csv(actor_events_path)
-        dfs.append(df)
+
+        df = read_csv(actor_events_path)
+        if len(df) > 0:
+            dfs.append(df)
 
     print(f"There were {len(dfs)} chunks")
     if len(dfs) > 0:
         actor_events = pd.concat(dfs)
-        return actor_events
+        return actor_events.fillna("")
     else:
         print("No chunks - no events")
         return False
     
     
-def read_beam_events(base_path, iteration):
-    events_path =  f"{base_path}/ITERS/it.{iteration}/{iteration}.events.csv"
-    if os.path.isfile(events_path): 
-        events = pd.read_csv(events_path)
-        return events
     
-    events_path =  f"{base_path}/ITERS/it.{iteration}/{iteration}.events.csv.gz"
+def read_beam_events(base_path, iteration):        
+    base_path = f"{base_path}/ITERS/it.{iteration}/{iteration}"
+    events_path =  f"{base_path}.events.csv"
+    if os.path.isfile(events_path):
+        return read_csv(events_path)
+    
+    events_path =  f"{base_path}.events.csv.gz"
     if os.path.isfile(events_path): 
-        events = pd.read_csv(events_path)
-        return events
+        return read_csv(events_path)
         
-    return False
+    raise Exception(f"There are no events files in {base_path}")
     
 
 def read_keys_from_full_config(base_path):
@@ -65,6 +79,32 @@ def read_keys_from_full_config(base_path):
                         config_map[key] = config_val
     return config_map
 
+def read_corrupted_file(filename, CHUNKSIZE=1024):
+    d = zlib.decompressobj(zlib.MAX_WBITS | 32)
+    with open(filename, 'rb') as f:
+        result_str = ''
+        buffer = f.read(CHUNKSIZE)
+        try:
+            while buffer:
+                result_str += d.decompress(buffer).decode('utf-8')
+                buffer = f.read(CHUNKSIZE)
+        except Exception as e:
+            print('Error: %s -> %s' % (filename, e))
+        return result_str
+
+    
+def fix_corrupted_gzip_file(file_path, fixed_copy_file_path = ""):
+    if not fixed_copy_file_path:
+        if file_path.endswith('.csv.gz'):
+            fixed_copy_file_path = file_path + "-fixed.csv"
+        else:
+            fixed_copy_file_path = file_path + "-fixed" + file_path.split('.')[-1]
+    file_content = read_corrupted_file(file_path)
+    with open(fixed_copy_file_path, "w") as text_file:
+        text_file.write(file_content)
+    print(f"the content of '{file_path}' is read and \nwritten to '{fixed_copy_file_path}'")
+
+    
 print("init complete")
 
 
