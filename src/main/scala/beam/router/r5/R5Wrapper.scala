a5703d44d30c6ef2cde8101ae30e924bd837deec
Justin Pihony <justin.pihony@gmail.com>
2022-11-21 11:59:50 -0500

    Fix boxing issue

--- src/main/scala/beam/router/r5/R5Wrapper.scala
+++ src/main/scala/beam/router/r5/R5Wrapper.scala
@@ -36,6 +36,10 @@ import scala.collection.mutable.ArrayBuffer
 import scala.language.postfixOps
 import scala.util.Try
 
+trait TravelTimeByLinkCalculator {
+  def apply(time: Double, linkId: Int, streetMode: StreetMode): Double
+}
+
 class R5Wrapper(workerParams: R5Parameters, travelTime: TravelTime, travelTimeNoiseFraction: Double)
     extends MetricsSupport
     with StrictLogging
@@ -1167,9 +1171,11 @@ class R5Wrapper(workerParams: R5Parameters, travelTime: TravelTime, travelTimeNo
     startTime: Int,
     shouldAddNoise: Boolean
   ): TravelTimeCalculator = {
-    val ttc = travelTimeByLinkCalculator(vehicleType, shouldAddNoise, shouldApplyBicycleScaleFactor = true)
-    (edge: EdgeStore#Edge, durationSeconds: Int, streetMode: StreetMode, _) => {
-      ttc(startTime + durationSeconds, edge.getEdgeIndex, streetMode).floatValue().ceil
+    new TravelTimeCalculator {
+      val ttc = travelTimeByLinkCalculator(vehicleType, shouldAddNoise, shouldApplyBicycleScaleFactor = true)
+      override def getTravelTimeSeconds(edge: EdgeStore#Edge, durationSeconds: Int, streetMode: StreetMode, req: ProfileRequest): Float = {
+        ttc(startTime + durationSeconds, edge.getEdgeIndex, streetMode).floatValue().ceil
+      }
     }
   }
 
@@ -1177,19 +1183,21 @@ class R5Wrapper(workerParams: R5Parameters, travelTime: TravelTime, travelTimeNo
     vehicleType: BeamVehicleType,
     shouldAddNoise: Boolean,
     shouldApplyBicycleScaleFactor: Boolean = false
-  ): (Double, Int, StreetMode) => Double = {
+  ): TravelTimeByLinkCalculator = {
     val profileRequest = createProfileRequest
-    (time: Double, linkId: Int, streetMode: StreetMode) => {
-      val edge = transportNetwork.streetLayer.edgeStore.getCursor(linkId)
-      val maxSpeed: Double = vehicleType.maxVelocity.getOrElse(profileRequest.getSpeedForMode(streetMode))
-      val minTravelTime = edge.getLengthM / maxSpeed
-      if (streetMode == StreetMode.CAR) {
-        carWeightCalculator.calcTravelTime(linkId, travelTime, Some(vehicleType), time, shouldAddNoise)
-      } else if (streetMode == StreetMode.BICYCLE && shouldApplyBicycleScaleFactor) {
-        val scaleFactor = bikeLanesAdjustment.scaleFactor(vehicleType, linkId)
-        minTravelTime * scaleFactor
-      } else {
-        minTravelTime
+    new TravelTimeByLinkCalculator {
+      override def apply(time: Double, linkId: Int, streetMode: StreetMode): Double = {
+        val edge = transportNetwork.streetLayer.edgeStore.getCursor(linkId)
+        val maxSpeed: Double = vehicleType.maxVelocity.getOrElse(profileRequest.getSpeedForMode(streetMode))
+        val minTravelTime = edge.getLengthM / maxSpeed
+        if (streetMode == StreetMode.CAR) {
+          carWeightCalculator.calcTravelTime(linkId, travelTime, Some(vehicleType), time, shouldAddNoise)
+        } else if (streetMode == StreetMode.BICYCLE && shouldApplyBicycleScaleFactor) {
+          val scaleFactor = bikeLanesAdjustment.scaleFactor(vehicleType, linkId)
+          minTravelTime * scaleFactor
+        } else {
+          minTravelTime
+        }
       }
     }
   }
