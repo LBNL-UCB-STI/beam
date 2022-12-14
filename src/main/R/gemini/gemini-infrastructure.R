7aad49c8488dcd5cc939be093e39e127a6b281ac
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2022-12-14 12:13:08 -0800

    updating jupyter notebook

--- src/main/R/gemini/gemini-infrastructure.R
+++ src/main/R/gemini/gemini-infrastructure.R
@@ -7,16 +7,39 @@ library(dplyr)
 library(ggplot2)
 
 dataDir <- normalizePath("~/Workspace/Data")
-geminiDir <- normalizePath("~/Workspace/Data/GEMINI")
-infraDir <- pp(geminiDir,"/2022-07-05/_models/infrastructure")
+projectDir <- pp(dataDir, "/GEMINI")
+workDir <- pp(projectDir,"/2022-07-05")
+infraDir <- pp(workDir,"/_models/nrel_infrastructure")
 
+
+###########################
+###########################
+###########################
+
+#infra8MaxEV <- readCsv(pp(infraDir, "/scen_8_infrastructure_withFees.csv.gz"))
+infra_converted_withFees <- readCsv(pp(infraDir, "/scen_8_infrastructure_withFees.csv.gz"))
+infra_converted_withFees[startsWith(reservedFor,"household")]$reservedFor <- "Any"
+infra_converted_withFees_noHousehold <- infra_converted_withFees[
+  ,.(parkingZoneId=first(parkingZoneId),feeInCents=mean(feeInCents),numStalls=sum(numStalls))
+  ,b=.(taz,parkingType,chargingPointType,X,Y,reservedFor,pricingModel)]
+names(infra_converted_withFees_noHousehold)[names(infra_converted_withFees_noHousehold) == "X"] <- "locationX"
+names(infra_converted_withFees_noHousehold)[names(infra_converted_withFees_noHousehold) == "Y"] <- "locationY"
+write.csv(
+  infra_converted_withFees_noHousehold,
+  file = pp(infraDir, "/scen_8_infrastructure_withFees_noHousehold.csv"),
+  row.names=FALSE,
+  quote=FALSE)
+
+###########################
 
 infra5aBase <- readCsv(pp(infraDir, "/4a_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"))
 infra5bBase <- readCsv(pp(infraDir, "/4b_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"))
 infra6Base <- readCsv(pp(infraDir, "/6_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"))
+infra7Advanced <- readCsv(pp(infraDir,"/scen_7_infrastructure_withFees_noHousehold.csv"))
+infra8MaxEV <- readCsv(pp(infraDir,"/scen_8_infrastructure_withFees_noHousehold.csv"))
 
-aggregateInfrastructure <- function(FILE_NAME) {
-  DATA <- readCsv(pp(infraDir, "/", FILE_NAME, ".csv"))
+aggregateInfrastructure <- function(FULL_FILE_NAME) {
+  DATA <- readCsv(FULL_FILE_NAME)
   DATA_agg <- DATA[,.(feeInCents=mean(feeInCents),numStalls=sum(numStalls)),by=.(taz,parkingType,chargingPointType,reservedFor,pricingModel)]
   DATA_agg <- DATA_agg[,`:=`(IDX = 1:.N),by=taz]
   DATA_agg$parkingZoneId <- paste("site",DATA_agg$taz,DATA_agg$IDX,sep="-")
@@ -27,61 +50,26 @@ aggregateInfrastructure <- function(FILE_NAME) {
   setnames(DATA_merge, "numStalls.x", "numStalls")
   setnames(DATA_merge, "parkingZoneId.y", "siteId")
   DATA_merge <- DATA_merge[,-c("feeInCents.y","numStalls.y")]
-  print(pp("printing... ", pp(infraDir, "/", FILE_NAME, "_siteId.csv")))
+  FILE_NAME_NO_EXT <- tools::file_path_sans_ext(FULL_FILE_NAME)
+  print(pp("printing... ", pp(FILE_NAME_NO_EXT, "_siteId.csv")))
   write.csv(
     DATA_merge,
-    file = pp(infraDir, "/", FILE_NAME, "_siteId.csv"),
+    file = pp(FILE_NAME_NO_EXT, "_siteId.csv"),
     row.names=FALSE,
     quote=FALSE)
-  print(pp("printing... ", pp(infraDir, "/", FILE_NAME, "_aggregated.csv")))
+  print(pp("printing... ", pp(FILE_NAME_NO_EXT, "_aggregated.csv")))
   write.csv(
     DATA_agg,
-    file = pp(infraDir, "/", FILE_NAME, "_aggregated.csv"),
+    file = pp(FILE_NAME_NO_EXT, "_aggregated.csv"),
     row.names=FALSE,
     quote=FALSE)
   print("END aggregateInfrastructure")
 }
 
-aggregateInfrastructure("4b_output_2022_Apr_13_pubClust_withFees_noHousehold")
-
-aggregateInfrastructure("6_output_2022_Apr_13_pubClust_withFees_noHousehold")
+aggregateInfrastructure(pp(infraDir, "/scen_7_infrastructure_withFees_noHousehold.csv"))
 
 
-
-
-
-infra5bBase_agg <- infra5bBase[,.(feeInCents=mean(feeInCents),numStalls=sum(numStalls)),by=.(taz,parkingType,chargingPointType,reservedFor,pricingModel)]
-infra5bBase_agg <- infra5bBase_agg[,`:=`(IDX = 1:.N),by=taz]
-infra5bBase_agg$parkingZoneId <- paste(infra5bBase_agg$taz,infra5bBase_agg$IDX,sep="-")
-infra5bBase_agg <- infra5bBase_agg[,-c("IDX")]
-infra5bBase_merge <- merge(infra5bBase, infra5bBase_agg, by=c("taz","parkingType","chargingPointType","reservedFor","pricingModel"), all = TRUE)
-
-infra5bBase_merge <- infra5bBase_merge[,c-("")]
-setnames(infra5bBase_merge, "parkingZoneId.x", "parkingZoneId")
-setnames(infra5bBase_merge, "feeInCents.x", "feeInCents")
-setnames(infra5bBase_merge, "numStalls.x", "numStalls")
-setnames(infra5bBase_merge, "parkingZoneId.y", "siteId")
-infra5bBase_merge <- infra5bBase_merge[,-c("feeInCents.y","numStalls.y")]
-write.csv(
-  infra5bBase_merge,
-  file = pp(infraDir, "/6_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"),
-  row.names=FALSE,
-  quote=FALSE)
-
-
-
-
-infra5aBase <- readCsv(pp(workDir, "/2022-04-28/_models/infrastructure/4a_output_2022_Apr_13_pubClust_withFees_aggregated.csv"))
-infra5bBase <- readCsv(pp(workDir, "/2022-04-28/_models/infrastructure/6_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"))
-infra5bBase[startsWith(reservedFor,"household")]$reservedFor <- "Any"
-infra5bBase_NH <- infra5bBase[
-  ,.(parkingZoneId=first(parkingZoneId),feeInCents=mean(feeInCents),numStalls=sum(numStalls))
-  ,b=.(taz,parkingType,chargingPointType,X,Y,reservedFor,pricingModel)]
-write.csv(
-  infra5bBase_NH,
-  file = pp(workDir, "/2022-04-28/_models/infrastructure/6_output_2022_Apr_13_pubClust_withFees_noHousehold.csv"),
-  row.names=FALSE,
-  quote=FALSE)
+######
 
 vehicles1 <- readCsv(pp(workDir, "/vehicles.4Base.csv"))
 vehicles1$stateOfCharge <- as.double(vehicles1$stateOfCharge)
@@ -151,10 +139,83 @@ write.csv(
 
 # chargingBehaviorFunc2(refueling)
 
+###########################
+###########################
+###########################
+
+convertToFCSPlanInput <- function(DT, run_name) {
+  DT2 <- DT[,c("time", "duration", "vehicle", "numPassengers", "length", 
+               "startX", "fuel","startY", "endX", "endY", "primaryFuel", "type", 
+               "departureTime", "arrivalTime", "locationX", "locationY")]
+  names(DT2)[names(DT2) == "numPassengers"] <- "num_passengers"
+  names(DT2)[names(DT2) == "startX"] <- "start.x"
+  names(DT2)[names(DT2) == "startY"] <- "start.y"
+  names(DT2)[names(DT2) == "endX"] <- "end.x"
+  names(DT2)[names(DT2) == "endY"] <- "end.y"
+  names(DT2)[names(DT2) == "type"] <- "type2"
+  DT2[type2=="RefuelSessionEvent", start.x:=locationX]
+  DT2[type2=="RefuelSessionEvent", start.y:=locationY]
+  DT2[type2=="PathTraversal", time:=departureTime]
+  DT2[type2=="RefuelSessionEvent", time:=time-duration]
+  DT2[,kwh:=0]
+  DT2[type2=="PathTraversal", kwh:=primaryFuel/3.6e+6]
+  DT2[type2=="RefuelSessionEvent", kwh:=fuel/3.6e+6]
+  DT2[type2=="PathTraversal", duration:=arrivalTime-departureTime]
+  DT2$run <- run_name
+  DT2 <- DT2[,-c("departureTime", "arrivalTime", "primaryFuel", "fuel", "locationX", "locationY")]
+  DT2[,speed:=0.0]
+  DT2[type2=="PathTraversal", speed:=ifelse(duration==0, NA, length/duration)*2.237]
+  DT2[type2=="RefuelSessionEvent", speed:=NA]
+  DT2[,reposition:=NA]
+  DT2[type2=="PathTraversal", reposition:=ifelse(num_passengers==0, TRUE, FALSE)]
+  DT2[,type:="Movement"]
+  DT2[type2=="RefuelSessionEvent",type:="Charge"]
+  DT2[,hour:=as.integer(time/3600)]
+  DT2 <- DT2[,-c("type2")]
+  return(DT2)
+}
 
 hongcai_siting_input <- readCsv(pp(dataDir,"/DepotSiting/hczhang/beam_ev_rhrf_outputs.csv"))
 
+work_dir <- pp(dataDir,"/GEMINI/2022-07-05")
+events_plus_dir <- pp(work_dir,"/events-plus")
+
+gemini7Advanced <- readCsv(pp(events_plus_dir,"/rhev-siting.0.events.7Advanced.csv.gz"))
+gemini8MaxEV <- readCsv(pp(events_plus_dir,"/rhev-siting.0.events.8MaxEV.csv.gz"))
+
+gemini7AdvancedFiltered <- gemini7Advanced[startsWith(vehicleType, "ev-") | startsWith(vehicleType, "phev-")]
+gemini8MaxEVFiltered <- gemini8MaxEV[startsWith(vehicleType, "ev-") | startsWith(vehicleType, "phev-")]
+
+gemini7AdvancedNumVeh <- length(unique(gemini7AdvancedFiltered$vehicle))
+gemini8MaxEVNumVeh <- length(unique(gemini8MaxEVFiltered$vehicle))
 
 
+gemini7Advanced1 <- convertToFCSPlanInput(gemini7AdvancedFiltered, "chargerLevel_400kW__vehicleRange_300mi__ridehailNumber_35k")
+gemini8MaxEV1 <- convertToFCSPlanInput(gemini8MaxEVFiltered, "chargerLevel_400kW__vehicleRange_300mi__ridehailNumber_74k")
+gemini7Advanced2 <- convertToFCSPlanInput(gemini7AdvancedFiltered, "chargerLevel_300kW__vehicleRange_250mi__ridehailNumber_35k")
+gemini8MaxEV2 <- convertToFCSPlanInput(gemini8MaxEVFiltered, "chargerLevel_300kW__vehicleRange_250mi__ridehailNumber_74k")
+gemini7Advanced3 <- convertToFCSPlanInput(gemini7AdvancedFiltered, "chargerLevel_200kW__vehicleRange_200mi__ridehailNumber_35k")
+gemini8MaxEV3 <- convertToFCSPlanInput(gemini8MaxEVFiltered, "chargerLevel_200kW__vehicleRange_200mi__ridehailNumber_74k")
+
+beam_ev_gemini_outputs <- rbind(gemini7Advanced1, gemini8MaxEV1, 
+                                gemini7Advanced2, gemini8MaxEV2,
+                                gemini7Advanced3, gemini8MaxEV3)
+write.csv(
+  beam_ev_gemini_outputs,
+  file = pp(events_plus_dir, "/beam_ev_gemini_outputs.csv"),
+  row.names=FALSE,
+  quote=FALSE)
 
 
+rh7Advanced_file <- pp(workDir,"/events-plus/FCS_planning_results/taz-parking_S80_P300_R250_F35k.csv")
+rh8MaxEV_file <- pp(workDir,"/events-plus/FCS_planning_results/taz-parking_S80_P300_R250_F74k.csv")
+
+rhInfra <- readCsv(rh8MaxEV_file)
+rhInfra$parkingZoneId2 <- apply(rhInfra[,c("taz","parkingZoneId")],1,paste,collapse="-")
+rhInfra$parkingZoneId <- rhInfra$parkingZoneId2
+rhInfra <- rhInfra[,-c("parkingZoneId2")]
+write.csv(
+  rhInfra,
+  file = pp(workDir, "/events-plus/FCS_planning_results/taz-parking_S80_P300_R250_F74k_Bis.csv"),
+  row.names=FALSE,
+  quote=FALSE)
--- src/main/python/gemini/events_processing.py
+++ src/main/python/gemini/events_processing.py
@@ -3,13 +3,6 @@ import os
 import sys
 
 
-def print2(msg):
-    with open(full_filename + ".out", 'w') as f:
-        print(msg, file=f)
-
-
-work_directory = '~/Data/GEMINI/2022-07-05/'
-filename = '0.events.a.csv.gz'
 events1 = ["RefuelSessionEvent", "ChargingPlugInEvent", "ChargingPlugOutEvent", "actstart"]
 columns1 = ["vehicle", "time", "type", "parkingTaz", "chargingPointType", "parkingType", "locationY", "locationX",
             "duration", "vehicleType", "person", "fuel", "parkingZoneId", "pricingModel", "actType"]
@@ -18,40 +11,51 @@ columns2 = []
 events3 = ["PathTraversal", "RefuelSessionEvent"]
 columns3 = []
 
-event_set_type = 1
+event_set_type = "pev-siting"
 event_set = events1
 columns_set = columns1
-full_filename = os.path.expanduser(work_directory + "/" + filename)
+full_filename = os.path.expanduser('~/Data/GEMINI/2022-07-05/0.events.a.csv.gz')
 # full_filename = os.path.expanduser(work_directory + "events-raw/" + filename)
 
 if len(sys.argv) >= 2:
     full_filename = str(sys.argv[1])
-print("reading " + filename)
+
+
+def print2(msg):
+    with open(full_filename + ".out", 'w') as f:
+        print(msg)
+        print(msg, file=f)
+
+dirname = os.path.dirname(full_filename)
+basename = os.path.basename(full_filename)
+
 compression = None
-if full_filename.endswith(".gz"):
+if basename.endswith(".gz"):
     compression = 'gzip'
 
 if len(sys.argv) >= 3:
     event_set_type = str(sys.argv[2])
 
-if event_set_type == 2:
+if event_set_type == "pt-mc":
     event_set = events2
     columns_set = columns2
-elif event_set_type == 3:
+elif event_set_type == "rhev-siting":
     event_set = events3
     columns_set = columns3
 else:
     event_set = events1
     columns_set = columns1
 
-
-print2("reading " + filename)
+print2("reading " + full_filename)
 data = pd.read_csv(full_filename, sep=",", index_col=None, header=0, compression=compression)
-print2("filtering 1/2...")
+print2("filtering 1/3...")
 data_filtered = data.loc[data.type.isin(event_set)]
-print2("filtering 2/2...")
+print2("filtering 2/3...")
+if event_set_type == "rhev-siting":
+    data_filtered = data_filtered.loc[data.vehicle.astype(str).str.startswith('rideHail')]
+print2("filtering 3/3...")
 if columns_set:
     data_filtered = data_filtered[columns_set]
 print2("writing...")
-data_filtered.to_csv(work_directory + "/filtered." + filename)
+data_filtered.to_csv(dirname + "/" + event_set_type + "." + basename)
 print2("END")
