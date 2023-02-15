b834abe4b28cec04ecc9126c4cd512dede559df8
Nikolay Ilyin <irishwithaxe@gmail.com>
2023-02-15 17:47:05 +0300

    less timeout

--- src/main/bash/helics/run_pydss_federate.sh
+++ src/main/bash/helics/run_pydss_federate.sh
@@ -15,15 +15,15 @@ cd /home/ubuntu/git/beam/src/main/python/gemini/cosimulation
 now="$(date +"%Y_%m_%d_%I_%M_%p")"
 python3 beam_pydss_broker.py 3 > "output_${now}_broker.log" &
 echo "broker started"
-sleep 5s
+sleep 1s
 
 python3 beam_to_pydss_federate.py > "output_${now}_federate.log" &
 echo "federate started"
-sleep 5s
+sleep 1s
 
 helics_recorder beam_recorder.txt --output=recording_output.txt > "output_${now}_recorder.log" &
 echo "recorder started"
-sleep 5s
+sleep 1s
 
 cd -
 
--- src/main/bash/helics/run_site_power_controller.sh
+++ src/main/bash/helics/run_site_power_controller.sh
@@ -15,14 +15,14 @@ cd /home/ubuntu/git/beam/src/main/python/gemini/cosimulation
 now="$(date +"%Y_%m_%d_%I_%M_%p")"
 python3 beam_pydss_broker.py 3 > "output_${now}_broker.log" &
 echo "broker started"
-sleep 5s
+sleep 1s
 
 python3 site_power_controller_federate.py 1 > "output_${now}_site_power_controller.log" &
 echo "site power controller started"
-sleep 5s
+sleep 1s
 
 helics_recorder beam_recorder.txt --output=recording_output.txt > "output_${now}_recorder.log" &
 echo "recorder started"
-sleep 5s
+sleep 1s
 
 cd -
