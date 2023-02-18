75aab3d157a7104d2479a27687e4cd1a2c71dc82
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2023-02-17 17:52:33 -0800

    updating site power controller utils

--- src/main/python/gemini/cosimulation/site_power_controller_utils.py
+++ src/main/python/gemini/cosimulation/site_power_controller_utils.py
@@ -4,36 +4,79 @@
 import helics as h
 import logging
 from threading import Thread
-from nrel_spmc_controller.rudimentary_spmc_rev3 import SPM_Control
+from nrel_spmc_controller.rudimentary_spmc_rev3 import SPM_Control as SPM_Control_Rudimentary
+from nrel_spmc_controller.spmc_v4 import SPM_Control as SPM_Control_Advanced
 from xfc_btms_saev_controller import components
 import xfc_btms_saev_controller.components
+from abc import abstractmethod
 
 
-class AdvancedSPMC:
+def print2(to_print):
+    logging.info(to_print)
+    print(to_print)
+
+
+# if len(sys.argv) < 2:
+#     logging.error("infrastructure file is missing")
+
+def create_federate(helics_conf, fed_info, taz_id):
+    fed_name = helics_conf["spmFederatesPrefix"] + taz_id
+
+    # create federate
+    cfed = h.helicsCreateCombinationFederate(fed_name, fed_info)
+    logging.info("Create combination federate " + fed_name)
+
+    # Register a publication of control signals
+    # Power in kW
+    pub = helics_conf["spmSubscription"]
+    h.helicsFederateRegisterTypePublication(cfed, pub, "string", "")
+    logging.info("Registered to publication " + pub)
+
+    # register subscriptions
+    # subscribe to information from TEMPO such that you can map to PyDSS modeled charging stations
+    # Power in kW and Energy in Joules
+    sub = helics_conf["federatesPrefix"] + str(taz_id) + "/" + helics_conf["federatesPublication"]
+    h.helicsFederateRegisterSubscription(cfed, sub, "string")
+    logging.info("Registered to subscription " + sub)
+
+    # subscriptions to PyDSS
+    # The Power is in kW, and to get the energy multiply by 60 seconds (1 minute interval) and convert it to Joules
+    # Then sum it up for the 5 minutes total interval (battery)
+    # While for the load take the average
+    # kW * 60/3600 => X KWH
+    # kWH * 3.6e+6 => Y Joules
+    # Z = Sum(Y_i); i = 1min -> 5min => Storage
+    # Z* = Mean(kW_i) => Load
+    return cfed
+
+class DataForSPMC:
+    vehicle_id = []
+    vehicle_type = []
+    primary_fuel_level_in_k_wh = []
+    arrival_time = []
+    desired_departure_time = []
+    desired_fuel_level_in_k_wh = []
+    max_power_in_kw = []  # min [plug, vehicle]
+    site_power_in_kw = 0
+    battery_capacity_in_k_wh = []  # TODO Julius @ HL can you please add this to the BEAM output?
+
+    def __init__(self):
+        pass
+
+
+class AbstractSPMC:
     # Myungsoo's advanced SPM Controller
     control_commands = []
     thread = Thread()
 
-    def __init__(self, name, taz_id, site_id):
-        self.taz_id = taz_id
-        self.site_id = site_id
-        self.site_prefix_logging = name + "[" + str(taz_id) + ":" + str(site_id) + "]. "
-        Pmax = [20, 25, 15, 20]  # max EV charging power (EVSE power rate)
-        Pmin = [0, 0, 0, 0]  # min EV charging power
-        ESS_capacity = 50  # in kWh
-        self.spm_c = SPM_Control(time_step_mins=15, num_ess=1, ess_size=ESS_capacity, max_power_evse=Pmax, min_power_evse=Pmin)
-        print2(self.site_prefix_logging + "Initialized!")
-
-class RudimentarySPMC:
-    # Myungsoo's rudimentary SPM Controller
-    control_commands = []
-    thread = Thread()
+    @abstractmethod
+    def run_model(self, t, vehicles, data_for_spmc):
+        return []
 
     def __init__(self, name, taz_id, site_id):
         self.taz_id = taz_id
         self.site_id = site_id
         self.site_prefix_logging = name + "[" + str(taz_id) + ":" + str(site_id) + "]. "
-        self.spm_c = SPM_Control(time_step_mins=1, max_power_evse=[], min_power_evse=[])
         print2(self.site_prefix_logging + "Initialized!")
 
     def log(self, log_message):
@@ -50,58 +93,56 @@ class RudimentarySPMC:
 
     def run(self, t, charging_events):
         self.control_commands = []
-        vehicle_id = []
-        vehicle_type = []
-        primary_fuel_level_in_k_wh = []
-        arrival_time = []
-        desired_departure_time = []
-        desired_fuel_level_in_k_wh = []
-        max_power_in_kw = []  # min [plug, vehicle]
-        site_power_in_kw = 0
-        battery_capacity_in_k_wh = []  # TODO Julius @ HL can you please add this to the BEAM output?
+        data_for_spmc = DataForSPMC()
 
         for charging_event in charging_events:
-            vehicle_id.append(str(charging_event['vehicleId']))
-            vehicle_type.append(str(charging_event['vehicleType']))
+            data_for_spmc.vehicle_id.append(str(charging_event['vehicleId']))
+            data_for_spmc.vehicle_type.append(str(charging_event['vehicleType']))
             # MJ: What is this? Is it the current energy level of each EV? Or battery size?
-            primary_fuel_level_in_k_wh.append(int(charging_event['primaryFuelLevelInJoules']) / 3600000)
+            data_for_spmc.primary_fuel_level_in_k_wh.append(int(charging_event['primaryFuelLevelInJoules']) / 3600000)
             # MJ: Should be in minutes of day. What is the unit of this?
-            arrival_time.append(int(charging_event['arrivalTime']))
+            data_for_spmc.arrival_time.append(int(charging_event['arrivalTime']))
             # MJ: Should be in minutes of day. What is the unit of this?
-            desired_departure_time.append(int(charging_event['departureTime']))
+            data_for_spmc.desired_departure_time.append(int(charging_event['departureTime']))
             # MJ: I assume that this is remaining energy to be delivered to each EV
             # and updated each time,right?
-            desired_fuel_level_in_k_wh.append(int(charging_event['desiredFuelLevelInJoules']) / 3600000)
+            data_for_spmc.desired_fuel_level_in_k_wh.append(int(charging_event['desiredFuelLevelInJoules']) / 3600000)
             # MJ: I assume that this is the EV charging power
-            max_power_in_kw.append(float(charging_event['maxPowerInKW']))
+            data_for_spmc.max_power_in_kw.append(float(charging_event['maxPowerInKW']))
             # Julius @ HL can you please add this to the BEAM output?
-            battery_capacity_in_k_wh.append(int(charging_event['primaryFuelCapacityInJoule']) / 3600000)
+            data_for_spmc.battery_capacity_in_k_wh.append(int(charging_event['primaryFuelCapacityInJoule']) / 3600000)
             # total site power
-            site_power_in_kw = float(charging_event['parkingZonePowerInKW'])
+            data_for_spmc.site_power_in_kw = float(charging_event['parkingZonePowerInKW'])
 
-        # Myungsoo is SPM Controller (NOT RIDE HAIL DEPOT)
-        # 1) SPM Controller takes list(charging_events) (and/or siteId)
-        # 2) SPM Controller returns control_commands
-        # 2.a) example
-        # 3) add control_commands to control_commands_list
-        # control_commands_list = control_commands_list + control_commands
-        self.spm_c.max_power_evse = max_power_in_kw
-        self.spm_c.min_power_evse = [0] * len(self.spm_c.max_power_evse)
-        pmin_site_in_kw = 0
-        pmax_site_in_kw = site_power_in_kw
-        tdep = [(tt - t) / 60.0 for tt in desired_departure_time]
-        # self.log("Optimizing EVSE setpoints by the regular SPM Controller")
+        self.control_commands = self.run_model(t, charging_events, data_for_spmc)
 
-        [p_evse_opt, e_evse_opt, delta_t] = self.spm_c.get_evse_setpoint(
-            tdep,
-            desired_fuel_level_in_k_wh,
-            pmin_site_in_kw,
-            pmax_site_in_kw
-        )
+        return self.control_commands
 
+
+class AdvancedSPMC(AbstractSPMC):
+    # Myungsoo's advanced SPM Controller
+    control_commands = []
+    thread = Thread()
+
+    def __init__(self, name, taz_id, site_id):
+        Pmax = [20, 25, 15, 20]  # max EV charging power (EVSE power rate)
+        Pmin = [0, 0, 0, 0]  # min EV charging power
+        ESS_capacity = 50  # in kWh
+        self.spm_c = SPM_Control_Advanced(time_step_mins=15, num_ess=1, ess_size=ESS_capacity, max_power_evse=Pmax, min_power_evse=Pmin)
+        AbstractSPMC.__init__(self, name, taz_id, site_id)
+
+    def run_model(self, t, vehicles, data_for_spmc):
+        t_dep = [(tt - t) / 60.0 for tt in data_for_spmc.desired_departure_time] # departure time in minute from the current time
+        e_req = data_for_spmc.desired_fuel_level_in_k_wh # energy remaining for each EV
+        Pmax_site = 30
+        Pmin_site = 0
+        ESS_soc = 0.7
+        [p_evse_setpoint, p_ess_setpoint, p_evse_opt, e_evse_opt, p_ess_opt, e_ess_opt, delta_t, flag] = \
+            self.spm_c.get_evse_setpoint(t_dep, e_req, Pmin_site, Pmax_site, ESS_soc)
         i = 0
-        for vehicle in charging_events:
-            self.control_commands = self.control_commands + [{
+        spmc_commands = []
+        for vehicle in vehicles:
+            spmc_commands = spmc_commands + [{
                 'time': str(t),
                 'taz_id': str(self.taz_id),
                 'siteId': str(self.site_id),
@@ -109,10 +150,53 @@ class RudimentarySPMC:
                 'powerInKW': str(p_evse_opt[i])
             }]
             i = i + 1
-        return self.control_commands
+        return spmc_commands
 
 
-class RideHailSPMC:
+class RudimentarySPMC(AbstractSPMC):
+    # Myungsoo's rudimentary SPM Controller
+    control_commands = []
+    thread = Thread()
+
+    def __init__(self, name, taz_id, site_id):
+        self.spm_c = SPM_Control_Rudimentary(time_step_mins=1, max_power_evse=[], min_power_evse=[])
+        AbstractSPMC.__init__(self, name, taz_id, site_id)
+
+    def run_model(self, t, vehicles, data_for_spmc):
+        # Myungsoo is SPM Controller (NOT RIDE HAIL DEPOT)
+        # 1) SPM Controller takes list(charging_events) (and/or siteId)
+        # 2) SPM Controller returns control_commands
+        # 2.a) example
+        # 3) add control_commands to control_commands_list
+        # control_commands_list = control_commands_list + control_commands
+        self.spm_c.max_power_evse = data_for_spmc.max_power_in_kw
+        self.spm_c.min_power_evse = [0] * len(self.spm_c.max_power_evse)
+        pmin_site_in_kw = 0
+        pmax_site_in_kw = data_for_spmc.site_power_in_kw
+        tdep = [(tt - t) / 60.0 for tt in data_for_spmc.desired_departure_time]
+        # self.log("Optimizing EVSE setpoints by the regular SPM Controller")
+
+        [p_evse_opt, e_evse_opt, delta_t] = self.spm_c.get_evse_setpoint(
+            tdep,
+            data_for_spmc.desired_fuel_level_in_k_wh,
+            pmin_site_in_kw,
+            pmax_site_in_kw
+        )
+        i = 0
+        spmc_commands = []
+        for vehicle in vehicles:
+            spmc_commands = spmc_commands + [{
+                'time': str(t),
+                'taz_id': str(self.taz_id),
+                'siteId': str(self.site_id),
+                'vehicleId': vehicle['vehicleId'],
+                'powerInKW': str(p_evse_opt[i])
+            }]
+            i = i + 1
+        return spmc_commands
+
+
+class RideHailSPMC(AbstractSPMC):
     # Julius Is SPM Controller (IS RIDE HAIL DEPOT)
     control_commands = []
     thread = Thread()
@@ -155,47 +239,9 @@ class RideHailSPMC:
         self.depotController = components.GeminiWrapper.ControlWrapper(
             init_mpc, t_start, timestep_interval, result_directory, ride_hail_depot_id, ch_ba_max_power,
             ch_ba_parking_zone_id, ch_ba_num, path_beam_prediction_file, dtype_predictions, t_max)
-        print2(self.site_prefix_logging + "Initialized!")
+        AbstractSPMC.__init__(self, name, taz_id, site_id)
 
-    def log(self, log_message):
-        logging.info(self.site_prefix_logging + log_message)
-
-    def run_as_thread(self, t, charging_events):
-        self.thread = Thread(target=self.run, args=(t, charging_events))
-        self.thread.start()
-
-    def get_output_from_latest_run(self):
-        self.thread.join()
-        return self.control_commands
-
-    def run(self, t, charging_events):
-        self.control_commands = []
-        # Julius Is SPM Controller (IS RIDE HAIL DEPOT)
-        vehicle_id = []
-        vehicle_type = []
-        primary_fuel_level_in_k_wh = []
-        arrival_time = []
-        desired_departure_time = []
-        desired_fuel_level_in_k_wh = []
-        max_power_in_kw = []  # min [plug, vehicle]
-        battery_capacity_in_k_wh = []  # TODO Julius @ HL can you please add this to the BEAM output?
-
-        for charging_event in charging_events:
-            vehicle_id.append(str(charging_event['vehicleId']))
-            vehicle_type.append(str(charging_event['vehicleType']))
-            # MJ: What is this? Is it the current energy level of each EV? Or battery size?
-            primary_fuel_level_in_k_wh.append(int(charging_event['primaryFuelLevelInJoules']) / 3600000)
-            # MJ: Should be in minutes of day. What is the unit of this?
-            arrival_time.append(int(charging_event['arrivalTime']))
-            # MJ: Should be in minutes of day. What is the unit of this?
-            desired_departure_time.append(int(charging_event['departureTime']))
-            # MJ: I assume that this is remaining energy to be delivered to each EV
-            # and updated each time,right?
-            desired_fuel_level_in_k_wh.append(int(charging_event['desiredFuelLevelInJoules']) / 3600000)
-            # MJ: I assume that this is the EV charging power
-            max_power_in_kw.append(float(charging_event['maxPowerInKW']))
-            # Julius @ HL can you please add this to the BEAM output?
-            battery_capacity_in_k_wh.append(int(charging_event['primaryFuelCapacityInJoule']) / 3600000)
+    def run_model(self, t, vehicles, data_for_spmc):
         # 1) SPM Controller takes list(charging_events) (and/or siteId)
         # 1.a) Maybe a loop with => juliusObject.arrival(vehicle) and/or juliusObject.departure(vehicle)
         # We decide to share vehicle information every interval and when they disappear they plugged out
@@ -217,25 +263,26 @@ class RideHailSPMC:
         # Julius @ HL can you please add the actual time here?
         self.log("Optimizing EVSE setpoints by the ride-hail SPM Controller")
         # TODO uncomment
-        self.depotController.synchronizeVehiclesAtStation(vehicleIdsAtStation=vehicle_id, t_act=t)
+        self.depotController.synchronizeVehiclesAtStation(vehicleIdsAtStation=data_for_spmc.vehicle_id, t_act=t)
         #
         # # VEHICLE ARRIVAL
-        vehicleInDepot = []
+        vehicles_in_depot = []
         for vehicle in self.depotController.ChargingStation.ChBaVehicles:
-            vehicleInDepot.append(vehicle.vehicle_id)
+            vehicles_in_depot.append(vehicle.vehicle_id)
         for vehicle in self.depotController.ChargingStation.Queue:
-            vehicleInDepot.append(vehicle.vehicle_id)
+            vehicles_in_depot.append(vehicle.vehicle_id)
         #
-        for i in range(0, len(vehicle_id)):
-            if vehicle_id[i] not in vehicleInDepot:
-                self.depotController.arrival(VehicleId=vehicle_id[i],
-                                             VehicleType=vehicle_type[i],
-                                             VehicleArrival=arrival_time[i],
-                                             VehicleDesEnd=desired_departure_time[i],
-                                             VehicleEngyInKwh=primary_fuel_level_in_k_wh[i],
-                                             VehicleDesEngyInKwh=desired_fuel_level_in_k_wh[i],
-                                             VehicleMaxEngy=battery_capacity_in_k_wh[i],
-                                             VehicleMaxPower=max_power_in_kw[i], # this doesn't change within one charging session
+        for i in range(0, len(data_for_spmc.vehicle_id)):
+            if data_for_spmc.vehicle_id[i] not in vehicles_in_depot:
+                self.depotController.arrival(VehicleId=data_for_spmc.vehicle_id[i],
+                                             VehicleType=data_for_spmc.vehicle_type[i],
+                                             VehicleArrival=data_for_spmc.arrival_time[i],
+                                             VehicleDesEnd=data_for_spmc.desired_departure_time[i],
+                                             VehicleEngyInKwh=data_for_spmc.primary_fuel_level_in_k_wh[i],
+                                             VehicleDesEngyInKwh=data_for_spmc.desired_fuel_level_in_k_wh[i],
+                                             VehicleMaxEngy=data_for_spmc.battery_capacity_in_k_wh[i],
+                                             VehicleMaxPower=data_for_spmc.max_power_in_kw[i],
+                                             # this doesn't change within one charging session
                                              t_act=int(t))  # Julius @ HL can you provide the actual time
         #
         # # OBTAIN CONTROL COMMANDS
@@ -248,8 +295,9 @@ class RideHailSPMC:
         )
         #
 
+        spmc_commands = []
         for i in range(0, len(vehicles)):
-            self.control_commands = self.control_commands + [{
+            spmc_commands = spmc_commands + [{
                 'time': str(t),
                 'taz_id': str(self.taz_id),
                 'siteId': str(self.site_id),
@@ -257,45 +305,7 @@ class RideHailSPMC:
                 'powerInKW': str(power[i]),
                 'release': str(release[i])
             }]
-        num_commands = len(self.control_commands)
+        num_commands = len(spmc_commands)
         self.log(str(num_commands) + " EVSE setpoints from the ride-hail SPM Controller")
-        return self.control_commands
+        return spmc_commands
 
-
-def print2(to_print):
-    logging.info(to_print)
-    print(to_print)
-
-
-# if len(sys.argv) < 2:
-#     logging.error("infrastructure file is missing")
-
-def create_federate(helics_conf, fed_info, taz_id):
-    fed_name = helics_conf["spmFederatesPrefix"] + taz_id
-
-    # create federate
-    cfed = h.helicsCreateCombinationFederate(fed_name, fed_info)
-    logging.info("Create combination federate " + fed_name)
-
-    # Register a publication of control signals
-    # Power in kW
-    pub = helics_conf["spmSubscription"]
-    h.helicsFederateRegisterTypePublication(cfed, pub, "string", "")
-    logging.info("Registered to publication " + pub)
-
-    # register subscriptions
-    # subscribe to information from TEMPO such that you can map to PyDSS modeled charging stations
-    # Power in kW and Energy in Joules
-    sub = helics_conf["federatesPrefix"] + str(taz_id) + "/" + helics_conf["federatesPublication"]
-    h.helicsFederateRegisterSubscription(cfed, sub, "string")
-    logging.info("Registered to subscription " + sub)
-
-    # subscriptions to PyDSS
-    # The Power is in kW, and to get the energy multiply by 60 seconds (1 minute interval) and convert it to Joules
-    # Then sum it up for the 5 minutes total interval (battery)
-    # While for the load take the average
-    # kW * 60/3600 => X KWH
-    # kWH * 3.6e+6 => Y Joules
-    # Z = Sum(Y_i); i = 1min -> 5min => Storage
-    # Z* = Mean(kW_i) => Load
-    return cfed
