0d1afed50c2d209a6c8538f2314fc0f199e99cd3
Justin Pihony <justin.pihony@gmail.com>
2023-03-23 00:37:43 -0400

    debug

--- src/main/scala/beam/utils/scenario/urbansim/censusblock/entities/InputHousehold.scala
+++ src/main/scala/beam/utils/scenario/urbansim/censusblock/entities/InputHousehold.scala
@@ -14,11 +14,15 @@ case class InputHousehold(
 object InputHousehold extends EntityTransformer[InputHousehold] {
 
   override def transform(rec: Record): InputHousehold = {
-    val householdId = getStringIfNotNull(rec, "household_id")
-    val income = Math.round(getFloatIfNotNull(rec, "income"))
-    val cars = getIntIfNotNull(rec, "cars", Some("auto_ownership"))
-    val blockId = getLongIfNotNull(rec, "block_id")
+    try {
+      val householdId = getStringIfNotNull(rec, "household_id")
+      val income = Math.round(getFloatIfNotNull(rec, "income"))
+      val cars = getIntIfNotNull(rec, "cars", Some("auto_ownership"))
+      val blockId = getLongIfNotNull(rec, "block_id")
 
-    InputHousehold(householdId, income, cars, blockId)
+      InputHousehold(householdId, income, cars, blockId)
+    } catch {
+      case nfe: NumberFormatException => println(s"ERROR: ${rec.getString("income")}"); throw nfe
+    }
   }
 }
