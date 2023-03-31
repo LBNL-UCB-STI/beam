6c0e0e55412ce36775bfe65b94b7f0286aa57ded
HoneyTauOverTwo <31476381+HoneyTauOverTwo@users.noreply.github.com>
2023-03-31 14:31:53 -0300

    Adds more coverage to the tests.
    More changing stations were added that should not be suitable for the BEVs to charge (not enough power rating).
    
    Adds a randomized seed to the test instead of being a constant one. Had to crank up prices to 200 USD on expensive charging stations to consistently have them used less than the free ones (might need to discuss parameter tuning).

--- src/test/scala/beam/agentsim/agents/ElectricVehicleChargingBehaviorTest.scala
+++ src/test/scala/beam/agentsim/agents/ElectricVehicleChargingBehaviorTest.scala
@@ -14,6 +14,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
+import scala.util.Random
+
 class ElectricVehicleChargingBehaviorTest
     extends AnyFlatSpec
     with Matchers
@@ -22,10 +24,13 @@ class ElectricVehicleChargingBehaviorTest
 
   private val filesPath = s"""$${beam.inputDirectory}"/../../test-resources/ElectricVehicleChargingBehaviorTestData"""
 
+  private val seed = Random.nextInt()
+
   private val defaultConfig: Config =
     ConfigFactory
       .parseString(
         s"""
+           |matsim.modules.global.randomSeed = $seed
            |beam.outputs.events.fileOutputFormats = xml
            |beam.physsim.skipPhysSim = true
            |beam.routing.transitOnStreetNetwork = false
@@ -69,7 +74,7 @@ class ElectricVehicleChargingBehaviorTest
     val (matsimConfig, _, _) = runBeamWithConfig(config)
 
     val events = EventReader.fromXmlFile(
-      EventReader.getEventsFilePath(matsimConfig, "events", "xml", 0).getAbsolutePath
+      EventReader.getEventsFilePath(matsimConfig, "events", "xml").getAbsolutePath
     )
 
     val homeTaz = "10"
@@ -117,11 +122,12 @@ class ElectricVehicleChargingBehaviorTest
     val (matsimConfig, _, _) = runBeamWithConfig(config)
 
     val events = EventReader.fromXmlFile(
-      EventReader.getEventsFilePath(matsimConfig, "events", "xml", 0).getAbsolutePath
+      EventReader.getEventsFilePath(matsimConfig, "events", "xml").getAbsolutePath
     )
 
     val centerTAZs = List("15", "16")
     val borderTAZs = List("20", "21", "30", "31")
+    val unsuitableChargersTAZs = List("18", "28", "38")
 
     val centerPluginEvents = filterEvents(
       events,
@@ -133,9 +139,16 @@ class ElectricVehicleChargingBehaviorTest
       ("type", a => a.equals("ChargingPlugInEvent")),
       ("parkingTaz", (a: String) => borderTAZs.contains(a))
     )
+    val unsuitablePluginEvents = filterEvents(
+      events,
+      ("type", a => a.equals("ChargingPlugInEvent")),
+      ("parkingTaz", (a: String) => unsuitableChargersTAZs.contains(a))
+    )
 
     val vehicleIds = findAllElectricVehicles(events).map(id => id.toString)
     vehicleIds.size shouldEqual 50 withClue ", expecting 50 electric vehicles."
+
+    unsuitablePluginEvents.size shouldEqual 0 withClue ", vehicles should not be connecting to chargers without sufficient power rating."
 
     centerPluginEvents.size + borderPluginEvents.size shouldEqual 200 withClue
     ", expecting 4 enroute events for each of the 50 vehicles."
@@ -148,7 +161,7 @@ class ElectricVehicleChargingBehaviorTest
     val config = ConfigFactory
       .parseString(
         s"""
-           |beam.agentsim.taz.parkingFilePath = $filesPath/taz-parking-enroute-only-expensive-bottom-no-center.csv"
+           |beam.agentsim.taz.parkingFilePath = $filesPath/taz-parking-enroute-only-mixed-prices.csv"
            |beam.agentsim.agents.vehicles.vehicleTypesFilePath = $filesPath/vehicleTypes-very-low-capacity.csv"
         """.stripMargin
       )
@@ -158,30 +171,39 @@ class ElectricVehicleChargingBehaviorTest
     val (matsimConfig, _, _) = runBeamWithConfig(config)
 
     val events = EventReader.fromXmlFile(
-      EventReader.getEventsFilePath(matsimConfig, "events", "xml", 0).getAbsolutePath
+      EventReader.getEventsFilePath(matsimConfig, "events", "xml").getAbsolutePath
     )
 
-    val topFreeTAZs = List("20", "21")
-    val bottomExpensiveTAZs = List("30", "31")
+    val freeChargersTAZs = List("20", "31")
+    val expensiveChargersTAZs = List("30", "21")
+    val unsuitableChargersTAZs = List("15", "16", "18", "28", "38")
 
-    val topPluginEvents = filterEvents(
+    val freePluginEvents = filterEvents(
       events,
       ("type", a => a.equals("ChargingPlugInEvent")),
-      ("parkingTaz", (a: String) => topFreeTAZs.contains(a))
+      ("parkingTaz", (a: String) => freeChargersTAZs.contains(a))
     )
-    val bottomPluginEvents = filterEvents(
+    val expensivePluginEvents = filterEvents(
       events,
       ("type", a => a.equals("ChargingPlugInEvent")),
-      ("parkingTaz", (a: String) => bottomExpensiveTAZs.contains(a))
+      ("parkingTaz", (a: String) => expensiveChargersTAZs.contains(a))
+    )
+    val unsuitablePluginEvents = filterEvents(
+      events,
+      ("type", a => a.equals("ChargingPlugInEvent")),
+      ("parkingTaz", (a: String) => unsuitableChargersTAZs.contains(a))
     )
 
     val vehicleIds = findAllElectricVehicles(events).map(id => id.toString)
     vehicleIds.size shouldEqual 50 withClue ", expecting 50 electric vehicles."
 
-    topPluginEvents.size + bottomPluginEvents.size should be >= 200 withClue
+    unsuitablePluginEvents.size shouldEqual 0 withClue
+    ", vehicles should not be connecting to chargers without sufficient power rating."
+
+    freePluginEvents.size + expensivePluginEvents.size should be >= 200 withClue
     ", expecting at least 4 enroute events for each of the 50 vehicles."
 
-    topPluginEvents.size should be > bottomPluginEvents.size withClue
+    freePluginEvents.size should be > expensivePluginEvents.size withClue
     ", agents should prefer top chargers for enrouting (smaller ParkingTicketCost)."
   }
 
