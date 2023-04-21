914e6da3c47d28957e2620c477d555a8a6a9ff80
Nikolay Ilyin <irishwithaxe@gmail.com>
2023-04-21 19:58:34 +0300

    removing docker installation

--- gcp/src/main/bash/cloud-init.sh
+++ gcp/src/main/bash/cloud-init.sh
@@ -133,18 +133,18 @@ if [ "${RUN_JUPYTER,,}" = "true" ]; then
   echo "Preparing to run Jupyter - installing docker"
   set -x
 
-  sudo apt-get remove docker docker-engine docker.io containerd runc -y
-  sudo apt-get update -y
-  sudo apt-get install ca-certificates curl gnupg -y
-  sudo install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update -y
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  #  sudo apt-get remove docker docker-engine docker.io containerd runc -y
+  #  sudo apt-get update -y
+  #  sudo apt-get install ca-certificates curl gnupg -y
+  #  sudo install -m 0755 -d /etc/apt/keyrings
+  #  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  #  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  #  echo \
+  #    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  #    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  #    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  #  sudo apt-get update -y
+  #  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
   echo "Checking docker is working"
   sudo docker run hello-world
