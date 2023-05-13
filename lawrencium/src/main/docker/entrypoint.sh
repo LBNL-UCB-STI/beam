c4d14538368a48c5847f4cbf74aac858e198a037
Nikolay Ilyin <irishwithaxe@gmail.com>
2023-05-13 21:20:10 +0300

    to be able to pull or not to pull

--- lawrencium/src/main/docker/entrypoint.sh
+++ lawrencium/src/main/docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# TODO use more universal approach for instance Id and Type
 INSTANCE_ID=$SLURMD_NODENAME
 INSTANCE_TYPE=$SLURM_JOB_PARTITION
+
 HOST_NAME=$HOSTNAME
 WEB_BROWSER="??" # i.e. "http://$HOST_NAME:8000"
 REGION=""
@@ -9,9 +11,18 @@ SHUTDOWN_WAIT=0
 
 SIMULATION_OUTPUT_LINK="TODO" ## the link to be able to view progress of simulation
 
-PROFILER="??"
-SIGOPT_CLIENT_ID="??"
-SIGOPT_DEV_ID="??"
+PROFILER="TODO"
+
+# TODO maybe completely remove these
+SIGOPT_CLIENT_ID=""
+SIGOPT_DEV_ID=""
+
+if [[ -z "$PULL_CODE" ]]; then PULL_CODE=true; fi
+if [[ -z "$PULL_DATA" ]]; then PULL_DATA=true; fi
+if [[ -z "$SEND_NOTIFICATION" ]]; then SEND_NOTIFICATION=true; fi
+if [[ -z "$MAX_RAM" ]]; then MAX_RAM="8"; fi
+
+echo "Started at $(date "+%Y-%m-%d-%H:%M:%S")"
 
 env
 
@@ -27,7 +38,7 @@ function send_slack_notification() {
   printf "\nSending the following json to slack:"
   echo "$json_data"
   echo " "
-  curl -X POST -H 'Content-type:application/json' --data "$json_data" $SLACK_HOOK_WITH_TOKEN
+  curl -X POST -H 'Content-type:application/json' --data "$json_data" "$SLACK_HOOK_WITH_TOKEN"
 }
 
 function send_json_to_spreadsheet() {
@@ -38,103 +49,124 @@ function send_json_to_spreadsheet() {
   curl -X POST -H 'Content-type:application/json' --data "$json_data" "https://ca4ircx74d.execute-api.us-east-2.amazonaws.com/production/spreadsheet"
 }
 
+# monitoring CPU | RAM usage
 /app/write_cpu_ram_usage.sh > /app/sources/cpu_ram_usage.csv &
 
 PATH_TO_PROJECT_PARENT="/app/sources"
 
-if [[ -z "$GRADLE_CACHE_PATH" ]]; then
-  GRADLE_CACHE_PATH="$PATH_TO_PROJECT_PARENT/gradle_cache"
-  echo "GRADLE_CACHE_PATH is not set, creating a directory by path '$GRADLE_CACHE_PATH'"
-  mkdir -p "$GRADLE_CACHE_PATH"
-fi
-
-echo "Gradle cache will be at '$GRADLE_CACHE_PATH'"
-
 cd "$PATH_TO_PROJECT_PARENT" || echo "ERROR: The path '$PATH_TO_PROJECT_PARENT' is not available"
-echo "Cloning BEAM into $PWD/beam"
-git clone --single-branch --branch $BEAM_BRANCH_NAME https://github.com/LBNL-UCB-STI/beam.git
 
-cd beam || echo "ERROR: The path 'beam' is not available"
+# either pull the code or use the code from mounted folder
+if [ "$PULL_CODE" = true ]; then
+  BEAM_NAME="beam"
+  echo "Pulling the code from github (PULL_CODE set to '$PULL_CODE'), cloning BEAM into $(pwd)/$BEAM_NAME"
+  git clone --single-branch --branch "$BEAM_BRANCH_NAME" https://github.com/LBNL-UCB-STI/beam.git "$BEAM_NAME"
+  cd "$BEAM_NAME" || echo "ERROR: The dir '$BEAM_NAME' is not available"
 
-if [[ $BEAM_COMMIT_SHA ]]; then
-  echo "Resetting the branch '$BEAM_BRANCH_NAME' to commit '$BEAM_COMMIT_SHA'"
-  git reset --hard $BEAM_COMMIT_SHA;
+  if [[ $BEAM_COMMIT_SHA ]]; then
+    echo "Resetting the branch '$BEAM_BRANCH_NAME' to commit '$BEAM_COMMIT_SHA'"
+    git reset --hard "$BEAM_COMMIT_SHA";
+  else
+    BEAM_COMMIT_SHA=$(git log -1 --pretty=format:%H)
+    echo "Using the latest commit in the branch '$BEAM_BRANCH_NAME' - '$BEAM_COMMIT_SHA'"
+  fi
 else
+  echo "Pulling the code is disabled (PULL_CODE set to '$PULL_CODE'), using code mounted to '$PATH_TO_PROJECT_PARENT'."
   BEAM_COMMIT_SHA=$(git log -1 --pretty=format:%H)
+  BEAM_BRANCH_NAME=$(git symbolic-ref --short HEAD)
   echo "Using the latest commit in the branch '$BEAM_BRANCH_NAME' - '$BEAM_COMMIT_SHA'"
 fi
 
-production_data_submodules=$(git submodule | awk '{ print $2 }')
-for i in $production_data_submodules;
-  do
-    if [[ $BEAM_CONFIG  == $i* ]];
-    then
-      echo "Loading remote production data for $i"
-      git config submodule.$i.branch $BEAM_DATA_BRANCH_NAME
-      git submodule update --init --remote $i
-      cd $i || echo "ERROR: The path '$i' is not available"
-      if [[ $BEAM_DATA_COMMIT_SHA ]]; then
-        echo "Checking out the data commit '$BEAM_DATA_COMMIT_SHA'"
-        git checkout "$BEAM_DATA_COMMIT_SHA"
-      else
-        BEAM_DATA_COMMIT_SHA=$(git log -1 --pretty=format:%H)
-        echo "Latest commit is '$BEAM_DATA_COMMIT_SHA'"
-      fi
-      cd - || echo "ERROR: Can't move to the previous location"
-    fi;
-  done
+echo "Working from $(pwd)"
 
-git lfs pull
+if [ "$PULL_DATA" = true ]; then
+  echo "Pulling the data from github (PULL_DATA set to '$PULL_DATA')."
+  production_data_submodules=$(git submodule | awk '{ print $2 }')
+  for i in $production_data_submodules;
+    do
+      if [[ $BEAM_CONFIG  == $i* ]];
+      then
+        echo "Loading remote production data for $i"
+        git config "submodule.$i.branch" "$BEAM_DATA_BRANCH_NAME"
+        git submodule update --init --remote "$i"
+        cd "$i" || echo "ERROR: The path '$i' is not available"
+        if [[ $BEAM_DATA_COMMIT_SHA ]]; then
+          echo "Checking out the data commit '$BEAM_DATA_COMMIT_SHA'"
+          git checkout "$BEAM_DATA_COMMIT_SHA"
+        else
+          BEAM_DATA_COMMIT_SHA=$(git log -1 --pretty=format:%H)
+          echo "Latest commit is '$BEAM_DATA_COMMIT_SHA'"
+        fi
+        cd - || echo "ERROR: Can't move to the previous location"
+      fi;
+    done
 
-send_slack_notification "Run Started
-  Run Name $TITLED
-  Instance ID $INSTANCE_ID
-  Instance type $INSTANCE_TYPE
-  Host name $HOST_NAME
-  Web browser $WEB_BROWSER
-  Branch $BEAM_BRANCH_NAME
-  Commit $BEAM_COMMIT_SHA"
+  echo "Doing lfs pull"
+  git lfs pull
+else
+  echo "Pulling the data from github is disabled (PULL_DATA set to '$PULL_DATA')."
+fi
 
-# shellcheck disable=SC2089
-send_json_to_spreadsheet "\"status\":\"Run Started\",
-  \"name\":\"$TITLED\",
-  \"instance_id\":\"$INSTANCE_ID\",
-  \"instance_type\":\"$INSTANCE_TYPE\",
-  \"host_name\":\"$HOST_NAME\",
-  \"browser\":\"$WEB_BROWSER\",
-  \"branch\":\"$BEAM_BRANCH_NAME\",
-  \"commit\":\"$BEAM_COMMIT_SHA\",
-  \"data_branch\":\"$BEAM_DATA_BRANCH_NAME\",
-  \"data_commit\":\"$BEAM_DATA_COMMIT_SHA\",
-  \"region\":\"$REGION\",
-  \"batch\":\"\",
-  \"s3_link\":\"$SIMULATION_OUTPUT_LINK\",
-  \"max_ram\":\"$MAX_RAM\",
-  \"profiler_type\":\"$PROFILER\",
-  \"config_file\":\"$BEAM_CONFIG\",
-  \"sigopt_client_id\":\"$SIGOPT_CLIENT_ID\",
-  \"sigopt_dev_id\":\"$SIGOPT_DEV_ID\""
+if [ "$SEND_NOTIFICATION" = true ]; then
+  send_slack_notification "Run Started
+    Run Name $TITLED
+    Instance ID $INSTANCE_ID
+    Instance type $INSTANCE_TYPE
+    Host name $HOST_NAME
+    Web browser $WEB_BROWSER
+    Branch $BEAM_BRANCH_NAME
+    Commit $BEAM_COMMIT_SHA"
 
+  send_json_to_spreadsheet "\"status\":\"Run Started\",
+    \"name\":\"$TITLED\",
+    \"instance_id\":\"$INSTANCE_ID\",
+    \"instance_type\":\"$INSTANCE_TYPE\",
+    \"host_name\":\"$HOST_NAME\",
+    \"browser\":\"$WEB_BROWSER\",
+    \"branch\":\"$BEAM_BRANCH_NAME\",
+    \"commit\":\"$BEAM_COMMIT_SHA\",
+    \"data_branch\":\"$BEAM_DATA_BRANCH_NAME\",
+    \"data_commit\":\"$BEAM_DATA_COMMIT_SHA\",
+    \"region\":\"$REGION\",
+    \"batch\":\"\",
+    \"s3_link\":\"$SIMULATION_OUTPUT_LINK\",
+    \"max_ram\":\"$MAX_RAM\",
+    \"profiler_type\":\"$PROFILER\",
+    \"config_file\":\"$BEAM_CONFIG\",
+    \"sigopt_client_id\":\"$SIGOPT_CLIENT_ID\",
+    \"sigopt_dev_id\":\"$SIGOPT_DEV_ID\""
+else
+  echo "Sending notifications is disabled (SEND_NOTIFICATION set to '$SEND_NOTIFICATION')."
+fi
+
+# where the gradle cache should be
+if [[ -z "$GRADLE_CACHE_PATH" ]]; then
+  GRADLE_CACHE_PATH="$(pwd)/.gradle"
+  echo "GRADLE_CACHE_PATH is not set, creating and using directory by path '$GRADLE_CACHE_PATH'"
+  mkdir -p "$GRADLE_CACHE_PATH"
+else
+  echo "GRADLE_CACHE_PATH set, using directory by path '$GRADLE_CACHE_PATH'"
+fi
 
 ## we shouldn't use the gradle daemon on NERSC, it seems that it's somehow shared within different nodes
 ## and all the subsequent runs have output dir somewhere else.
 ./gradlew --no-daemon --gradle-user-home="$GRADLE_CACHE_PATH" clean :run -PappArgs="['--config', '$BEAM_CONFIG']" -PmaxRAM="$MAX_RAM"
 
 ## Calculate the final status of simulation
-log_file="$(find "$PATH_TO_PROJECT_PARENT"/beam/output -maxdepth 2 -mindepth 2 -type d -print -quit)/beamLog.out"
+log_file="$(find "$(pwd)/output" -maxdepth 2 -mindepth 2 -type d -print -quit)/beamLog.out"
 if [[ ! -f $log_file ]]; then
     echo "Unable to locate the beamLog.out file"
     final_status="Unable to start"
 else
-  last_line=$(tail $log_file -n 1)
+  last_line=$(tail "$log_file" -n 1)
   if [[ $last_line == *"Exiting BEAM"* ]]; then
       final_status="Run Completed"
   else
       final_status="Run Failed"
   fi
 fi
-echo "The final status of simulation is '$final_status'"
 
+echo "The final status of simulation is '$final_status'"
 
 ## TODO calculate health metrics
 health_metrics_for_slack_notification="TODO"
@@ -144,12 +176,11 @@ error="TODO"
 warning="TODO"
 
 
-if [ "$S3_PUBLISH" == true ]
-then
+if [ "$S3_PUBLISH" = true ]; then
   sleep 10s
   finalPath=""
   for file in output/*; do
-     for path2 in $file/*; do
+     for path2 in "$file"/*; do
        finalPath="$path2";
      done;
   done;
@@ -165,45 +196,48 @@ then
   s3output_url="https://s3.$S3_REGION.amazonaws.com/beam-outputs/index.html#$finalPath"
   echo "Uploaded to $s3output_url"
 else
-  echo "S3 publishing disabled"
+  echo "S3 publishing is disabled (S3_PUBLISH set to '$S3_PUBLISH')."
 fi
 
+if [ "$SEND_NOTIFICATION" = true ]; then
+  send_slack_notification "Run Completed
+    Run Name $TITLED
+    Instance ID $INSTANCE_ID
+    Instance type $INSTANCE_TYPE
+    Host name $HOST_NAME
+    Web browser $WEB_BROWSER
+    Branch $BEAM_BRANCH_NAME
+    Commit $BEAM_COMMIT_SHA
+    Health Metrics $health_metrics_for_slack_notification
+    S3 output url $s3output_url
+    Shutdown in $SHUTDOWN_WAIT minutes"
 
-send_slack_notification "Run Completed
-  Run Name $TITLED
-  Instance ID $INSTANCE_ID
-  Instance type $INSTANCE_TYPE
-  Host name $HOST_NAME
-  Web browser $WEB_BROWSER
-  Branch $BEAM_BRANCH_NAME
-  Commit $BEAM_COMMIT_SHA
-  Health Metrics $health_metrics_for_slack_notification
-  S3 output url $s3output_url
-  Shutdown in $SHUTDOWN_WAIT minutes"
+  # shellcheck disable=SC2089
+  send_json_to_spreadsheet "\"status\":\"$final_status\",
+    \"name\":\"$TITLED\",
+    \"instance_id\":\"$INSTANCE_ID\",
+    \"instance_type\":\"$INSTANCE_TYPE\",
+    \"host_name\":\"$HOST_NAME\",
+    \"browser\":\"$WEB_BROWSER\",
+    \"branch\":\"$BEAM_BRANCH_NAME\",
+    \"commit\":\"$BEAM_COMMIT_SHA\",
+    \"data_branch\":\"$BEAM_DATA_BRANCH_NAME\",
+    \"data_commit\":\"$BEAM_DATA_COMMIT_SHA\",
+    \"region\":\"$REGION\",
+    \"batch\":\"\",
+    \"s3_link\":\"$SIMULATION_OUTPUT_LINK\",
+    \"max_ram\":\"$MAX_RAM\",
+    \"profiler_type\":\"$PROFILER\",
+    \"stacktrace\":\"$stacktrace\",
+    \"died_actors\":\"$died_actors\",
+    \"error\":\"$error\",
+    \"warning\":\"$warning\",
+    \"config_file\":\"$BEAM_CONFIG\",
+    \"sigopt_client_id\":\"$SIGOPT_CLIENT_ID\",
+    \"sigopt_dev_id\":\"$SIGOPT_DEV_ID\""
+else
+  echo "Sending notifications is disabled (SEND_NOTIFICATION set to '$SEND_NOTIFICATION')."
+fi
 
-
-# shellcheck disable=SC2089
-send_json_to_spreadsheet "\"status\":\"$final_status\",
-  \"name\":\"$TITLED\",
-  \"instance_id\":\"$INSTANCE_ID\",
-  \"instance_type\":\"$INSTANCE_TYPE\",
-  \"host_name\":\"$HOST_NAME\",
-  \"browser\":\"$WEB_BROWSER\",
-  \"branch\":\"$BEAM_BRANCH_NAME\",
-  \"commit\":\"$BEAM_COMMIT_SHA\",
-  \"data_branch\":\"$BEAM_DATA_BRANCH_NAME\",
-  \"data_commit\":\"$BEAM_DATA_COMMIT_SHA\",
-  \"region\":\"$REGION\",
-  \"batch\":\"\",
-  \"s3_link\":\"$SIMULATION_OUTPUT_LINK\",
-  \"max_ram\":\"$MAX_RAM\",
-  \"profiler_type\":\"$PROFILER\",
-  \"stacktrace\":\"$stacktrace\",
-  \"died_actors\":\"$died_actors\",
-  \"error\":\"$error\",
-  \"warning\":\"$warning\",
-  \"config_file\":\"$BEAM_CONFIG\",
-  \"sigopt_client_id\":\"$SIGOPT_CLIENT_ID\",
-  \"sigopt_dev_id\":\"$SIGOPT_DEV_ID\""
-
-printf '\n script completed.'
+echo ""
+echo "Completed at $(date "+%Y-%m-%d-%H:%M:%S")"
