3a40c9514538bd2cf8a9b3a887787bde2a982c85
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2023-05-24 13:15:20 -0700

    update event processing script

--- src/main/python/freight/events_processing.py
+++ src/main/python/freight/events_processing.py
@@ -1,30 +1,43 @@
 import pandas as pd
 import os
+import sys
 
 city = "austin"
 # city = "sfbay"
-scenario = "2030_low"
+scenario = "price-sensitivity"
 # batch = "Oct30/"
-batch = ""
+batch = "2050_HOP_highp4/"
 iteration = 0
 prefix = ""
 filename = prefix+str(iteration)+'.events.csv.gz'
-
 work_directory = '~/Workspace/Data/FREIGHT/'+city+'/beam/runs/'+scenario+'/'+batch
-
 full_filename = os.path.expanduser(work_directory + filename)
+
+if len(sys.argv) >= 2:
+    full_filename = str(sys.argv[1])
+
+def print2(msg):
+    with open(full_filename + ".out", 'w') as f:
+        print(msg)
+        print(msg, file=f)
+
+dirname = os.path.dirname(full_filename)
+basename = os.path.basename(full_filename)
+
 compression = None
-if full_filename.endswith(".gz"):
+if basename.endswith(".gz"):
     compression = 'gzip'
-print("reading: " + full_filename)
+
+
+print2("reading: " + full_filename)
 data = pd.read_csv(full_filename, sep=",", index_col=None, header=0, compression=compression)
-print(data.type.unique())
+print2(data.type.unique())
 data_filtered = data.loc[data.type.isin(["PathTraversal", "actstart", "actend"])]
-print(data_filtered.type.unique())
+print2(data_filtered.type.unique())
 data_filtered = data_filtered.loc[data_filtered.vehicle.str.startswith("freight", na=True)]
-print(data_filtered.type.unique())
+print2(data_filtered.type.unique())
 data_filtered2 = data_filtered.loc[data_filtered.actType.isin(["Warehouse", "Unloading", "Loading"]) | data_filtered.actType.isnull()]
-print(data_filtered.type.unique())
+print2(data_filtered.type.unique())
 # data_filtered2 = data_filtered[
 #     ["time","type","vehicleType","vehicle","secondaryFuelLevel",
 #      "primaryFuelLevel","driver","mode","seatingCapacity","startX",
@@ -33,5 +46,6 @@ print(data_filtered.type.unique())
 #      "numPassengers", "length", "primaryFuel", "actType", "fuel", "person",
 #      "locationY", "locationX", "duration", "chargingPointType", "parkingType", "parkingTaz"]
 # ]
+print2("writing to " + work_directory + "filtered." + filename)
 data_filtered2.to_csv(work_directory + "filtered." + filename)
-print("END")
+print2("END")
