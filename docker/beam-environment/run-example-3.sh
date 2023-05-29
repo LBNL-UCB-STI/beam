5b2c692426e58ac05028de3303aad644f8774846
Nikolay Ilyin <irishwithaxe@gmail.com>
2023-05-29 20:00:18 +0300

    do not create out folder + example of running without data folder

--- docker/beam-environment/run-example-3.sh
+++ docker/beam-environment/run-example-3.sh
@@ -3,6 +3,8 @@
 #
 # This script will start beam-environment docker image,
 #   it will use the code from mounted folder, the mounted folder should point to BEAM root,
+#   it will use the data from mounted data folder if selected config is in there,
+#     otherwise it will try to use data from code folder (i.e. from test/input folder)
 #   it won't send notifications and won't publish output to s3,
 #   the selected config - beamville, will be used from the pulled code.
 #
@@ -12,6 +14,7 @@
 MAX_RAM="16"
 BEAM_CONFIG="test/input/beamville/beam.conf"
 EXISTING_CODE_PATH="/mnt/data/work/beam/beam"
+EXISTING_DATA_PATH="/mnt/data/work/beam/beam-production/test"
 
 docker run \
   --network host \
@@ -22,4 +25,5 @@ docker run \
   --env S3_PUBLISH=false \
   --env SEND_NOTIFICATION=false \
   --mount source="$EXISTING_CODE_PATH",destination=/app/sources,type=bind \
+  --mount source="$EXISTING_DATA_PATH",destination=/app/data,type=bind \
   "beammodel/beam-environment:latest"
