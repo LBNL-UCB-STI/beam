19381abcb871235afab2f8f43794496c5c08693b
Justin Pihony <justin.pihony@gmail.com>
2023-07-25 12:50:51 -0400

    forgot one java class

--- src/main/java/beam/utils/gtfs/MATSim2GTFS.java
+++ src/main/java/beam/utils/gtfs/MATSim2GTFS.java
@@ -143,8 +143,8 @@ public class MATSim2GTFS {
                         gtfsStopTime.trip_id = gtfsTrip.trip_id;
                         gtfsStopTime.stop_sequence = stopWithinTrip;
                         gtfsStopTime.stop_id = trs.getStopFacility().getId().toString();
-                        gtfsStopTime.arrival_time = (int) (firstDepartureTime + trs.getArrivalOffset().seconds());
-                        gtfsStopTime.departure_time = (int) (firstDepartureTime + trs.getDepartureOffset().seconds());
+                        gtfsStopTime.arrival_time = (int) (firstDepartureTime + trs.getArrivalOffset().orElse(Double.NEGATIVE_INFINITY));
+                        gtfsStopTime.departure_time = (int) (firstDepartureTime + trs.getDepartureOffset().orElse(Double.NEGATIVE_INFINITY));
                         gtfsFeed.stop_times.put(
                                 new Fun.Tuple2(gtfsStopTime.trip_id, gtfsStopTime.stop_sequence),
                                 gtfsStopTime);
