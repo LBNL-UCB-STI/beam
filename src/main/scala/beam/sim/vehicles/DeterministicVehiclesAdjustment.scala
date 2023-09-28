eae036470db86338ecd87e704861e7c9a06807be
Zach Needell <zneedell@gmail.com>
2023-09-28 11:59:50 -0700

    better handling of emergency household vehicle generation

--- src/main/scala/beam/sim/vehicles/DeterministicVehiclesAdjustment.scala
+++ src/main/scala/beam/sim/vehicles/DeterministicVehiclesAdjustment.scala
@@ -13,12 +13,28 @@ case class DeterministicVehiclesAdjustment(
   householdIdToVehicleIds: Map[HouseholdId, Iterable[VehicleInfo]]
 ) extends VehiclesAdjustment {
 
+  private lazy val totalNumberOfVehicles =
+    beamScenario.privateVehicles.values.groupBy(_.beamVehicleType.vehicleCategory).map(x => x._1 -> x._2.toList.length)
+
   override def sampleVehicleTypes(
     numVehicles: Int,
     vehicleCategory: VehicleCategory,
     realDistribution: UniformRealDistribution
   ): List[BeamVehicleType] = {
-    throw new NoSuchMethodError("Can't use deterministic vehicle sample for non-household fleets")
+    (0 until numVehicles).map(_ => sampleAnyVehicle(vehicleCategory, realDistribution)).toList
+  }
+
+  private def sampleAnyVehicle(
+    vehicleCategory: VehicleCategory,
+    realDistribution: UniformRealDistribution
+  ): BeamVehicleType = {
+    val indexToTake =
+      (realDistribution.sample() * totalNumberOfVehicles.getOrElse(vehicleCategory, 0)).floor.toInt
+    beamScenario.privateVehicles.values
+      .filter(_.beamVehicleType.vehicleCategory == vehicleCategory)
+      .drop(indexToTake)
+      .head
+      .beamVehicleType
   }
 
   override def sampleVehicleTypesForHousehold(
@@ -31,26 +47,35 @@ case class DeterministicVehiclesAdjustment(
     realDistribution: UniformRealDistribution,
     householdId: Option[HouseholdId]
   ): List[BeamVehicleType] = {
-    householdId match {
-      case Some(hhId) =>
-        val vehiclesToSampleFrom = householdIdToVehicleIds
-          .getOrElse(hhId, Iterable.empty[VehicleInfo])
-          .flatMap(vtid => beamScenario.vehicleTypes.get(Id.create(vtid.vehicleTypeId, classOf[BeamVehicleType])))
-          .toList
-        if (numVehicles == vehiclesToSampleFrom.length) {
-          vehiclesToSampleFrom
-        } else {
-          // TODO -- don't sample with replacement for numVehicles < the correct number of vehicles
-          realDistribution
-            .sample(numVehicles)
-            .flatMap(x =>
-              beamScenario.vehicleTypes.get(
-                Id.create(vehiclesToSampleFrom((x * numVehicles).floor.toInt).id, classOf[BeamVehicleType])
-              )
-            )
+    // In both of these cases it would be better to have an integer distribution rather than real, but this works fine
+    if (numVehicles == 0) {
+      List.empty[BeamVehicleType]
+    } else {
+      householdId match {
+        case Some(hhId) =>
+          val vehiclesToSampleFrom = householdIdToVehicleIds
+            .getOrElse(hhId, Iterable.empty[VehicleInfo])
+            .flatMap(vtid => beamScenario.vehicleTypes.get(Id.create(vtid.vehicleTypeId, classOf[BeamVehicleType])))
+            .filter(_.vehicleCategory == vehicleCategory)
             .toList
-        }
-      case _ => List.empty[BeamVehicleType]
+          if (numVehicles == vehiclesToSampleFrom.length) {
+            vehiclesToSampleFrom
+          } else if (vehiclesToSampleFrom.isEmpty) {
+            (0 until numVehicles).map(_ => sampleAnyVehicle(vehicleCategory, realDistribution)).toList
+          } else {
+            // TODO -- don't sample with replacement for numVehicles < the correct number of vehicles
+            realDistribution
+              .sample(numVehicles)
+              .flatMap(x =>
+                beamScenario.vehicleTypes.get(
+                  Id.create(vehiclesToSampleFrom((x * numVehicles).floor.toInt).id, classOf[BeamVehicleType])
+                )
+              )
+              .toList
+          }
+        case _ => (0 until numVehicles).map(_ => sampleAnyVehicle(vehicleCategory, realDistribution)).toList
+      }
     }
+
   }
 }
