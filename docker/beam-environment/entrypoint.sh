389b55bd28508fc66fc84384afec546116442f66
Nikolay Ilyin <irishwithaxe@gmail.com>
2023-12-01 20:42:11 +0300

    beam-environment image based off ubuntu

--- docker/beam-environment/entrypoint.sh
+++ docker/beam-environment/entrypoint.sh
@@ -8,7 +8,7 @@ echo "Recognized command '$command'"
 print_help() {
   echo ""
   echo "First argument provided will be used as a command, the rest arguments will be used as parameters for the command."
-  echo "Available commands: 'gradle' 'git' 'bash'."
+  echo "Available commands: 'gradle' 'git' 'bash' 'ssh'."
   echo ""
 }
 
@@ -23,6 +23,7 @@ case "$command" in
 "gradle") /app/execute-gradle.sh "${@:2}" ;;
 "git") /app/execute-git.sh "${@:2}" ;;
 "sh" | "bash" | "/bin/bash" | "/bash") /bin/bash "${@:2}" ;;
+"ssh") /usr/sbin/sshd -D ;;
 "slack") /app/execute-message-slack.sh "${@:2}" ;;
 "help" | "?") print_help ;;
 *) print_unexpected  ;;
