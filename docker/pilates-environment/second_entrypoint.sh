3d44c197e802fa3f603d0dfe793b3f9240645fb3
Nikolay Ilyin <irishwithaxe@gmail.com>
2024-04-23 16:52:49 +0300

    refactoring

--- docker/pilates-environment/second_entrypoint.sh
+++ docker/pilates-environment/second_entrypoint.sh
@@ -5,6 +5,9 @@ if [ -n "$command" ]; then
   echo "Recognized command '$command'"
 fi
 
+echo "starting .. "
+printenv >>/root/all_env.txt
+
 print_help() {
   echo ""
   echo "First argument provided will be used as a command, the rest arguments will be used as parameters for the command."
@@ -22,12 +25,8 @@ add_to_root_bashrc() {
   echo "$1" >>/root/.bashrc
 }
 
-activate_micromamba_and_run_ssh() {
-  echo "starting .. "
-  printenv >>/root/all_env.txt
-
-  # ## in order to change terminal prompt string (but let's avoid it for now)
-  # add_to_root_bashrc "export PS1=\"${debian_chroot:+($debian_chroot)}\u@\h:\W\$ \""
+prepare_pilates() {
+  echo "preparing container to run pilates..."
 
   if [ -z "$PILATES_FOLDER" ]; then PILATES_FOLDER="/app/pilates"; fi
   if [ -z "$SHARED_FOLDER" ]; then SHARED_FOLDER="/app/shared"; fi
@@ -38,32 +37,44 @@ activate_micromamba_and_run_ssh() {
   add_to_root_profile "cd \$PILATES"
   add_to_root_profile "micromamba activate"
   add_to_root_profile "echo \"\$(python --version) is available from miniconda.\"; echo \"\""
-  add_to_root_profile "echo \"PILATES is copied in \$PILATES (path is stored in PILATES environment variable)\""
-  add_to_root_profile "echo \"Please do not move PILATES from its original folder, some of PILATES parts will break if run from different place.\"; echo \"\""
+  add_to_root_profile "echo \"PILATES is in \$PILATES (path is stored in PILATES environment variable)\""
+  add_to_root_profile "echo \"Please do not move PILATES from its original folder, docker calls inside PILATES will break if run from different place.\"; echo \"\""
   add_to_root_profile "echo \"Shared folder is mounted to \$SHARED (path is stored in SHARED environment variable)\"; echo \"\""
-  add_to_root_profile "echo \"In order to stop this container you could do 'docker ps' and then use docker stop with id of this container.\""
-  add_to_root_profile "echo \"Leaving with 'exit' will not stop this container, you could later just connect with ssh again.\"; echo \"\""
+  add_to_root_profile "echo \"In order to stop this container you could do 'docker ps' and then use 'docker stop <ID>' with id of this container.\""
+  add_to_root_profile "echo \"Otherwise you could stop or cancel the job.\"; echo \"\""
 
   micromamba shell init --shell bash --root-prefix=/opt/conda
   sudo service docker start
   echo "Docker started"
+}
 
-  if [ -n "$RUN_PILATES" ]; then
-    cd $PILATES_FOLDER || echo "Pilates folder ($PILATES_FOLDER) is not available!"
+run_pilates_async() {
+  echo "running pilates..."
 
-    echo "Current pwd: $(pwd)"
-    echo "Files available: $(ls -lah)"
+  cd $PILATES_FOLDER || echo "Pilates folder ($PILATES_FOLDER) is not available!"
 
-    echo "Running pilates: 'python3 run.py'"
-    python3 run.py 2>&1 | tee -a "pilates_execution_$(date +'%Y%m%d_%H%M%S').log" &
-  fi
+  echo "Current pwd: $(pwd)"
+  echo "Files available: $(ls -lah)"
 
+  echo "Running pilates: 'python3 run.py'"
+  python3 run.py 2>&1 | tee -a "pilates_execution_$(date +'%Y%m%d_%H%M%S').log" &
+}
+
+run_sshd() {
   /usr/sbin/sshd -D
 }
 
+if [ -n "$RUN_PILATES" ] || [ -n "$PREPARE_PILATES" ]; then
+  prepare_pilates
+fi
+
+if [ -n "$RUN_PILATES" ]; then
+  run_pilates_async
+fi
+
 case "$command" in
 "sh" | "bash" | "/bin/bash" | "/bash") /bin/bash "${@:2}" ;;
-"ssh") activate_micromamba_and_run_ssh ;;
+"ssh") run_sshd ;;
 "help" | "?") print_help ;;
 *) print_help ;;
 esac
