58b99e7ae3d7d2e44284558ac44f252aa6c49b08
zneedell <zneedell@gmail.com>
2025-02-28 13:24:27 -0800

    better check for whether a person has departed before giving up their tour personal vehicle

--- src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
@@ -1607,14 +1607,15 @@ trait ChoosesMode {
 
   private def gotoChoosingModeWithoutPredefinedMode(choosesModeData: ChoosesModeData) = {
     // TODO: Check modes for subsequent trips here
-    val onFirstTrip = isFirstTripWithinTour(currentActivity(choosesModeData.personData))
+    val onFirstTrip =
+      isFirstTripWithinTour(currentActivity(choosesModeData.personData)) && !choosesModeData.personData.hasDeparted
     val outcomeTourMode = if (onFirstTrip) { None }
     else { Some(WALK_BASED) }
     val newTourVehicle = choosesModeData.personData.currentTourPersonalVehicle match {
       case Some(id) if beamVehicles.contains(id) =>
         if (
           (choosesModeData.personData.currentTourMode.contains(WALK_BASED) && !onFirstTrip) ||
-          choosesModeData.isWithinTripReplanning
+          choosesModeData.personData.hasDeparted
         ) {
           /*
            * This code block only runs when someone needs to re-plan and re-do mode choice.
