2b316d2184672d4fb7d24130c718901abf892164
zneedell <zneedell@gmail.com>
2025-03-21 10:56:08 -0700

    merge fixes

--- src/main/scala/beam/sim/config/BeamConfig.scala
+++ src/main/scala/beam/sim/config/BeamConfig.scala
@@ -1873,7 +1873,7 @@ object BeamConfig {
                 c: com.typesafe.config.Config
               ): BeamConfig.Beam.Agentsim.Agents.Vehicles.SharedFleets$Elm.FixedNonReservingFleetByTaz = {
                 BeamConfig.Beam.Agentsim.Agents.Vehicles.SharedFleets$Elm.FixedNonReservingFleetByTaz(
-                  fleetSize = 60,
+                  fleetSize = if (c.hasPathOrNull("fleetSize")) c.getInt("fleetSize") else 10,
                   maxWalkingDistance =
                     if (c.hasPathOrNull("maxWalkingDistance")) c.getInt("maxWalkingDistance") else 500,
                   vehicleTypeId =
