fa4833b1142aa31de5aa8b04e7b1833186fe4c58
zneedell <zneedell@gmail.com>
2025-08-18 09:34:23 -0700

    parquet writing option (still need to fix test and update beam-template)

--- src/main/scala/beam/router/LinkTravelTimeContainer.scala
+++ src/main/scala/beam/router/LinkTravelTimeContainer.scala
@@ -8,6 +8,11 @@ import org.matsim.api.core.v01.network.Link
 import org.matsim.api.core.v01.population.Person
 import org.matsim.core.router.util.TravelTime
 import org.matsim.vehicles.Vehicle
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.parquet.avro.AvroParquetReader
+import org.apache.avro.generic.GenericRecord
+import org.apache.parquet.hadoop.util.HadoopInputFile
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
@@ -25,15 +30,37 @@ class LinkTravelTimeContainer(fileName: String, timeBinSizeInSeconds: Int, maxHo
     val linkTravelTimeMap: mutable.HashMap[String, Array[Double]] = mutable.HashMap()
     logger.info(s"Stats fileName [$fileName] is being loaded")
 
-    val (iterator, closable) =
-      GenericCsvReader.readAs(fileName, mapper, (row: (String, Int, Double)) => !isBlank(row._1))
-    try {
-      iterator.foreach { case (linkId, hour, travelTime) =>
-        val travelTimePerHourArr = linkTravelTimeMap.getOrElseUpdate(linkId, Array.ofDim[Double](maxHour))
-        travelTimePerHourArr.update(hour, travelTime)
+    if (fileName.endsWith(".parquet")) {
+      val conf = new Configuration()
+      val inputFile = HadoopInputFile.fromPath(new Path(fileName), conf)
+      val reader = AvroParquetReader.builder[GenericRecord](inputFile).withConf(conf).build()
+      try {
+        var record = reader.read()
+        while (record != null) {
+          val linkId = record.get("link").toString
+          val hour = record.get("hour").asInstanceOf[Int]
+          val travelTime = record.get("traveltime").asInstanceOf[Double]
+          val stat = record.get("stat").toString
+          if (!isBlank(linkId) && stat.equalsIgnoreCase("avg")) {
+            val travelTimePerHourArr = linkTravelTimeMap.getOrElseUpdate(linkId, Array.ofDim[Double](maxHour))
+            travelTimePerHourArr.update(hour, travelTime)
+          }
+          record = reader.read()
+        }
+      } finally {
+        Try(reader.close())
       }
-    } finally {
-      Try(closable.close())
+    } else {
+      val (iterator, closable) =
+        GenericCsvReader.readAs(fileName, mapper, (row: (String, Int, Double)) => !isBlank(row._1))
+      try {
+        iterator.foreach { case (linkId, hour, travelTime) =>
+          val travelTimePerHourArr = linkTravelTimeMap.getOrElseUpdate(linkId, Array.ofDim[Double](maxHour))
+          travelTimePerHourArr.update(hour, travelTime)
+        }
+      } finally {
+        Try(closable.close())
+      }
     }
 
     val end = System.currentTimeMillis()
