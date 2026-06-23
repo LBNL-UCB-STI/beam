f637dc969f9a52601a2d92e135d292c0a73f5c8e
Rashid <info+beam-bot2@simrise.tech>
2026-06-23 18:09:35 +0300

    fmt

--- src/main/scala/beam/sim/RideHailFleetInitializer.scala
+++ src/main/scala/beam/sim/RideHailFleetInitializer.scala
@@ -640,8 +640,10 @@ class ProceduralRideHailFleetInitializer(
 
   private def computeNumRideHailAgents: Long = {
     val fleet: Double = beamServices.beamConfig.beam.agentsim.agents.vehicles.fractionOfInitialVehicleFleet
-    if (fleet < 0.000001){
-      logger.error(f"The configuration value of beam.agentsim.agents.vehicles.fractionOfInitialVehicleFleet is too low: $fleet")
+    if (fleet < 0.000001) {
+      logger.error(
+        f"The configuration value of beam.agentsim.agents.vehicles.fractionOfInitialVehicleFleet is too low: $fleet"
+      )
       logger.error(f"Very low values might create too many household vehicles.")
     }
 
